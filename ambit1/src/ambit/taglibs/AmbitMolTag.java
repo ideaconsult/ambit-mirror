050de24c569a35dc04aa2b0d8ac2c5d4863ca784
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2007-12-21 14:15:40 +0000

    few more taglibs

---
+++ ambit1/src/ambit/taglibs/AmbitMolTag.java
@@ -0,0 +1,48 @@
+package ambit.taglibs;
+
+import java.net.URLDecoder;
+
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+import org.openscience.cdk.interfaces.IMolecule;
+
+import ambit.data.molecule.Compound;
+import ambit.data.molecule.MoleculeTools;
+import ambit.data.molecule.SmilesParserWithTimeout;
+
+public abstract class AmbitMolTag extends SimpleTagSupport {
+	public static final String type_mol = "mol";
+	public static final String type_cml = "cml";
+	public static final String type_smiles = "smiles";
+	protected String  mol = null;
+	protected String molType = "mol";
+	
+	public String getMol() {
+		return mol;
+	}
+
+	public void setMol(String mol) {
+		this.mol = mol;
+	}
+
+	public String getMolType() {
+		return molType;
+	}
+
+	public void setMolType(String molType) {
+		this.molType = molType;
+	}
+	
+	protected IMolecule getMolecule(String mol) throws Exception {
+		if (type_cml.equals(molType)) {
+			return Compound.readMolecule(mol);
+		} else if (type_mol.equals(molType)) { 
+			String s = URLDecoder.decode(mol,"UTF-8");
+		    return  MoleculeTools.readMolfile(s);
+		} else if (type_smiles.equals(molType)) {
+			SmilesParserWithTimeout p = new SmilesParserWithTimeout();
+			return p.parseSmiles(mol, 30000);
+    
+		} else throw new Exception("Invalid type "+molType);
+	}	
+}
