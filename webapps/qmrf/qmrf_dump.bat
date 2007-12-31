dc500097f0d314830bdfd88c15754b811e4e2368
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2007-12-31 12:39:19 +0000

    cleanup

--- webapps/qmrf/qmrf_dump.bat
+++
@@ -1,3 +0,0 @@
-mysqldump --user=root --port=3306 -p --opt --add-drop-database  --databases ambit_qmrf > ambit_qmrf.sql
-mysqldump --user=root --port=3306 -p --opt --add-drop-database  --databases qmrf_documents > qmrf_documents.sql
-mysqldump --user=root --port=3306 -p --opt --add-drop-database  --databases tomcat_users > tomcat_users.sql
