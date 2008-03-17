0950d2650a6c5b0472ea3f2cf1b07d1f91d018d3
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2008-03-17 08:38:49 +0000

    Test for user interface for workflow and workflowcontext monitoring

--- ambit2/src/ambit2/test/workflow/TestMicroworkflow.java
+++ ambit2/src/ambit2/test/workflow/TestMicroworkflow.java
@@ -31,23 +31,38 @@ package ambit2.test.workflow;
 
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Vector;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
 import junit.framework.TestCase;
 
 import org.openscience.cdk.interfaces.IMolecule;
 import org.openscience.cdk.io.iterator.IIteratingChemObjectReader;
 import org.openscience.cdk.io.iterator.IteratingSMILESReader;
+import org.openscience.cdk.qsar.DescriptorValue;
+import org.openscience.cdk.qsar.IMolecularDescriptor;
+import org.openscience.cdk.qsar.descriptors.molecular.RuleOfFiveDescriptor;
 
 import com.microworkflow.execution.Performer;
 import com.microworkflow.process.Activity;
+import com.microworkflow.process.ICompositeActivity;
+import com.microworkflow.process.IConditionalActivity;
+import com.microworkflow.process.IEmbeddedActivity;
 import com.microworkflow.process.Primitive;
 import com.microworkflow.process.TestCondition;
 import com.microworkflow.process.While;
 import com.microworkflow.process.Workflow;
 import com.microworkflow.process.WorkflowContext;
+import com.microworkflow.ui.IWorkflowContextFactory;
+import com.microworkflow.ui.IWorkflowFactory;
+import com.microworkflow.ui.WorkflowContextPanel;
+import com.microworkflow.ui.WorkflowMonitor;
+import com.microworkflow.ui.WorkflowPanel;
 
 public class TestMicroworkflow extends TestCase {
 
@@ -58,113 +73,221 @@ public class TestMicroworkflow extends TestCase {
     protected void tearDown() throws Exception {
         super.tearDown();
     }
+
+    void printActivity(Activity activity, int level) {
+        if (activity == null)
+            return;
+        for (int i = 0; i < level; i++)
+            System.out.print(' ');
+        System.out.print(level);
+        System.out.print('.');
+        System.out.println(activity);
+        if (activity instanceof ICompositeActivity) {
+            ArrayList<Activity> a = ((ICompositeActivity) activity)
+                    .getComponents();
+            for (int i = 0; i < a.size(); i++)
+                printActivity(a.get(i), level + 1);
+        }
+        if (activity instanceof IEmbeddedActivity) {
+            printActivity(((IEmbeddedActivity) activity).getBody(), level + 1);
+        }
+        if (activity instanceof IConditionalActivity) {
+            printActivity(((IConditionalActivity) activity).getThenBranch(),
+                    level + 1);
+            printActivity(((IConditionalActivity) activity).getElseBranch(),
+                    level + 1);
+        }
+
+    }
+
     public void test() throws Exception {
-        WorkflowContext wc=new WorkflowContext();
-        wc.put("Source",new IteratingSMILESReader(new StringReader("c1ccccc1\nCC\nCCC\nCCCC\nCCCCC")));
-        wc.put("Molecule",null);
-        wc.put("Writer",new ArrayList<Integer>());
-        
+        int n = 1000;
+        final WorkflowContext wc = new WorkflowContext();
+        StringBuffer b = new StringBuffer();
+        StringBuffer C = new StringBuffer();
+        for (int i = 0; i < n; i++) {
+            if ((i % 30) == 0) C=new StringBuffer("C");
+            else C.append("C");
+            b.append(C);
+
+            b.append("\n");
+        }
+
+        // wc.put("SMILES","c1ccccc1\nCC\nCCC\nCCCC\nCCCCC");
+        wc.put("SMILES", b.toString());
+        wc.put("Molecule", null);
+        wc.put("Writer", new Vector<Double>());
+
         ConsoleHandler ch = new ConsoleHandler();
-        //ch.setLevel(Level.FINEST);
-        Logger.getLogger("com.microworkflow.execution.WorkflowContext").addHandler(ch);
-        Logger.getLogger("com.microworkflow.execution.WorkflowContext").setLevel(Level.FINEST);
-        Logger.getLogger("com.microworkflow.execution.Scheduler").addHandler(ch);
-        Logger.getLogger("com.microworkflow.execution.Scheduler").setLevel(Level.FINEST);
+        // ch.setLevel(Level.FINEST);
+        Logger.getLogger("com.microworkflow.execution.WorkflowContext")
+                .addHandler(ch);
+        Logger.getLogger("com.microworkflow.execution.WorkflowContext")
+                .setLevel(Level.FINEST);
+        Logger.getLogger("com.microworkflow.execution.Scheduler")
+                .addHandler(ch);
+        Logger.getLogger("com.microworkflow.execution.Scheduler").setLevel(
+                Level.FINEST);
         Logger.getLogger("com.microworklfow.process.workflow").addHandler(ch);
-        Logger.getLogger("com.microworklfow.process.workflow").setLevel(Level.FINEST);        
-        
-        
-        Workflow molWorkflow=new Workflow();
+        Logger.getLogger("com.microworklfow.process.workflow").setLevel(
+                Level.FINEST);
+
+        final Workflow molWorkflow = new Workflow();
+        /*
+         * molWorkflow.addPropertyChangeListener(new PropertyChangeListener() {
+         * public void propertyChange(PropertyChangeEvent arg0) {
+         * System.out.println("WFListener:\t"+arg0); if (arg0.getNewValue()
+         * instanceof Activity)
+         * 
+         * WorkflowTools.traverseActivity((Activity)arg0.getNewValue(),0, new
+         * ILookAtActivity() { public void look(Activity activity, int level) {
+         * for (int i=0; i < level; i++) System.out.print(' ');
+         * System.out.print(level); System.out.print('.');
+         * System.out.println(activity); } }); } });
+         */
         molWorkflow.setDefinition(getWorkflow());
-        WorkflowContext result=molWorkflow.executeWith(wc);
+        /*
+         * wc.addPropertyChangeListener(new PropertyChangeListener() { public
+         * void propertyChange(PropertyChangeEvent arg0) {
+         * System.out.println("WFCListener:\t" + arg0); } });
+         */
+        IWorkflowFactory wff = new IWorkflowFactory() {
+            public Workflow getWorkflow() {
+                  return molWorkflow;
+            }
+        }  ;      
+        IWorkflowContextFactory wfcf = new IWorkflowContextFactory() {
+            public WorkflowContext getWorkflowContext() {
+                return wc;
+            }
+        };              
+        
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());        
+        WorkflowMonitor panel = new WorkflowMonitor(new WorkflowPanel(
+                molWorkflow), new WorkflowContextPanel(wfcf),
+                wff,wfcf
+        );
+        // panel.setPreferredSize(new Dimension(200,200));
+
+        JOptionPane.showMessageDialog(null, panel,molWorkflow.toString(),JOptionPane.PLAIN_MESSAGE);
+
+        WorkflowContext result = molWorkflow.executeWith(wc);
         Object r = wc.get("Writer");
         System.out.println("Writer = " + r);
         assertNull(wc.get("Source"));
         assertTrue(r instanceof ArrayList);
-        ArrayList<Integer> ri = (ArrayList<Integer>)r;
-        assertEquals(5,ri.size());
-        assertEquals(new Integer(6),ri.get(0));
-        assertEquals(new Integer(2),ri.get(1));
-        assertEquals(new Integer(3),ri.get(2));
-        assertEquals(new Integer(4),ri.get(3));
-        assertEquals(new Integer(5),ri.get(4));
-        
+        ArrayList<Integer> ri = (ArrayList<Integer>) r;
+        assertEquals(n, ri.size());
+        for (int i = 0; i < n; i++)
+            assertEquals(new Double(i + 1), ri.get(i));
+
     }
+
     protected Activity getWorkflow() {
 
-        Primitive readMolecule  =
-            new Primitive("Source","Molecule",
+        Primitive initMolecule = new Primitive("SMILES", "Source",
                 new Performer() {
                     public Object execute() {
-                        IIteratingChemObjectReader reader=(IIteratingChemObjectReader)getTarget();
-                        return reader.next();
-                    }
-                }
-            );
 
-        
-        Primitive close =
-            new Primitive("Source","Source",
-                new Performer() {
-                    public Object execute() {
-                        IIteratingChemObjectReader reader = (IIteratingChemObjectReader)getTarget();
-                        try {
-                            if (reader != null)
-                                reader.close();
-                        } catch (Exception x) {
-                            x.printStackTrace();
-                        }
-                        reader = null;
+                        ((Vector<Double>) get("Writer")).clear();
+                        IIteratingChemObjectReader reader = new IteratingSMILESReader(
+                                new StringReader(getTarget().toString()));
                         return reader;
                     }
-                }
-            );
+                });
+        initMolecule.setName("Init reader");
 
-        Primitive calc =
-            new Primitive("Molecule","Result",
+        Primitive readMolecule = new Primitive("Source", "Molecule",
                 new Performer() {
                     public Object execute() {
-                        IMolecule m=(IMolecule)getTarget();
-                        if (m ==null) return 0;
-                        return m.getAtomCount();
+                        IIteratingChemObjectReader reader = (IIteratingChemObjectReader) getTarget();
+                        return reader.next();
                     }
+                });
+        readMolecule.setName("Read molecule");
+
+        Primitive close = new Primitive("Source", "Source", new Performer() {
+            public Object execute() {
+                IIteratingChemObjectReader reader = (IIteratingChemObjectReader) getTarget();
+                try {
+                    if (reader != null)
+                        reader.close();
+                } catch (Exception x) {
+                    x.printStackTrace();
                 }
-            );
-        //write somewhere
-        Primitive write =
-            new Primitive("Result",
-                new Performer() {
-                    public Object execute() {
-                        Object m=getTarget();
-                        ((ArrayList<Integer>)get("Writer")).add((Integer)m);
-                        return null;
-                    }
+                reader = null;
+                return reader;
+            }
+        });
+        close.setName("Close file");
+        Primitive calc = new Primitive("Molecule", "Result", new Performer() {
+
+            public Object execute() {
+                IMolecule m = (IMolecule) getTarget();
+                if (m == null)
+                    return 0;
+                try {
+                    return new Double(m.getAtomCount());
+                } catch (Exception x) {
+                    x.printStackTrace();
+                    return 0;
                 }
-            );        
+                // return m.getAtomCount();
+            }
+        });
+        calc.setName("Calculate atom number");
         
-        
+        Primitive calcMore = new Primitive("Molecule",  new Performer() {
+             IMolecularDescriptor d = new RuleOfFiveDescriptor();
+
+            public Object execute() {
+                IMolecule m = (IMolecule) getTarget();
+                if (m == null)
+                    return null;
+                try {
+                    DescriptorValue v = d.calculate(m);
+                    return null;
+                } catch (Exception x) {
+                    return null;
+                }
+                // return m.getAtomCount();
+            }
+        });
+        calcMore.setName("Calculate RuleOfFiveDescriptor");        
+        // write somewhere
+        Primitive write = new Primitive("Result", new Performer() {
+            public Object execute() {
+                Object m = getTarget();
+                ((Vector<Double>) get("Writer")).add((Double) m);
+                return null;
+            }
+        });
+
+        write.setName("Write result");
         While w = new While();
-        w.setBody(readMolecule.addStep(calc).addStep(write));
+        w.setName("While");
+        w.setBody(readMolecule.addStep(calc).addStep(calcMore).addStep(write));
         w.setTestCondition(new TestCondition() {
             public boolean evaluate() {
-                IIteratingChemObjectReader reader=(IIteratingChemObjectReader)get("Source");
+                IIteratingChemObjectReader reader = (IIteratingChemObjectReader) get("Source");
                 return reader.hasNext();
-            }});
-        
+            }
+
+            @Override
+            public String toString() {
+                // TODO Auto-generated method stub
+                return "Test: hasNext()";
+            }
+        });
+
         /*
-        Conditional ifhasNext =
-            new Conditional(new TestCondition() {
-                public boolean evaluate() {
-                    IIteratingChemObjectReader reader=(IIteratingChemObjectReader)get("Source");
-                    return reader.hasNext();
-                }},
-                readMolecule,close);
-        */
-                        
+         * Conditional ifhasNext = new Conditional(new TestCondition() { public
+         * boolean evaluate() { IIteratingChemObjectReader
+         * reader=(IIteratingChemObjectReader)get("Source"); return
+         * reader.hasNext(); }}, readMolecule,close);
+         */
 
+        return initMolecule.addStep(w.addStep(close));
+    }
 
-        
-        
-        return w.addStep(close);
-    }    
 }
