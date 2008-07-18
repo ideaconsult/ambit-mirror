9803cb41aa206d4275a4c8c0b6b33f0f2d4167e2
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2008-07-18 14:40:29 +0000

    more tests for smarts matching

--- ambit1/src/ambit/test/smarts/SmartsQueryTest.java
+++ ambit1/src/ambit/test/smarts/SmartsQueryTest.java
@@ -26,8 +26,11 @@ package ambit.test.smarts;
 
 import junit.framework.TestCase;
 
+import org.openscience.cdk.aromaticity.HueckelAromaticityDetector;
+import org.openscience.cdk.interfaces.IMolecule;
 import org.openscience.cdk.templates.MoleculeFactory;
 
+import ambit.data.molecule.Compound;
 import ambit.data.molecule.SmartsQuery;
 
 public class SmartsQueryTest extends TestCase  {
@@ -59,6 +62,52 @@ public class SmartsQueryTest extends TestCase  {
 		assertEquals("C=O",q.getSmarts());
 	}
 
+
+	public void testMatchAromatic()throws Exception {
+		String smarts = "a";
+		SmartsQuery q = new SmartsQuery(smarts);
+		assertEquals(smarts,q.getSmarts());
+		IMolecule mol = MoleculeFactory.makeBenzene();
+		HueckelAromaticityDetector.detectAromaticity(mol);
+		assertTrue(q.match(mol)>0);
+		
+
+	}
+	public void testMatchAromaticCML()throws Exception {
+		String smarts = "a";
+		SmartsQuery q = new SmartsQuery(smarts);
+		assertEquals(smarts,q.getSmarts());
+		IMolecule mol = Compound.readMolecule(getCML());
+		assertTrue(HueckelAromaticityDetector.detectAromaticity(mol));
+		assertTrue(q.match(mol)>0);
+		
+
+	}	
+	
+	
+	protected String getCML() {
+		return
+	"<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n"+
+	"<molecule id=\"m1\" xmlns=\"http://www.xml-cml.org/schema\">\n"+
+	"  <atomArray>\n"+
+	"    <atom id=\"a1\" elementType=\"C\" x2=\"1.9361\" y2=\"-0.6\" formalCharge=\"0\" hydrogenCount=\"1\"/>\n"+
+	"    <atom id=\"a2\" elementType=\"C\" x2=\"1.9349\" y2=\"-1.4273\" formalCharge=\"0\" hydrogenCount=\"1\"/>\n"+
+	"    <atom id=\"a3\" elementType=\"C\" x2=\"2.6497\" y2=\"-1.8402\" formalCharge=\"0\" hydrogenCount=\"1\"/>\n"+
+	"    <atom id=\"a4\" elementType=\"C\" x2=\"3.3662\" y2=\"-1.4269\" formalCharge=\"0\" hydrogenCount=\"1\"/>\n"+
+	"    <atom id=\"a5\" elementType=\"C\" x2=\"3.3633\" y2=\"-0.5963\" formalCharge=\"0\" hydrogenCount=\"1\"/>\n"+
+	"    <atom id=\"a6\" elementType=\"C\" x2=\"2.6479\" y2=\"-0.1872\" formalCharge=\"0\" hydrogenCount=\"1\"/>\n"+
+	"  </atomArray>\n"+
+	"  <bondArray>\n"+
+	"    <bond id=\"b1\" atomRefs2=\"a1 a2\" order=\"A\"/>\n"+
+	"    <bond id=\"b2\" atomRefs2=\"a3 a4\" order=\"A\"/>\n"+
+	"    <bond id=\"b3\" atomRefs2=\"a4 a5\" order=\"A\"/>\n"+
+	"    <bond id=\"b4\" atomRefs2=\"a2 a3\" order=\"A\"/>\n"+
+	"    <bond id=\"b5\" atomRefs2=\"a5 a6\" order=\"A\"/>\n"+
+	"    <bond id=\"b6\" atomRefs2=\"a6 a1\" order=\"A\"/>\n"+
+	"  </bondArray>\n"+
+	"</molecule>\n";
+	}
+	
 }
 
 
