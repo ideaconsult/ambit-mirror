2eb90170ddf457ccf260632d3cfeb472527b27b7
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2008-10-03 14:01:56 +0000

    moved

--- ambit2-dbui/src/main/java/ambit2/dbui/CachedRowSetTableModel.java
+++ ambit2-dbui/src/main/java/ambit2/dbui/CachedRowSetTableModel.java
@@ -22,7 +22,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
 */
 
-package ambit2.workflow.ui;
+package ambit2.dbui;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
