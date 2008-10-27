650851430637fc70b6019cfb1da497cd72d79072
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2008-10-27 16:16:56 +0000

    forgotten class

---
+++ ambit2-all/ambit2-workflow/src/main/java/ambit2/workflow/ui/WorkflowConsolePanel.java
@@ -0,0 +1,53 @@
+package ambit2.workflow.ui;
+
+import java.awt.BorderLayout;
+import java.beans.PropertyChangeEvent;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import com.microworkflow.events.WorkflowListener;
+import com.microworkflow.process.Workflow;
+
+public class WorkflowConsolePanel<T> extends AbstractEditor<T> implements WorkflowListener {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8977175757180829561L;
+	protected JTextArea text = null;
+
+	public WorkflowConsolePanel() {
+		this(null);
+	}	
+	public WorkflowConsolePanel(Workflow workflow) {
+		super(null);
+		text = new JTextArea();
+		if (workflow!= null)
+			workflow.addPropertyChangeListener(this);
+		setAnimate(true);
+		
+		setEditable(false);
+		add(new JScrollPane(text),BorderLayout.CENTER);
+	}	
+	@Override
+	protected void animate(PropertyChangeEvent arg0) {
+		text.setText(arg0.getPropertyName() + "\t"+ arg0.getNewValue().toString());
+		
+	}
+
+	@Override
+	public void clear() {
+		text.setText("");
+		
+	}
+
+	public boolean isEditable() {
+		return text.isEditable();
+	}
+
+	public void setEditable(boolean editable) {
+		text.setEditable(editable);
+		
+	}
+
+}
