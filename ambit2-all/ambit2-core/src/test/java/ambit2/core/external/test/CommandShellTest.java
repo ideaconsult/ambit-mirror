f1f1d389d8355dbab9d932e564b91e73a9a3f65e
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2008-10-27 06:20:38 +0000

    Empty test for CommandShell.java  (TODO add a test)

---
+++ ambit2-all/ambit2-core/src/test/java/ambit2/core/external/test/CommandShellTest.java
@@ -0,0 +1,13 @@
+package ambit2.core.external.test;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+
+public class CommandShellTest {
+	@Test
+	public void test() throws Exception {
+		Assert.fail("Not imlemented");
+	}
+}
