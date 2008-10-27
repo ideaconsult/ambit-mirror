162dae523aa5fe9efa379af876e26de1f392c104
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2008-10-27 06:19:52 +0000

    A placeholder for class definition/display

---
+++ ambit2-all/ambit2-core/src/main/java/ambit2/core/data/ClassHolder.java
@@ -0,0 +1,79 @@
+/*
+Copyright (C) 2005-2008  
+
+Contact: nina@acad.bg
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License
+as published by the Free Software Foundation; either version 2.1
+of the License, or (at your option) any later version.
+All we ask is that proper credit is given for our work, which includes
+- but is not limited to - adding the above copyright notice to the beginning
+of your source code files, and to any copyright notice that you may distribute
+with programs based on this work.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+ */
+package ambit2.core.data;
+
+import com.jgoodies.binding.beans.Model;
+
+/**
+ * A place holder for class name and description, in order to allow display of light list of objects, instantiated on demand only.
+ * @author nina
+ *
+ */
+public class ClassHolder extends Model {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6667954841743487524L;
+	protected String clazz;
+	protected String title;
+	protected String description;
+	protected String icon;
+	
+	public ClassHolder(String clazz, String title, String description, String icon) {
+		setClazz(clazz);
+		setTitle(title);
+		setDescription(description);
+		setIcon(icon);
+	}
+	public String getClazz() {
+		return clazz;
+	}
+	public void setClazz(String clazz) {
+		this.clazz = clazz;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getIcon() {
+		return icon;
+	}
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+	@Override
+	public String toString() {
+		return getTitle();
+	}
+
+
+}
