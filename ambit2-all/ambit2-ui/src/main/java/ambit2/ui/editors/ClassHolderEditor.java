20ff3cb1affca219529d4274b62b7a3eb5512d18
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2008-10-27 06:53:07 +0000

    editors for AmbitUser and ClassHolder objects

---
+++ ambit2-all/ambit2-ui/src/main/java/ambit2/ui/editors/ClassHolderEditor.java
@@ -0,0 +1,182 @@
+package ambit2.ui.editors;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.util.List;
+
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.ListCellRenderer;
+import javax.swing.border.Border;
+
+import ambit2.core.data.ClassHolder;
+import ambit2.ui.Utils;
+
+public class ClassHolderEditor extends ListEditor<List<ClassHolder>, ClassHolder> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7682349265145859359L;
+
+	public ClassHolderEditor(String listCaption, String detailsCaption) {
+		super(null,new String[] {},listCaption,detailsCaption);
+		setLayoutOrientation(JList.HORIZONTAL_WRAP);		
+		setCellRenderer(createListCellRenderer());
+	}
+	@Override
+	public void setObject(List<ClassHolder> object) {
+		super.setObject(object);
+		setLayoutOrientation(JList.HORIZONTAL_WRAP);		
+	}
+	public static ListCellRenderer createListCellRenderer() {
+		return new ImageCellRenderer();
+	}
+}
+
+class ImageCellRenderer extends DefaultListCellRenderer {
+	protected final int size = 48;
+	protected Border shadow = BorderFactory.createLineBorder(new Color(216,240,250));
+	protected Color selectedClr = new Color(244,250,253);
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5986970740680430732L;
+
+	public Component getListCellRendererComponent(JList list, Object value,
+			int index, boolean isSelected, boolean cellHasFocus) {
+        JLabel label =   new JLabel(value.toString());
+        /*
+            (JLabel) super.getListCellRendererComponent(list,   
+                value, index, isSelected, cellHasFocus);   
+  */
+		ClassHolder item = (ClassHolder)value;
+		label.setText("<html><b>"+value.toString()+"</b></html>");
+		try {
+			String iconFile = item.getIcon();
+			if (iconFile !=null) {
+				ImageIcon icon = Utils.createImageIcon(iconFile);
+				icon.setImage(icon.getImage().getScaledInstance(size,size,1));
+				label.setIcon(icon);
+			}
+			label.setOpaque(true);
+			
+			if (isSelected) {
+				label.setBackground(selectedClr);
+				label.setBorder(shadow);
+			} else { 
+				label.setBorder(null);
+				label.setBackground(Color.white);
+			}
+ 
+		} catch (Exception x) {
+			x.printStackTrace();
+			label.setIcon(null);
+		}
+		label.setPreferredSize(new Dimension(size*3,size));
+        return label;   
+	}
+	
+}
+
+class ImageListRenderer extends JPanel implements ListCellRenderer {
+
+	private static final long serialVersionUID = 1L;
+
+ 
+	private JLabel icon = new JLabel();
+
+	private JLabel title = new JLabel();
+
+	private JTextArea description = new JTextArea();
+
+	
+	private Color evenRowColor = Color.white;
+
+	private Color oddRowColor = new Color(235, 235, 235);
+
+	public ImageListRenderer() {
+		super(new BorderLayout());
+		add(icon,BorderLayout.WEST);
+		add(title,BorderLayout.NORTH);
+		add(description,BorderLayout.CENTER);
+	}
+	
+	public Component getListCellRendererComponent(JList list, Object value,
+
+			int index, boolean isSelected, boolean cellHasFocus) {
+
+ 
+		if (isSelected) 
+
+		{
+
+		    setBackground(list.getSelectionBackground());
+
+		    setForeground(list.getSelectionForeground());
+
+		   
+		    title.setBackground(list.getSelectionBackground());
+
+		    title.setForeground(list.getSelectionForeground());
+
+		   
+		    description.setBackground(list.getSelectionBackground());
+
+		    description.setForeground(list.getSelectionForeground());
+
+		}
+
+		else 
+
+		{
+
+			Color c = ((index & 0x1) == 1) ? evenRowColor : oddRowColor;
+
+			
+			setBackground(c);
+
+			setForeground(list.getForeground());
+
+			
+			title.setBackground(c);
+
+			title.setForeground(list.getForeground());
+
+		   
+			description.setBackground(c);
+
+		    description.setForeground(list.getForeground());
+
+		}
+
+		
+		
+		ClassHolder item = (ClassHolder)value;
+
+		try {
+			String iconFile = item.getIcon();
+			if (iconFile==null) iconFile = "images/experiment.png";
+			icon.setIcon(Utils.createImageIcon(iconFile));
+ 
+		} catch (Exception x) {
+			x.printStackTrace();
+		}
+
+		title.setText(item.getTitle());
+
+		description.setText(item.getDescription());
+
+		
+		return this;
+
+	}
+
+	
+} 
