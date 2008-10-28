ce0bbef2ba5ef192f8d6668f400a2151bb18b80b
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2008-10-28 17:32:08 +0000

    now remembers previous values

--- ambit2-all/ambit2-workflow/src/main/java/ambit2/workflow/UserInteraction.java
+++ ambit2-all/ambit2-workflow/src/main/java/ambit2/workflow/UserInteraction.java
@@ -1,5 +1,7 @@
 package ambit2.workflow;
 
+import ambit2.db.LoginInfo;
+
 import com.microworkflow.execution.Performer;
 import com.microworkflow.process.Primitive;
 import com.microworkflow.process.ValueLatchPair;
@@ -9,13 +11,19 @@ public class UserInteraction<T> extends Primitive {
 	public UserInteraction(final T query, String resultTag) {
 		this(query,resultTag,"User input");
 	}
-	public UserInteraction(final T query, String resultTag, String title) {
+	public UserInteraction(final T query, final String resultTag, String title) {
 	    super(DBWorkflowContext.USERINTERACTION,resultTag,new Performer() {
 	        @Override
 	        public Object execute() {
 	            Object o = getTarget();
+	            
+            	Object ol = context.get(resultTag);
+            	if (ol == null) {
+            		ol = query;
+            	}
+            		
 	            if (o == null) {
-	                ValueLatchPair<T> latch = new ValueLatchPair<T>(query);
+	                ValueLatchPair<T> latch = new ValueLatchPair<T>((T)ol);
 	                context.put(DBWorkflowContext.USERINTERACTION,latch);
 	                //This is a blocking operation!
 	                try {
