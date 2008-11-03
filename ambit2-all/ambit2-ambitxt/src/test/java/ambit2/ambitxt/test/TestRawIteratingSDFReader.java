e57b1ecd96f6a2bd7d5554af7afb6b24954121cd
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2008-11-03 07:35:20 +0000

    fingerprint writer test (to be moved to ambit-db test package)

--- ambit2-all/ambit2-ambitxt/src/test/java/ambit2/ambitxt/test/TestRawIteratingSDFReader.java
+++ ambit2-all/ambit2-ambitxt/src/test/java/ambit2/ambitxt/test/TestRawIteratingSDFReader.java
@@ -5,6 +5,8 @@ import java.io.StringReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.BitSet;
+import java.util.Hashtable;
 import java.util.Vector;
 
 import javax.sql.rowset.CachedRowSet;
@@ -15,12 +17,17 @@ import javax.swing.UIManager;
 
 import org.openscience.cdk.DefaultChemObjectBuilder;
 import org.openscience.cdk.interfaces.IMolecule;
-import org.openscience.cdk.io.iterator.IteratingMDLReader;
+import org.openscience.cdk.io.iterator.IIteratingChemObjectReader;
 
+import ambit2.core.config.AmbitCONSTANTS;
 import ambit2.core.data.IStructureRecord;
+import ambit2.core.io.MyIteratingMDLReader;
 import ambit2.core.io.RawIteratingSDFReader;
+import ambit2.core.processors.structure.FingerprintGenerator;
 import ambit2.db.CachedRowSetFactory;
+import ambit2.db.DatasourceFactory;
 import ambit2.db.RepositoryReader;
+import ambit2.db.processors.FP1024Writer;
 import ambit2.db.processors.PropertyWriter;
 import ambit2.db.processors.RepositoryWriter;
 import ambit2.workflow.DBWorkflowContext;
@@ -61,8 +68,59 @@ public class TestRawIteratingSDFReader extends RepositoryTest {
 		System.out.println("Records "+records + " " + now + " ms.");
 	}
 
+	public void testFingerprintWriter() throws Exception {
+		datasource = DatasourceFactory.getDataSource(
+				DatasourceFactory.getConnectionURI(
+						"jdbc:mysql", 
+						"localhost", "33060", "ambit2", "guest","guest" ));		
+        Connection connection = datasource.getConnection();
+        RepositoryReader reader = new RepositoryReader();
+        reader.setConnection(connection);
+        FingerprintGenerator gen = new FingerprintGenerator();
+        FP1024Writer fpWriter = new FP1024Writer();
+        fpWriter.setConnection(connection);
+        fpWriter.open();
+        reader.open();
+        int records = 0;
+		long now = System.currentTimeMillis();
+		DefaultChemObjectBuilder b = DefaultChemObjectBuilder.getInstance();
+		IStructureRecord o  ;
+		while (reader.hasNext()) {
+			o = reader.next();
+			if (o.getProperties() == null) o.setProperties(new Hashtable());
+			String content = reader.getStructure(o.getIdstructure());
+			if (content == null) continue;
+			IIteratingChemObjectReader mReader = new MyIteratingMDLReader(new StringReader(content),b);
+			
+			if (mReader.hasNext()) {
+				Object mol = mReader.next();
+				if (mol instanceof IMolecule) {
+					long mark = System.currentTimeMillis();
+					BitSet bitset = gen.process((IMolecule)mol);
+	      			o.getProperties().put(AmbitCONSTANTS.FingerprintTIME,System.currentTimeMillis()-mark);
+					o.getProperties().put(AmbitCONSTANTS.Fingerprint,bitset);
+					fpWriter.write(o);
+				}
+				
+			}
+			
+			o.clear();
+			mReader.close();
+			mReader = null;
+			records ++;
+			if ((records % 50) == 0)
+				System.out.println(records);
+		}
+		reader.close();
+		fpWriter.close();
+		now = System.currentTimeMillis() - now;
+		System.out.println("msec/records "+(double)now/(double)records);
+	}	
 	public void xtestPropertyWriter() throws Exception {
-		
+		datasource = DatasourceFactory.getDataSource(
+				DatasourceFactory.getConnectionURI(
+						"jdbc:mysql", 
+						"localhost", "33060", "ambit2", "guest","guest" ));		
         Connection connection = datasource.getConnection();
         RepositoryReader reader = new RepositoryReader();
         reader.setConnection(connection);
@@ -76,9 +134,9 @@ public class TestRawIteratingSDFReader extends RepositoryTest {
 		IStructureRecord o  ;
 		while (reader.hasNext()) {
 			o = reader.next();
-			String content = reader.getStructure(o.getIdstructure()).trim();
-			
-			IteratingMDLReader mReader = new IteratingMDLReader(new StringReader(content),b);
+			String content = reader.getStructure(o.getIdstructure());
+			if (content == null) continue;
+			IIteratingChemObjectReader mReader = new MyIteratingMDLReader(new StringReader(content),b);
 			
 			if (mReader.hasNext()) {
 				Object mol = mReader.next();
@@ -101,7 +159,7 @@ public class TestRawIteratingSDFReader extends RepositoryTest {
 		now = System.currentTimeMillis() - now;
 		System.out.println("msec/records "+(double)now/(double)records);
 	}
-    public void testCachedRecordSet() throws Exception {
+    public void xtestCachedRecordSet() throws Exception {
         /**
   Contacts.setDataSourceName("Databases/ContactsDB/Datasource");
   Contacts.setCommand("SELECT name, telephone from Contacts");
