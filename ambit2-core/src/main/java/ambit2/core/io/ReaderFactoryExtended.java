95c1d7d7e80be4639c73958c526d559f4420cbcf
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2008-11-03 07:09:17 +0000

    ReaderFactoryExtended introduced, in order to return MDL2000 reader with SGroup support

---
+++ ambit2-core/src/main/java/ambit2/core/io/ReaderFactoryExtended.java
@@ -0,0 +1,16 @@
+package ambit2.core.io;
+
+import org.openscience.cdk.io.IChemObjectReader;
+import org.openscience.cdk.io.ReaderFactory;
+import org.openscience.cdk.io.formats.IChemFormat;
+import org.openscience.cdk.io.formats.MDLV2000Format;
+
+public class ReaderFactoryExtended extends ReaderFactory {
+	@Override
+	public IChemObjectReader createReader(IChemFormat arg0) {
+		if (MDLV2000Format.getInstance().equals(arg0))
+				return new MDLV2000ReaderExtended();
+		else
+			return super.createReader(arg0);
+	}
+}
