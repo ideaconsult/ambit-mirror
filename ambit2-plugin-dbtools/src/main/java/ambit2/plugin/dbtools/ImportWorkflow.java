c67cc5f4731e78c159e5799911ae93b105e81bb5
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2008-11-04 08:39:28 +0000

    Import structures from file workflow

---
+++ ambit2-plugin-dbtools/src/main/java/ambit2/plugin/dbtools/ImportWorkflow.java
@@ -0,0 +1,126 @@
+package ambit2.plugin.dbtools;
+
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Iterator;
+
+import org.openscience.cdk.io.iterator.IIteratingChemObjectReader;
+
+import ambit2.core.data.IStructureRecord;
+import ambit2.core.data.StructureRecord;
+import ambit2.core.exceptions.AmbitException;
+import ambit2.core.io.FileInputState;
+import ambit2.core.io.IInputState;
+import ambit2.core.io.RawIteratingSDFReader;
+import ambit2.core.processors.batch.IBatchStatistics;
+import ambit2.db.IDBProcessor;
+import ambit2.db.SessionID;
+import ambit2.db.exceptions.DbAmbitException;
+import ambit2.db.processors.BatchDBProcessor;
+import ambit2.db.processors.DBProcessorsChain;
+import ambit2.db.processors.RepositoryWriter;
+import ambit2.workflow.ActivityPrimitive;
+import ambit2.workflow.DBWorkflowContext;
+import ambit2.workflow.library.DatasetSelection;
+import ambit2.workflow.library.InputFileSelection;
+import ambit2.workflow.library.LoginSequence;
+
+import com.microworkflow.process.Sequence;
+import com.microworkflow.process.Workflow;
+
+public class ImportWorkflow extends Workflow {
+
+	public ImportWorkflow()  {
+        Sequence seq=new Sequence();
+        seq.setName("[Import chemical structures]");    	
+
+        
+        IDBProcessor<String,IStructureRecord> processor = 
+        	new IDBProcessor<String,IStructureRecord>() {
+        	protected final StructureRecord r = new StructureRecord();
+        	public IStructureRecord process(String target)
+        			throws AmbitException {
+        		r.setIdchemical(-1);
+        		r.setIdstructure(-1);
+        		r.setFormat("SDF");
+        		r.setContent(target);
+        		return r;
+                //return new StructureRecord(-1,-1,target,"SDF");
+        	}
+
+			public boolean isEnabled() {
+				return true;
+			}
+
+			public void setEnabled(boolean value) {
+			}
+			public Connection getConnection() {
+					// TODO Auto-generated method stub
+					return null;
+				}
+			public SessionID getSession() {
+					// TODO Auto-generated method stub
+					return null;
+				}
+
+			public void setConnection(Connection connection)
+					throws DbAmbitException {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void close() throws SQLException {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void open() throws DbAmbitException {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void setSession(SessionID session) {
+				// TODO Auto-generated method stub
+				
+			}
+        };
+        
+        DBProcessorsChain<String,IStructureRecord, IDBProcessor> chain = 
+        		new DBProcessorsChain<String,IStructureRecord,IDBProcessor>();
+        chain.add(processor);
+        chain.add(new RepositoryWriter());
+
+        
+        BatchDBProcessor batch = new BatchDBProcessor<String,IStructureRecord>(chain) {
+        	protected Iterator getIterator(IInputState target) throws AmbitException {
+        		if (target instanceof FileInputState)
+        			try {
+        			return new RawIteratingSDFReader(
+        					new FileReader(((FileInputState)target).getFile())
+        					);
+        			} catch (Exception x) {
+        				throw new AmbitException(x);
+        			}
+        		else throw new AmbitException("Not a file");
+        	}
+        	protected void closeIterator(Iterator iterator) throws AmbitException {
+        		if (iterator instanceof IIteratingChemObjectReader)
+        			try {
+        			((IIteratingChemObjectReader)iterator).close();
+        			} catch (Exception x) {
+        				throw new AmbitException(x);
+        			}
+        	}        	
+		};
+    	ActivityPrimitive<IInputState,IBatchStatistics> p1 = 
+    		new ActivityPrimitive<IInputState,IBatchStatistics>( 
+    			InputFileSelection.INPUTFILE,
+    			DBWorkflowContext.BATCHSTATS,
+    			(IDBProcessor)batch,false);
+
+        p1.setName("Read file and import structures");
+        
+        setDefinition(new LoginSequence(new InputFileSelection(new DatasetSelection(p1))));
+	}
+}
