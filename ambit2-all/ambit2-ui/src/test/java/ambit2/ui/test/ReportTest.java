0c796d903a35281b771e1670f073dc3349d0162c
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2008-12-05 12:06:51 +0000

    include Jasper reports

---
+++ ambit2-all/ambit2-ui/src/test/java/ambit2/ui/test/ReportTest.java
@@ -0,0 +1,59 @@
+package ambit2.ui.test;
+
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.HashMap;
+
+import junit.framework.Assert;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperRunManager;
+import net.sf.jasperreports.view.JasperViewer;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReportTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	public ReportTest() {
+
+	}
+	public void show() throws Exception {
+		InputStream io;
+/*
+		 io = getClass().getClassLoader().getResourceAsStream("test.jrxml");
+		System.out.println(io);
+		JasperDesignViewer v = new JasperDesignViewer(io,true);
+		v.show();		
+		io.close();
+		*/
+		
+		io = getClass().getClassLoader().getResourceAsStream("FirstReport.jrprint");
+		JasperViewer v = new JasperViewer(io,false);
+		v.show();
+		io.close();		
+	}
+	@Test
+	public void test() throws Exception {
+		String file = "src/test/resources/test.jasper";
+		JasperCompileManager.compileReportToFile("src/test/resources/test.jrxml",file);
+		Assert.assertTrue(new File(file).exists());
+		String result = JasperFillManager.fillReportToFile(file, new HashMap(), new JREmptyDataSource());		
+		Assert.assertTrue(new File(result).exists());
+		String pdf = "src/test/resources/test.pdf";
+		JasperRunManager.runReportToPdfFile(file,pdf,
+				 new HashMap(), new JREmptyDataSource());
+
+
+	}
+	public static void main(String[] args) throws Exception {
+		ReportTest t = new ReportTest();
+		t.show();
+	}
+}
