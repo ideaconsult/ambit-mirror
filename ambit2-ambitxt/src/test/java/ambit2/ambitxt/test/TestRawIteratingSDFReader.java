8214c80f89ba19d9ca5b287db2cef344d6b4824b
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-02-08 21:07:40 +0000

    update

--- ambit2-ambitxt/src/test/java/ambit2/ambitxt/test/TestRawIteratingSDFReader.java
+++ ambit2-ambitxt/src/test/java/ambit2/ambitxt/test/TestRawIteratingSDFReader.java
@@ -3,17 +3,8 @@ package ambit2.ambitxt.test;
 import java.io.FileReader;
 import java.io.StringReader;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.BitSet;
 import java.util.Hashtable;
-import java.util.Vector;
-
-import javax.sql.rowset.CachedRowSet;
-import javax.sql.rowset.spi.SyncProviderException;
-import javax.sql.rowset.spi.SyncResolver;
-import javax.swing.JOptionPane;
-import javax.swing.UIManager;
 
 import org.openscience.cdk.DefaultChemObjectBuilder;
 import org.openscience.cdk.interfaces.IMolecule;
@@ -24,17 +15,10 @@ import ambit2.core.data.IStructureRecord;
 import ambit2.core.io.MyIteratingMDLReader;
 import ambit2.core.io.RawIteratingSDFReader;
 import ambit2.core.processors.structure.FingerprintGenerator;
-import ambit2.db.CachedRowSetFactory;
 import ambit2.db.DatasourceFactory;
 import ambit2.db.RepositoryReader;
 import ambit2.db.processors.FP1024Writer;
 import ambit2.db.processors.RepositoryWriter;
-import ambit2.workflow.DBWorkflowContext;
-import ambit2.workflow.ui.AmbitWorkflowContextPanel;
-
-import com.microworkflow.events.WorkflowContextEvent;
-import com.microworkflow.process.WorkflowContext;
-import com.microworkflow.ui.IWorkflowContextFactory;
 
 
 public class TestRawIteratingSDFReader extends RepositoryTest {
@@ -160,86 +144,7 @@ public class TestRawIteratingSDFReader extends RepositoryTest {
 		System.out.println("msec/records "+(double)now/(double)records);
 	}
 	*/
-    public void xtestCachedRecordSet() throws Exception {
-       
-        Connection c = datasource.getConnection();
-        PreparedStatement ps = c.prepareStatement("SELECT idstructure,uncompress(structure),format FROM structure where idstructure>?",
-                        ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        ps.setInt(1,100);
-        //ps.setFetchSize(10);
-        ps.setMaxRows(100);
-        ResultSet rs = ps.executeQuery();
-        
-        CachedRowSet join = CachedRowSetFactory.getCachedRowSet();
-        //join.setFetchSize(10);
-        join.setPageSize(10);
-        //join.setMaxRows(30);
-        //join.setCommand("SELECT idstructure,uncompress(structure),format FROM structure");
-        //join.setCommand("SELECT s.idstructure,selected,uncompress(structure),format FROM query_results as q join structure as s using (idstructure)");
-        //join.setCommand("SELECT s.idstructure,selected,format FROM query_results as q join structure as s using (idstructure)");
-        join.populate(rs,1);
 
-        int [] keys = {1,2};
-        //join.execute(c);
-        //c.close();
-
-        int records = 0;
-        int pages = 0;
-
-        assertTrue(join.size() > 0);
-        join.nextPage();
-        join.first();
-        final WorkflowContext wc = new WorkflowContext();
-        IWorkflowContextFactory f = new IWorkflowContextFactory() {
-            public WorkflowContext getWorkflowContext() {
-                return wc;
-            }
-        };
-        AmbitWorkflowContextPanel panel = new AmbitWorkflowContextPanel(wc);
-        Vector<String> p = new Vector<String>();
-        p.add(DBWorkflowContext.STOREDQUERY);
-        p.add(DBWorkflowContext.ERROR);
-        panel.setProperties(p);        
-        wc.put(WorkflowContextEvent.WF_ANIMATE, new Boolean(true));
-        wc.put(DBWorkflowContext.STOREDQUERY, join);
-        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        JOptionPane.showMessageDialog(null,panel);
-
-
-        try {
-            join.acceptChanges(datasource.getConnection());
-        } catch (SyncProviderException spe) {
-        	SyncResolver resolver = spe.getSyncResolver();
-        	Object crsValue; // value in crs
-        	Object resolverValue; // value in the SyncResolver object
-        	Object resolvedValue; // value to be persisted
-        	while (resolver.nextConflict()) {
-	        	if (resolver.getStatus() == SyncResolver.UPDATE_ROW_CONFLICT) {
-	        	int row = resolver.getRow();
-	        	join.absolute(row);
-	        	int colCount = join.getMetaData().getColumnCount();
-	        	for (int j = 1; j <= colCount; j++) {
-		        	if (resolver.getConflictValue(j) != null) {
-			        	crsValue = join.getObject(j);
-			        	resolverValue = resolver.getConflictValue(j);
-			        	 // compare crsValue and resolverValue to determine the
-			//        	 value to be persisted
-			        	if ("uncompress(structure)".equals(join.getMetaData().getColumnName(j))) continue;
-			        	
-			        	System.out.println(join.getMetaData().getColumnName(j)+ crsValue);
-			        	resolvedValue = crsValue;
-			        	resolver.setResolvedValue(j, resolvedValue);
-			        }
-	        	}
-	        	}
-        	}
-        }
-
-        join.close();
-        c.close();
-        //System.out.println(pages+"\t"+records);
-
-    }
  
     /**
 select SUBSTRING_INDEX(user(),'@',1)
