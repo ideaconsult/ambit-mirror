6920c02336524e24e0e5d974736e02858a572ec6
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-02-23 21:01:42 +0000

    two separate workflows for start/stop

--- ambit2-plugin-dbtools/src/test/java/ambit2/plugin/dbtools/test/MysqlServerLauncherTest.java
+++ ambit2-plugin-dbtools/src/test/java/ambit2/plugin/dbtools/test/MysqlServerLauncherTest.java
@@ -2,32 +2,76 @@ package ambit2.plugin.dbtools.test;
 
 
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import junit.framework.Assert;
+
+import org.dbunit.database.IDatabaseConnection;
 import org.junit.Test;
 
+import ambit2.db.processors.MySQLCommand;
+import ambit2.plugin.dbtools.MySQLServerStop;
 import ambit2.plugin.dbtools.MysqlServerLauncher;
 import ambit2.workflow.DBWorkflowContext;
 
-public class MysqlServerLauncherTest  {
+public class MysqlServerLauncherTest extends DbUnitTest {
 
 
-	/*
-	public void testPing() throws Exception {
-		IDatabaseConnection c = getConnection();
-		if (c.getConnection() instanceof com.mysql.jdbc.Connection) {
+	
+	public boolean testPing() throws Exception {
+		IDatabaseConnection c = null;
+		try {
+			c = getConnection();
+			if (c.getConnection() instanceof com.mysql.jdbc.Connection) 
 			((com.mysql.jdbc.Connection)c.getConnection()).ping();
+			return true;
+		} catch (Exception x) {
+			return false;
+		} finally {
+			try {
+			c.close();
+			} catch (Exception x) {}
 		}
-		c.close();
+		
 		
 	}
-	*/
+	
 	@Test
 	public void test() {
 		
 	}
-	public static void main(String[] args) {
-		MysqlServerLauncher w = new MysqlServerLauncher();
+
+	
+	public void testLauncher() throws Exception {
+		MysqlServerLauncher start = new MysqlServerLauncher();
+		MySQLServerStop stop = new MySQLServerStop();
 		DBWorkflowContext context = new DBWorkflowContext();
-		w.executeWith(context);		
+		MySQLCommand cmd = new MySQLCommand();
+		cmd.setMysqlPath("E:/Ideaconsult/AmbitXT-v2.00/mysql");
+		context.put(MySQLCommand.MYSQLCOMMAND, cmd);
+		context.addPropertyChangeListener(new PropertyChangeListener() {
+			public void propertyChange(PropertyChangeEvent evt) {
+				System.out.println(evt.getNewValue());
+				
+			}
+		});
+		start.executeWith(context);		
+		if (testPing())
+			stop.executeWith(context);		
+		if (testPing())
+			System.out.println("Error");
+		start.executeWith(context);		
+		if (cmd.getProcess() != null)
+			stop.executeWith(context);
+	}
+	public static void main(String[] args) {
+		MysqlServerLauncherTest test = new MysqlServerLauncherTest();
+		try {
+			test.testLauncher();
+		} catch (Exception x) {
+			x.printStackTrace();
+		}
 	}
 
 }
