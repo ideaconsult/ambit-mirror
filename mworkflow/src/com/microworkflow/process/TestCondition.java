1d25367762b4b7ef1778f49005582cd06d2aee21
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-02-24 05:59:15 +0000

    1.0.4

--- mworkflow/src/com/microworkflow/process/TestCondition.java
+++ mworkflow/src/com/microworkflow/process/TestCondition.java
@@ -15,12 +15,22 @@ import com.microworkflow.execution.WorkflowClosure;
  * @author Dragos Manolescu (dam@micro-workflow.com)
  */
 public abstract class TestCondition extends WorkflowClosure {
+	protected enum STATUS {NOTEXECUTED,YES,NO};
+	protected STATUS status = STATUS.NOTEXECUTED;
 	
+	public boolean getResult() {
+		return status.equals(STATUS.YES);
+	}
+
 	public abstract boolean evaluate();
 	
 	public boolean evaluateInContext(WorkflowContext aContext) {
 		setContext(aContext);
-		return evaluate();
+		status =  evaluate() ? STATUS.YES : STATUS.NO;
+		return status.equals(STATUS.YES);
+	}
+	public boolean hasExecuted() {
+		return !status.equals(STATUS.NOTEXECUTED);
 	}
 
 }
