10512364eead377053f303d0efff7176a262eab2
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-03-21 11:27:17 +0000

    moving pubchem utilities to a separate module

---
+++ ambit2-core/src/test/java/ambit2/core/data/test/EINECSTest.java
@@ -0,0 +1,57 @@
+/* EINECSTest.java
+ * Author: nina
+ * Date: Mar 21, 2009
+ * Revision: 0.1 
+ * 
+ * Copyright (C) 2005-2009  Ideaconsult Ltd.
+ * 
+ * Contact: nina
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
+
+package ambit2.core.data.test;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ambit2.core.data.EINECS;
+
+public class EINECSTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testIsValid() {
+		Assert.assertTrue(EINECS.isValid("200-006-5"));
+		Assert.assertTrue(EINECS.isValid("200-002-3"));
+		Assert.assertFalse(EINECS.isValid("200-005-1"));
+	}
+
+}
