210317d0d0792d7e89c2b8160f90d4b86c6da68f
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-03-28 07:51:50 +0000

    test updated to execute a query

--- ambit2-all/ambit2-db/src/test/java/ambit2/db/search/test/QuerySimilarityBitSetTest.java
+++ ambit2-all/ambit2-db/src/test/java/ambit2/db/search/test/QuerySimilarityBitSetTest.java
@@ -1,5 +1,6 @@
 package ambit2.db.search.test;
 
+import java.sql.ResultSet;
 import java.util.List;
 
 import junit.framework.Assert;
@@ -9,6 +10,7 @@ import org.openscience.cdk.fingerprint.Fingerprinter;
 import org.openscience.cdk.templates.MoleculeFactory;
 
 import ambit2.db.search.NumberCondition;
+import ambit2.db.search.QueryField;
 import ambit2.db.search.QueryParam;
 import ambit2.db.search.QuerySimilarityBitset;
 
@@ -17,26 +19,39 @@ import ambit2.db.search.QuerySimilarityBitset;
  * @author Nina Jeliazkova nina@acad.bg
  *
  */
-public class QuerySimilarityBitSetTest  {
+public class QuerySimilarityBitSetTest extends QueryTest<QuerySimilarityBitset>  {
 	@Test
 	public void testQ() throws Exception {
-		Fingerprinter fp = new Fingerprinter();
-		QuerySimilarityBitset qf = new QuerySimilarityBitset();
-		qf.setValue(fp.getFingerprint(MoleculeFactory.makeAlkane(10)));
-		qf.setCondition(NumberCondition.getInstance(">"));
-		qf.setThreshold(0.5);
-		qf.setId(1);
-		Assert.assertEquals("select ? as idquery,L.idchemical,L.idstructure,1 as selected,round(cbits/(bc+?-cbits),2) as metric from\n(select fp1024.idchemical,structure.idstructure,(bit_count(? & fp1) + bit_count(? & fp2) + bit_count(? & fp3) + bit_count(? & fp4) + bit_count(? & fp5) + bit_count(? & fp6) + bit_count(? & fp7) + bit_count(? & fp8) + bit_count(? & fp9) + bit_count(? & fp10) + bit_count(? & fp11) + bit_count(? & fp12) + bit_count(? & fp13) + bit_count(? & fp14) + bit_count(? & fp15) + bit_count(? & fp16))  as cbits,bc from fp1024 join structure using(idchemical) ) as L, chemicals where bc > 0 and cbits > 0 and (cbits/(bc+?-cbits)>?) and L.idchemical=chemicals.idchemical order by metric desc",qf.getSQL());
+
+		Assert.assertEquals("select ? as idquery,L.idchemical,L.idstructure,1 as selected,round(cbits/(bc+?-cbits),2) as metric from\n(select fp1024.idchemical,structure.idstructure,(bit_count(? & fp1) + bit_count(? & fp2) + bit_count(? & fp3) + bit_count(? & fp4) + bit_count(? & fp5) + bit_count(? & fp6) + bit_count(? & fp7) + bit_count(? & fp8) + bit_count(? & fp9) + bit_count(? & fp10) + bit_count(? & fp11) + bit_count(? & fp12) + bit_count(? & fp13) + bit_count(? & fp14) + bit_count(? & fp15) + bit_count(? & fp16))  as cbits,bc from fp1024 join structure using(idchemical) ) as L, chemicals where bc > 0 and cbits > 0 and (cbits/(bc+?-cbits)>?) and L.idchemical=chemicals.idchemical order by metric desc",
+					query.getSQL());
 		//select cbits,bc,? as NA,round(cbits/(bc+?-cbits),2) as similarity,smiles,formula,L.idchemical from (select fp1024.idchemical,(bit_count(?& fp1) + bit_count(?& fp2) + bit_count(?& fp3) + bit_count(?& fp4) + bit_count(?& fp5) + bit_count(?& fp6) + bit_count(?& fp7) + bit_count(?& fp8) + bit_count(?& fp9) + bit_count(?& fp10) + bit_count(?& fp11) + bit_count(?& fp12) + bit_count(?& fp13) + bit_count(?& fp14) + bit_count(?& fp15) + bit_count(?& fp16))  as cbits,bc from fp1024 ) as L, chemicals where bc > 0 and cbits > 0 and (cbits/(bc+?-cbits)>?) and L.idchemical=chemicals.idchemical order by similarity desc
-		List<QueryParam> params = qf.getParameters();
+		List<QueryParam> params = query.getParameters();
 		Assert.assertNotNull(params);
 		Assert.assertEquals(20,params.size());
-		/*O
-		assertEquals(Integer.class,params.get(0).getType());
-		assertEquals(String.class,params.get(1).getType());
-		assertEquals(1,params.get(0).getValue());
-		assertEquals("CC",params.get(1).getValue());
-		*/
 	
 	}
+
+	@Override
+	protected QuerySimilarityBitset createQuery() throws Exception {
+		Fingerprinter fp = new Fingerprinter();
+		QuerySimilarityBitset qf = new QuerySimilarityBitset();
+		qf.setValue(fp.getFingerprint(MoleculeFactory.makeAlkane(3)));
+		qf.setCondition(NumberCondition.getInstance(">"));
+		qf.setThreshold(0.24);
+		qf.setId(1);
+		return qf;
+	}
+
+	@Override
+	protected void verify(QuerySimilarityBitset query, ResultSet rs)
+			throws Exception {
+		while (rs.next()) {
+			Assert.assertEquals(query.getId().intValue(),rs.getInt(1));
+			Assert.assertEquals(10,rs.getInt(2));
+			Assert.assertEquals(100214,rs.getInt(3));
+			Assert.assertEquals(1,rs.getInt(4));
+			Assert.assertEquals(0.25,rs.getFloat(5),1E-4);				
+		}
+	}	
 }
