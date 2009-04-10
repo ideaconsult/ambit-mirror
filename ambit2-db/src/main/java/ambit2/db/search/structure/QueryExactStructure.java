394ad518eac012bcff0fb3e859f1102ce2d062df
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-04-10 06:49:51 +0000

    queries for fragments and exact structure

---
+++ ambit2-db/src/main/java/ambit2/db/search/structure/QueryExactStructure.java
@@ -0,0 +1,105 @@
+/* QueryExactStructureBitSet.java
+ * Author: nina
+ * Date: Apr 10, 2009
+ * Revision: 0.1 
+ * 
+ * Copyright (C) 2005-2009  Ideaconsult Ltd.
+ * 
+ * Contact: nina
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
+
+package ambit2.db.search.structure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openscience.cdk.interfaces.IMolecule;
+import org.openscience.cdk.interfaces.IMoleculeSet;
+import org.openscience.cdk.nonotify.NoNotificationChemObjectBuilder;
+
+import ambit2.base.exceptions.AmbitException;
+import ambit2.core.processors.structure.key.SmilesKey;
+import ambit2.db.search.NumberCondition;
+import ambit2.db.search.QueryParam;
+
+public class QueryExactStructure extends AbstractStructureQuery<String,IMoleculeSet,NumberCondition>  {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4140784963337312377L;
+	public final static String sqlSMILES = 
+		"select ? as idquery,idchemical,idstructure,1 as selected,1 as metrics from structure join chemicals using(idchemical) where (smiles = ?)";
+	protected SmilesKey smilesKey = new SmilesKey();
+
+	
+	public String getSQL() throws AmbitException {
+		return String.format(sqlSMILES);
+	}
+	public List<QueryParam> getParameters() throws AmbitException {
+		List<QueryParam> params = new ArrayList<QueryParam>();
+		params.add(new QueryParam<Integer>(Integer.class, getId()));
+		params.add(new QueryParam<String>(String.class, getSmiles(getValue())));		
+		return params;
+	}
+	
+	public String getSmiles(IMoleculeSet set) throws AmbitException {
+		IMolecule a = NoNotificationChemObjectBuilder.getInstance().newMolecule();
+		for (int i=0; i < set.getAtomContainerCount();i++)
+			a.add(set.getMolecule(i));
+		return smilesKey.process(a);
+	}
+	/*
+	public String getSQL() throws AmbitException {
+
+		//int bc = bitset.cardinality();
+		StringBuffer b = new StringBuffer();
+			b.append("select ? as idquery,idchemical,idstructure,1 as selected,bc as metric from\n");
+			b.append("fp1024 join structure using(idchemical) where ");
+			for (int h=0; h < 16; h++) {
+				b.append("(? = fp");
+				b.append(Integer.toString(h+1));
+				b.append(")");
+				if (h<15) b.append(" and ");
+			}
+
+	
+		return b.toString();
+
+	}
+	
+	public List<QueryParam> getParameters() throws AmbitException {
+		long[] h16 = new long[16];
+		MoleculeTools.bitset2Long16(getValue(),64,h16);
+		int bc = getValue().cardinality();
+		List<QueryParam> params = new ArrayList<QueryParam>();
+		params.add(new QueryParam<Integer>(Integer.class, getId()));
+		for (int h=0; h < 16; h++)
+			params.add(new QueryParam<Long>(Long.class, h16[h]));
+	
+		return params;
+	}
+	*/	
+	@Override
+	public String toString() {
+		return "Exact structure";
+	}
+}
