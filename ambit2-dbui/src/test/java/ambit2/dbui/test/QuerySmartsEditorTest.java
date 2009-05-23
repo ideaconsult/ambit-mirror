2ead9ffff2a8e76c071de897b07e71d57a1fcda7
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-05-23 17:31:29 +0000

    Enhance QuerySmarts editor with UI to verify the smarts on a tets structure https://sourceforge.net/tracker/?func=detail&aid=2795873&group_id=191756&atid=938660

---
+++ ambit2-dbui/src/test/java/ambit2/dbui/test/QuerySmartsEditorTest.java
@@ -0,0 +1,29 @@
+package ambit2.dbui.test;
+
+import javax.swing.JOptionPane;
+
+import org.junit.After;
+import org.junit.Before;
+
+import ambit2.db.search.structure.QuerySMARTS;
+import ambit2.ui.EditorPreferences;
+
+public class QuerySmartsEditorTest {
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	public static void main(String args[]) {
+		QuerySMARTS query = new QuerySMARTS();
+		try {
+			JOptionPane.showMessageDialog(null,EditorPreferences.getEditor(query).getJComponent());
+			System.out.println(query.toString());
+			System.out.println(query.getSQL());
+		} catch (Exception x) {
+			x.printStackTrace();
+		}
+	}
+}
