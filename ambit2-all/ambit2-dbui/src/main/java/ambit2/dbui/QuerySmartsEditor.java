60948c9a5090309c50c4ce8703edfee02dfe9184
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-05-23 17:31:29 +0000

    Enhance QuerySmarts editor with UI to verify the smarts on a tets structure https://sourceforge.net/tracker/?func=detail&aid=2795873&group_id=191756&atid=938660

--- ambit2-all/ambit2-dbui/src/main/java/ambit2/dbui/QuerySmartsEditor.java
+++ ambit2-all/ambit2-dbui/src/main/java/ambit2/dbui/QuerySmartsEditor.java
@@ -1,13 +1,20 @@
 package ambit2.dbui;
 
+import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.AbstractAction;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
+
+import org.openscience.cdk.interfaces.IAtomContainer;
 
 import ambit2.base.interfaces.IStructureRecord;
 import ambit2.db.exceptions.DbAmbitException;
@@ -15,6 +22,9 @@ import ambit2.db.search.BooleanCondition;
 import ambit2.db.search.structure.QuerySMARTS;
 import ambit2.descriptors.FuncGroupsDescriptorFactory;
 import ambit2.descriptors.FunctionalGroup;
+import ambit2.descriptors.VerboseDescriptorResult;
+import ambit2.ui.Utils;
+import ambit2.ui.editors.Panel2D;
 
 import com.jgoodies.binding.adapter.BasicComponentFactory;
 import com.jgoodies.binding.beans.BeanAdapter;
@@ -25,13 +35,22 @@ import com.jgoodies.forms.builder.PanelBuilder;
 import com.jgoodies.forms.layout.CellConstraints;
 import com.jgoodies.forms.layout.FormLayout;
 
+/**
+ * An editor for {@link QuerySMARTS}
+ * @author nina
+ *
+ */
 public class QuerySmartsEditor extends QueryEditor<String,FunctionalGroup,BooleanCondition, IStructureRecord, QuerySMARTS> {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9201007230031542807L;
 	public JComponent buildPanel() {
 
 		FormLayout layout = new FormLayout(
 	            "125dlu,3dlu,75dlu,3dlu,125dlu",
-				"pref,pref,pref,pref,pref");        
+				"pref,pref,pref,pref,pref,pref,pref,pref,pref,pref:grow");        
 	     PanelBuilder panel = new PanelBuilder(layout);
 	     panel.setDefaultDialogBorder();
 
@@ -42,7 +61,7 @@ public class QuerySmartsEditor extends QueryEditor<String,FunctionalGroup,Boolea
 			try {
 				gf = factory.process("ambit2/descriptors/strucfeatures.xml");
 			} catch (Exception x) {gf = new ArrayList<FunctionalGroup>();}
-			SelectionInList<FunctionalGroup> selectionInList = new SelectionInList<FunctionalGroup>(
+			final SelectionInList<FunctionalGroup> selectionInList = new SelectionInList<FunctionalGroup>(
             		gf,
             		presentationModel.getModel("value"));
 			JComboBox box = BasicComponentFactory.createComboBox(selectionInList);
@@ -68,9 +87,62 @@ public class QuerySmartsEditor extends QueryEditor<String,FunctionalGroup,Boolea
 	        			((JTextField)e.getSource()).setToolTipText(((JTextField)e.getSource()).getText());
 	        		}
 	        	});
-	        	panel.add(t,cc.xywh(2,j+2,4,1));	   
+	        	panel.add(t,cc.xywh(3,j+2,3,1));	   
 	        }
+	        
+	        final Panel2D panel2D = new Panel2D();
+	 	    panel2D.setToolTipText("Double click here to edit structure");
+	 	    panel2D.setPreferredSize(new Dimension(200,200));
+	 	    panel2D.setMinimumSize(new Dimension(150,150));
+	        panel2D.setBorder(BorderFactory.createTitledBorder("Test compound"));
+	        
+	       
+	       
 	          
+	        final JLabel status = new JLabel("");
+	        status.setBorder(BorderFactory.createTitledBorder("Test result"));
+	    	JButton b = new JButton(new AbstractAction("Test SMARTS") {
+				public void actionPerformed(java.awt.event.ActionEvent arg0) {
+					try {
+						if (selectionInList.getSelection()==null) {
+							status.setText("No SMARTS selected!");
+							status.setIcon(null);
+						} else if (panel2D.getObject()==null) {
+							status.setText("Empty test structure!");
+							status.setIcon(null);
+						} else {
+							selectionInList.getSelection().setVerboseMatch(true);
+							VerboseDescriptorResult result = selectionInList.getSelection().process(panel2D.getObject());
+							
+							if (result.getExplanation() instanceof String) {
+								status.setText(result.getExplanation().toString());
+								status.setIcon(null);
+							} else {
+								boolean notfound = result.getResult().toString().equals("0");
+								status.setText((notfound)?"NOT FOUND":"FOUND");
+								status.setIcon((notfound)?Utils.createImageIcon("images/cross.png"):Utils.createImageIcon("images/tick.png"));
+								panel2D.setSelected((notfound)?null:(IAtomContainer)result.getExplanation());
+							}
+						}
+					} catch (Exception x) {
+						status.setText(x.getMessage());
+						status.setIcon(null);
+					} finally {
+						if (selectionInList.getSelection()!=null)
+							selectionInList.getSelection().setVerboseMatch(false);
+					}
+					
+				};
+			});
+			b.setMinimumSize(new Dimension(64,18));
+			b.setToolTipText("Click here to verify SMARTS validity");	  
+			
+			panel.addSeparator("Test Structure",cc.xywh(3,config.length+2,3,1));
+			panel.add(panel2D,cc.xywh(3,config.length+3,3,4));
+			//panel.add(new JLabel("Test structure"),cc.xywh(1,config.length+3,1,1));
+
+			panel.add(b,cc.xywh(1,config.length+2,1,1));
+			panel.add(status,cc.xywh(1,config.length+5,1,2));
 	     return panel.getPanel();
 	}		
 
