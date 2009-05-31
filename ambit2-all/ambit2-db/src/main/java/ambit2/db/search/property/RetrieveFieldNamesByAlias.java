c063462d4a60c32b207c4348fba8b48813cb196c
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-05-31 13:08:23 +0000

    Retrieve names by comments field

---
+++ ambit2-all/ambit2-db/src/main/java/ambit2/db/search/property/RetrieveFieldNamesByAlias.java
@@ -0,0 +1,74 @@
+package ambit2.db.search.property;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.openscience.cdk.CDKConstants;
+
+import ambit2.base.data.Profile;
+import ambit2.base.data.Property;
+import ambit2.base.exceptions.AmbitException;
+import ambit2.db.search.QueryParam;
+import ambit2.db.search.StringCondition;
+
+public class RetrieveFieldNamesByAlias extends AbstractPropertyRetrieval<String, String, StringCondition> {
+
+	public static String sql = "select idproperty,name,units,title,url,idreference,comments from properties join catalog_references using(idreference)";
+	public static String where = " where comments %s ?";
+		/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8369867048140756850L;
+	public RetrieveFieldNamesByAlias() {
+		this(CDKConstants.NAMES);
+	}
+	public RetrieveFieldNamesByAlias(String alias) {
+		super();
+		setCondition(StringCondition.getInstance(StringCondition.C_EQ));
+		setValue(alias);
+	}
+		public List<QueryParam> getParameters() throws AmbitException {
+			if (getValue()!=null) {
+				List<QueryParam> params = new ArrayList<QueryParam>();
+				params.add(new QueryParam<String>(String.class, getValue()));				
+				return params;
+			} else return null;
+			
+		}
+		public String getSQL() throws AmbitException {
+			if (getValue()!=null) {
+				return sql + String.format(where,getCondition().getSQL());
+			}
+			return sql;
+		}
+		/**
+		 * returns field name
+		 */
+		public Property getObject(ResultSet rs) throws AmbitException {
+			try {
+				Property p = Property.getInstance(rs.getString(2),rs.getString(4),rs.getString(5));
+				p.setId(rs.getInt(1));
+				p.setUnits(rs.getString(3));
+				p.setLabel(rs.getString(7));
+				p.getReference().setId(rs.getInt(6));
+				return p;
+			} catch (SQLException x) {
+				throw new AmbitException(x);
+			}
+		}
+		public String getFieldID() {
+			return "idproperty";
+		}
+		public String getValueID() {
+			return "name";
+		}
+		public Class getFieldType() {
+			return String.class;
+		}
+		public Class getValueType() {
+			return String.class;
+		}
+}
---
+++ ambit2-all/ambit2-db/src/test/java/ambit2/db/readers/test/RetrieveFieldNamesByAliasTest.java
@@ -0,0 +1,137 @@
+package ambit2.db.readers.test;
+
+import java.sql.ResultSet;
+
+import junit.framework.Assert;
+
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.ITable;
+import org.junit.Test;
+import org.openscience.cdk.CDKConstants;
+
+import ambit2.base.data.Property;
+import ambit2.db.readers.IQueryRetrieval;
+import ambit2.db.results.AmbitRows;
+import ambit2.db.search.IQueryObject;
+import ambit2.db.search.QueryExecutor;
+import ambit2.db.search.property.RetrieveFieldNamesByAlias;
+
+public class RetrieveFieldNamesByAliasTest extends RetrieveTest<Property> {
+
+	@Override
+	protected IQueryRetrieval<Property> createQuery() {
+		RetrieveFieldNamesByAlias q = new RetrieveFieldNamesByAlias();
+		return q;
+	}
+	@Test
+	public void testGetParameters() throws Exception {
+		Assert.assertNull(((IQueryObject)query).getParameters());
+	}
+
+	@Test
+	public void testGetSQL() throws Exception {
+		Assert.assertEquals(RetrieveFieldNamesByAlias.sql, ((IQueryObject)query).getSQL());
+	}
+
+	@Override
+	protected String getTestDatabase() {
+		return "src/test/resources/ambit2/db/processors/test/dataset-properties.xml";
+	}
+	@Test
+	public void testGetObject() throws Exception {
+		setUpDatabase(getTestDatabase());
+
+		IDatabaseConnection c = getConnection();
+		ITable names = 	c.createQueryTable("EXPECTED_NAMES","SELECT * FROM properties");		
+		Assert.assertEquals(4,names.getRowCount());
+
+		QueryExecutor<RetrieveFieldNamesByAlias> qe = new QueryExecutor<RetrieveFieldNamesByAlias>();		
+		qe.setConnection(c.getConnection());
+
+		ResultSet rs = qe.process((RetrieveFieldNamesByAlias)query);
+		
+		int count = 0; 
+		while (rs.next()) {
+			names = 	c.createQueryTable("EXPECTED_NAME","SELECT * FROM properties where name='"+query.getObject(rs)+"'");		
+			Assert.assertEquals(1,names.getRowCount());
+			count++;
+		}
+		Assert.assertEquals(4,count);
+		rs.close();
+		qe.close();
+		c.close();
+	}	
+	
+	@Test
+	public void testGetObjectByName() throws Exception {
+		setUpDatabase(getTestDatabase());
+
+		IDatabaseConnection c = getConnection();
+		ITable names = 	c.createQueryTable("EXPECTED_NAMES","SELECT * FROM properties");		
+		Assert.assertEquals(4,names.getRowCount());
+
+		QueryExecutor<RetrieveFieldNamesByAlias> qe = new QueryExecutor<RetrieveFieldNamesByAlias>();		
+		qe.setConnection(c.getConnection());
+
+		RetrieveFieldNamesByAlias q = new RetrieveFieldNamesByAlias();
+		q.setValue(CDKConstants.NAMES);
+		ResultSet rs = qe.process(q);
+		
+		int count = 0; 
+		while (rs.next()) {
+			names = 	c.createQueryTable("EXPECTED_NAME","SELECT * FROM properties where idproperty = "+ query.getObject(rs).getId());		
+			Assert.assertEquals(1,names.getRowCount());
+			count++;
+		}
+		Assert.assertEquals(2,count);
+		rs.close();
+		qe.close();
+		c.close();
+	}		
+
+	@Test
+	public void testGetFieldID() {
+		Assert.assertEquals("idproperty", ((RetrieveFieldNamesByAlias)query).getFieldID());
+	}
+
+	@Test
+	public void testGetValueID() {
+		Assert.assertEquals("name", ((RetrieveFieldNamesByAlias)query).getValueID());
+	}
+
+	@Test
+	public void testGetFieldType() throws Exception {
+		Assert.assertEquals(String.class,((RetrieveFieldNamesByAlias)query).getFieldType());
+	}
+
+	@Test
+	public void testGetValueType() {
+		Assert.assertEquals(String.class,((RetrieveFieldNamesByAlias)query).getValueType());
+	}
+	@Override
+	protected AmbitRows<Property> createRows() throws Exception {
+		return new AmbitRows<Property>();
+	}
+	@Override
+	protected void verifyRows(AmbitRows<Property> rows) throws Exception {
+		IDatabaseConnection c = getConnection();
+		Assert.assertNotNull(rows);
+		Assert.assertEquals(4,rows.size());
+		while (rows.next()) {
+			Property p = rows.getObject();
+			ITable table = 	c.createQueryTable("EXPECTED",
+					"select idproperty,name,units,title,url,idreference,comments from properties join catalog_references using(idreference) where name='"+p.getName()+"' and title='"+p.getReference().getTitle()+"'");		
+			Assert.assertEquals(1,table.getRowCount());			
+			for (int i=1; i <= rows.getMetaData().getColumnCount();i++) {
+				Object expected = table.getValue(0,rows.getMetaData().getColumnName(i));
+				Object actual = rows.getObject(i);
+				if ((expected == null) && (actual == null)) continue;
+				else
+					Assert.assertEquals(expected.toString(),actual.toString());
+
+				
+			}
+			
+		}
+	}
+}
