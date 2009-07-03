cb1f6fd36637c7e0cf168bea57cc587156547762
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-07-03 14:10:16 +0000

    enhanced dataset query, dataset column in browser added

--- ambit2-all/ambit2-db/src/main/java/ambit2/db/search/structure/QueryStructureByQualityPairLabel.java
+++ ambit2-all/ambit2-db/src/main/java/ambit2/db/search/structure/QueryStructureByQualityPairLabel.java
@@ -5,73 +5,66 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import ambit2.base.data.AmbitUser;
-import ambit2.base.data.QLabel;
-import ambit2.base.data.QLabel.QUALITY;
 import ambit2.base.exceptions.AmbitException;
-import ambit2.db.search.EQCondition;
 import ambit2.db.search.QueryParam;
+import ambit2.db.search.StringCondition;
 
-public class QueryStructureByQualityPairLabel extends 	AbstractStructureQuery<AmbitUser, Integer, EQCondition> {
-
+public class QueryStructureByQualityPairLabel extends 	AbstractStructureQuery<String, String, StringCondition> {
+	public static enum CONSENSUS_LABELS {Consensus,Majority,Unconfirmed,Ambiguous,Unknown};
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -3232148472829083139L;
-	/*
+
 	public final static String sql = 
-		"select ? as idquery,idchemical,idstructure1,min(same) as selected,sum(same) as metric from quality_pair\n"+
-		"%s\n"+
-		"group by idchemical\n";
-	*/
-	public final static String sql = 
-		"select ? as idquery,idchemical,idstructure1,same as selected,same as metric from quality_pair\n"+
-		"%s\n"+
+		"select ? as idquery,idchemical,idstructure,case 1 when (q.label='Consensus') then 1 else 0 end as selected,cast(q.label as unsigned) as metric from\n"+
+		"quality_chemicals q join structure using(idchemical) where q.label = ? %s\n"+
 		"order by idchemical\n";	
-	public final static String where = " where same %s ?";
-	
-	/*
-select selected,idchemical,pairs,consensus,pairs-consensus as diff, consensus/pairs from (
-SELECT idchemical,count(*) pairs,min(same) selected,sum(same) consensus FROM quality_pair q
-group by idchemical
-) as d
-order by diff,1/pairs
-	 */
+	public final static String where = " and text = ?";
 
 	public QueryStructureByQualityPairLabel() {
-		setFieldname(new AmbitUser("quality"));
-		setValue(null);
-		setCondition(EQCondition.getInstance());
+		this("Consensus");
 	}
 	
-	public QueryStructureByQualityPairLabel(Integer label) {
-		setFieldname(new AmbitUser("quality"));
-		setValue(label);
-		setCondition(EQCondition.getInstance());
+	public QueryStructureByQualityPairLabel(String label) {
+		setFieldname(label);
+		setValue(null);
+		setCondition(StringCondition.getInstance("="));
 	}	
 
 	public String getSQL() throws AmbitException {
+		String result = null;
 		if (getValue()==null)
-			return String.format(sql,"");
+			result = String.format(sql,"");
 		else
 			if (getValue()!=null)
-				return  String.format(String.format(sql,where),getCondition().getSQL());
+				result =   String.format(sql,where);
 			else
-				return  String.format(sql,"");
+				result =  String.format(sql,"");
+		System.out.println(result);
+		return result;
 	}
 
 	public List<QueryParam> getParameters() throws AmbitException {
-		
+		/*
+		"select ? as idquery,idchemical,idstructure,case 1 when (q.label='Consensus') then 1 else 0 end as selected,cast(q.label as unsigned) as metric from\n"+
+		"quality_chemicals q join structure using(idchemical) where q.label = ? %s\n"+
+		"order by idchemical\n";	
+		 */
 		List<QueryParam> params = new ArrayList<QueryParam>();
 		params.add(new QueryParam<Integer>(Integer.class, getId()));
+		if (getFieldname()==null) throw new AmbitException("No label!");
+		params.add(new QueryParam<String>(String.class,getFieldname()));
+		
 		if (getValue()!=null)
-			params.add(new QueryParam<Integer>(Integer.class, getValue()));
+			params.add(new QueryParam<String>(String.class, getValue()));
 		return params;
 	}
 	@Override  
 	public String toString() {
 		StringBuilder b = new StringBuilder();
-		b.append(getFieldname() + "quality");
+		b.append(getFieldname());
+		b.append(getValue()==null?"":getValue());
 		if (getValue()!=null) {
 			b.append(getCondition().toString());
 			b.append(getValue());
@@ -81,7 +74,8 @@ order by diff,1/pairs
 	@Override
 	public Object retrieveValue(ResultSet rs) throws SQLException {
 		if (rs.getObject(5)!=null)
-			return new QLabel(QUALITY.values()[rs.getInt(5)-1]);
+			
+			return CONSENSUS_LABELS.values()[rs.getInt(5)-1];
 		else return null;
 	}
 	
