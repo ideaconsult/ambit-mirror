992509f23a96efd8dfcff79284434b670097ac5c
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-07-26 11:52:46 +0000

    moved

--- ambit2-all/ambit2-www/src/test/java/ambit2/rest/resource/test/structure/ConformerResourceTest.java
+++
@@ -1,9 +0,0 @@
-package ambit2.rest.resource.structure;
-
-
-public class ConformerResourceTest extends CompoundResourceTest {
-	@Override
-	public String getTestURI() {
-		return String.format("http://localhost:%d/compound/10/conformer/100214", port);
-	}
-}
