bd61585dae51c0485d8c291f36c5679fce50e25c
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-07-27 20:36:32 +0000

    support for chemicals vs multiple structures per chemical

--- ambit2-all/ambit2-www/src/main/java/ambit2/rest/AmbitApplication.java
+++ ambit2-all/ambit2-www/src/main/java/ambit2/rest/AmbitApplication.java
@@ -23,7 +23,8 @@ import ambit2.rest.pubchem.PubchemResource;
 import ambit2.rest.query.PropertyQueryResource;
 import ambit2.rest.query.QueryListResource;
 import ambit2.rest.similarity.SimilarityResource;
-import ambit2.rest.structure.StructureResource;
+import ambit2.rest.structure.CompoundResource;
+import ambit2.rest.structure.ConformerResource;
 import ambit2.rest.structure.build3d.Build3DResource;
 import ambit2.rest.structure.diagram.CDKDepict;
 import ambit2.rest.structure.diagram.DaylightDepict;
@@ -34,13 +35,18 @@ import ambit2.rest.structure.diagram.DaylightDepict;
  *
  */
 public class AmbitApplication extends Application {
+	
 	public final static String datasets = "/dataset";		
-	public final static String dataset = datasets+"/{dataset_id}";
-	public final static String dataset_structures = datasets+"/{dataset_id}/structure";
-	public final static String dataset_structure = datasets+"/{dataset_id}/structure/{idstructure}";
+	public final static String datasetID =  String.format("%s%s",datasets,"/{dataset_id}");
+	
+	public final static String dataset_structures = String.format("%s%s",datasetID,CompoundResource.compound);
+	
+	public final static String datasetID_structure = String.format("%s%s",datasetID,CompoundResource.compoundID);
+	public final static String datasetID_structure_media = String.format("%s%s",datasetID,CompoundResource.compoundID_media);
+
 	public final static String query = "/query";	
-	public final static String similarity = query + "/similarity/method";		
-	public final static String fp_dataset = similarity + "/fp1024/distance/tanimoto/{threshold}" + dataset;
+	public final static String similarity = String.format("%s%s",query ,"/similarity/method");		
+	public final static String fp_dataset = String.format("%s%s%s",similarity,"/fp1024/distance/tanimoto/{threshold}",datasetID);
 	public final static String tanimoto = similarity + "/fp1024/distance/tanimoto";
 	public final static String fp =  tanimoto + "/{threshold}";
 	public final static String property =  query + "/property/{condition}" + "/{value}";
@@ -49,22 +55,33 @@ public class AmbitApplication extends Application {
 	protected DataSource datasource = null;
 	public AmbitApplication(Context context) {
 		super(context);
-		LoginInfo li = new LoginInfo();
-		li.setDatabase("ambit2");
-		li.setUser("guest");
-		li.setPassword("guest");
-		if (getContext().getParameters().size()>0) {
-			li.setDatabase(getContext().getParameters().getValues(Preferences.DATABASE));
-			li.setUser(getContext().getParameters().getValues(Preferences.USER));
-			li.setPassword(getContext().getParameters().getValues(Preferences.PASSWORD));
-			li.setHostname(getContext().getParameters().getValues(Preferences.HOST));
-			li.setPort(getContext().getParameters().getValues(Preferences.PORT));
+		/*
+		String tmpDir = System.getProperty("java.io.tmpdir");
+        File logFile = new File(tmpDir,"ambit2-www.log");		
+		System.setProperty("java.util.logging.config.file",logFile.getAbsolutePath());
+		*/
+		try {
+			LoginInfo li = new LoginInfo();
+			li.setDatabase("ambit2");
+			li.setUser("guest");
+			li.setPassword("guest");
+			li.setPort("3306");
+			if (getContext().getParameters().size()>0) {
+				li.setDatabase(getContext().getParameters().getValues(Preferences.DATABASE));
+				li.setUser(getContext().getParameters().getValues(Preferences.USER));
+				li.setPassword(getContext().getParameters().getValues(Preferences.PASSWORD));
+				li.setHostname(getContext().getParameters().getValues(Preferences.HOST));
+				li.setPort(getContext().getParameters().getValues(Preferences.PORT));
+			}
+			
+			connectionURI = DatasourceFactory.getConnectionURI(
+	                li.getScheme(), li.getHostname(), li.getPort(), 
+	                li.getDatabase(), li.getUser(), li.getPassword()); 
+		} catch (Exception x) {
+
+			connectionURI = null;
+			
 		}
-		
-		connectionURI = DatasourceFactory.getConnectionURI(
-                li.getScheme(), li.getHostname(), li.getPort(), 
-                li.getDatabase(), li.getUser(), li.getPassword()); 
-	
 	}
 	
 	
@@ -74,8 +91,11 @@ public class AmbitApplication extends Application {
 		router.attach("/", AmbitResource.class);
 		
 		router.attach(datasets, DatasetsResource.class);
-		router.attach(dataset, DatasetResource.class);
-		router.attach(dataset_structure, StructureResource.class);
+		router.attach(datasetID, DatasetResource.class);
+		router.attach(datasetID_structure, CompoundResource.class);
+		router.attach(datasetID_structure_media, CompoundResource.class);
+		
+		
 		router.attach(dataset_structures, DatasetStructuresResource.class);
 		
 		//router.attach("/smiles/{smiles}"+fp,SimilarityResource.class);
@@ -85,7 +105,14 @@ public class AmbitApplication extends Application {
 		
 		//router.attach("/cas/{cas}"+fp,SimilarityResource.class);
 		//router.attach("/name/{name}"+fp,SimilarityResource.class);		
-		router.attach("/structure/{idstructure}",StructureResource.class);
+		router.attach(CompoundResource.compoundID,CompoundResource.class);
+		router.attach(CompoundResource.compoundID_media, CompoundResource.class);		
+		
+		router.attach(ConformerResource.conformers,ConformerResource.class);
+		router.attach(ConformerResource.conformerID,ConformerResource.class);
+		router.attach(ConformerResource.conformerID_media, ConformerResource.class);		
+
+		
 		router.attach("/pubchem/query/{term}",PubchemResource.class);
 		
 		router.attach("/daylight/depict/{smiles}",DaylightDepict.class);
@@ -135,6 +162,7 @@ public class AmbitApplication extends Application {
         // Attach the application to the component and start it
         component.getDefaultHost().attach(application);
         component.start();
+
     }
     	
 }
