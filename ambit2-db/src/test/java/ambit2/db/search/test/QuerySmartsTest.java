fdcfad2685df9a8fb6c3bf807dad265c5acb430c
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-07-31 15:23:30 +0000

    updating querysmarts class to use Ambit smarts (FastSmarts class doesn't yet support recursive smarts) Tests for smarts accelerator added

--- ambit2-db/src/test/java/ambit2/db/search/test/QuerySmartsTest.java
+++ ambit2-db/src/test/java/ambit2/db/search/test/QuerySmartsTest.java
@@ -1,5 +1,6 @@
 package ambit2.db.search.test;
 
+import java.io.InputStreamReader;
 import java.sql.ResultSet;
 
 import junit.framework.Assert;
@@ -8,28 +9,39 @@ import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.ITable;
 import org.junit.Test;
 
+import ambit2.base.config.Preferences;
+import ambit2.base.data.Property;
 import ambit2.base.interfaces.IStructureRecord;
+import ambit2.core.io.RawIteratingSDFReader;
 import ambit2.db.SessionID;
 import ambit2.db.processors.ProcessorCreateQuery;
 import ambit2.db.processors.ProcessorCreateSession;
 import ambit2.db.search.IStoredQuery;
 import ambit2.db.search.structure.QuerySMARTS;
 import ambit2.descriptors.FunctionalGroup;
+import ambit2.smarts.CMLUtilities;
+import ambit2.smarts.processors.SMARTSPropertiesGenerator;
 
 public class QuerySmartsTest extends QueryTest<QuerySMARTS> {
 
 	@Override
 	protected QuerySMARTS createQuery() throws Exception {
 		QuerySMARTS q = new QuerySMARTS();
-		q.setValue(new FunctionalGroup("Br","Br",""));
+		q.setValue(new FunctionalGroup("aromatic aldehyde - example","c1ccccc1[$(C(C)C(=O)),$(CC(C)C(=O))]",""));
 		q.setId(999);
 		return q;
 	}
 
 	@Test
 	public void testSelect() throws Exception {
+		Preferences.setProperty(Preferences.FASTSMARTS,"true");		
+		dbFile = "src/test/resources/ambit2/db/processors/test/smarts-dataset.xml";	
 		setUpDatabase(dbFile);
 		IDatabaseConnection c = getConnection();
+		
+		ITable fp = c.createQueryTable("EXPECTED","select * from structure");
+		Assert.assertEquals(4, fp.getRowCount());
+		
 		ProcessorCreateSession p = new ProcessorCreateSession();
 		p.setConnection(c.getConnection());
 		SessionID id = p.process(null);
@@ -45,12 +57,12 @@ public class QuerySmartsTest extends QueryTest<QuerySMARTS> {
 		c.close();
 
 		c = getConnection();
-		ITable fp = c.createQueryTable("EXPECTED_query","select * from query where idquery="+sq.getId());
+		fp = c.createQueryTable("EXPECTED_query","select * from query where idquery="+sq.getId());
 		Assert.assertEquals(1, fp.getRowCount());
 		fp = c.createQueryTable("EXPECTED_query","select idchemical,idstructure from query_results where idquery="+sq.getId());
-		Assert.assertEquals(1, fp.getRowCount());
-		Assert.assertEquals(11,fp.getValue(0,"idchemical"));
-		Assert.assertEquals(100215,fp.getValue(0,"idstructure"));
+		Assert.assertEquals(4, fp.getRowCount());
+		Assert.assertEquals(2050,fp.getValue(0,"idchemical"));
+		
 		c.close();
 	}
 	
@@ -66,10 +78,53 @@ public class QuerySmartsTest extends QueryTest<QuerySMARTS> {
 			Assert.assertEquals(0.25,rs.getFloat("metric"),1E-4);
 			*/
 			IStructureRecord r = query.getObject(rs);
-			System.out.println(r);
 		}
 		
 	}
 
-
+	@Test
+	public void testMatchNoAccelerator() {
+		Preferences.setProperty(Preferences.FASTSMARTS,"false"); 
+		QuerySMARTS q = new QuerySMARTS();
+		q.setValue(new FunctionalGroup("aromatic aldehyde example","c1ccccc1[$(C(C)C(=O)),$(CC(C)C(=O))]",""));
+				//"c1ccccc1C(C)C(=O)",""));
+		RawIteratingSDFReader reader = new RawIteratingSDFReader(new InputStreamReader(
+			getClass().getClassLoader().getResourceAsStream("ambit2/db/processors/test/smartssearch.sdf")	
+			));
+		int count = 0;
+		while (reader.hasNext()) {
+			IStructureRecord record = reader.nextRecord();
+			Assert.assertEquals(1.0,q.calculateMetric(record));
+			count++;
+		}
+		Assert.assertEquals(2,count);
+	}
+	@Test
+	public void testMatchWithAccelerator() {
+		Preferences.setProperty(Preferences.FASTSMARTS,"true");
+		QuerySMARTS q = new QuerySMARTS();
+		q.setValue(new FunctionalGroup("aromatic aldehyde example","c1ccccc1[$(C(C)C(=O)),$(CC(C)C(=O))]",""));
+		RawIteratingSDFReader reader = new RawIteratingSDFReader(new InputStreamReader(
+			getClass().getClassLoader().getResourceAsStream("ambit2/db/processors/test/smartssearch.sdf")	
+			));
+		int count = 0;
+		SMARTSPropertiesGenerator gen = new SMARTSPropertiesGenerator();
+		while (reader.hasNext()) {
+			IStructureRecord record = reader.nextRecord();
+			try {
+				gen.process(record);
+			} catch (Exception x) {
+				x.printStackTrace();
+			}
+			Assert.assertNotNull(record.getProperty(Property.getInstance(CMLUtilities.SMARTSProp, CMLUtilities.SMARTSProp)));
+			
+			/*
+			record.setProperty(Property.getInstance(CMLUtilities.SMARTSProp, CMLUtilities.SMARTSProp),
+			"0210,1430,1440,04310600,14310600,14310600,14310600,14310600,14310600,3440,0110,0110,0110,0110,0110,0110,0110,0110,0110,0110,\n0,0,0,0,0,0,1,1,1,0,1,0,1,0,1,0,0,0,0,0,");
+			*/
+			Assert.assertEquals(1.0,q.calculateMetric(record));
+			count++;
+		}
+		Assert.assertEquals(2,count);
+	}	
 }
--- ambit2-db/src/test/java/ambit2/db/test/ExtractData.java
+++ ambit2-db/src/test/java/ambit2/db/test/ExtractData.java
@@ -19,9 +19,9 @@ public class ExtractData {
         // database connection
         Class driverClass = Class.forName("com.mysql.jdbc.Driver");
         Connection jdbcConnection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/ambit-test", "guest", "guest");
+                "jdbc:mysql://localhost:3306/ambit2", "guest", "guest");
         IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
-        String ids = "(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)";
+        String ids = "(2050)";
         // partial database export
         QueryDataSet partialDataSet = new QueryDataSet(connection);
         partialDataSet.addTable("users", "SELECT * FROM users WHERE user_name=\"guest\"");
@@ -32,7 +32,7 @@ public class ExtractData {
 
    
         partialDataSet.addTable("chemicals", "SELECT * FROM chemicals WHERE idchemical in "+ids);
-        partialDataSet.addTable("structure", "SELECT idstructure,idchemical,structure,format,updated,user_name,type_structure FROM structure join chemicals using(idchemical) WHERE idchemical in "+ids);
+        partialDataSet.addTable("structure", "SELECT idstructure,idchemical,structure,format,user_name,type_structure,atomproperties FROM structure join chemicals using(idchemical) WHERE idchemical in "+ids);
 
         partialDataSet.addTable("properties", "SELECT * FROM properties");         
   
@@ -41,21 +41,21 @@ public class ExtractData {
         
         partialDataSet.addTable("dictionary", "SELECT * FROM dictionary");        
             
-        partialDataSet.addTable("src_dataset", "SELECT * FROM src_dataset");
-        partialDataSet.addTable("tuples","select * from tuples"); 
+        partialDataSet.addTable("src_dataset", "SELECT id_srcdataset,name,user_name,idreference FROM src_dataset");
+        partialDataSet.addTable("tuples",String.format("select * from tuples where idtuple in (select idtuple from property_tuples join property_values using(id) join structure using(idstructure) where idchemical in %s)",ids)); 
  
-        partialDataSet.addTable("property_string", "SELECT * FROM property_string");
-        partialDataSet.addTable("property_values", "SELECT id,idproperty,idstructure,idvalue_string,value_num,status,property_values.user_name FROM property_values join structure using(idstructure) where idchemical in "+ids);     
-        partialDataSet.addTable("property_tuples", "SELECT idtuple,id FROM property_tuples join property_values using(id) where idstructure in (select idstructure from structure where idchemical in "+ids+")");          
-        partialDataSet.addTable("struc_dataset", "SELECT * FROM struc_dataset  where idstructure in (select idstructure from structure where idchemical in "+ids+")");        
+       // partialDataSet.addTable("property_string", "SELECT * FROM property_string");
+       // partialDataSet.addTable("property_values", "SELECT id,idproperty,idstructure,idvalue_string,value_num,status,property_values.user_name FROM property_values join structure using(idstructure) where idchemical in "+ids);     
+       // partialDataSet.addTable("property_tuples", "SELECT idtuple,id FROM property_tuples join property_values using(id) where idstructure in (select idstructure from structure where idchemical in "+ids+")");          
+        partialDataSet.addTable("struc_dataset", "SELECT idstructure,id_srcdataset FROM struc_dataset  where idstructure in (select idstructure from structure where idchemical in "+ids+")");        
         
-        partialDataSet.addTable("sessions", "SELECT idsessions,user_name FROM sessions");        
-        partialDataSet.addTable("query", "SELECT * FROM query");
-        partialDataSet.addTable("query_results", "SELECT * FROM query_results where idstructure in (select idstructure from structure where idchemical in "+ ids+")");
+       // partialDataSet.addTable("sessions", "SELECT idsessions,user_name FROM sessions");        
+       // partialDataSet.addTable("query", "SELECT * FROM query");
+       // partialDataSet.addTable("query_results", "SELECT * FROM query_results where idstructure in (select idstructure from structure where idchemical in "+ ids+")");
 
         //BIGINT serializes to long and gives errors for 
-        //partialDataSet.addTable("fp1024", "SELECT idchemical,fp1,fp2,fp3,fp4,fp5,fp6,fp7,fp8,fp9,fp10,fp11,fp12,fp13,fp14,fp15,fp16,time,bc,status FROM fp1024 join chemicals using(idchemical) WHERE idchemical in "+ids);
-        //partialDataSet.addTable("sk1024", "SELECT idchemical,fp1,fp2,fp3,fp4,fp5,fp6,fp7,fp8,fp9,fp10,fp11,fp12,fp13,fp14,fp15,fp16,time,bc,status FROM sk1024 join chemicals using(idchemical) WHERE idchemical in "+ids);
+        partialDataSet.addTable("fp1024", "SELECT idchemical,fp1,fp2,fp3,fp4,fp5,fp6,fp7,fp8,fp9,fp10,fp11,fp12,fp13,fp14,fp15,fp16,time,bc,status FROM fp1024 join chemicals using(idchemical) WHERE idchemical in "+ids);
+        partialDataSet.addTable("sk1024", "SELECT idchemical,fp1,fp2,fp3,fp4,fp5,fp6,fp7,fp8,fp9,fp10,fp11,fp12,fp13,fp14,fp15,fp16,time,bc,status FROM sk1024 join chemicals using(idchemical) WHERE idchemical in "+ids);
                 
         FlatDtdDataSet.write(partialDataSet, new FileOutputStream("src/test/resources/ambit2/db/processors/test/partial-dataset.dtd"));
         FlatXmlDataSet.write(partialDataSet, 
