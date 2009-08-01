2dd648ddeade29be6198d5253a10e9ef30b4777c
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-08-01 16:30:03 +0000

    Refactoring, adding skeletons for algorithm resources

---
+++ ambit2-www/src/main/java/ambit2/rest/algorithm/AlgorithmResource.java
@@ -0,0 +1,78 @@
+package ambit2.rest.algorithm;
+
+import java.io.Writer;
+import java.util.List;
+
+import org.restlet.Context;
+import org.restlet.data.MediaType;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.resource.Representation;
+import org.restlet.resource.Variant;
+
+import ambit2.base.exceptions.AmbitException;
+import ambit2.base.interfaces.IProcessor;
+import ambit2.rest.AbstractResource;
+import ambit2.rest.StringConvertor;
+import ambit2.rest.algorithm.descriptors.AlgorithmDescriptorResource;
+
+/**
+ * Algorithms as per http://opentox.org/wiki/opentox/Algorithm
+ * @author nina
+ *
+ */
+public class AlgorithmResource extends AbstractResource<List<String>,String,IProcessor<List<String>, Representation>> {
+	public final static String algorithm = "/algorithm";	
+	public final static String algorithmKey =  "algorithm_id";
+			
+	
+	protected String category = "";
+	public AlgorithmResource(Context context, Request request, Response response) {
+		super(context,request,response);
+		query.add("util");
+		query.add("preprocessing");
+		query.add("clustering");
+		query.add(AlgorithmDescriptorResource.descriptorcalculation);
+		query.add("learning");
+		this.getVariants().add(new Variant(MediaType.TEXT_HTML));
+		this.getVariants().add(new Variant(MediaType.TEXT_XML));
+		this.getVariants().add(new Variant(MediaType.TEXT_URI_LIST));		
+
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public static String getAlgorithmURI(String category) {
+		return String.format("%s%s/{%s}",algorithm,category,algorithmKey);
+	}
+	@Override
+	public IProcessor<List<String>, Representation> createConvertor(
+			Variant variant) throws AmbitException {
+		/*
+		if (variant.getMediaType().equals(MediaType.TEXT_XML)) {
+			return new DocumentConvertor(new DatasetsXMLReporter(getRequest().getRootRef()));	
+		} else if (variant.getMediaType().equals(MediaType.TEXT_HTML)) {
+			return new OutputStreamConvertor(
+					new DatasetsHTMLReporter(getRequest().getRootRef()),MediaType.TEXT_HTML);
+		} else if (variant.getMediaType().equals(MediaType.TEXT_URI_LIST)) {
+		*/
+			return new StringConvertor(	new AlgorithmURIReporter(getRequest().getRootRef()) {
+				@Override
+				public void processItem(Object src, Writer output) {
+					super.processItem(src, output);
+					try {
+					output.write('\n');
+					} catch (Exception x) {}
+				}
+			},MediaType.TEXT_URI_LIST);
+			/*
+		} else //html 	
+			return new OutputStreamConvertor(
+					new DatasetHTMLReporter(getRequest().getRootRef()),MediaType.TEXT_HTML);
+					*/
+	}
+	
+}
---
+++ ambit2-www/src/main/java/ambit2/rest/algorithm/quantumchemical/Build3DResource.java
@@ -0,0 +1,126 @@
+package ambit2.rest.algorithm.quantumchemical;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.exception.InvalidSmilesException;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IMolecule;
+import org.openscience.cdk.nonotify.NoNotificationChemObjectBuilder;
+import org.openscience.cdk.smiles.SmilesParser;
+import org.restlet.Context;
+import org.restlet.data.Reference;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.data.Status;
+import org.restlet.resource.OutputRepresentation;
+import org.restlet.resource.Representation;
+import org.restlet.resource.StringRepresentation;
+import org.restlet.resource.Variant;
+
+import ambit2.core.io.MDLWriter;
+import ambit2.mopac.MopacShell;
+import ambit2.rest.ChemicalMediaType;
+import ambit2.rest.algorithm.AlgorithmResource;
+
+public class Build3DResource extends AlgorithmResource {
+	
+	protected static String delim = null;	
+	protected String smiles = null;
+	protected static String bracketLeft="[";
+	protected static String bracketRight="]";
+	protected MopacShell shell;
+	
+	public Build3DResource(Context context, Request request, Response response) {
+		super(context,request,response);
+		setCategory("");
+		try {
+			shell = new MopacShell();
+		} catch (Exception x) {
+			x.printStackTrace();
+			shell = null;
+		}
+		this.getVariants().add(new Variant(ChemicalMediaType.CHEMICAL_MDLSDF));	
+		try {
+			this.smiles = Reference.decode(request.getAttributes().get("smiles").toString());
+		} catch (Exception x) {
+			this.smiles = null;
+		}		
+		if (delim == null) {
+			StringBuilder d = new StringBuilder();
+			d.append("-=#+-()/\\.@");
+			for (char a='a';a<='z';a++)	d.append(a);
+			for (char a='A';a<='Z';a++)	d.append(a);
+			delim = d.toString();
+		}		
+	}
+	public Representation getRepresentation(Variant variant) {
+		
+		try {
+	        if (smiles != null) {
+	        	IAtomContainer mol = getMolecule(smiles);
+	        	if ((mol ==  null) || (mol.getAtomCount()==0)) {
+		        	getResponse().setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
+	        		return new StringRepresentation("No image",variant.getMediaType());	   
+	        	}
+	        	final IAtomContainer newmol = shell.process(mol);	        	
+	        	return new OutputRepresentation(ChemicalMediaType.CHEMICAL_MDLSDF) {
+	        		@Override
+	        		public void write(OutputStream out) throws IOException {
+	        			MDLWriter writer  = new MDLWriter(out);
+	        			try {
+	        				writer.setSdFields(newmol.getProperties());
+	        				writer.write(newmol);
+	        			} catch (CDKException x) {
+	        				x.printStackTrace();
+	        			} finally {
+		        			out.flush();
+		        			out.close();
+	        			}
+	        		}
+	        	};
+	        } else {
+	        	getResponse().setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
+	        	return new StringRepresentation("Undefined query",variant.getMediaType());	        	
+	        }
+		} catch (Exception x) {
+			x.printStackTrace();
+			getResponse().setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
+			return new StringRepresentation("No results for query "+smiles,variant.getMediaType());	
+		
+		}
+	}			
+	public IMolecule getMolecule(String smiles) throws InvalidSmilesException {
+		SmilesParser parser = new SmilesParser(NoNotificationChemObjectBuilder.getInstance());
+		//This is a workaround for a bug in CDK smiles parser
+
+		StringTokenizer t = new StringTokenizer(smiles,"[]",true);
+		int bracket = 0;
+		Hashtable<String, Integer> digits = new Hashtable<String, Integer>();
+		while (t.hasMoreTokens())  {
+			String token = t.nextToken();
+			if (bracketLeft.equals(token)) { bracket++; continue;}
+			if (bracketRight.equals(token)) { bracket = 0; continue;}
+			if (bracket>0) continue;
+			
+			StringTokenizer t1 = new StringTokenizer(token,delim,false);
+			while (t1.hasMoreTokens()) {
+				String d = t1.nextToken();
+				Integer i = digits.get(d);
+				if (i==null) digits.put(d,1);
+				else digits.put(d,i+1);
+			}
+			Iterator<Integer> d = digits.values().iterator();
+			while (d.hasNext())
+				if ((d.next() %2)==1) throw new InvalidSmilesException(smiles);
+		}
+	
+
+	
+		return parser.parseSmiles(smiles);
+	}	
+}
