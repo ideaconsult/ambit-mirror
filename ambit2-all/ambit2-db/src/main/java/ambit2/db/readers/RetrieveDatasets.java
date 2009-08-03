670f3491c1b8272b8b980554be5cf995547dc839
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-08-03 10:36:20 +0000

    introduced condition in the query (was only "equal" )

--- ambit2-all/ambit2-db/src/main/java/ambit2/db/readers/RetrieveDatasets.java
+++ ambit2-all/ambit2-db/src/main/java/ambit2/db/readers/RetrieveDatasets.java
@@ -1,25 +1,24 @@
 package ambit2.db.readers;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import ambit2.base.data.LiteratureEntry;
 import ambit2.base.exceptions.AmbitException;
 import ambit2.base.interfaces.IStructureRecord;
 import ambit2.db.SourceDataset;
 import ambit2.db.search.AbstractQuery;
-import ambit2.db.search.EQCondition;
 import ambit2.db.search.QueryParam;
+import ambit2.db.search.StringCondition;
+import ambit2.db.search.StringCondition.STRING_CONDITION;
 import ambit2.db.update.dataset.ReadDataset;
 
-public class RetrieveDatasets extends AbstractQuery<IStructureRecord,SourceDataset,EQCondition,SourceDataset>  implements IQueryRetrieval<SourceDataset>{
+public class RetrieveDatasets extends AbstractQuery<IStructureRecord,SourceDataset,StringCondition,SourceDataset>  implements IQueryRetrieval<SourceDataset>{
     
     public static final String select_datasets_bystruc = "SELECT id_srcdataset,name,user_name,idreference,title,url FROM struc_dataset join src_dataset using(id_srcdataset) join catalog_references using(idreference) where idstructure=? %s order by name";
     
-    public static final String select_WHERE = "where name=?";
-    public static final String select_AND = "and name=?";
+    public static final String select_WHERE = "where name %s ?";
+    public static final String select_AND = "and name %s ?";
     protected ReadDataset readDataset = new ReadDataset();
 	/**
 	 * 
@@ -30,6 +29,7 @@ public class RetrieveDatasets extends AbstractQuery<IStructureRecord,SourceDatas
 		super();
 		setFieldname(record);
 		setValue(dataset);
+		setCondition(StringCondition.getInstance("="));
 	}
 	public RetrieveDatasets() {
 		this(null,null);
@@ -37,8 +37,8 @@ public class RetrieveDatasets extends AbstractQuery<IStructureRecord,SourceDatas
 	public String getSQL() throws AmbitException {
 		return 
 		(getFieldname()!=null)
-				?String.format(select_datasets_bystruc,(getValue()==null)?"":select_AND)
-				:String.format(ReadDataset.select_datasets,(getValue()==null)?"":select_WHERE);
+				?String.format(select_datasets_bystruc,(getValue()==null)?"":String.format(select_AND,getCondition().getSQL()))
+				:String.format(ReadDataset.select_datasets,(getValue()==null)?"":String.format(select_WHERE,getCondition().getSQL()));
 	}
 
 	public List<QueryParam> getParameters() throws AmbitException {
