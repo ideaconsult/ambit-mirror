3e412702aa105a6ed7f484746c40d5679ff2189e
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-08-08 12:03:04 +0000

    retrieve features per chemical/conformer

--- ambit2-db/src/main/java/ambit2/db/update/property/ReadProperty.java
+++ ambit2-db/src/main/java/ambit2/db/update/property/ReadProperty.java
@@ -1,11 +1,11 @@
 package ambit2.db.update.property;
 
-import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
 import ambit2.base.data.Property;
 import ambit2.base.exceptions.AmbitException;
+import ambit2.base.interfaces.IStructureRecord;
 import ambit2.db.search.EQCondition;
 import ambit2.db.search.QueryParam;
 import ambit2.db.search.property.AbstractPropertyRetrieval;
@@ -15,12 +15,20 @@ import ambit2.db.search.property.AbstractPropertyRetrieval;
  * @author nina
  *
  */
-public class ReadProperty extends AbstractPropertyRetrieval<String, Integer, EQCondition> {
+public class ReadProperty extends AbstractPropertyRetrieval<IStructureRecord, Integer, EQCondition> {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 6247086921731939782L;
+	public static String sqlPerStructure = 
+		"select idproperty,name,units,title,url,idreference,comments from properties join catalog_references using(idreference)\n"+
+		"where idproperty in (select idproperty from property_values where idstructure = ?) \n";
+
+	public static String sqlPerChemical = 
+		"select idproperty,name,units,title,url,idreference,comments from properties join catalog_references using(idreference)\n"+
+		"where idproperty in (select idproperty from structure join property_values using(idstructure) where idchemical = ?) \n";
+
 	public ReadProperty(Integer id) {
 		setValue(id);
 	}
@@ -29,16 +37,25 @@ public class ReadProperty extends AbstractPropertyRetrieval<String, Integer, EQC
 	}
 		
 	public List<QueryParam> getParameters() throws AmbitException {
+		List<QueryParam> params = new ArrayList<QueryParam>();
+		if (getFieldname()!=null)
+			params.add(new QueryParam<Integer>(Integer.class, isChemicalsOnly()?getFieldname().getIdchemical():getFieldname().getIdstructure()));				
+
 		if (getValue()!=null) {
-			List<QueryParam> params = new ArrayList<QueryParam>();
-			params.add(new QueryParam<Integer>(Integer.class, getValue()));				
+			params.add(new QueryParam<Integer>(Integer.class, getValue()));
 			return params;
-		} else return null;
+		} 
+		return params.size()==0?null:params;
+	}
+	public String getSQL() throws AmbitException {
+		if (getFieldname() == null)
+			return getValue()==null?base_sql:String.format("%s where idproperty=?", base_sql);
+		else {
+			String sql = isChemicalsOnly()?ReadProperty.sqlPerChemical:ReadProperty.sqlPerStructure;
+			return getValue()==null?sql:(sql + "where idproperty=?");
+		}	
 	}
 
-	public String getSQL() throws AmbitException {
-		return getValue()==null?base_sql:String.format("%s where idproperty=?", base_sql);
-	}
 	@Override
 	public String toString() {
 		return getValue()==null?"Properties":String.format("Property id=%s",getValue());
