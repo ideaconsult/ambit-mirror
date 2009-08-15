e8c322699ecd627f6a4d30cb6a02b3c0c8385e75
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-08-15 07:28:16 +0000

    Dataset resource now accepts File representation with SDF file format on POST, in addition to multipart form

--- ambit2-www/src/test/java/ambit2/rest/test/dataset/DatasetsResourceTest.java
+++ ambit2-www/src/test/java/ambit2/rest/test/dataset/DatasetsResourceTest.java
@@ -4,9 +4,27 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-import org.junit.Test;
-import org.restlet.data.MediaType;
+import junit.framework.Assert;
 
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.ITable;
+import org.junit.Test;
+import org.restlet.Client;
+import org.restlet.data.ChallengeResponse;
+import org.restlet.data.ChallengeScheme;
+import org.restlet.data.Form;
+import org.restlet.data.MediaType;
+import org.restlet.data.Method;
+import org.restlet.data.Preference;
+import org.restlet.data.Protocol;
+import org.restlet.data.Reference;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.data.Status;
+import org.restlet.resource.FileRepresentation;
+import org.restlet.resource.Representation;
+
+import ambit2.rest.ChemicalMediaType;
 import ambit2.rest.test.ResourceTest;
 
 
@@ -63,4 +81,60 @@ public class DatasetsResourceTest extends ResourceTest {
 		}
 		return count>0;
 	}		
+	public Response testPost(String uri, MediaType media, Form headers) throws Exception {
+		Request request = new Request();
+		Client client = new Client(Protocol.HTTP);
+		request.setResourceRef(uri);
+		request.setMethod(Method.POST);
+		request.getAttributes().put("org.restlet.http.headers", headers);		
+		request.getClientInfo().getAcceptedMediaTypes().add(new Preference<MediaType>(media));
+		return client.handle(request);
+	}
+	
+	public Response testPostFile(String uri, MediaType media, Representation file) throws Exception {
+		Request request = new Request();
+		Client client = new Client(Protocol.HTTP);
+		ChallengeScheme scheme = ChallengeScheme.HTTP_BASIC;  
+		ChallengeResponse authentication = new ChallengeResponse(scheme,  
+		         "opentox", "opentox");  
+		request.setChallengeResponse(authentication);  
+		request.setResourceRef(uri);
+		request.setMethod(Method.POST);
+		request.setEntity(file);
+		request.getClientInfo().getAcceptedMediaTypes().add(new Preference<MediaType>(media));
+		return client.handle(request);
+	}	
+		
+	@Test
+	public void testCreateEntry() throws Exception {
+		
+		FileRepresentation rep = new FileRepresentation(
+				"E:/src/ambit2-all/ambit2-www/src/test/resources/input.sdf", 
+				ChemicalMediaType.CHEMICAL_MDLSDF, 0);
+				//EncodeRepresentation encodedRep = new EncodeRepresentation(Encoding.GZIP,rep);
+				
+		Response r = testPostFile(getTestURI(), ChemicalMediaType.CHEMICAL_MDLSDF, rep);
+		Reference uri = r.getLocationRef();
+		while (!r.getStatus().equals(Status.SUCCESS_OK)) {
+			//System.out.println(r.getStatus() + " " +r.getLocationRef());
+			
+			Request request = new Request();
+			Client client = new Client(Protocol.HTTP);
+			request.setResourceRef(uri);
+			request.setMethod(Method.GET);
+			r = client.handle(request);
+			uri = r.getLocationRef();
+		}
+		System.out.println(r.getStatus());
+		//System.out.println(r.getLocationRef());
+	//	Assert.assertEquals(Status.SUCCESS_OK, response.getStatus());
+		
+        IDatabaseConnection c = getConnection();	
+		ITable table = 	c.createQueryTable("EXPECTED","SELECT * FROM structure");
+		Assert.assertEquals(11,table.getRowCount());
+		c.close();
+		
+	}	
+	
+	
 }
