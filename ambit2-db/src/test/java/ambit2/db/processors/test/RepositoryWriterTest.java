f5cf3db2a188869781b5a37bbc169050d2cce888
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-09-01 13:59:59 +0000

    test for importing from ECHA preregistration list

--- ambit2-db/src/test/java/ambit2/db/processors/test/RepositoryWriterTest.java
+++ ambit2-db/src/test/java/ambit2/db/processors/test/RepositoryWriterTest.java
@@ -48,6 +48,7 @@ import ambit2.base.data.LiteratureEntry;
 import ambit2.base.data.StructureRecord;
 import ambit2.base.exceptions.AmbitException;
 import ambit2.base.interfaces.IStructureRecord;
+import ambit2.core.io.ECHAPreregistrationListReader;
 import ambit2.core.io.FileInputState;
 import ambit2.core.io.IRawReader;
 import ambit2.core.io.RawIteratingFolderReader;
@@ -316,6 +317,57 @@ select idstructure,id_srcdataset from struc_dataset where idstructure>3
 on duplicate key update idstructure=3
 delete from struc_dataset where idstructure>3
 		 */
+
+	}		
+	
+	@Test
+	public void testECHAPreregistrationList() throws Exception {
+		
+		setUpDatabase("src/test/resources/ambit2/db/processors/test/empty-datasets.xml");
+        IDatabaseConnection c = getConnection();
+        
+		ITable chemicals = 	c.createQueryTable("EXPECTED","SELECT * FROM chemicals");
+		Assert.assertEquals(0,chemicals.getRowCount());
+		ITable strucs = 	c.createQueryTable("EXPECTED","SELECT * FROM structure");
+		Assert.assertEquals(0,strucs.getRowCount());
+		ITable srcdataset = 	c.createQueryTable("EXPECTED","SELECT * FROM src_dataset");
+		Assert.assertEquals(0,srcdataset.getRowCount());
+		ITable struc_src = 	c.createQueryTable("EXPECTED","SELECT * FROM struc_dataset");
+		Assert.assertEquals(0,struc_src.getRowCount());
+		ITable property = 	c.createQueryTable("EXPECTED","SELECT * FROM properties");
+		Assert.assertEquals(0,property.getRowCount());
+		ITable property_values = 	c.createQueryTable("EXPECTED","SELECT * FROM property_values");
+		Assert.assertEquals(0,property_values.getRowCount());
+		
+		InputStream in = this.getClass().getClassLoader().getResourceAsStream("ambit2/db/processors/test/echa_preregistration.xml");
+		Assert.assertNotNull(in);
+		ECHAPreregistrationListReader reader = new ECHAPreregistrationListReader(in);
+		write(reader,c.getConnection());
+        c.close();
+        
+        c = getConnection();
+		chemicals = 	c.createQueryTable("EXPECTED","SELECT * FROM chemicals");
+		Assert.assertEquals(9,chemicals.getRowCount());
+		strucs = 	c.createQueryTable("EXPECTED","SELECT * FROM structure");
+		Assert.assertEquals(9,strucs.getRowCount());
+		srcdataset = 	c.createQueryTable("EXPECTED","SELECT * FROM src_dataset where name='TEST INPUT'");
+		Assert.assertEquals(1,srcdataset.getRowCount());
+		struc_src = 	c.createQueryTable("EXPECTED","SELECT * FROM struc_dataset");
+		Assert.assertEquals(9,struc_src.getRowCount());
+		
+		property = 	c.createQueryTable("EXPECTED","SELECT * FROM properties");
+		Assert.assertEquals(7,property.getRowCount());
+		property_values = 	c.createQueryTable("EXPECTED","SELECT * FROM property_values");
+		Assert.assertEquals(40,property_values.getRowCount());		
+		ITable tuples = 	c.createQueryTable("EXPECTED","SELECT * FROM tuples");
+		Assert.assertEquals(9,tuples.getRowCount());			
+		ITable p_tuples = 	c.createQueryTable("EXPECTED","SELECT * FROM property_tuples");
+		Assert.assertEquals(40,p_tuples.getRowCount());	
+		ITable p_str = 	c.createQueryTable("EXPECTED","SELECT * FROM property_string where value=\"5\u03b2,14-dihydroxy-19-oxocard-20(22)enolide 3\u03b2-acetate\"");
+		Assert.assertEquals(1,p_str.getRowCount());	
+		
+		c.close();
+
 
 	}		
 	public int write(IRawReader<IStructureRecord> reader,Connection connection) throws Exception  {
