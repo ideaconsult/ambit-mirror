34b8f2a680fb8793f94efb005e29ebfdef051667
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-09-05 17:01:31 +0000

    Query to retrieve all property values from a given template

---
+++ ambit2-db/src/main/java/ambit2/db/readers/RetrieveTemplatePropertyValue.java
@@ -0,0 +1,38 @@
+package ambit2.db.readers;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import ambit2.base.data.LiteratureEntry;
+import ambit2.base.data.Property;
+import ambit2.base.exceptions.AmbitException;
+
+/**
+ * Retrieves properties by given template
+ * @author nina
+ *
+ */
+public class RetrieveTemplatePropertyValue extends RetrieveTemplate<PropertyValue> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -743370966710931692L;
+	@Override
+	public PropertyValue getObject(ResultSet rs) throws AmbitException {
+		try {
+			//"select name,idreference,idproperty,idstructure,ifnull(text,value) as value_string,value_num,title,url from property_values \n"+
+			LiteratureEntry le = LiteratureEntry.getInstance(rs.getString(7),rs.getString(8),rs.getInt(2));
+			Property p = Property.getInstance(rs.getString(1),le);
+			p.setId(rs.getInt(3));
+			Object value = rs.getObject(5);
+			PropertyValue pv ;
+			if (value == null) pv = new PropertyValue<Float>(p,rs.getFloat(6));
+			else pv =  new PropertyValue<String>(p,rs.getString(5));
+			pv.setId(rs.getInt(10));
+			return pv;
+		} catch (SQLException x) {
+			throw new AmbitException(x);
+		}
+	}	
+}
