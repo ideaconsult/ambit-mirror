b00999402bde3b5fc6f5392fee8c241a3bd4d8ca
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-10-20 10:35:47 +0000

    2D Depiction by querying Cactvs service

---
+++ ambit2-all/ambit2-www/src/main/java/ambit2/rest/structure/diagram/CSLSDepict.java
@@ -0,0 +1,36 @@
+package ambit2.rest.structure.diagram;
+
+import java.awt.image.BufferedImage;
+
+import org.restlet.data.MediaType;
+import org.restlet.data.Method;
+import org.restlet.data.Reference;
+import org.restlet.representation.Variant;
+import org.restlet.resource.ResourceException;
+
+import ambit2.base.exceptions.AmbitException;
+import ambit2.pubchem.CSLSImageRequest;
+
+/**
+ * http://cactus.nci.nih.gov/chemical/structure/c1ccccc1/image
+ * @author nina
+ *
+ */
+public class CSLSDepict extends AbstractDepict {
+	protected CSLSImageRequest depict = new CSLSImageRequest();
+	@Override
+	protected void doInit() throws ResourceException {
+		super.doInit();
+		this.getVariants().clear();
+		this.getVariants().put(Method.GET,new Variant(MediaType.IMAGE_PNG));		
+	}
+	@Override
+	protected BufferedImage getImage(String smiles,int w, int h) throws AmbitException {
+		depict.setWidth(w);
+		depict.setHeight(h);
+		return depict.process(smiles);
+	}
+	protected String getTitle(Reference ref, String smiles) {
+		return String.format("SMILES: %s<br><img src='%s' alt='%s' title='%s'>", smiles,ref,smiles,smiles);
+	}
+}
