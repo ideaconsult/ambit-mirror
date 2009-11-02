0f2b50ad49e601706e8990215f8d6af8b3a01d2b
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-11-02 17:03:20 +0000

    modified to throw same exception asother CDK descriptos, test added

---
+++ ambit2-all/ambit2-descriptors/src/test/java/ambit2/descriptors/test/Descriptors3DTest.java
@@ -0,0 +1,58 @@
+package ambit2.descriptors.test;
+
+import java.io.InputStream;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.io.iterator.IIteratingChemObjectReader;
+import org.openscience.cdk.io.iterator.IteratingMDLReader;
+import org.openscience.cdk.nonotify.NoNotificationChemObjectBuilder;
+import org.openscience.cdk.qsar.DescriptorValue;
+import org.openscience.cdk.qsar.IMolecularDescriptor;
+import org.openscience.cdk.qsar.descriptors.molecular.LengthOverBreadthDescriptor;
+import org.openscience.cdk.qsar.descriptors.molecular.WHIMDescriptor;
+
+import ambit2.core.io.FileInputState;
+import ambit2.core.io.MyIteratingMDLReader;
+import ambit2.descriptors.SizeDescriptor;
+import ambit2.descriptors.SpherosityDescriptor;
+
+/**
+ * Test for descriptors, requiring 3D coordinates. Some descriptors fail to recognise 3D coordinates are present
+ * @author nina
+ *
+ */
+public class Descriptors3DTest {
+	@Test
+	public void testWHIMDescriptor() throws Exception {
+		testDescriptor(new WHIMDescriptor());
+	}
+	@Test
+	public void testSpherosityDescriptor() throws Exception {
+		testDescriptor(new SpherosityDescriptor());
+	}
+	@Test
+	public void testSizeDescriptor() throws Exception {
+		testDescriptor(new SizeDescriptor());
+	}	
+	@Test
+	public void testLengthOverBreadthDescriptor() throws Exception {
+		testDescriptor(new LengthOverBreadthDescriptor());
+	}
+
+	//succeeds if no exception is thrown
+	public void testDescriptor(IMolecularDescriptor descriptor) throws Exception {
+		InputStream in = getClass().getClassLoader().getResourceAsStream("ambit2/descriptors/3d/test.sdf");
+		IIteratingChemObjectReader reader = FileInputState.getReader(
+				in,"test.sdf");
+
+		while (reader.hasNext()) {
+			Object next = reader.next();
+			Assert.assertTrue(next instanceof IAtomContainer);
+			DescriptorValue value = descriptor.calculate((IAtomContainer)next);
+		}
+		in.close();
+	}
+}
