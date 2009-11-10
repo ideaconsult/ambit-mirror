f75280201cffbf4d080d34cb7ffe3fb1a2ff958d
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-11-10 11:14:10 +0000

    A query to mimic free text searching - verifies both names and values of proprties

---
+++ ambit2-db/src/test/java/ambit2/db/search/test/FreeTextQueryTest.java
@@ -0,0 +1,35 @@
+package ambit2.db.search.test;
+
+import java.sql.ResultSet;
+
+import junit.framework.Assert;
+
+import ambit2.base.interfaces.IStructureRecord;
+import ambit2.db.search.structure.FreeTextQuery;
+
+public class FreeTextQueryTest extends QueryTest<FreeTextQuery> {
+
+	@Override
+	public void setUp() throws Exception {
+
+		dbFile = "src/test/resources/ambit2/db/processors/test/dataset-properties.xml";
+		super.setUp();
+	}
+	@Override
+	protected FreeTextQuery createQuery() throws Exception {
+		FreeTextQuery q = new FreeTextQuery();
+		q.setFieldname(new String[] {"Names","Property 2"});
+		q.setValue(new String[] {"value"});
+		return q;
+	}
+
+	@Override
+	protected void verify(FreeTextQuery query, ResultSet rs) throws Exception {
+		while (rs.next()) {
+			IStructureRecord record = query.getObject(rs);
+			Assert.assertEquals(11,record.getIdchemical());
+		}
+		
+	}
+
+}
