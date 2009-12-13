4127363ae67372073a60cd0e52314c0f9bf0801c
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-12-13 19:48:13 +0000

    query to retrieve model variables

---
+++ ambit2-all/ambit2-db/src/main/java/ambit2/db/search/property/ModelTemplates.java
@@ -0,0 +1,78 @@
+package ambit2.db.search.property;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ambit2.base.exceptions.AmbitException;
+import ambit2.db.model.ModelQueryResults;
+import ambit2.db.search.QueryParam;
+import ambit2.db.search.StringCondition;
+
+public class ModelTemplates extends AbstractPropertyRetrieval<ModelQueryResults, String, StringCondition> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4888110865139617053L;
+	
+	protected String sql = String.format("%s\n%s%s",
+			base_sql,
+			"join template_def using(idproperty)\n",
+			"join models on models.%s=template_def.idtemplate\n%s"
+			);
+	protected String where = "where idmodel=?";
+	protected String whereName = "where models.name %s ?";
+	
+	protected enum _ModelTemplates {
+		predicted {
+			@Override
+			public String getTableColumn() {
+				return "dependent";
+			}
+		},
+		independent{
+			@Override
+			public String getTableColumn() {
+				return "predictors";
+			}
+		},
+		dependent {
+			@Override
+			public String getTableColumn() {
+				return "dependent";
+			}
+		};
+		public abstract String getTableColumn();
+	};
+	
+	public ModelTemplates() {
+		super();
+		setCondition(StringCondition.getInstance("regexp"));
+	}
+	@Override
+	public void setValue(String value) {
+		try {
+			super.setValue(_ModelTemplates.valueOf(value).getTableColumn());
+		} catch (Exception x) {
+			super.setValue(_ModelTemplates.predicted.getTableColumn());
+		}
+	}
+	public List<QueryParam> getParameters() throws AmbitException {
+		if (getFieldname()!= null) {
+			List<QueryParam> param = new ArrayList<QueryParam>();
+			if (getFieldname().getId()>0)
+				param.add(new QueryParam<Integer>(Integer.class,getFieldname().getId()));
+			else
+				param.add(new QueryParam<String>(String.class,getFieldname().getName()));
+			return param;
+		} else return null;
+	}
+
+	public String getSQL() throws AmbitException {
+		return String.format(sql, 
+				_ModelTemplates.valueOf(getValue()).getTableColumn(),
+				getFieldname()==null?"":
+					getFieldname().getId()>0?where:String.format(whereName,getCondition().getSQL())
+				);
+	}
+
+}
