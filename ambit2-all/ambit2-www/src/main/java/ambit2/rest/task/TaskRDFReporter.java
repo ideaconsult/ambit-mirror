6eb0687a93cb71d54df29dfeeaafc3e0745b0483
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-12-21 20:42:24 +0000

    beautify Task html rendering, fixing rdf/xml output

--- ambit2-all/ambit2-www/src/main/java/ambit2/rest/task/TaskRDFReporter.java
+++ ambit2-all/ambit2-www/src/main/java/ambit2/rest/task/TaskRDFReporter.java
@@ -1,19 +1,18 @@
 package ambit2.rest.task;
 
 import java.io.Writer;
-import java.util.Date;
 import java.util.Iterator;
 
 import org.restlet.data.MediaType;
 import org.restlet.data.Reference;
 import org.restlet.data.Request;
 
+import ambit2.rest.algorithm.CatalogRDFReporter;
+import ambit2.rest.rdf.OT;
+
 import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
 import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.vocabulary.DC;
-
-import ambit2.rest.algorithm.CatalogRDFReporter;
-import ambit2.rest.rdf.OT;
 
 /**
  * RDF generation for {@link Task}
@@ -26,14 +25,18 @@ public class TaskRDFReporter extends CatalogRDFReporter<Task<Reference>> {
 	 * 
 	 */
 	private static final long serialVersionUID = 3789102915378513270L;
-
+	protected Reference baseRef;
 	public TaskRDFReporter(Request request, MediaType mediaType) {
 		super(request, mediaType);
+		baseRef = request.getRootRef();
 	}
 	@Override
 	public void header(Writer output, Iterator<Task<Reference>> query) {
 		super.header(output, query);
 		OT.OTClass.Task.createOntClass(getJenaModel());
+		getJenaModel().createAnnotationProperty(DC.title.getURI());
+		getJenaModel().createAnnotationProperty(DC.identifier.getURI());
+		getJenaModel().createAnnotationProperty(DC.date.getURI());
 	}
 
 	@Override
@@ -42,11 +45,9 @@ public class TaskRDFReporter extends CatalogRDFReporter<Task<Reference>> {
 		try {
 			ref = item.getReference().toString();
 		} catch (Exception x) {
-			ref = "";
+			ref = item.getUri().toString();
 		}
-		Individual task = getJenaModel().createIndividual(
-				ref,
-				OT.OTClass.Task.getOntClass(getJenaModel()));
+		Individual task = getJenaModel().createIndividual(OT.OTClass.Task.getOntClass(getJenaModel()));
 		task.addLiteral(DC.title,
 				 getJenaModel().createTypedLiteral(item.getName(),XSDDatatype.XSDstring));
 		task.addLiteral(DC.identifier,
