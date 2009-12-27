077c96e9cf795b0fe6bcd42e77a3883290d4dbb6
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2009-12-27 20:50:13 +0000

    Getting rid of DomRepresentation, buggy in Restlet 2.0-M6

--- ambit2-www/src/main/java/ambit2/rest/DocumentConvertor.java
+++ ambit2-www/src/main/java/ambit2/rest/DocumentConvertor.java
@@ -1,9 +1,18 @@
 package ambit2.rest;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.restlet.data.MediaType;
-import org.restlet.ext.xml.DomRepresentation;
+import org.restlet.representation.OutputRepresentation;
 import org.restlet.representation.Representation;
 import org.w3c.dom.Document;
 
@@ -23,7 +32,7 @@ public class DocumentConvertor<T,Q extends IQueryRetrieval<T>>
 										extends AbstractObjectConvertor<T,Q,Document>  {
 
 	public DocumentConvertor(QueryReporter<T,Q,Document> reporter) {
-		super(reporter,MediaType.APPLICATION_PDF);
+		super(reporter,MediaType.TEXT_XML);
 		if (this.reporter != null) ((QueryReporter<T,Q,Document>)this.reporter).setMaxRecords(5000);
 	}
 	/**
@@ -38,8 +47,32 @@ public class DocumentConvertor<T,Q extends IQueryRetrieval<T>>
 			throw new AmbitException(x);
 		}
 	};
-	public Representation process(Document doc) throws AmbitException {
-			return new DomRepresentation(MediaType.TEXT_XML,doc);
+	public Representation process(final Document doc) throws AmbitException {
+		 return new OutputRepresentation(MediaType.TEXT_XML) {
+			 @Override
+			public void write(OutputStream out) throws IOException {
+
+				try {
+					DOMSource source = new DOMSource(doc);
+					Transformer xformer = TransformerFactory.newInstance().newTransformer();
+			        xformer.setOutputProperty(OutputKeys.INDENT,"yes");
+			        xformer.setOutputProperty(OutputKeys.STANDALONE,"yes");
+			    
+			        Result result = new StreamResult(out);
+			        xformer.transform(source, result);
+			        
+				} catch (Exception  x) {
+					throw new IOException(x.getMessage());
+				} finally {
+					try { out.flush();} catch (Exception x) {}
+				}
+				
+			}
+		 };
+
+		
+
+	
 	}
 
 }
