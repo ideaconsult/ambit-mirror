0d899f78a6fa49a7ad33965681eea432acb899b2
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-01-01 17:42:16 +0000

    post parameters

--- ambit2-all/ambit2-rest/src/main/java/ambit2/rest/OpenTox.java
+++ ambit2-all/ambit2-rest/src/main/java/ambit2/rest/OpenTox.java
@@ -1,10 +1,12 @@
 package ambit2.rest;
 
 import org.restlet.Request;
+import org.restlet.data.Form;
 import org.restlet.data.Reference;
 
 public class OpenTox {
 	public static enum URI {
+		task,
 		algorithm,
 		model,
 		dataset {
@@ -38,6 +40,49 @@ public class OpenTox {
 		public int getIntValue(Request request) throws Exception {
 			return Integer.parseInt(getValue(request));
 		}		
+	};
+	
+	public enum params  {
+		feature_uris {
+			@Override
+			public String toString() {
+				return String.format("%s[]",super.toString());
+			}
+			@Override
+			public String getDescription() {
+				return "Feature URI(s)";
+			}
+		},
+		dataset_uri {
+			public boolean isMandatory() {
+				return true;
+			}
+			public String getDescription() {
+				return "Dataset URI to be used as input to model generation (ALgorithm POST) or prediction (Model POST)";
+			}			
+		},
+		target {
+			public boolean isMandatory() {
+				return false;
+			}			
+			public String getDescription() {
+				return "Feature URI, target variable for model generation of classification and regression models";
+			}			
+		},
+		source_uri {
+		}; 
+		public boolean isMandatory() {
+			return true;
+		}
+		public String getDescription() {
+			return "either use ?source_uri=URI, or POST with text/uri-list or RDF representation of the object to be created";
+		}
+		public Object getFirstValue(Form form) {
+			return form.getFirstValue(toString());
+		}
+		public String[] getValuesArray(Form form) {
+			return form.getValuesArray(toString());
+		}
 	};
 	private OpenTox() {
 	}
