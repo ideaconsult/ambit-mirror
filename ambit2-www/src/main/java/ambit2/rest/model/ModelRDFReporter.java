0da8ec6a14f2b32d685f4e8d66359f0b35a6bc6c
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-01-10 07:58:34 +0000

    fixed to include features and algorithm URI into model RDF representation

--- ambit2-www/src/main/java/ambit2/rest/model/ModelRDFReporter.java
+++ ambit2-www/src/main/java/ambit2/rest/model/ModelRDFReporter.java
@@ -1,12 +1,10 @@
 package ambit2.rest.model;
 
-import java.util.Iterator;
-
 import org.restlet.Request;
 import org.restlet.data.MediaType;
+import org.restlet.data.Reference;
 
 import ambit2.base.data.Property;
-import ambit2.base.data.Template;
 import ambit2.base.exceptions.AmbitException;
 import ambit2.db.exceptions.DbAmbitException;
 import ambit2.db.model.ModelQueryResults;
@@ -15,6 +13,7 @@ import ambit2.rest.QueryRDFReporter;
 import ambit2.rest.QueryURIReporter;
 import ambit2.rest.property.PropertyURIReporter;
 import ambit2.rest.rdf.OT;
+import ambit2.rest.rdf.RDFPropertyIterator;
 
 import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
 import com.hp.hpl.jena.ontology.Individual;
@@ -70,11 +69,19 @@ public class ModelRDFReporter<Q extends IQueryRetrieval<ModelQueryResults>> exte
 		model.addProperty(DC.date,"N/A");
 		model.addProperty(DC.format,"N/A");
 		
-		Individual algorithm = getJenaModel().createIndividual(
-				String.format("%s/algorithm/any",uriReporter.getBaseReference().toString()),
+		Individual algorithm = getJenaModel().createIndividual(item.getAlgorithm(),
 				OT.OTClass.Algorithm.getOntClass(getJenaModel()));
 		model.addProperty(OT.OTProperty.algorithm.createProperty(getJenaModel()), algorithm);
 		
+		
+		readProperties(new Reference(String.format("%s/independent",uriReporter.getURI(item))), 
+					OT.OTProperty.independentVariables.createProperty(getJenaModel()), model);
+		readProperties(new Reference(String.format("%s/dependent",uriReporter.getURI(item))), 
+					OT.OTProperty.dependentVariables.createProperty(getJenaModel()), model);
+		readProperties(new Reference(String.format("%s/predicted",uriReporter.getURI(item))), 
+					OT.OTProperty.predictedVariables.createProperty(getJenaModel()), model);
+		
+		/*
 		Template t = item.getPredictors();
 		Iterator<Property> i = t.getProperties(true);
 		while (i.hasNext()) {
@@ -101,10 +108,27 @@ public class ModelRDFReporter<Q extends IQueryRetrieval<ModelQueryResults>> exte
 					OT.OTClass.Feature.getOntClass(getJenaModel()));
 			model.addProperty(OT.OTProperty.predictedVariables.createProperty(getJenaModel()), feature);
 		}			
-
+		*/
 		return model;
 	}
+	protected void readProperties(Reference reference, com.hp.hpl.jena.rdf.model.Property predicate, Individual model) {
+		RDFPropertyIterator reader = null;
+		try {
+			reader = new RDFPropertyIterator(reference);
+			reader.setBaseReference(uriReporter.getBaseReference());
+			while (reader.hasNext()) {
+				Property property = reader.next();
+				Individual feature = getJenaModel().createIndividual(propertyReporter.getURI(property),
+						OT.OTClass.Feature.getOntClass(getJenaModel()));
+				model.addProperty(predicate, feature);				
+			}
 
+		} catch (Exception x) {
+			logger.error(x);
+		} finally {
+			try {reader.close(); } catch (Exception x) {}
+		}
+	}
 	public void open() throws DbAmbitException {
 	}
 
