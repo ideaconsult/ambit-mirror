f6c5fe692e6a1c245c279f9d5dc639a1eb2cd7a1
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-02-10 18:59:23 +0000

    test

---
+++ ambit2-all/ambit2-db/src/test/java/ambit2/db/search/test/QueryStrucTypeTest.java
@@ -0,0 +1,30 @@
+package ambit2.db.search.test;
+
+import java.sql.ResultSet;
+
+import junit.framework.Assert;
+
+import ambit2.base.interfaces.IStructureRecord.STRUC_TYPE;
+import ambit2.db.search.structure.QueryStrucType;
+
+public class QueryStrucTypeTest extends QueryTest<QueryStrucType>{
+
+	@Override
+	protected QueryStrucType createQuery() throws Exception {
+		return new QueryStrucType(STRUC_TYPE.D2noH);
+	}
+
+	@Override
+	protected void verify(QueryStrucType query, ResultSet rs) throws Exception {
+
+		int records = 0;
+		while (rs.next()) {
+			records ++;
+			Assert.assertEquals(query.getId().intValue(),rs.getInt(1));
+			Assert.assertEquals(10,rs.getInt(2));
+			Assert.assertEquals(100214,rs.getInt(3));
+		}
+		Assert.assertEquals(1,records);		
+	}
+
+}
