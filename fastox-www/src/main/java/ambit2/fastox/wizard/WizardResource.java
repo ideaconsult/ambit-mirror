d0b763b23b16eb30d424947ef91285926696c409
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-02-15 10:02:39 +0000

    keeps aspect ratio of opentox logo

--- fastox-www/src/main/java/ambit2/fastox/wizard/WizardResource.java
+++ fastox-www/src/main/java/ambit2/fastox/wizard/WizardResource.java
@@ -266,7 +266,7 @@ public abstract class WizardResource extends ServerResource {
 		output.write("<div class=\"footer\">");
 
 		output.write("<span class=\"right\">");
-		output.write(String.format("<a href='http://www.opentox.org'><img src=%s/images/logo.png border='0' width='115' height='60'></a>",baseReference));
+		output.write(String.format("<a href='http://www.opentox.org'><img src=%s/images/logo.png border='0' width='185' height='60'></a>",baseReference));
 		output.write(String.format("<a href='http://ambit.sourceforge.net'><img src=%s/images/ambit-logo.png border='0' width='115' height='50'></a>&nbsp;",baseReference));
 		//output.write(String.format("<a href='http://www.cefic.be'><img src=%s/images/logocefic.png border='0' width='115' height='60'></a>&nbsp;",baseReference));
 		//output.write(String.format("<a href='http://www.cefic-lri.org'><img src=%s/images/logolri.png border='0' width='115' height='60'></a>&nbsp;",baseReference));
