e8a85530e74afb548f4dc88c742b880e3bedc281
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-03-26 13:13:14 +0000

    applicability domain by fingerptints

---
+++ ambit2-all/ambit2-model/src/main/java/ambit2/model/structure/DataCoverageFingerprints.java
@@ -0,0 +1,179 @@
+/*
+ * Created on 2005-4-1
+ *
+ */
+package ambit2.model.structure;
+
+import java.util.BitSet;
+import java.util.List;
+
+import ambit2.core.data.MoleculeTools;
+import ambit2.model.numeric.ADomainMethodType;
+import ambit2.model.numeric.DataCoverage;
+
+
+
+/**
+ * A class to estimate applicability domain by fingerprints
+ * 
+ * @author Nina Jeliazkova <br>
+ * <b>Modified</b> 2005-4-7
+ */
+public abstract class DataCoverageFingerprints extends DataCoverage<List<BitSet>> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8338115538206282818L;
+	protected int fingerPrintLength = MoleculeTools._FPLength;
+	protected int fingerprintsCalculated = 0;
+	protected BitSet consensusFingerprint = null;
+	protected double[] fpProfile = null;
+
+	/**
+	 * @param mode
+	 */
+	public DataCoverageFingerprints() {
+		super(ADomainMethodType._modeFINGERPRINTS);
+		fpProfile = new double[fingerPrintLength];
+	}
+	public void clear() {
+		super.clear();
+		fingerprintsCalculated = 0;
+		if (consensusFingerprint != null) consensusFingerprint.clear();
+		clearProfile(fpProfile);
+	}
+	
+	protected void clearProfile(double[] profile) {
+		if (profile != null) 
+			for (int i = 0; i < profile.length; i++) profile[i] = 0;
+	}
+
+
+	public boolean buildStart() {
+		clear();
+		/*
+		if (fpProfile == null) 
+			fpProfile = new double[fingerPrintLength];
+		else clearProfile(fpProfile);
+		*/
+		return true;
+	}
+	public abstract void evaluate(List<BitSet> bitsets) ;
+
+	
+	
+	public boolean build(List<BitSet> bitsets) {
+		buildStart();	
+		//BitSet bs[] = new BitSet[data.size()];
+		fingerprintsCalculated = 0; 
+		for (int i = 0; i < bitsets.size(); i++ ) {
+			BitSet bs = bitsets.get(i);
+			if (bs != null) {
+				for (int j=bs.nextSetBit(0); j>= 0; j=bs.nextSetBit(j+1) )  
+					fpProfile[j]++;
+				fingerprintsCalculated++;
+			}			
+		}
+
+		evaluate(bitsets);
+		return (fingerprintsCalculated > 0);
+	}
+	protected void estimateConsensusFP(double[] profile, double athreshold) {
+		if (consensusFingerprint == null) consensusFingerprint = new BitSet();
+		else consensusFingerprint.clear();
+		System.out.println("\nBits set to one by less than " + Math.floor(athreshold) + " compounds are set to zero");
+		for (int i=0; i < fingerPrintLength; i++) 
+			if (profile[i] > athreshold) consensusFingerprint.set(i);
+		System.out.println("Consensus Fingerprint cardinality\t"+consensusFingerprint.cardinality()
+		        	+"\n"+consensusFingerprint.toString());
+		        
+	}
+
+	public boolean isEmpty() {
+		return (consensusFingerprint == null) || (fingerprintsCalculated == 0);
+	}
+
+	
+	public int getFingerPrintLength() {
+		return fingerPrintLength;
+	}
+	public void setFingerPrintLength(int fpLength) {
+		if (this.fingerPrintLength != fpLength) {
+			this.fingerPrintLength = fpLength;
+			clear();
+			fpProfile = null;
+
+		}
+	}
+	public static String arrayToString(double[] array) {
+		StringBuffer b = new StringBuffer();
+		for (int i = 0; i < array.length; i++) {
+			b.append(array[i]);
+			b.append("\t");
+		}
+		return b.toString();
+	}
+	public String histToString(double[][] hist) {
+		if (hist == null) return "";
+		StringBuffer b = new StringBuffer();
+		for (int i = 0; i < hist[0].length; i++) {
+			b.append(">");			
+			b.append(hist[1][i]);			
+			b.append(":");			
+			b.append((int)Math.floor(hist[0][i]));
+			b.append("\t");
+		}
+		return b.toString();
+	}	
+	public String profileToString() {
+		if (fpProfile == null) return "";
+		StringBuffer b = new StringBuffer();
+		for (int i = 0; i < fpProfile.length; i++) {
+			b.append((int)Math.floor(fpProfile[i]));
+			b.append("\t");
+		}
+		return b.toString();
+	}
+	public int getFingerprintsCalculated() {
+		return fingerprintsCalculated;
+	}
+	public double[][] profileToHistogram(int bins) {
+		if (isEmpty()) return null;
+		if (bins < 1) bins = 10;
+		double[][] hist = new double[2][bins];	
+		for (int j = 0; j < bins; j++) { 
+			hist[0][j] = 0;
+			hist[1][j] = ((double) j)/bins;
+		}
+		
+		for (int i=0; i < fingerPrintLength; i++) { 
+			double r = fpProfile[i] / fingerprintsCalculated;
+			for (int j=(bins-1); j >=0; j--) 
+				if (r >= ((double) j)/bins) {
+					hist[0][j]++; 
+					break;
+				}
+		}
+		return hist;
+	}
+	public String getName() {
+		return "Fingerprints";
+	}
+
+	public int getDomain(double coverage) {
+		int domain;
+		if (coverage >= threshold) domain = 0;
+		else domain =1;
+		return domain;
+	}
+	public int[] getDomain(double[] coverage) {
+		if (coverage == null) return null;
+		int np = coverage.length;
+		int[] domain = new int[np];
+		for (int i = 0; i < np; i++) 
+			if (new Double(coverage[i]).isNaN()) domain[i] = 2;
+			else if (coverage[i] >= threshold) domain[i] = 0;
+			else domain[i] =1;
+		return domain;
+	}	
+}
---
+++ ambit2-all/ambit2-model/src/main/java/ambit2/model/structure/DataCoverageFingerprintsTanimoto.java
@@ -0,0 +1,107 @@
+package ambit2.model.structure;
+
+import java.util.BitSet;
+import java.util.List;
+
+import ambit2.domain.stats.Tools;
+import ambit2.domain.stats.datastructures.Sort;
+
+public class DataCoverageFingerprintsTanimoto extends DataCoverageFingerprints {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2253589622326446088L;
+
+	public void evaluate(List<BitSet> bitsets) {
+		estimateConsensusFP(fpProfile,0);			
+		double [] values = new double[bitsets.size()];
+		threshold = 0;
+		int v = 0;
+		for (int i = 0; i < bitsets.size(); i++ ){ 
+			double t = tanimoto(bitsets.get(i),consensusFingerprint);
+			if (new Double(t).isNaN()) continue;
+			else {
+				values[v] = t; v++;
+			}
+		}	
+		threshold = estimateThresholdValues(pThreshold,values,v);
+
+	}
+
+	/**
+	 * Returns (1-Tanimoto distance) where 
+	 * Tanimoto = ( bc / (b1.cardinality() + b2.cardinality() - bc)
+	 * bc = bs1.and(bs2).cardinality (the number of common bits set to 1)
+	 * 
+	 * (1- Tanimoto) is used to make it compatible with distances approach 
+	 * (the larger the distance value, the more dissimilar the compounds 
+	 * @param bs1  fingerprint 1
+	 * @param bs2  fingerprint 2
+	 * @return (1-Tanimoto distance)
+	 */
+	protected double tanimoto(BitSet bs1, BitSet bs2) {
+		int b1 = 0, bc = 0, b2=  bs2.cardinality();
+		double t = 0;
+		if (bs1 != null) {
+			//System.out.println(bs.toString());
+			b1 = bs1.cardinality();
+			bs1.and(bs2);
+			bc = bs1.cardinality();
+			//t = 1- (double)  bc / ( (double) (b1+b2-bc));
+			t =  (double)  bc / ( (double) (b1+b2-bc));
+		} else {
+			return Double.NaN;
+		}
+		return t;
+	}
+	protected double[] tanimoto(List<BitSet> data) {
+		double[] values = new double[data.size()];
+		BitSet bs = null;
+		int b1 = 0, bc = 0, b2=  consensusFingerprint.cardinality();
+		for (int i = 0; i < data.size(); i++ ) {
+		    try {
+			bs = data.get(i);
+		    } catch (Exception x) {
+		        //TODO exception
+		        bs = null;
+		    }
+			if (bs != null) {
+				b1 = bs.cardinality();
+				bs.and(consensusFingerprint);
+				bc = bs.cardinality();
+				//values[i] = 1 - (double)  bc / ( (double) (b1+b2-bc));
+				values[i] = (double)  bc / ( (double) (b1+b2-bc));
+			} else {
+				values[i] = Double.NaN;
+			}
+		}
+		return values;
+	}
+	
+	protected double estimateThresholdValues(double percent, double[] values, int npoints) {
+		if (npoints == 0) return 0;
+		double t;
+		int tIndex = npoints;
+		if (percent == 1) {
+			t = Tools.min(values,npoints);
+		}
+		else {
+		    Sort sort = new Sort();
+		    sort.QuickSortArray(values, npoints);
+		    sort = null;
+		    tIndex = (int) Math.round(npoints * percent);
+		    if (tIndex > npoints) tIndex = npoints;
+			t = values[tIndex-1];
+		}
+		System.err.println("Percent\t"+pThreshold*100+"\tThreshold\t"+t+"\tIndex\t"+tIndex);
+		return t;
+		
+	}	
+
+	public double[] predict(List<BitSet> data) {
+		return tanimoto(data);
+	}
+	public String getName() {
+		return "Fingerprints (Tanimoto)";
+	}
+}
