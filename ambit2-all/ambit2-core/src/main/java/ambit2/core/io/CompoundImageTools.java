fa9ab2e08318469f2e541f09b9acae8aaf8b8c34
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-04-11 13:56:20 +0000

    filled ovals for selection - couldn't find better way than copying selectatomgenerator code and changing single parameter

--- ambit2-all/ambit2-core/src/main/java/ambit2/core/io/CompoundImageTools.java
+++ ambit2-all/ambit2-core/src/main/java/ambit2/core/io/CompoundImageTools.java
@@ -23,6 +23,7 @@ import javax.vecmath.Vector2d;
 import org.openscience.cdk.MoleculeSet;
 import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.graph.ConnectivityChecker;
+import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IMolecule;
 import org.openscience.cdk.interfaces.IMoleculeSet;
@@ -33,13 +34,18 @@ import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 import org.openscience.jchempaint.renderer.Renderer;
 import org.openscience.jchempaint.renderer.RendererModel;
 import org.openscience.jchempaint.renderer.RenderingParameters.AtomShape;
+import org.openscience.jchempaint.renderer.elements.ElementGroup;
+import org.openscience.jchempaint.renderer.elements.IRenderingElement;
+import org.openscience.jchempaint.renderer.elements.OvalElement;
+import org.openscience.jchempaint.renderer.elements.RectangleElement;
 import org.openscience.jchempaint.renderer.font.AWTFontManager;
 import org.openscience.jchempaint.renderer.generators.BasicAtomGenerator;
 import org.openscience.jchempaint.renderer.generators.BasicBondGenerator;
 import org.openscience.jchempaint.renderer.generators.IGenerator;
-import org.openscience.jchempaint.renderer.generators.SelectAtomGenerator;
+import org.openscience.jchempaint.renderer.generators.IGeneratorParameter;
 import org.openscience.jchempaint.renderer.generators.SelectBondGenerator;
 import org.openscience.jchempaint.renderer.selection.IChemObjectSelection;
+import org.openscience.jchempaint.renderer.selection.IncrementalSelection;
 import org.openscience.jchempaint.renderer.visitor.AWTDrawVisitor;
 
 import ambit2.base.interfaces.IProcessor;
@@ -95,7 +101,7 @@ public class CompoundImageTools {
        List<IGenerator> generators = new ArrayList<IGenerator>();
        generators.add(new BasicBondGenerator());
        generators.add(new BasicAtomGenerator());
-       generators.add(new SelectAtomGenerator());
+       generators.add(new MySelectAtomGenerator());
        generators.add(new SelectBondGenerator());
        //generators.add(new RingGenerator());
     	
@@ -109,6 +115,7 @@ public class CompoundImageTools {
 		r2dm.setColorAtomsByType(true);
 		r2dm.setShowImplicitHydrogens(false);
 		r2dm.setShowAromaticity(true);  
+
 
 		return renderer;
     }
@@ -319,6 +326,8 @@ public class CompoundImageTools {
     	    		r2dm.setSelectionShape(AtomShape.OVAL);
     	    		r2dm.setSelection(highlighted);
     	    		r2dm.setColorAtomsByType(true);
+    	    		r2dm.setShowAtomTypeNames(true);
+
     	    	} 	  
 				renderer.paintMolecule(molecules.getAtomContainer(i),new AWTDrawVisitor(g),r,true);
 				
@@ -377,4 +386,62 @@ public class CompoundImageTools {
 	public void setDefaultImage(BufferedImage defaultImage) {
 		this.defaultImage = defaultImage;
 	}
+}
+
+/**
+ * Can't find how to highlight atoms with filled -in ovals
+ * @author nina
+ *
+ */
+class MySelectAtomGenerator  implements IGenerator  {
+	 private boolean autoUpdateSelection = true;
+
+	    public MySelectAtomGenerator() {}
+
+	    public IRenderingElement generate(IAtomContainer ac, RendererModel model) {
+	        Color selectionColor = model.getSelectedPartColor();
+	        AtomShape shape = model.getSelectionShape();
+	        IChemObjectSelection selection = model.getSelection();
+	        ElementGroup selectionElements = new ElementGroup();
+
+	        if(selection==null)
+	        	return selectionElements;
+	        if (this.autoUpdateSelection || selection.isFilled()) {
+	            double r = model.getSelectionRadius() / model.getScale();
+
+	            double d = 2 * r;
+	            IAtomContainer selectedAC = selection.getConnectedAtomContainer();
+	            if (selectedAC != null) {
+	                for (IAtom atom : selectedAC.atoms()) {
+	                    Point2d p = atom.getPoint2d();
+	                    IRenderingElement element;
+	                    switch (shape) {
+	                        case SQUARE:
+	                            element =
+	                                new RectangleElement(
+	                                    p.x - r, p.y - r, d, d, true,
+	                                    selectionColor);
+	                            break;
+	                        case OVAL:
+	                        default:
+	                            element = new OvalElement(
+	                                            p.x, p.y, d, true, selectionColor);
+	                    }
+	                    selectionElements.add(element);
+	                }
+	            }
+	        }
+
+	        if (selection instanceof IncrementalSelection) {
+				IncrementalSelection sel = (IncrementalSelection) selection;
+				if (!sel.isFinished())
+					selectionElements.add(sel.generate(selectionColor));
+			}
+	        return selectionElements;
+	    }
+
+	    public List<IGeneratorParameter> getParameters() {
+	        // TODO Auto-generated method stub
+	        return null;
+	    }
 }
