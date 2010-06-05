ffae904579bc5ce4f98e1a020a1987d2b1272a57
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-06-05 20:08:54 +0000

    QueryReporter descendants, read structures into memory

---
+++ ambit2-all/ambit2-db/src/main/java/ambit2/db/reporters/StructureListReporter.java
@@ -0,0 +1,32 @@
+package ambit2.db.reporters;
+
+import java.util.List;
+
+import ambit2.base.data.Profile;
+import ambit2.base.data.Template;
+import ambit2.base.exceptions.AmbitException;
+import ambit2.base.interfaces.IStructureRecord;
+
+public class StructureListReporter extends AbstractStructureRecordReporter<List<IStructureRecord>> {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4591388340477136425L;
+	public StructureListReporter(Profile groupedProperties,Template template) {
+		this(null,groupedProperties,template);
+	}
+	
+	public StructureListReporter(List<IStructureRecord> record,Profile groupedProperties,Template template) {
+		super(record,groupedProperties,template);
+	}
+	@Override
+	public Object processItem(IStructureRecord item) throws AmbitException {
+		try {if (item.isSelected()) getOutput().add((IStructureRecord)((IStructureRecord) item).clone());}
+		catch (Exception x) {}
+		return item;
+
+	}
+	
+}
+
