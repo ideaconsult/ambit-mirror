aa61ecbb9d5cbc3a370c0d0cbc2f9a1a8bc9a6ec
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-08-11 18:40:25 +0000

    bookmark resource http://localhost:8080/bookmark/{creator}/{id}?search=title&hasTopic={Model,Algorithm,Dataset,Compound} , uses http://www.w3.org/2003/07/Annotea/BookmarkSchema-20030707

---
+++ ambit2-db/src/main/java/ambit2/db/update/bookmark/UpdateBookmark.java
@@ -0,0 +1,51 @@
+package ambit2.db.update.bookmark;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ambit2.base.data.Bookmark;
+import ambit2.base.exceptions.AmbitException;
+import ambit2.db.search.QueryParam;
+import ambit2.db.update.AbstractObjectUpdate;
+
+/**
+ * Updates content of the model
+ * @author nina
+ *
+ */
+public class UpdateBookmark extends AbstractObjectUpdate<Bookmark>{
+
+	public static final String update_sql = "update bookmark set recalls=?,hasTopic=?,title=?,description=?,date=now() where idbookmark=? and creator=?";
+
+	public UpdateBookmark(Bookmark ref) {
+		super(ref);
+	}
+	public UpdateBookmark() {
+		this(null);
+	}			
+	public List<QueryParam> getParameters(int index) throws AmbitException {
+		List<QueryParam> params = new ArrayList<QueryParam>();
+		params.add(new QueryParam<String>(String.class, getObject().getRecalls()));
+		params.add(new QueryParam<String>(String.class, getObject().getHasTopic()));
+		params.add(new QueryParam<String>(String.class, getObject().getTitle()));
+		params.add(new QueryParam<String>(String.class, getObject().getDescription()==null?"":getObject().getDescription()));
+		if (getObject().getId() > 0) 
+			params.add(new QueryParam<Integer>(Integer.class, getObject().getId()));
+		else
+			throw new AmbitException("no bookmark specified");
+		
+		params.add(new QueryParam<String>(String.class, getObject().getCreator()));
+		return params.size()==0?null:params;		
+				
+	}
+
+	public String[] getSQL() throws AmbitException {
+		if (getObject().getId() > 0) {
+			return new String[] {String.format(update_sql,ReadBookmark.whereID)};
+		}
+		throw new AmbitException("no bookmark specified");
+	}
+	public void setID(int index, int id) {
+			
+	}
+}
