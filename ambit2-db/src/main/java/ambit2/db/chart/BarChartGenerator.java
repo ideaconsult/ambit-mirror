909dd28b870a26ae78f4a6300805302fe8d094fd
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-08-11 18:41:12 +0000

    bar chart /chart/bar

---
+++ ambit2-db/src/main/java/ambit2/db/chart/BarChartGenerator.java
@@ -0,0 +1,43 @@
+package ambit2.db.chart;
+
+import ambit2.base.data.Property;
+import ambit2.db.exceptions.DbAmbitException;
+
+public abstract class BarChartGenerator<T> extends ChartGenerator<T> {
+
+	
+	protected Property propertyX;
+	public Property getPropertyX() {
+		return propertyX;
+	}
+
+	public void setPropertyX(Property propertyX) {
+		this.propertyX = propertyX;
+	}
+
+	public Property getPropertyY() {
+		return propertyY;
+	}
+
+	public void setPropertyY(Property propertyY) {
+		this.propertyY = propertyY;
+	}
+
+	protected Property propertyY;
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 81990940570873057L;
+
+	protected abstract int getID(T target);
+	protected abstract String getSQL();
+	
+	
+
+	public void open() throws DbAmbitException {
+
+		
+	}
+
+}
