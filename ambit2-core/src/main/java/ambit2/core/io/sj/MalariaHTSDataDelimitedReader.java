beb21ade330a376a493bea21dade887841bb7afd
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-09-09 12:45:56 +0000

    a hack to allow uploading Malaria HTS data sheets - .csv files should be renamed to .sht

---
+++ ambit2-core/src/main/java/ambit2/core/io/sj/MalariaHTSDataDelimitedReader.java
@@ -0,0 +1,46 @@
+package ambit2.core.io.sj;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IMolecule;
+
+import ambit2.core.io.IteratingDelimitedFileReader;
+
+/**
+ * XLS file
+SAMPLE	batch
+SJ000001054	1
+SJ000001111	1
+SJ000001119	1
+
+SDF file
+SJ000001054-1
+ * @author nina
+ *
+ */
+public class MalariaHTSDataDelimitedReader extends IteratingDelimitedFileReader {
+	protected static final String col_SAMPLE="SAMPLE";
+	protected static final String col_batch="batch";
+	public MalariaHTSDataDelimitedReader(InputStream in) throws CDKException {
+		super(new InputStreamReader(in));
+	}
+
+	@Override
+	public Object next() {
+		IMolecule mol = (IMolecule) super.next();
+		Object sample = mol.getProperty(col_SAMPLE);
+		Object batch = mol.getProperty(col_batch);
+		if (sample==null) return mol;
+		if (sample.toString().indexOf("-")<0) {
+			batch = batch==null?"1":batch;
+			mol.setProperty(col_SAMPLE,String.format("%s-%s",sample,batch));
+		}
+		return mol;
+	}
+	
+
+
+
+}
