60463b364ad1ac4fde53c3c382c7a5324d0123a1
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-09-09 10:31:45 +0000

    SOME rank visualisation

--- ambit2-all/ambit2-some/src/main/java/ambit2/some/SOMEVisualizer.java
+++ ambit2-all/ambit2-some/src/main/java/ambit2/some/SOMEVisualizer.java
@@ -2,6 +2,10 @@ package ambit2.some;
 
 import java.awt.Dimension;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
@@ -62,16 +66,50 @@ public class SOMEVisualizer extends SOMEResultsParser implements IStructureDiagr
 	*/
 
 	@Override
+	public void parseRecord(String some) throws Exception {
+		super.parseRecord(some);
+		double[] size = new double[] {1.66,1.33,1.0};
+		//assign ranks
+		List<IAtom> atoms = new ArrayList<IAtom>();
+		Comparator<IAtom> cmp = null;
+		for (int i = someindex.aliphaticHydroxylation.ordinal() ; i <= someindex.SOxidation.ordinal(); i ++) {
+			atoms.clear();
+			for (IAtom atom : mol.atoms()) {
+				Object value = atom.getProperty(someindex.values()[i].name());
+				if (value !=null) {
+					atoms.add(atom);
+				}
+			}
+			if (atoms.size()>0) {
+				 cmp = new SOMERank(someindex.values()[i]);
+				 Collections.sort(atoms,cmp);
+				 
+				 int rank = 1;
+				 Double prevValue = null;
+				 for (int r=0; r < atoms.size();r++ ) {
+					 Object value = atoms.get(r).getProperty(someindex.values()[i].name());
+					 atoms.get(r).setProperty(SOMEShell.SOME_RESULT, r+1);
+					 //atoms.get(r).setProperty(CompoundImageTools.ATOM_ANNOTATION,String.format("[%s]",someindex.values()[i].name()));
+					 atoms.get(r).setProperty(CompoundImageTools.SELECTED_ATOM_SIZE,rank>3?0.66:size[rank-1]);
+					 
+					 if ((prevValue==null) || (prevValue<((Double)value))) rank++;
+					 prevValue = (Double) value; 	 
+					 
+				 }
+			}
+					
+		}
+
+	}
+	@Override
 	protected void process(int atomNum, String atomSymbol, someindex index,
 			double value, boolean star) {
 
 		if (!star) return;
 		if ((ruleid==null) || ruleid.equals(index.name())) {
-			mol.getAtom(atomNum-1).setProperty(SOMEShell.SOME_RESULT, value);
-			mol.getAtom(atomNum-1).setProperty(CompoundImageTools.SELECTED_ATOM_SIZE,1+value);
+			mol.getAtom(atomNum-1).setProperty(index.name(), value);
 			mol.getAtom(atomNum-1).setProperty(CompoundImageTools.SELECTED_ATOM_COLOR,index.getColor(0.75));
-			mol.getAtom(atomNum-1).setProperty(CompoundImageTools.ATOM_ANNOTATION,
-					String.format("(%d)",index.ordinal()-1));
+
 		}
 			
 	}
@@ -111,4 +149,28 @@ public class SOMEVisualizer extends SOMEResultsParser implements IStructureDiagr
 		
 	}
 
+}
+
+
+class SOMERank implements Comparator<IAtom> {
+	protected someindex index;
+	public SOMERank(someindex index) {
+		this.index = index;
+	}
+	@Override
+	public int compare(IAtom o1, IAtom o2) {
+		Object value1 = o1.getProperty(index.name());
+		Object value2 = o2.getProperty(index.name());
+		if ((value1==null) && (value2==null)) return 0;
+		
+		if (value1==null) 
+			return - ((int) (100 * ((Double)value2).doubleValue()));
+		if (value2==null) 
+			return (int) (100 * ((Double)value1).doubleValue());
+		
+		return (((Double)value2).doubleValue()-((Double)value1).doubleValue())>0?1:-1;
+
+
+	}
+	
 }
