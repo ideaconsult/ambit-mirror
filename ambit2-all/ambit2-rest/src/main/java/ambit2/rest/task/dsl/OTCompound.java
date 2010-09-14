d8a8a913d63547ee05fd440edfeb7a95c846a2e1
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-09-14 07:18:16 +0000

    OTCompound implementation

--- ambit2-all/ambit2-rest/src/main/java/ambit2/rest/task/dsl/OTCompound.java
+++ ambit2-all/ambit2-rest/src/main/java/ambit2/rest/task/dsl/OTCompound.java
@@ -1,5 +1,20 @@
 package ambit2.rest.task.dsl;
 
+import org.restlet.data.Reference;
+
 public class OTCompound extends OTObject {
 
+	 protected OTCompound(Reference ref) {
+			super(ref);
+		 }
+		 protected OTCompound(String ref) {
+			super(ref);
+		}
+		 public static OTCompound compound(Reference uri) throws Exception  { 
+			    return new OTCompound(uri);
+		 }
+
+		 public static OTCompound compound(String uri) throws Exception  { 
+			    return new OTCompound(uri);
+		 }
 }
