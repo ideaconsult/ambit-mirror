ba2b2affad0e455e0acc11fd078196fdbba1a953
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-10-09 17:10:55 +0000

    fingerprint prescreening queries optimized to avoid group by idchemical

--- ambit2-db/src/main/java/ambit2/db/search/structure/QueryPrescreenBitSet.java
+++ ambit2-db/src/main/java/ambit2/db/search/structure/QueryPrescreenBitSet.java
@@ -15,20 +15,56 @@ import ambit2.db.search.QueryParam;
  * First bitset is from structural keys, second from fingerprints
  * @author nina
  *
+ <pre>
+select SQL_NO_CACHE -1 as idquery,s1.idchemical,s1.idstructure,if(s1.type_structure='NA',0,1) as selected,s1.preference as metric,null as text,-1 as idproperty
+FROM structure s1
+LEFT JOIN structure s2 ON s1.idchemical = s2.idchemical  AND (1E10*s1.preference+s1.idstructure) > (1E10*s2.preference+s2.idstructure)
+join (
+
+select idchemical from fp1024 fp join sk1024 sk using(idchemical)
+where (bit_count('207704626823199' & sk.fp1) + bit_count('0' & sk.fp2) + bit_count('0' & sk.fp3) +
+bit_count('0' & sk.fp4) + bit_count('0' & sk.fp5) + bit_count('0' & sk.fp6) + bit_count('0' & sk.fp7) +
+bit_count('0' & sk.fp8) + bit_count('0' & sk.fp9) + bit_count('0' & sk.fp10) + bit_count('0' & sk.fp11) +
+bit_count('0' & sk.fp12) + bit_count('0' & sk.fp13) + bit_count('0' & sk.fp14) + bit_count('0' & sk.fp15) +
+bit_count('0' & sk.fp16))=15
+and
+(bit_count('0' & fp.fp1) + bit_count('2147483648' & fp.fp2) +
+bit_count('8589934592' & fp.fp3) + bit_count('2305843009215799296' & fp.fp4) +
+bit_count('0' & fp.fp5) + bit_count('0' & fp.fp6) + bit_count('128' & fp.fp7) +
+bit_count('0' & fp.fp8) + bit_count('4611686018427387904' & fp.fp9) + bit_count('137438953472' & fp.fp10) +
+bit_count('0' & fp.fp11) + bit_count('2615635083264' & fp.fp12) + bit_count('0' & fp.fp13) +
+bit_count('0' & fp.fp14) + bit_count('8589934592' & fp.fp15) + bit_count('0' & fp.fp16))=13
+
+) a on a.idchemical=s1.idchemical
+where s2.idchemical is null
+and s1.type_structure != 'NA'
+ limit 0,1000;
+ </pre>
  */
 public class QueryPrescreenBitSet extends AbstractStructureQuery<BitSet,BitSet,NumberCondition> {
 	protected String sql_struc = 
-	"select ? as idquery,idchemical,idstructure,if(type_structure='NA',0,1) as selected,fp.bc+sk.bc as metric,null as text from structure\n";
-	protected String sql_chemical = 
-	"select ? as idquery,idchemical,idstructure,if(type_structure='NA',0,1) as selected,fp.bc+sk.bc as metric,null as text from structure "+group;
-
-	protected String sql = 
+	"select ? as idquery,s1.idchemical,s1.idstructure,if(type_structure='NA',0,1) as selected,m as metric,null as text from structure s1\n"+
+	"join (\n"+
 	"%s\n"+
-	"join fp1024 fp\n"+
-	"using(idchemical)\n"+
+	") a on a.idchemical=s1.idchemical\n"+
+	"where s1.type_structure != 'NA'";
+	
+	protected String sql_chemical = 
+	//"select ? as idquery,idchemical,idstructure,if(type_structure='NA',0,1) as selected,m as metric,null as text from structure "+group;
+	"select ? as idquery,s1.idchemical,s1.idstructure,0 as selected,m as metric,null as text\n"+
+	"FROM structure s1\n"+
+	"LEFT JOIN structure s2 ON s1.idchemical = s2.idchemical  AND (1E10*s1.preference+s1.idstructure) > (1E10*s2.preference+s2.idstructure)\n"+
+	"join (\n"+
+	"%s\n"+
+	") a on a.idchemical=s1.idchemical\n"+
+	"where s2.idchemical is null\n"+
+	"and s1.type_structure != 'NA'";
+
+	protected String sql_prescreen = 
+	"select fp.idchemical,fp.bc+sk.bc as m from fp1024 fp\n"+
 	"join sk1024 sk\n"+
 	"using(idchemical)\n"+
-	"where %s\n"+
+	"where \n"+
 	"(bit_count(? & sk.fp1) + bit_count(? & sk.fp2) + bit_count(? & sk.fp3) +\n"+
 	"bit_count(? & sk.fp4) + bit_count(? & sk.fp5) + bit_count(? & sk.fp6) + bit_count(? & sk.fp7) +\n"+
 	"bit_count(? & sk.fp8) + bit_count(? & sk.fp9) + bit_count(? & sk.fp10) + bit_count(? & sk.fp11) +\n"+
@@ -40,14 +76,12 @@ public class QueryPrescreenBitSet extends AbstractStructureQuery<BitSet,BitSet,N
 	"bit_count(? & fp.fp5) + bit_count(? & fp.fp6) + bit_count(? & fp.fp7) +\n"+
 	"bit_count(? & fp.fp8) + bit_count(? & fp.fp9) + bit_count(? & fp.fp10) +\n"+
 	"bit_count(? & fp.fp11) + bit_count(? & fp.fp12) + bit_count(? & fp.fp13) +\n"+
-	"bit_count(? & fp.fp14) + bit_count(? & fp.fp15) + bit_count(? & fp.fp16))=?\n"+
-	"and structure.type_structure != 'NA'\n";
+	"bit_count(? & fp.fp14) + bit_count(? & fp.fp15) + bit_count(? & fp.fp16))=?\n";
 	
 
+
 	protected String sql_exact = 
-	"%s\n"+
-	"join fp1024 fp\n"+
-	"using(idchemical)\n"+
+	"select idchemical,fp.bc+sk.bc as m from fp1024 fp\n"+
 	"join sk1024 sk\n"+
 	"using(idchemical)\n"+
 	"where %s\n"+
@@ -62,8 +96,7 @@ public class QueryPrescreenBitSet extends AbstractStructureQuery<BitSet,BitSet,N
 	"(? = fp.fp5) and (? = fp.fp6) and (? = fp.fp7) and\n"+
 	"(? = fp.fp8) and (? = fp.fp9) and (? = fp.fp10) and\n"+
 	"(? = fp.fp11) and (? = fp.fp12) and (? = fp.fp13) and\n"+
-	"(? = fp.fp14) and (? = fp.fp15) and (? = fp.fp16)\n"+
-	"and structure.type_structure != 'NA'\n";
+	"(? = fp.fp14) and (? = fp.fp15) and (? = fp.fp16)\n";
 	
 	/**
 	 * 
@@ -85,10 +118,14 @@ public class QueryPrescreenBitSet extends AbstractStructureQuery<BitSet,BitSet,N
 	
 	public String getSQL() throws AmbitException {
 		
+		return String.format(isChemicalsOnly()?sql_chemical:sql_struc,
+				getCondition().equals(NumberCondition.getInstance("="))?sql_exact:sql_prescreen
+				);
+		/*
 		return String.format(getCondition().equals(NumberCondition.getInstance("="))?sql_exact:sql,
 				isChemicalsOnly()?sql_chemical:sql_struc,
 				isChemicalsOnly()?where_group:"");
-
+*/
 	}
 	protected void bitset2params(BitSet bitset, List<QueryParam> params) throws AmbitException {
 		BigInteger[] h16 = new BigInteger[16];
