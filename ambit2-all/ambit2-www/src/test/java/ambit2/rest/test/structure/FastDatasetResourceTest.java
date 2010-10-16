37e7aa37a2e8e1591432f2200360ad3055e8945f
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-10-16 15:06:29 +0000

    test

---
+++ ambit2-all/ambit2-www/src/test/java/ambit2/rest/test/structure/FastDatasetResourceTest.java
@@ -0,0 +1,34 @@
+package ambit2.rest.test.structure;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.junit.Test;
+import org.restlet.data.MediaType;
+
+import ambit2.rest.test.ResourceTest;
+
+public class FastDatasetResourceTest extends ResourceTest {
+	@Override
+	public String getTestURI() {
+		return String.format("http://localhost:%d/fastdataset/1", port);
+	}
+	
+	@Test
+	public void testCSV() throws Exception {
+		testGet(getTestURI(),MediaType.TEXT_CSV);
+	}
+	@Override
+	public boolean verifyResponseCSV(String uri, MediaType media, InputStream in)
+			throws Exception {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		String line = null;
+		int count=0;
+		while ((line = reader.readLine())!=null) {
+			System.out.println(line);
+			count++;
+		}
+		return count >0;
+	}	
+}
