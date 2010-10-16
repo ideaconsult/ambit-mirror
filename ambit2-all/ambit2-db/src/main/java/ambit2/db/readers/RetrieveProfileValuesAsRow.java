47b86698369999a1e5853320175a85163452d1c4
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-10-16 14:41:48 +0000

    refactoring to introduce faster dataset retrieval Now available via /fastdataset/{id}  , only text/csv supported

---
+++ ambit2-all/ambit2-db/src/main/java/ambit2/db/readers/RetrieveProfileValuesAsRow.java
@@ -0,0 +1,205 @@
+package ambit2.db.readers;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import ambit2.base.data.Profile;
+import ambit2.base.data.Property;
+import ambit2.base.data.StructureRecord;
+import ambit2.base.exceptions.AmbitException;
+import ambit2.base.interfaces.IStructureRecord;
+import ambit2.db.search.AbstractQuery;
+import ambit2.db.search.QueryParam;
+import ambit2.db.search.SetCondition;
+import ambit2.db.search.SetCondition.conditions;
+
+public class RetrieveProfileValuesAsRow extends AbstractQuery<Profile<Property>,int[],SetCondition,IStructureRecord> 
+					implements IQueryRetrieval<IStructureRecord> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6445434992210833166L;
+	public static final String NaN = "NaN";
+	protected IStructureRecord record;
+	public IStructureRecord getRecord() {
+		return record;
+	}
+
+	public void setRecord(IStructureRecord record) {
+		this.record = record;
+	}
+	protected boolean addNew = false;
+	public boolean isAddNew() {
+		return addNew;
+	}
+
+	public void setAddNew(boolean addNew) {
+		this.addNew = addNew;
+	}
+
+	
+	public RetrieveProfileValuesAsRow() {
+		this(null,false);
+		
+	}
+	public RetrieveProfileValuesAsRow(Profile profile,boolean chemicalsOnly) {
+		super();
+		setFieldname(profile);
+		setCondition(new SetCondition(conditions.in));
+
+	}		
+	protected final String sql_property = 
+		"MAX(CASE WHEN st.idproperty = ? THEN value ELSE NULL END) AS v%d,\n"+
+		"MAX(CASE WHEN st.idproperty = ? THEN value_num ELSE NULL END) AS n%d\n";
+	/*
+	protected final String sql_chemical = 
+	    "JOIN (SELECT idstructure FROM structure WHERE idchemical=?) a USING(idstructure)\n";
+	    */
+	protected final String sql_structure = 
+		 "WHERE idstructure %s %s\n";
+		
+	protected final String sql = 
+		"SELECT pv.idstructure, \n" +
+		"%s\n"+
+	    "FROM\n"+
+	    "property_values AS pv\n"+
+	    "INNER JOIN properties AS st ON pv.idproperty = st.idproperty\n"+
+	    "LEFT JOIN property_string s ON s.idvalue_string=pv.idvalue_string\n"+
+	    "%s\n"+
+	    "GROUP BY pv.idstructure ORDER by idstructure\n";
+		
+	
+	protected String getStructureWhere() throws AmbitException {
+		if ((getValue()==null) || (getValue().length==0)) throw new AmbitException("Structure id missing");
+		//if (getValue().length==1) return "?";
+		StringBuilder b = new StringBuilder();
+
+		String delimiter = "(";
+		for (int i=0;i < getValue().length;i++)  {
+			if (getValue()[i]<=0) continue;
+			b.append(String.format("%s?",delimiter));
+			delimiter = ",";
+		}
+		b.append(")");
+		return b.toString();
+	}
+	
+	protected String getPropertySQL() {
+		StringBuilder b = new StringBuilder();
+
+		if ((getFieldname()!=null) &&(getFieldname().size()>0)) {
+			Iterator<Property> i = getFieldname().getProperties(true);
+			
+			int count = 0;
+		
+			String delimiter = "";
+			while (i.hasNext()) {
+				int p = i.next().getId();
+				if (p > 0) {
+					b.append(delimiter);
+					b.append(String.format(sql_property,p,p));
+					count++;
+					delimiter = ",";
+				}
+				
+			}
+		}
+		return b.toString();
+	}
+	public String getSQL() throws AmbitException {
+		return String.format(sql,
+				getPropertySQL(),
+				String.format(sql_structure,getCondition().getSQL(),getStructureWhere()));
+	}
+
+	public List<QueryParam> getParameters() throws AmbitException {
+		List<QueryParam> params = new ArrayList<QueryParam>();
+
+			if ((getFieldname()!=null) &&(getFieldname().size()>0)) {
+				Iterator<Property> i = getFieldname().getProperties(true);
+				while (i.hasNext()) {
+					int p = i.next().getId();
+					if (p >0) {
+						params.add(new QueryParam<Integer>(Integer.class, p));
+						params.add(new QueryParam<Integer>(Integer.class, p));
+					}
+					
+				}
+			}
+
+		if (getValue()!=null)
+		for (int i=0;i < getValue().length;i++)  
+			if (getValue()[i]<=0) continue;
+			else params.add(new QueryParam<Integer>(Integer.class, getValue()[i]));
+		
+		return params;		
+	}
+
+	public IStructureRecord getObject(ResultSet rs) throws AmbitException {
+		try {
+			IStructureRecord record = getRecord();
+			if (record==null) record = new StructureRecord();
+			record.setIdchemical(-1);
+			record.setIdstructure(rs.getInt(1));
+			
+			Iterator<Property> i = getFieldname().getProperties(true);
+			while (i.hasNext()) {
+				Property p = i.next();
+				if (p.getId() >0) try {
+					String name = String.format("v%d",p.getId());
+					Object value = rs.getString(name);
+					if (value == null) {
+						name = String.format("n%d",p.getId());
+						 value = rs.getFloat(name);
+					}
+					record.setProperty(p,value);
+				} catch (Exception x) {
+					x.printStackTrace();
+				}
+				
+			}
+
+			/*
+			if (chemicalsOnly && record.getIdstructure()>0) 
+				; //skip
+			else record.setIdstructure(rs.getInt(4));
+			record.setIdchemical(rs.getInt(9));
+			LiteratureEntry le = LiteratureEntry.getInstance(rs.getString(7),rs.getString(8),rs.getInt(2));
+			Property p = Property.getInstance(rs.getString(1),le); 
+			Object value = rs.getObject(5);
+			
+			if (value == null) {
+				value = rs.getObject(6);
+				record.setProperty(p,value==null?Double.NaN:rs.getFloat(6));
+				p.setClazz(Number.class);
+			}
+			else {
+				if (NaN.equals(value.toString())) {
+					record.setProperty(p,Double.NaN);
+					p.setClazz(Number.class);
+				} else {
+					record.setProperty(p,rs.getString(5));
+					p.setClazz(String.class);
+				}
+			}
+			*/
+			return record;
+		} catch (SQLException x) {
+			throw new AmbitException(x);
+		}
+		
+	}
+
+	public double calculateMetric(IStructureRecord object) {
+		return 1;
+	}
+	public boolean isPrescreen() {
+		return false;
+	}
+	
+	
+}
---
+++ ambit2-all/ambit2-db/src/main/java/ambit2/db/reporters/QueryPacketReporter.java
@@ -0,0 +1,120 @@
+package ambit2.db.reporters;
+
+
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import ambit2.base.data.Profile;
+import ambit2.base.data.Property;
+import ambit2.base.data.StructureRecord;
+import ambit2.base.exceptions.AmbitException;
+import ambit2.base.interfaces.IBatchStatistics;
+import ambit2.base.interfaces.IProcessor;
+import ambit2.base.interfaces.IStructureRecord;
+import ambit2.base.processors.DefaultAmbitProcessor;
+import ambit2.base.processors.ProcessorsChain;
+import ambit2.db.exceptions.DbAmbitException;
+import ambit2.db.readers.IQueryRetrieval;
+import ambit2.db.readers.RetrieveProfileValuesAsRow;
+import ambit2.db.search.QueryExecutor;
+
+public abstract class QueryPacketReporter<Q extends IQueryRetrieval<IStructureRecord>,Output> extends QueryAbstractReporter<IStructureRecord,Q,Output>	 {
+	protected QueryExecutor exec;
+	protected RetrieveProfileValuesAsRow chunkQuery;
+
+	protected Profile<Property> template;
+	protected int index = 0;
+	protected int[] chunks = new int[10];
+	
+	public int getChunkSize() {
+		return chunks.length;
+	}
+
+	public void setChunkSize(int chunkSize) {
+		chunks  = new int[chunkSize];
+		for (int i=0; i < chunks.length;i++) chunks[i] = 0;
+	}
+	
+	@Override
+	public void setConnection(Connection connection) throws DbAmbitException {
+		super.setConnection(connection);
+		exec.setConnection(connection);
+	}
+	
+	@Override
+	public void close() throws SQLException {
+		exec.close();
+		super.close();
+	}
+	public QueryPacketReporter(Profile<Property> template) {
+		super();
+		this.template = template;
+		exec = new QueryExecutor();
+		exec.setCloseConnection(false);
+
+		chunkQuery = new RetrieveProfileValuesAsRow(template,false);
+		chunkQuery.setRecord(new StructureRecord());
+		
+		processors = new ProcessorsChain<IStructureRecord,IBatchStatistics,IProcessor>();
+		processors.add(new DefaultAmbitProcessor<IStructureRecord,IStructureRecord>() {
+			public IStructureRecord process(IStructureRecord target) throws AmbitException {
+				
+				chunks[index] = target.getIdstructure();
+				index++;
+				if (index>=chunks.length) { 
+					
+					processChunks();
+
+					index = 0;
+				}
+				return target;
+			};
+		});		
+	}
+
+	protected void processChunks() throws AmbitException {
+		ResultSet rs = null ;
+		try {
+			chunkQuery.setValue(chunks);
+			rs = exec.process(chunkQuery);
+			while (rs.next()) {
+				IStructureRecord record = chunkQuery.getObject(rs);
+				processItem(record);
+			}
+		} catch (Exception x) {
+			
+		} finally {
+			try { rs.close();} catch (Exception x) {}
+		}
+		for (int i=0; i < chunks.length;i++) chunks[i] = 0; 
+	}
+	
+	@Override
+	protected void wrapup() throws AmbitException {
+		processChunks();
+		super.wrapup();
+	}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7517219316610459335L;
+
+	@Override
+	public void footer(Output output, Q query) {
+	
+	}
+
+	@Override
+	public void header(Output output, Q query) {
+	
+	}
+
+	@Override
+	public void open() throws DbAmbitException {
+		
+	}
+
+}
+
