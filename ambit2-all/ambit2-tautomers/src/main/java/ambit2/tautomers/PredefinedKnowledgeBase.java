f865074104e5a621c208815bf4e47427a1f8432b
Nikolay Kochev <nick@uni-plovdiv.net>
2010-10-25 13:16:15 +0000

    Implemented functionality for the rule parsing process.

--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/PredefinedKnowledgeBase.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/PredefinedKnowledgeBase.java
@@ -2,9 +2,16 @@ package ambit2.tautomers;
 
 public class PredefinedKnowledgeBase 
 {
+	/**
+	 * GROUP_POS describes the position of mobile group i.e. the number of atom 
+	 * it is attached to (SMILES position, 1-based atom indexing)
+	 * 
+	 */
+	
 	public static final String rules[] = 
 	{
-		"$$NAME=keto/enol $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES=   $$GROUP_POS=   $$INFO=",
+		"$$NAME=keto/enol     $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= O=CC,OC=C   $$GROUP_POS=3,1   $$INFO=",
+		"$$NAME=amide/imid    $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= O=CN,OC=N   $$GROUP_POS=3,1   $$INFO=",
 		""
 	};
 }
--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/RuleParser.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/RuleParser.java
@@ -3,10 +3,38 @@ package ambit2.tautomers;
 public class RuleParser 
 {
 	String errors = "";
+	Rule curRule = null;
 	
 	public Rule parse(String ruleString)
 	{	
-		//TODO
-		return(null);
+		Rule rule = new Rule();
+		curRule = rule;
+		int curPos = 0;
+		int res = ruleString.indexOf(TautomerConst.KeyWordPrefix, curPos);
+		
+		while (res != -1)
+		{	
+			res = ruleString.indexOf(TautomerConst.KeyWordPrefix, curPos+TautomerConst.KeyWordPrefix.length());
+			String keyword;
+			if (res == -1)
+				keyword = ruleString.substring(curPos);
+			else
+			{	
+				keyword = ruleString.substring(curPos,res);
+				curPos = res;	
+			}
+			
+			parseKeyWord(keyword);
+		}
+		
+		return(rule);
 	}
+	
+	
+	void parseKeyWord(String keyWord)
+	{
+		
+	}
+	
+	
 }
