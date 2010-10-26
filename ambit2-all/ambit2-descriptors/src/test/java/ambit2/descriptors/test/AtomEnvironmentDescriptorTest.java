e3b375ccb669e9354c228f2fc5842b049ebace39
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-10-26 15:40:14 +0000

    Reviving atomenvironments code. hybridization_atomtypes.xml were used for atom typing in the old implementation, now it is ignored and SybylAtomTypeMatcher is used instead

---
+++ ambit2-all/ambit2-descriptors/src/test/java/ambit2/descriptors/test/AtomEnvironmentDescriptorTest.java
@@ -0,0 +1,147 @@
+/**
+ * <b>Filename</b> AtomEnvironmentDescriptorTest.java 
+ * @author Nina Jeliazkova nina@acad.bg
+ * <b>Created</b> 2005-6-15
+ * <b>Project</b> ambit
+ */
+package ambit2.descriptors.test;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.openscience.cdk.Bond;
+import org.openscience.cdk.atomtype.SybylAtomTypeMatcher;
+import org.openscience.cdk.config.AtomTypeFactory;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IAtomType;
+import org.openscience.cdk.interfaces.IBond;
+import org.openscience.cdk.interfaces.IMolecule;
+import org.openscience.cdk.nonotify.NoNotificationChemObjectBuilder;
+import org.openscience.cdk.qsar.IMolecularDescriptor;
+import org.openscience.cdk.qsar.result.IntegerArrayResult;
+import org.openscience.cdk.tools.CDKHydrogenAdder;
+
+import ambit2.core.processors.structure.AtomConfigurator;
+import ambit2.core.smiles.SmilesParserWrapper;
+import ambit2.descriptors.AtomEnvironmentDescriptor;
+
+/**
+ * 
+ * @author Nina Jeliazkova <br>
+ * <b>Modified</b> 2005-6-15
+ */
+/**
+ * i have a problem with HybridizationStateATMatcher. 
+findMatchingAtomType method doesn't recognise some atom types defined in /cdk/config/data/hybridization_atomtypes.xml
+
+i think this is due to cdk:formalNeighbourCount entry missing for some of the types and therefore zero neighbors are assumed.
+ * 
+ * @author Nina Jeliazkova <br>
+ * <b>Modified</b> 2005-6-16
+ */
+public class AtomEnvironmentDescriptorTest {
+
+    protected IMolecule getMol() {
+            IMolecule mol = new org.openscience.cdk.Molecule();
+            IAtom a1 = new org.openscience.cdk.Atom("C");
+            mol.addAtom(a1);
+            IAtom a2 = new org.openscience.cdk.Atom("C");
+            mol.addAtom(a2);
+            IAtom a3 = new org.openscience.cdk.Atom("O");
+            mol.addAtom(a3);
+            IAtom a4 = new org.openscience.cdk.Atom("O");
+            mol.addAtom(a4);
+            IAtom a5 = new org.openscience.cdk.Atom("H");
+            mol.addAtom(a5);
+            IAtom a6 = new org.openscience.cdk.Atom("H");
+            mol.addAtom(a6);
+            IAtom a7 = new org.openscience.cdk.Atom("H");
+            mol.addAtom(a7);
+            IAtom a8 = new org.openscience.cdk.Atom("H");
+            mol.addAtom(a8);
+            IBond b1 = new org.openscience.cdk.Bond(a2, a1, IBond.Order.SINGLE);
+            mol.addBond(b1);
+            IBond b2 = new org.openscience.cdk.Bond(a3, a2, IBond.Order.SINGLE);
+            mol.addBond(b2);
+            IBond b3 = new org.openscience.cdk.Bond(a4, a2, IBond.Order.DOUBLE);
+            mol.addBond(b3);
+            IBond b4 = new org.openscience.cdk.Bond(a1, a5, IBond.Order.SINGLE);
+            mol.addBond(b4);
+            IBond b5 = new org.openscience.cdk.Bond(a1, a6, IBond.Order.SINGLE);
+            mol.addBond(b5);
+            Bond b6 = new org.openscience.cdk.Bond(a1, a7, IBond.Order.SINGLE);
+            mol.addBond(b6);
+            Bond b7 = new org.openscience.cdk.Bond(a3, a8, IBond.Order.SINGLE);
+            mol.addBond(b7);
+            return mol;
+    }
+    
+    @Test
+    public void testHybridizationStateATMatcher() throws Exception {
+	    IAtomContainer mol = getMol();
+	    SybylAtomTypeMatcher h = SybylAtomTypeMatcher.getInstance(NoNotificationChemObjectBuilder.getInstance());
+		for (int i = 0; i < mol.getAtomCount(); i++) {
+		        IAtomType a = h.findMatchingAtomType(mol,mol.getAtom(i));
+		        Assert.assertTrue(a != null);
+		}    
+    }
+    @Test
+    public void testAtomEnvironmentDescriptor() throws Exception {
+		IMolecularDescriptor descriptor = new AtomEnvironmentDescriptor();
+		int maxLevel = 1;
+		Object[] params = {null,new Integer(maxLevel),new Boolean(true),new Boolean(true)};
+
+	    SmilesParserWrapper sp = SmilesParserWrapper.getInstance();
+		    //IAtomContainer mol = sp.parseSmiles("CCCCCC=CCC=CCCCCCCCC(=O)NCCO");
+		    //IAtomContainer mol = sp.parseSmiles("CC(O)=O");
+		    IAtomContainer mol = sp.parseSmiles("CCCC(CCC(CCCC(CCCCCCC(CCCCCCC(CCC(CCCC(CCCCCCC(CCCCC)CCC)CCC(CC(CCCC(CCC(CCCC(CCCCCCC(CCCCC)CCC)CCC(CCCCC)CCC)CCCC(CCC(CCCC(CCCCCCC(CCCCC)CCC)CCC(CCCCC)CCC)CC)CCC)CCC(CCCCC)CCC)CCC)CCC)CCCC(CCC(CCCC(CCCCCCC(CCCCC)CCC)CCC(CCCCC)CCC)CC)CCC)CCC(CCCCC)CCCCC)CCC)CCC(CC(CCCC(CCC(CCCC(CCCCCCC(CCCCC)CCC)CCC(CCCCC)CCC)CCCC(CCC(CCCC(CCCCCCC(CCCCC)CCC)CCC(CCCCC)CCC)CC)CCC)CCC(CCCCC)CCC)CCC)CCC)CCCC(CCC(CCCC(CCCCCCC(CCCCC)CCC)CCC(CCCCC)CCC)CC)CCC)CCC(CCCCC)CCC");
+		    AtomConfigurator typer = new AtomConfigurator();
+		    typer.process(mol);
+		    
+		    //AtomContainer mol = sp.parseSmiles("N#CC(=Cc1ccc(O)c(O)c1)C(=O)NCCCNC(=O)C(C#N)=Cc2ccc(O)c(O)c2"); 
+		    CDKHydrogenAdder hAdder = CDKHydrogenAdder.getInstance(NoNotificationChemObjectBuilder.getInstance());
+		    hAdder.addImplicitHydrogens(mol);
+		    
+		    /*
+			File logfile = new File(String
+					.format("%s/%s", System.getProperty("java.io.tmpdir"),"mol.logger"));
+			logfile.deleteOnExit();
+		    
+		    CDKSourceCodeWriter w = new CDKSourceCodeWriter(new FileOutputStream(logfile));
+		    w.writeMolecule((Molecule) mol);
+		    w.close();
+		     */
+			for (int i = 0; i < mol.getAtomCount(); i++) {
+				//System.out.println(mol.getAtom(i));
+				params[0] = new Integer(i);
+			    descriptor.setParameters(params);
+	
+			    IntegerArrayResult r = (IntegerArrayResult) descriptor.calculate(mol).getValue();
+			    //System.out.println(r.size());
+			    if (i==0) { // just for test
+				    Object[] parameters = descriptor.getParameters();
+				    AtomTypeFactory factory = (AtomTypeFactory)parameters[4];
+	
+					IAtomType[] at = factory.getAllAtomTypes();
+			    	System.out.print("Sum\tAtomType\t");
+					for (int j=0; j < factory.getSize(); j++) {
+						System.out.print(j);
+						System.out.print(".");
+				    	System.out.print(at[j].getAtomTypeName());
+				    	System.out.print("\t");
+				    }			    
+				    System.out.println("");
+				    Assert.assertTrue(factory.getSize()>0);
+				    Assert.assertEquals(maxLevel * (factory.getSize()+1)+2,r.length());
+				}    
+			    System.out.println(r.toString());
+		    }
+		    
+    }
+
+
+
+}
+
+
--- ambit2-all/ambit2-model/src/test/java/ambit2/similarity/measure/test/AtomEnvironmentDistanceTest.java
+++ ambit2-all/ambit2-model/src/test/java/ambit2/similarity/measure/test/AtomEnvironmentDistanceTest.java
@@ -29,23 +29,44 @@
 
 package ambit2.similarity.measure.test;
 
+import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTable;
+
 import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.openscience.cdk.interfaces.IMolecule;
+import org.openscience.cdk.qsar.IMolecularDescriptor;
 import org.openscience.cdk.templates.MoleculeFactory;
 
 import ambit2.core.config.AmbitCONSTANTS;
 import ambit2.core.smiles.SmilesParserWrapper;
+import ambit2.descriptors.AtomEnvironment;
+import ambit2.descriptors.AtomEnvironmentDescriptor;
 import ambit2.descriptors.processors.AtomEnvironmentGenerator;
 import ambit2.descriptors.processors.AtomEnvironmentList;
+import ambit2.jchempaint.CompoundImageTools;
+import ambit2.model.AtomEnvironmentListTableModel;
+import ambit2.similarity.measure.AtomEnvironmentsDistance;
 
 
 /**
- * TODO add description
  * @author Nina Jeliazkova nina@acad.bg
  * <b>Modified</b> Mar 13, 2007
+ * <b>Modified</b> Oct 26, 2010
  */
 public class AtomEnvironmentDistanceTest {
 	AtomEnvironmentGenerator g;
@@ -57,6 +78,12 @@ public class AtomEnvironmentDistanceTest {
 
     }
 
+    /*
+     * Test how AE differ by adding two methyl groups
+     *
+     */
+   
+    
     protected AtomEnvironmentList getAE(AtomEnvironmentGenerator g,IMolecule mol) throws Exception {
     	g.process(mol);
         return(AtomEnvironmentList) mol.getProperty(AmbitCONSTANTS.AtomEnvironment);
@@ -78,7 +105,7 @@ public class AtomEnvironmentDistanceTest {
 	    	
 	    	
 	    	float f = ae1.hellinger(ae);
-	    	//System.out.println(f);
+	    	System.out.println(f);
 	    	Assert.assertEquals(f,ae.hellinger(ae1));
 	    	
 	    	Assert.assertTrue(f< 2.0);
@@ -89,6 +116,7 @@ public class AtomEnvironmentDistanceTest {
      /**
      * Test method for {@link ambit2.similarity.AtomEnvironmentHammingDistance#getDistance(ambit2.data.descriptors.AtomEnvironmentList, ambit2.data.descriptors.AtomEnvironmentList)}.
      */
+    @Test
     public void testGetDistance() throws Exception {
         
             SmilesParserWrapper p =  SmilesParserWrapper.getInstance();
@@ -98,9 +126,148 @@ public class AtomEnvironmentDistanceTest {
             AtomEnvironmentList ae = getAE(g,mol);
             Assert.assertEquals(2.0f,ae.hellinger(ae));
 
-        
-     
     }
+    @Test
+    public void testCH3difference() throws Exception {
+    	testCH3difference(false);
+    }
+    public void testCH3difference(boolean display) throws Exception {    	
+		IMolecularDescriptor descriptor = new AtomEnvironmentDescriptor();
+		int maxLevel = 3;
+		Object[] params = {null,new Integer(maxLevel),new Boolean(true),new Boolean(true)};
 
+		    SmilesParserWrapper sp = SmilesParserWrapper.getInstance();
+		    
+		    //IMolecule mol = sp.parseSmiles("O=N(=O)c1ccc(O)c(N)c1");
+		    //Amino-2nitro-3.4-5 hydroxy-methyl benzene (ANMB) 
+		    //IMolecule mol1 = sp.parseSmiles("Cc1c(C)c(cc(N)c1(O))N(=O)=O");
+		    
+		    IMolecule mol = sp.parseSmiles("Nc1c(C)cccc1");
+		    //Amino-2nitro-3.4-5 hydroxy-methyl benzene (ANMB) 
+		    IMolecule mol1 = sp.parseSmiles("Nc1ccccc1");
 
+		    
+		    //Amino-2nitro-3.4-5 hydroxy-methyl benzene (ANMB) 
+		    
+
+		    
+		    AtomEnvironmentGenerator g = new AtomEnvironmentGenerator();
+		    g.setNoDuplicates(false);
+		    g.setUseHydrogens(true);
+		    g.setMaxLevels(maxLevel);
+		   
+		    g.process(mol);
+		    g.process(mol1);
+		    
+		    AtomEnvironmentList ae = (AtomEnvironmentList) mol.getProperty(AmbitCONSTANTS.AtomEnvironment);
+		    AtomEnvironmentList ae1 = (AtomEnvironmentList) mol1.getProperty(AmbitCONSTANTS.AtomEnvironment);
+		    
+		    Collections.sort(ae);
+		    Collections.sort(ae1);
+		    
+		    AtomEnvironmentsDistance d = new AtomEnvironmentsDistance();
+		    System.out.println("distance " + d.getDistance(ae,ae1));
+		    
+		    
+		    Set<AtomEnvironment> set = new TreeSet<AtomEnvironment>();
+		    Set<AtomEnvironment> set1 = new TreeSet<AtomEnvironment>();
+		    
+		    for (int i=0; i < ae.size(); i++) set.add(ae.get(i));
+		    for (int i=0; i < ae1.size(); i++) set1.add(ae1.get(i));
+		    
+		    System.out.println(set);
+		    System.out.println();
+		    System.out.println(set1);
+		    
+		    Assert.assertNotSame(set,set1);
+		    Set intersection = (Set) ((TreeSet) set1).clone();
+		    intersection.retainAll(set);
+		    
+		    System.out.println("Intersection\t"+intersection.size());
+		    System.out.println(intersection);
+		    
+		    if (display)
+		    	displayAE(mol,ae,mol1,ae1);
+		    Assert.assertNotNull(ae);
+		    Assert.assertNotNull(ae1);
+
+    }
+    
+    public void  displayAE(IMolecule mol1, AtomEnvironmentList aelist1,IMolecule mol2, AtomEnvironmentList aelist2) {
+    	
+    	AEComparator aec = new AEComparator();
+    	Collections.sort(aelist1,aec);
+    	Collections.sort(aelist2,aec);
+    	final AtomEnvironmentListTableModel m1 = new AtomEnvironmentListTableModel(aelist1);
+    	final AtomEnvironmentListTableModel m2 = new AtomEnvironmentListTableModel(aelist2);
+    	
+    	CompoundImageTools tools = new CompoundImageTools();
+    	
+    	final JLabel p1 = new JLabel();
+    	p1.setIcon(new ImageIcon(tools.getImage(mol1)));
+    	p1.setPreferredSize(new Dimension(100,100));
+    	/*
+    	
+    	p1.r2dm.setColorAtomsByType(true);
+    	p1.r2dm.setHighlightedAtom(mol1.getAtom(0));
+    	p1.r2dm.setShowAromaticity(true);
+    	p1.setPreferredSize(new Dimension(100,100));
+
+    	*/
+
+    	final JLabel p2 = new JLabel();
+    	p2.setIcon(new ImageIcon(tools.getImage(mol2)));
+    	p2.setPreferredSize(new Dimension(100,100));
+    	
+    	final JTable t1 = new JTable(m1);
+    	t1.addMouseListener(new MouseAdapter() {
+    		@Override
+    		public void mouseClicked(MouseEvent e) {
+    			int row = t1.rowAtPoint(e.getPoint());
+    			int natom = ((Integer)m1.getValueAt(row, 0)).intValue()-1;
+    			    		
+    			/*
+    			p1.setHighlightedAtoms(new int[] {
+    					natom }
+    					);
+    			p1.repaint();
+    			*/
+    		};
+    		
+    	});
+    	JTable t2 = new JTable(m2);
+    	
+    	JOptionPane.showMessageDialog(null,
+    			new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+    					
+    			new JSplitPane(JSplitPane.VERTICAL_SPLIT,
+    					p1,
+    					new JScrollPane(t1)
+    			),
+    			new JSplitPane(JSplitPane.VERTICAL_SPLIT,
+    					p2,
+    					new JScrollPane(t2,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS)
+    			)
+    			));
+    }
+    
+    class AEComparator implements Comparator<AtomEnvironment> {
+
+    	public int compare(AtomEnvironment o1, AtomEnvironment o2) {
+    		int r = o1.getAtomno() - o2.getAtomno();
+    		if (r == 0) return o1.getSublevel() - o2.getSublevel();
+    		else return r;
+    	}
+
+    	
+    }
+    
+    public static void main(String[] args) {
+    	AtomEnvironmentDistanceTest test = new AtomEnvironmentDistanceTest();
+    	try {
+    		test.testCH3difference(true);
+    	} catch (Exception x) {
+    		x.printStackTrace();
+    	}
+    }
 }
