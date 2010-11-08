e0bc777f6bd7548cd2cb548caf4ed8427eeaeae1
Nikolay Kochev <nick@uni-plovdiv.net>
2010-11-08 11:54:59 +0000

    small modification in RuleParser: String.split() function is used.

--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/PredefinedKnowledgeBase.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/PredefinedKnowledgeBase.java
@@ -10,8 +10,8 @@ public class PredefinedKnowledgeBase
 	
 	public static final String rules[] = 
 	{
-		"$$NAME=keto/enol    $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= O=CC,OC=C  $$GROUP_POS=3,1   $$INFO=",
-		"$$NAME=amide/imid   $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= O=CN,OC=N  $$GROUP_POS=3,1   $$INFO=",
+		"$$NAME=keto/enol    $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= O=CC OC=C  $$GROUP_POS=3,1   $$INFO=",
+		"$$NAME=amide/imid   $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= O=CN OC=N  $$GROUP_POS=3,1   $$INFO=",
 		
 	};
 }
