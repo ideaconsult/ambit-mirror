804ecfc42dceb6ae99555ceccd05a38f9fb6a8cb
Nikolay Kochev <nick@uni-plovdiv.net>
2010-12-02 14:53:29 +0000

    Implemented some functionality for handling of overlapped Rule Instances.

--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/RuleManager.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/RuleManager.java
@@ -1,17 +1,73 @@
 package ambit2.tautomers;
 
+import java.util.Vector;
+
 public class RuleManager 
 {
-	TautomerManager man;	
+	TautomerManager tman;
+	Vector<IRuleInstance> extendedRuleInstances;
+	Vector<IRuleInstance> ruleInstances;
+	Vector<Rule> generatedRules;
+	Vector<IRuleInstance> unprocessedInstances = new Vector<IRuleInstance>();
+		
 	
-	public RuleManager(TautomerManager tman)
+	public RuleManager(TautomerManager man)
 	{
-		man = tman;
+		tman = man;
+		extendedRuleInstances = tman.extendedRuleInstances;
+		ruleInstances = tman.ruleInstances;
+		generatedRules = tman.generatedRules;
 	}
 	
 	public int handleOverlappingRuleInstances()
 	{
+		unprocessedInstances.clear();
+		unprocessedInstances.addAll(extendedRuleInstances);
+				
+		while (unprocessedInstances.size() > 1)
+		{
+			IRuleInstance r = unprocessedInstances.lastElement();
+			unprocessedInstances.remove(unprocessedInstances.size()-1);
+			
+			int k = 0;
+			while (k < unprocessedInstances.size())
+			{
+				IRuleInstance r1 = unprocessedInstances.get(k);
+				if (overlapInstances(r, r1))
+				{
+					r = combineRuleInstances(r,r1);
+					unprocessedInstances.remove(r1);
+					k=0;
+					continue;
+				}
+				k++;
+			}
+			
+			ruleInstances.add(r);
+			
+		}
+		
+		//Adding the last one 
+		if (unprocessedInstances.size() == 1)
+			ruleInstances.add(unprocessedInstances.get(0));
+		
 		return(0);
 	}
+	
+	
+	boolean overlapInstances(IRuleInstance r1, IRuleInstance r2)
+	{
+		//TODO
+		return(false);
+	}
+	
+	IRuleInstance combineRuleInstances(IRuleInstance r1, IRuleInstance r2)
+	{
+		//TODO
+		return(null);
+	}
+	
+	
+	
 	
 }
