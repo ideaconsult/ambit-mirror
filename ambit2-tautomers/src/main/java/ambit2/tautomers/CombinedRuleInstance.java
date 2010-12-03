1d40daefe25c4c6a8b9ff005918767433022f9e9
Nikolay Kochev <nick@uni-plovdiv.net>
2010-12-03 15:03:10 +0000

    Implemented some functionality in CombinedRuleInstance.

--- ambit2-tautomers/src/main/java/ambit2/tautomers/CombinedRuleInstance.java
+++ ambit2-tautomers/src/main/java/ambit2/tautomers/CombinedRuleInstance.java
@@ -1,23 +1,54 @@
 package ambit2.tautomers;
 
+import java.util.Vector;
+import org.openscience.cdk.interfaces.IAtomContainer;
+
 public class CombinedRuleInstance implements IRuleInstance
-{
+{	
+	IAtomContainer molecule;
+	int foundState = 0;
+	int curState = 0;  
+	int beginState = 0;
+	
+	Vector<RuleInstance> instances = new Vector<RuleInstance>(); 
+	Vector<int[]> combinedStates = new Vector<int[]>(); 
+		
+	
 	public int firstState()
 	{	
+		gotoState(0);
 		return(0);
 	}
 	
 	public int nextState()
 	{
-		return(0);
+		int nextState = curState + 1;
+		if (nextState == combinedStates.size())
+			nextState = 0;
+		
+		gotoState(nextState);
+	
+		return(nextState);
 	}
 	
 	public int getNumberOfStates()
 	{
-		return(0);
+		return(combinedStates.size());
 	}
 	public int getCurrentState()
 	{
-		return(0);
+		return(curState);
 	}
+	
+	int gotoState(int state)
+	{
+		if (curState == state)
+			return(state); //It is already at this state
+		
+		
+		//TODO
+		
+		return(0);
+	}	
+	
 }
