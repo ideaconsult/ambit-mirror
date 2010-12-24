56c0bf0ebe5e5e8f8ca80a3f03a33436428e0218
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-12-24 13:26:14 +0000

    customized authorizers

---
+++ ambit2-www/src/main/java/ambit2/rest/aa/opensso/OpenSSOVerifierSetUser.java
@@ -0,0 +1,30 @@
+package ambit2.rest.aa.opensso;
+
+import java.util.Hashtable;
+
+import org.opentox.aa.opensso.OpenSSOToken;
+import org.restlet.Request;
+import org.restlet.security.User;
+
+public class OpenSSOVerifierSetUser extends OpenSSOVerifier {
+	
+	public OpenSSOVerifierSetUser(boolean enabled) {
+		super(enabled);
+	}
+	public OpenSSOVerifierSetUser() {
+		super();
+	}
+	@Override
+	protected User createUser(OpenSSOToken ssoToken, Request request)
+			throws Exception {
+
+		User user = super.createUser(ssoToken, request);
+		Hashtable<String,String> results = new Hashtable<String, String>();
+		
+		try {
+			ssoToken.getAttributes(new String[] {"uid"}, results);
+			request.getClientInfo().getUser().setIdentifier(results.get("uid"));} 
+		catch (Exception x) {}
+		return user;
+	}
+}
