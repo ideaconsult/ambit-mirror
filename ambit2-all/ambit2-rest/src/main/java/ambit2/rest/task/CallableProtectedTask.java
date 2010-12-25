8450865cba16b8e4e9db4c16d8c0883a2fb012f5
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2010-12-25 18:36:34 +0000

    all tasks descend from CallableProtectedTask

---
+++ ambit2-all/ambit2-rest/src/main/java/ambit2/rest/task/CallableProtectedTask.java
@@ -0,0 +1,47 @@
+package ambit2.rest.task;
+
+import java.util.UUID;
+
+import org.restlet.data.Reference;
+
+import ambit2.rest.task.dsl.ClientResourceWrapper;
+import ambit2.rest.task.dsl.IAuthToken;
+
+public abstract class CallableProtectedTask<USERID> implements ICallableTask, IAuthToken {
+
+	protected UUID uuid;
+	private USERID token;
+	
+	public CallableProtectedTask(USERID token) {
+		this.token = token;
+	}
+	
+	@Override
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	@Override
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
+
+	}
+
+	@Override
+	public String getToken() {
+		return token==null?null:token.toString();
+	}
+	public abstract Reference doCall() throws Exception;
+	@Override
+	public Reference call() throws Exception {
+		try {
+			ClientResourceWrapper.setTokenFactory(this);
+			return doCall();
+		} catch (Exception x) {
+			throw x;
+		} finally {
+			ClientResourceWrapper.setTokenFactory(null);
+		}
+	}
+
+}
