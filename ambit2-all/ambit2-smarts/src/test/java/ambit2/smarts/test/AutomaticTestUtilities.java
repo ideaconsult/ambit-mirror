b53a9024b3d7d437a8a6750b0233633a1c834de8
Nikolay Kochev <nick@uni-plovdiv.net>
2011-01-19 14:40:30 +0000

    added utility for iteration of the input file.

--- ambit2-all/ambit2-smarts/src/test/java/ambit2/smarts/test/AutomaticTestUtilities.java
+++ ambit2-all/ambit2-smarts/src/test/java/ambit2/smarts/test/AutomaticTestUtilities.java
@@ -1,5 +1,7 @@
 package ambit2.smarts.test;
 
+import java.io.File;
+import java.io.RandomAccessFile;
 import java.util.Vector;
 
 import ambit2.smarts.ChemObjectFactory;
@@ -27,16 +29,17 @@ public class AutomaticTestUtilities
 	int nDBStr = 1000;
 	int nInputStr = 0;
 	int nOutputStr = 0;
+	int portionSize = 100;
 	
 	
 	
 	public static void main(String[] args)
 	{
 		AutomaticTestUtilities atu = new AutomaticTestUtilities();
-		//atu.handleArguments(args);		
+		atu.handleArguments(args);		
 		
-		atu.handleArguments(new String[] {"-db","/einecs_structures_V13Apr07.sdf", "-o","/test-out2.txt", 
-				"-nDBStr", "100", "-maxSeqStep", "10", "-c", "exhaustive-str" });
+		//atu.handleArguments(new String[] {"-db","/einecs_structures_V13Apr07.sdf", "-o","/test-out2.txt", 
+		//		"-nDBStr", "100", "-maxSeqStep", "10", "-c", "exhaustive-str" });
 		
 		//atu.produceRandomStructures();
 	}
@@ -212,15 +215,18 @@ public class AutomaticTestUtilities
 		System.out.println("-i            input file");
 		System.out.println("-o            output file");
 		System.out.println("-db           db file");
-		System.out.println("-c            command: ");
-		System.out.println("                 random-str - generates random structures");
-		System.out.println("                 exhaustive-str - generates structures exhaustively");
 		System.out.println("-cfg          config file");
 		System.out.println("-nDBStr       the number of used structures from DB");
 		System.out.println("-nInpStr      the number of used input structures");
 		System.out.println("-nOutStr      the number of used output structures");	
 		System.out.println("-maxSeqStep   maximal number of sequence steps");
 		System.out.println("-maxStrSize   maximal size of the generated structures");
+		System.out.println("-c            command: ");
+		System.out.println("                 random-str       generates random structures");
+		System.out.println("                 exhaustive-str   generates structures exhaustively");
+		System.out.println("                 sss-ambit        substructure searching with Ambit Algorithm");
+		System.out.println("                 sss-cdk          substructure searching with CDK Parser and Algorithm");
+		System.out.println("                 sss-ambit-cdk    substructure searching with Ambit Parser and CDK Algorithm");
 	}
 	
 	
@@ -285,6 +291,71 @@ public class AutomaticTestUtilities
 		}
 		
 		return null;
+	}
+	
+	
+	//Substructure search comparison and statistics utilities
+	
+	void iterateInputFileQueries()
+	{
+		try
+		{	
+			File file = new File(inFile);
+			RandomAccessFile f = new RandomAccessFile(file,"r");			
+			long length = f.length();
+			
+			Vector<String> lines = new Vector<String>();
+			Vector<String> allSmiles = new Vector<String>();
+			Vector<Integer> allStat = new Vector<Integer>();
+			int n = 0;
+			while (f.getFilePointer() < length)
+			{	
+				n++;
+				//System.out.print(" " + n);
+				String line = f.readLine();
+				lines.add(line);
+				if (lines.size() % portionSize == 0)
+				{
+					
+					System.out.println("line " + n);
+					/*
+					Vector<Integer> stat = doStatisticsForStructs(smiles, mdlFile, nUsedStr);
+					for (int i = 0; i < smiles.size(); i++)
+					{
+						allSmiles.add(smiles.get(i));
+						allStat.add(stat.get(i));
+					}
+					smiles.clear();
+					saveStructStatistics(allSmiles, allStat, outFile);
+					*/
+				}
+			}
+			
+			/*
+			//What left from smiles vector is processed as well
+			if (!smiles.isEmpty())
+			{	
+				Vector<Integer> stat = doStatisticsForStructs(smiles, mdlFile, nUsedStr);
+				for (int i = 0; i < smiles.size(); i++)
+				{
+					allSmiles.add(smiles.get(i));
+					allStat.add(stat.get(i));
+				}			
+				saveStructStatistics(allSmiles, allStat, outFile);
+			}
+			*/
+			
+			f.close();
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.toString());
+		}
+	}
+	
+	int doSSS_Ambit()
+	{
+		return(0);
 	}
 	
 	
