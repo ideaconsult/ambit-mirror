9ad3f8ad05e4a586e04da0596195d076ccbee74a
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2011-02-20 15:38:21 +0000

    pom file to deploy all individual cdk jars into maven repository jars are not built, but expected in /dist subfolder of each module, named as cdk-{module}-{version}.jar e.g. cdk-core-1.3.8.jar

---
+++ cdk-maven/cdk-qsarcml/src/main/java/org/cdk/qsarcml/App.java
@@ -0,0 +1,13 @@
+package org.cdk.qsarcml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
