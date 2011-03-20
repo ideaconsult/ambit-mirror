c7c3cf8b838b532deaad536757ad035f3a36133e
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2011-03-20 10:15:24 +0000

    support for group reports (e.g. number of compounds with value=<> , property <> and dataset <> /query/ncompound_value?dataset_uri=..&feature_uris[]=.. imports cleanup

---
+++ ambit2-all/ambit2-db/src/main/java/ambit2/db/facets/propertyvalue/PropertyDatasetFacet.java
@@ -0,0 +1,40 @@
+package ambit2.db.facets.propertyvalue;
+
+import ambit2.base.facet.AbstractFacet;
+
+public class PropertyDatasetFacet<PROPERTY,DATASET> extends AbstractFacet<String> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2260081381044742166L;
+	protected PROPERTY property;
+	protected DATASET dataset;
+	
+
+	public PropertyDatasetFacet() {
+		super();
+	}
+	
+	public PROPERTY getProperty() {
+		return property;
+	}
+
+	public void setProperty(PROPERTY property) {
+		this.property = property;
+	}
+
+
+	
+	public DATASET getDataset() {
+		return dataset;
+	}
+
+	public void setDataset(DATASET dataset) {
+		this.dataset = dataset;
+	}
+	@Override
+	public String toString() {
+		return String.format("%s (%d)",getValue(),getCount());
+	}
+
+}
---
+++ ambit2-all/ambit2-db/src/test/java/ambit2/db/facet/test/PropertyDatasetFacetQueryTest.java
@@ -0,0 +1,59 @@
+package ambit2.db.facet.test;
+
+import java.sql.ResultSet;
+import java.util.Hashtable;
+
+import junit.framework.Assert;
+import ambit2.base.data.Property;
+import ambit2.base.data.SourceDataset;
+import ambit2.base.facet.IFacet;
+import ambit2.db.facets.propertyvalue.PropertyDatasetFacetQuery;
+import ambit2.db.readers.IQueryRetrieval;
+import ambit2.db.search.test.QueryTest;
+
+/**
+ * Test for {@link PropertyDatasetFacetQuery}
+ * @author nina
+ *
+ */
+public class PropertyDatasetFacetQueryTest extends QueryTest<IQueryRetrieval<IFacet<String>>> {
+	
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		setDbFile("src/test/resources/ambit2/db/processors/test/query-datasets-string.xml");
+	}
+	
+	@Override
+	protected IQueryRetrieval<IFacet<String>> createQuery() throws Exception {
+		PropertyDatasetFacetQuery q = new PropertyDatasetFacetQuery();
+		Property p = new Property("");
+		p.setId(48);
+		q.setFieldname(p);
+		SourceDataset d = new SourceDataset();
+		d.setId(1);
+		q.setValue(d);
+		return q;
+	}
+	/**
+	 */
+	@Override
+	protected void verify(IQueryRetrieval<IFacet<String>> query, ResultSet rs)
+			throws Exception {
+		Hashtable<String, Integer> expected = new Hashtable<String, Integer>();
+		expected.put("Extreme", 1);
+		expected.put("Moderate", 6);
+		expected.put("Non", 2);
+		expected.put("Strong", 1);
+		expected.put("Weak", 5);
+		int count = 0;
+		while (rs.next()) {
+			IFacet<String> record = query.getObject(rs);
+			Assert.assertEquals(expected.get(record.getValue()).intValue(),record.getCount());
+			count++;
+		}
+		Assert.assertEquals(5,count);
+		
+	}
+
+}
