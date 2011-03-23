d43cae099e5ed03108f2be5b37b515fe7b0057ab
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2011-03-23 11:20:15 +0000

    /dataset/id?property={featureuri}&search=value now works with numbers

---
+++ ambit2-all/ambit2-db/src/main/java/ambit2/db/update/dataset/DatasetQueryFieldGeneric.java
@@ -0,0 +1,49 @@
+package ambit2.db.update.dataset;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ambit2.base.data.ISourceDataset;
+import ambit2.base.data.SourceDataset;
+import ambit2.base.exceptions.AmbitException;
+import ambit2.db.readers.PropertyValue;
+import ambit2.db.search.IQueryCondition;
+import ambit2.db.search.QueryParam;
+import ambit2.db.search.structure.AbstractStructureQuery;
+
+public abstract class DatasetQueryFieldGeneric<T, C extends IQueryCondition> extends AbstractStructureQuery<ISourceDataset, PropertyValue<T>, IQueryCondition> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4878852850705024770L;
+
+	@Override
+	public List<QueryParam> getParameters() throws AmbitException {
+		List<QueryParam> params = new ArrayList<QueryParam>();
+		params.add(new QueryParam<Integer>(Integer.class, getId()));
+		
+		if ((getFieldname()==null) || getFieldname().getID()<=0) throw new AmbitException("No dataset!");
+		params.add(new QueryParam<Integer>(Integer.class, getFieldname().getID()));
+		
+		if (getValue()==null) throw new AmbitException("No feature!");	
+		
+		if (getValue().getProperty()==null || getValue().getProperty().getId()<=0) throw new AmbitException("No feature!");		
+		params.add(new QueryParam<Integer>(Integer.class, getValue().getProperty().getId()));
+		
+		params.add(createSearchValueParam());
+		return params;
+	}
+	protected abstract QueryParam<T> createSearchValueParam();
+	protected abstract String getSqlDataset();
+	protected abstract String getSqlRDataset();
+	@Override
+	public String getSQL() throws AmbitException {
+		if ((getFieldname()==null) || getFieldname().getID()<=0) throw new AmbitException("No dataset!");
+		if (getFieldname() instanceof SourceDataset)
+			return getSqlDataset();
+		else
+			return getSqlRDataset();
+	}
+
+}
--- ambit2-all/ambit2-db/src/main/java/ambit2/db/update/dataset/DatasetQueryFieldNumeric.java
+++ ambit2-all/ambit2-db/src/main/java/ambit2/db/update/dataset/DatasetQueryFieldNumeric.java
@@ -1,23 +1,23 @@
 package ambit2.db.update.dataset;
 
+import ambit2.db.search.NumberCondition;
 import ambit2.db.search.QueryParam;
-import ambit2.db.search.StringCondition;
 
-public class DatasetQueryFieldString extends DatasetQueryFieldGeneric<String,StringCondition> {
+public class DatasetQueryFieldNumeric extends DatasetQueryFieldGeneric<Double,NumberCondition> {
 
 	public final static String sqlField = 
 		"select ? as idquery,s1.idchemical,s1.idstructure,if(s1.type_structure='NA',0,1) as selected,s1.preference as metric,null as text\n" +	
 		"FROM structure s1\n"+
 		
 		"join struc_dataset using(idstructure) join property_values using(idstructure)\n"+
-		"join property_string using(idvalue_string) where\n"+
-		" id_srcdataset = ? and idproperty = ? and value = ?";
+		"where\n"+
+		" id_srcdataset = ? and idproperty = ? and value_num = ?";
 
 	public final static String sqlField_Rdatasets = 
 		"select ? as idquery,s1.idchemical,s1.idstructure,if(s1.type_structure='NA',0,1) as selected,s1.preference as metric,null as text\n" +	
 		"FROM structure s1\n"+
 		"join query_results using(idstructure) join property_values using(idstructure)\n"+
-		"join property_string using(idvalue_string) where\n"+
+		"where\n"+
 		"idquery = ? and idproperty = ? and value = ?";	
 	/**
 	 * 
@@ -25,8 +25,8 @@ public class DatasetQueryFieldString extends DatasetQueryFieldGeneric<String,Str
 	private static final long serialVersionUID = 4878852850705024770L;
 
 	@Override
-	protected QueryParam<String> createSearchValueParam() {
-		return new QueryParam<String>(String.class, getValue().getValue());
+	protected QueryParam<Double> createSearchValueParam() {
+		return new QueryParam<Double>(Double.class, getValue().getValue().doubleValue());
 	}
 	@Override
 	protected String getSqlDataset() {
@@ -36,7 +36,4 @@ public class DatasetQueryFieldString extends DatasetQueryFieldGeneric<String,Str
 	protected String getSqlRDataset() {
 		return sqlField_Rdatasets;
 	}
-
-	
-
 }
