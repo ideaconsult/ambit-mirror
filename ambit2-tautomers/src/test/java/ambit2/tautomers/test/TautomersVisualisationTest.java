9fa0d6e6eb29edb98b208c26419567be72896b06
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2011-04-02 19:32:40 +0000

    added ambit-ui dependency with scope "test" and demo test class to visualize a molecule

---
+++ ambit2-tautomers/src/test/java/ambit2/tautomers/test/TautomersVisualisationTest.java
@@ -0,0 +1,26 @@
+package ambit2.tautomers.test;
+
+import javax.swing.JOptionPane;
+
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.templates.MoleculeFactory;
+
+import ambit2.ui.editors.Panel2D;
+
+public class TautomersVisualisationTest {
+
+	public void show(IAtomContainer mol) throws Exception {
+		Panel2D p = new Panel2D();
+		p.setAtomContainer(mol);
+		JOptionPane.showMessageDialog(null,p);
+		
+	}
+	public static void main(String[] args) {
+		TautomersVisualisationTest test = new TautomersVisualisationTest();
+		try {
+			test.show(MoleculeFactory.make123Triazole());
+		} catch (Exception x) {
+			x.printStackTrace();
+		}
+	}
+}
