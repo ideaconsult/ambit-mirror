cc008c6e8c3da4cdf4c517d5df6f4fe76f30ef7d
Nikolay Kochev <nick@uni-plovdiv.net>
2011-04-02 15:30:01 +0000

    Added new rules in the predefined knowledge base.

--- ambit2-tautomers/src/main/java/ambit2/tautomers/PredefinedKnowledgeBase.java
+++ ambit2-tautomers/src/main/java/ambit2/tautomers/PredefinedKnowledgeBase.java
@@ -10,8 +10,17 @@ public class PredefinedKnowledgeBase
 	
 	public static final String rules[] = 
 	{
-		"$$NAME=keto/enol    $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= O=CC OC=C  $$GROUP_POS=3,1   $$INFO=",
-		"$$NAME=amide/imid   $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= O=CN OC=N  $$GROUP_POS=3,1   $$INFO=",
-		
+		"$$NAME=keto/enol              $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= O=CC OC=C  $$GROUP_POS=3,1   $$INFO=",
+		"$$NAME=amide/imid             $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= O=CN OC=N  $$GROUP_POS=3,1   $$INFO=",
+		"$$NAME=nitroso/oxime          $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= O=NC ON=C  $$GROUP_POS=3,1   $$INFO=",
+		"$$NAME=azo/hydrazone          $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= N=NC NN=C  $$GROUP_POS=3,1   $$INFO=",
+		"$$NAME=thioketo/thioenol      $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= S=CC SC=C  $$GROUP_POS=3,1   $$INFO=",
+		"$$NAME=thionitroso/thiooxime  $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= S=NC SN=C  $$GROUP_POS=3,1   $$INFO=",
+		"$$NAME=amidine/imidine        $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= N=CN NC=N  $$GROUP_POS=3,1   $$INFO=",
+		"$$NAME=diazoamino/diazoamino  $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= N=NN NN=N  $$GROUP_POS=3,1   $$INFO=",
+		"$$NAME=thioamide/iminothiol   $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= S=CN SC=N  $$GROUP_POS=3,1   $$INFO=",
+		"$$NAME=nitrosamine/diazohydroxide  $$TYPE=MOBILE_GROUP $$GROUP=H   $$STATES= O=NN ON=N  $$GROUP_POS=3,1   $$INFO="
+			
+			
 	};
 }
