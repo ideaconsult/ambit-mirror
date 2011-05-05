23c6d34d500ebbd1bb4f2672352df0de0b1c68ba
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2011-05-05 17:48:39 +0000

    Upgraded to OPSIN 1.0.0 Now one can use name2smiles instead  of CML

--- ambit2-all/ambit2-namestructure/src/main/java/ambit2/namestructure/Name2StructureFinder.java
+++ ambit2-all/ambit2-namestructure/src/main/java/ambit2/namestructure/Name2StructureFinder.java
@@ -1,11 +1,5 @@
 package ambit2.namestructure;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-
-import nu.xom.Serializer;
 import uk.ac.cam.ch.wwmm.opsin.OpsinResult;
 import uk.ac.cam.ch.wwmm.opsin.OpsinResult.OPSIN_RESULT_STATUS;
 import ambit2.base.data.StructureRecord;
@@ -28,37 +22,12 @@ public class Name2StructureFinder extends AbstractFinder<Name2StructureProcessor
 
 	@Override
 	protected IStructureRecord query(String value) throws AmbitException {
-		OpsinResult result = request.name2structure(value);
+		String smiles = request.name2smiles(value);
 		record.clear();
-		if (!result.getStatus().equals(OPSIN_RESULT_STATUS.SUCCESS)) return record;
-		
-		   OutputStream out = new OutputStream() {
-			   private StringBuilder string = new StringBuilder();
-		        @Override
-		        public void write(int b) throws IOException {
-		            this.string.append((char) b );
-		        }
-		        public String toString(){
-		            return this.string.toString();
-		        }
-
-		   };
-		try {
-
-		       //Serializer serializer = 
-			   new Serializer(System.out, "ISO-8859-1");
-		       Serializer serializer = 
-			   new Serializer(out,"ISO-8859-1");
-		       serializer.setIndent(5);
-		       serializer.setMaxLength(100);
-		       serializer.write(result.getCml().getDocument());  
-		       } catch (IOException e) {
-		       } finally {
-		    	   try {out.close();} catch (Exception x) {}
-		       }	   
-		
-		record.setContent(out.toString());
-		record.setFormat("CML");
+		if (smiles==null) 
+			return null;
+		record.setContent(smiles);
+		record.setFormat("SMILES");
 		return record;
 	}
 
--- ambit2-all/ambit2-namestructure/src/test/java/ambit2/namestructure/test/Name2StructureProcessorTest.java
+++ ambit2-all/ambit2-namestructure/src/test/java/ambit2/namestructure/test/Name2StructureProcessorTest.java
@@ -1,6 +1,11 @@
 package ambit2.namestructure.test;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 import junit.framework.Assert;
+
+import nu.xom.Serializer;
 
 import org.junit.After;
 import org.junit.Before;
@@ -9,6 +14,12 @@ import org.openscience.cdk.interfaces.IMolecule;
 import org.openscience.cdk.isomorphism.UniversalIsomorphismTester;
 import org.openscience.cdk.templates.MoleculeFactory;
 
+import uk.ac.cam.ch.wwmm.opsin.NameToInchi;
+import uk.ac.cam.ch.wwmm.opsin.NameToStructure;
+import uk.ac.cam.ch.wwmm.opsin.OpsinResult;
+import uk.ac.cam.ch.wwmm.opsin.OpsinResult.OPSIN_RESULT_STATUS;
+import ambit2.base.exceptions.AmbitException;
+import ambit2.base.interfaces.IStructureRecord;
 import ambit2.core.processors.structure.HydrogenAdderProcessor;
 import ambit2.namestructure.Name2StructureProcessor;
 
@@ -30,4 +41,24 @@ public class Name2StructureProcessorTest {
 		HydrogenAdderProcessor hp = new HydrogenAdderProcessor();
 		Assert.assertTrue(UniversalIsomorphismTester.isIsomorph(hp.process(benzene),mol));
 	}
+	
+	
+	@Test
+	public void testSMILES() throws Exception {
+		 NameToStructure nts = NameToStructure.getInstance();
+		 String smiles = nts.parseToSmiles("benzene");
+		 Assert.assertEquals("C1=CC=CC=C1",smiles);
+	}	
+	@Test
+	public void testCML() throws Exception {
+		Name2StructureProcessor p = new Name2StructureProcessor();
+	
+		OpsinResult result = p.name2structure("benzene");
+
+		Assert.assertEquals(OPSIN_RESULT_STATUS.SUCCESS,result.getStatus()) ;
+		
+		System.out.println(result.getCml().toXML());
+
+	}
+	
 }
