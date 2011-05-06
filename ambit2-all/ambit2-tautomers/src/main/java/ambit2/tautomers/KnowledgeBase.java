ed13a1c2590d3ad47f3d61538beb7b0b325323ed
Nikolay Kochev <nick@uni-plovdiv.net>
2011-05-06 17:04:21 +0000

    Added new class Filter and implemented utilities for filtering.

--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/KnowledgeBase.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/KnowledgeBase.java
@@ -5,6 +5,8 @@ import java.util.Vector;
 public class KnowledgeBase 
 {
 	public Vector<Rule> rules = new Vector<Rule>();
+	public Vector<Filter> warningFilters = new Vector<Filter>();
+	public Vector<Filter> excludeFilters = new Vector<Filter>();
 	
 	Vector<String> errors = new Vector<String>(); 
 	RuleParser ruleParser = new RuleParser();
@@ -21,12 +23,20 @@ public class KnowledgeBase
 		for (int i = 0; i < PredefinedKnowledgeBase.rules.length; i++)
 			addRule(PredefinedKnowledgeBase.rules[i]);
 		
+		for (int i = 0; i < PredefinedKnowledgeBase.warningFragments.length; i++)
+			addWarningRule(PredefinedKnowledgeBase.warningFragments[i]);
+		
+		for (int i = 0; i < PredefinedKnowledgeBase.excludeFragments.length; i++)
+			addExcludeRule(PredefinedKnowledgeBase.excludeFragments[i]);
+		
 		if (errors.size() > 0)
 		{
 			System.out.println("There are errors in the knowledge base:");
 			for (int i = 0; i < errors.size(); i++)
 				System.out.println("Rule " + (i+1) + ":  " + errors.get(i));
 		}
+		
+		
 	}
 	
 	
@@ -40,6 +50,17 @@ public class KnowledgeBase
 			rules.add(rule);
 			//System.out.println(rule.toString());
 		}
+	}
+	
+	public void addWarningRule(String wRule)
+	{
+		warningFilters.add(new Filter());
+		//TODO
+	}
+	
+	public void addExcludeRule(String wRule)
+	{
+		//TODO
 	}
 	
 	public String getAllErrors()
