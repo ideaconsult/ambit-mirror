852328f00519c0adc9fdec1b844d700aac68a126
Nikolay Kochev <nick@uni-plovdiv.net>
2011-05-10 12:07:59 +0000

    Added basic classes for SMIRKS support.

---
+++ ambit2-smarts/src/main/java/ambit2/smarts/SMIRKSManager.java
@@ -0,0 +1,26 @@
+package ambit2.smarts;
+
+public class SMIRKSManager 
+{
+	public SMIRKSManager()
+	{	
+	}
+	
+	public SMIRKSReaction parse(String sm)
+	{
+		SMIRKSReaction reaction = new SMIRKSReaction();
+		
+		
+		//Separate the components of the SMIRKS string
+		//TODO
+		
+		//Parse the components
+		//TODO
+		
+		//Check the components
+		//TODO
+		
+		
+		return null;
+	}
+}
