a62c403135cccfa6806411cca06001da258f3123
Nikolay Kochev <nick@uni-plovdiv.net>
2011-05-10 12:07:59 +0000

    Added basic classes for SMIRKS support.

---
+++ ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/SMIRKSReaction.java
@@ -0,0 +1,13 @@
+package ambit2.smarts;
+
+import org.openscience.cdk.isomorphism.matchers.QueryAtomContainer;
+import java.util.Vector;
+
+public class SMIRKSReaction 
+{
+	public Vector<QueryAtomContainer> reactants = new Vector<QueryAtomContainer>();
+	public Vector<QueryAtomContainer> agents = new Vector<QueryAtomContainer>();
+	public Vector<QueryAtomContainer> products = new Vector<QueryAtomContainer>();
+	 
+	
+}
---
+++ ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/SMIRKSTransform.java
@@ -0,0 +1,15 @@
+package ambit2.smarts;
+
+public class SMIRKSTransform 
+{
+	public void setSMIRKSReaction()
+	{
+		
+	}
+	
+	public void applyTransform()
+	{
+		
+	}
+	
+}
