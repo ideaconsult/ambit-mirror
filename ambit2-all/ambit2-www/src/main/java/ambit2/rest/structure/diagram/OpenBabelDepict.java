7bfb7fef507326fabe6abc0324089a7092ca324d
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2011-07-28 21:12:07 +0000

    Optionally set obabel home via maven property <OBABEL_HOME>

--- ambit2-all/ambit2-www/src/main/java/ambit2/rest/structure/diagram/OpenBabelDepict.java
+++ ambit2-all/ambit2-www/src/main/java/ambit2/rest/structure/diagram/OpenBabelDepict.java
@@ -1,6 +1,8 @@
 package ambit2.rest.structure.diagram;
 
 import java.awt.image.BufferedImage;
+import java.io.InputStream;
+import java.util.Properties;
 
 import org.restlet.data.MediaType;
 import org.restlet.data.Reference;
@@ -10,7 +12,8 @@ import org.restlet.resource.ResourceException;
 import ambit2.core.smiles.OpenBabelDepiction;
 
 public class OpenBabelDepict extends AbstractDepict {
-
+	protected static String obabel_home;
+	
 	@Override
 	protected void doInit() throws ResourceException {
 		super.doInit();
@@ -21,7 +24,11 @@ public class OpenBabelDepict extends AbstractDepict {
 	@Override
 	protected BufferedImage getImage(String smiles,int w, int h) throws ResourceException {
 		try {
-			OpenBabelDepiction ob = new OpenBabelDepiction();
+			OpenBabelDepiction ob = new OpenBabelDepiction() {
+				protected String getOBabelHome() {
+					return getOpenBabelHome();
+				};
+			};
 			ob.setSize(w);
 			ob.process(smiles);
 			return ob.getImage();
@@ -32,4 +39,17 @@ public class OpenBabelDepict extends AbstractDepict {
 	protected String getTitle(Reference ref, String smiles) {
 		return String.format("SMILES: %s<br><img src='%s' alt='%s' title='%s'>", smiles,ref,smiles,smiles);
 	}
+	public synchronized String getOpenBabelHome()  {
+		if (obabel_home==null)
+		try {
+			Properties properties = new Properties();
+			InputStream in = OpenBabelDepict.class.getClassLoader().getResourceAsStream("ambit2/rest/config/ambit2.pref");
+			properties.load(in);
+			in.close();	
+			obabel_home = properties.getProperty(OpenBabelDepiction.OBABEL_HOME);
+		} catch (Exception x) {
+			return null;
+		}
+		 return obabel_home;
+	}	
 }
