7cf5a0e1c2baed178a701405c1046ed90bdae93f
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2011-07-28 18:11:58 +0000

    OpenBabel shell now depends on OBABEL_HOME environment variable, instead of distributing obabel binary files Tested with OpenBabel 2.3.0

---
+++ ambit2-all/ambit2-core/src/test/java/ambit2/core/obabel/test/TestOpenBabelShell.java
@@ -0,0 +1,26 @@
+package ambit2.core.obabel.test;
+
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.openscience.cdk.interfaces.IMolecule;
+
+import ambit2.base.external.CommandShell;
+import ambit2.core.smiles.OpenBabelShell;
+
+
+public class TestOpenBabelShell {
+
+	@Test
+	public void test() throws Exception {
+		String osName = System.getProperty("os.name");
+		if (CommandShell.os_WINDOWS.equals(osName) 
+				|| CommandShell.os_WINDOWSVISTA.equals(osName)
+				|| CommandShell.os_WINDOWS7.equals(osName)){
+			OpenBabelShell ob = new OpenBabelShell();
+			IMolecule mol = ob.process("c12-c3c(cccc3)Nc1nc(N)cc2");
+			Assert.assertTrue(mol.getAtomCount()>0);
+		}
+	}
+}
