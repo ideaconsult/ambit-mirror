677318294ad9f3af230970f5a681a4546c16e9cd
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2011-07-31 18:13:41 +0000

    new option

--- ambit2-base/src/main/java/ambit2/base/config/Preferences.java
+++ ambit2-base/src/main/java/ambit2/base/config/Preferences.java
@@ -60,6 +60,7 @@ public class Preferences {
 	public static String USER="User";
 	public static String PASSWORD="Password";
 	public static String TIMEOUT="timeout";
+	public static String REMOTELOOKUP="remotelookup";
 	
 	protected final static String filename="ambit2.pref";
 	protected static Properties props = null;
@@ -80,6 +81,7 @@ public class Preferences {
 		
 		{FASTSMARTS,"Use SMARTS accelerator","true",Boolean.class,"Accelerate SMARTS search",false,VTAGS.Structure},
 		{SHOW_AROMATICITY,"Show circle in an aromatic ring","true",Boolean.class,"Toggles displaying aromatic rings",true,VTAGS.Structure},
+		{REMOTELOOKUP,"Remote lookup enabled","false",Boolean.class,"Enable remote lookup for CAS and EINECS",false,VTAGS.Structure},
 		{GENERATE2D,"Generate 2d coordinates if none exist","true",Boolean.class,"Generate 2D coordinates of the structures, entered as SMILES",false,VTAGS.Structure},
         {SMILESPARSER,"Use Openbabel SMILES parser","true",Boolean.class,"Toggles usage of Openbabel vs. CDK SMILES parser. Openbabel available at http://openbabel.org/",false,VTAGS.Structure},
         {STOP_AT_UNKNOWNATOMTYPES,"Stop at unknown atom types","false",Boolean.class,"If checked, will report an error when an unknown atom type is encountered.",false,VTAGS.Structure},
