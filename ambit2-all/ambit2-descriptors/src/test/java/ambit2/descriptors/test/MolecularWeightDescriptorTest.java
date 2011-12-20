a137370488ce5238a328c04cf5d4324e0ef7078a
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2011-12-20 08:31:11 +0000

    more tests

--- ambit2-all/ambit2-descriptors/src/test/java/ambit2/descriptors/test/MolecularWeightDescriptorTest.java
+++ ambit2-all/ambit2-descriptors/src/test/java/ambit2/descriptors/test/MolecularWeightDescriptorTest.java
@@ -7,8 +7,12 @@ import junit.framework.Assert;
 import org.junit.Test;
 import org.openscience.cdk.interfaces.IMolecule;
 import org.openscience.cdk.qsar.DescriptorValue;
+import org.openscience.cdk.qsar.descriptors.molecular.BCUTDescriptor;
 import org.openscience.cdk.qsar.result.DoubleResult;
+import org.openscience.cdk.silent.SilentChemObjectBuilder;
+import org.openscience.cdk.smiles.SmilesParser;
 import org.openscience.cdk.templates.MoleculeFactory;
+import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 
 import ambit2.core.processors.structure.HydrogenAdderProcessor;
 import ambit2.descriptors.MolecularWeight;
@@ -28,5 +32,19 @@ public class MolecularWeightDescriptorTest {
 		f = new DescriptorResultFormatter(true);
 		f.setLocale(Locale.US);
 		Assert.assertEquals("78.111", f.process(value));		
+	}
+	
+	//this will hang!
+	public void testBCUT() throws Exception {
+		BCUTDescriptor mw = new BCUTDescriptor();
+		SmilesParser sp = new SmilesParser(SilentChemObjectBuilder.getInstance());
+		//IMolecule mol = sp.parseSmiles("O[As](O)O");
+		IMolecule mol = sp.parseSmiles("O1[As]2O[As]1O2");
+		
+		AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(mol);
+		HydrogenAdderProcessor p = new HydrogenAdderProcessor();
+		
+		DescriptorValue value = mw.calculate(p.process(mol));
+		System.out.println(value.getValue());
 	}
 }
