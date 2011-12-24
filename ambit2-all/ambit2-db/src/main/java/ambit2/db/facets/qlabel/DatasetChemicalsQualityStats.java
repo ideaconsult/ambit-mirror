a6152851c24343e1ddf48d73c3bb7960e796fa91
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2011-12-24 12:56:07 +0000

    consensus labels query

--- ambit2-all/ambit2-db/src/main/java/ambit2/db/facets/qlabel/DatasetChemicalsQualityStats.java
+++ ambit2-all/ambit2-db/src/main/java/ambit2/db/facets/qlabel/DatasetChemicalsQualityStats.java
@@ -4,15 +4,14 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-import ambit2.base.data.QLabel;
-import ambit2.base.data.QLabel.QUALITY;
+import ambit2.base.data.ConsensusLabel;
+import ambit2.base.data.ConsensusLabel.CONSENSUS_LABELS;
 import ambit2.base.data.SourceDataset;
 import ambit2.base.exceptions.AmbitException;
 import ambit2.base.facet.IFacet;
 import ambit2.db.facets.AbstractFacetQuery;
 import ambit2.db.search.QueryParam;
 import ambit2.db.search.StringCondition;
-import ambit2.db.search.structure.QueryStructureByQualityPairLabel.CONSENSUS_LABELS;
 
 public class DatasetChemicalsQualityStats  extends AbstractFacetQuery<CONSENSUS_LABELS,SourceDataset,StringCondition,IFacet<String>> {
 	protected DatasetConsensusLabelFacet record;
@@ -68,9 +67,12 @@ public class DatasetChemicalsQualityStats  extends AbstractFacetQuery<CONSENSUS_
 			SourceDataset dataset = new SourceDataset(rs.getString("name"));
 			dataset.setId(rs.getInt("id_srcdataset"));
 			record.setDataset(dataset);
-			record.setValue(String.format("[%s] Compound quality label: %s",dataset.getName(),rs.getString("label")));
+			record.setValue(String.format("[%s] Compound quality label: %s %s",
+						dataset.getName(),rs.getString("label"),rs.getString("text")));
 			record.setCount(rs.getInt(3));
-			record.setProperty(CONSENSUS_LABELS.valueOf(rs.getString("label")));
+			ConsensusLabel l = new ConsensusLabel(CONSENSUS_LABELS.valueOf(rs.getString("label")));
+			l.setText(rs.getString("text"));
+			record.setProperty(l);
 			
 			return record;
 		} catch (Exception x) {
--- ambit2-all/ambit2-db/src/main/java/ambit2/db/facets/qlabel/DatasetConsensusLabelFacet.java
+++ ambit2-all/ambit2-db/src/main/java/ambit2/db/facets/qlabel/DatasetConsensusLabelFacet.java
@@ -2,11 +2,25 @@ package ambit2.db.facets.qlabel;
 
 import java.net.URLEncoder;
 
+import ambit2.base.data.ConsensusLabel.CONSENSUS_LABELS;
+import ambit2.base.data.ConsensusLabel;
 import ambit2.base.data.SourceDataset;
 import ambit2.db.facets.propertyvalue.PropertyDatasetFacet;
-import ambit2.db.search.structure.QueryStructureByQualityPairLabel.CONSENSUS_LABELS;
 
-public class DatasetConsensusLabelFacet  extends PropertyDatasetFacet<CONSENSUS_LABELS, SourceDataset> {
+/**
+ * 
+'CHRIP-www.safe.nite.go.jp', 46, 'Consensus', '2'
+'CHRIP-www.safe.nite.go.jp', 799, 'Consensus', '3'
+'CHRIP-www.safe.nite.go.jp', 1, 'Majority', '1:1:2'
+'CHRIP-www.safe.nite.go.jp', 69, 'Majority', '1:2'
+'CHRIP-www.safe.nite.go.jp', 12, 'Unconfirmed', '1'
+'CHRIP-www.safe.nite.go.jp', 21, 'Ambiguous', '1:1'
+'CHRIP-www.safe.nite.go.jp', 7, 'Ambiguous', '1:1:1'
+
+ * @author nina
+ *
+ */
+public class DatasetConsensusLabelFacet  extends PropertyDatasetFacet<ConsensusLabel, SourceDataset> {
 
 	/**
 	 * 
@@ -19,10 +33,13 @@ public class DatasetConsensusLabelFacet  extends PropertyDatasetFacet<CONSENSUS_
 
 	@Override
 	public String getResultsURL(String... params) {
-		return String.format("%s/dataset/%d/query/consensuslabel?%s=%s",
+		return String.format("%s/dataset/%d/query/consensuslabel?%s=%s&%s=%s",
 				(params.length>0)?params[0]:"",
 				getDataset().getId(),		
 				"search",
-				URLEncoder.encode(getProperty().toString()));
+				URLEncoder.encode(getProperty().getLabel().toString()),
+				"text",
+				getProperty().getText()==null?"":URLEncoder.encode(getProperty().getText().toString())
+				);
 	}	
 }
