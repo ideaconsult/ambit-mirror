b3bb3628d2be8d272d30c7ada4f991f2da319dbf
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2011-12-24 12:56:07 +0000

    consensus labels query

--- ambit2-db/src/main/java/ambit2/db/search/structure/QueryStructureByQualityPairLabel.java
+++ ambit2-db/src/main/java/ambit2/db/search/structure/QueryStructureByQualityPairLabel.java
@@ -5,66 +5,96 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import ambit2.base.data.ConsensusLabel;
+import ambit2.base.data.ConsensusLabel.CONSENSUS_LABELS;
+import ambit2.base.data.ISourceDataset;
+import ambit2.base.data.SourceDataset;
 import ambit2.base.exceptions.AmbitException;
 import ambit2.db.search.QueryParam;
 import ambit2.db.search.StringCondition;
 
-public class QueryStructureByQualityPairLabel extends 	AbstractStructureQuery<String, String, StringCondition> {
-	public static enum CONSENSUS_LABELS {Consensus,Majority,Unconfirmed,Ambiguous,Unknown};
+
+public class QueryStructureByQualityPairLabel  extends AbstractStructureQuery<ISourceDataset, ConsensusLabel, StringCondition> {
+	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -3232148472829083139L;
-
 	public final static String sql = 
-		"select ? as idquery,idchemical,idstructure,case 1 when (q.label='Consensus') then 1 else 0 end as selected,cast(q.label as unsigned) as metric,q.label from\n"+
-		"quality_chemicals q join structure using(idchemical) where q.label = ? %s\n"+
-		"order by idchemical\n";	
-	public final static String where = " and text = ?";
+		"select ? as idquery,structure.idchemical,idstructure,if(type_structure='NA',0,1) as selected," +
+		"cast(quality_chemicals.label as unsigned) as metric,quality_chemicals.label as text from structure " +
+		"%s"+
+		"left join quality_chemicals using(idchemical)\n %s %s %s";
 
-	public QueryStructureByQualityPairLabel() {
-		this("Consensus");
-	}
+	public final static String where = " where quality_chemicals.label %s ? ";
+	public final static String where_null = " where quality_chemicals.label is null";
+
+	public final static String where_text = " and text = ?";
 	
-	public QueryStructureByQualityPairLabel(String label) {
-		setFieldname(label);
+	protected final static String join_struc_dataset = "join struc_dataset using(idstructure)\n";
+	protected final static String where_struc_dataset = "and id_srcdataset = ?\n";
+	
+	protected final static String join_struc_query = "join query_results using(idstructure)\n";
+	protected final static String where_struc_query = "and idquery = ?\n";
+	
+	public QueryStructureByQualityPairLabel() {
+		super();
 		setValue(null);
-		setCondition(StringCondition.getInstance("="));
+		setCondition(StringCondition.getInstance(StringCondition.C_EQ));
+	}
+	public QueryStructureByQualityPairLabel(CONSENSUS_LABELS label) {
+		super();
+		setCondition(StringCondition.getInstance(StringCondition.C_EQ));
+		setValue(new ConsensusLabel(label));
 	}	
-
+	public QueryStructureByQualityPairLabel(String label) {
+		this(CONSENSUS_LABELS.valueOf(label));
+	}	
 	public String getSQL() throws AmbitException {
-		String result = null;
+		String join = "";
+		String where_dataset = null;
+		if ((getFieldname()!=null) && (getFieldname().getID()>0)) {
+			if (getFieldname() instanceof SourceDataset) {
+				join = join_struc_dataset;
+				where_dataset = where_struc_dataset;
+			}
+			else {
+				join = join_struc_query;
+				where_dataset = where_struc_query;
+			}
+		};
+		
 		if (getValue()==null)
-			result = String.format(sql,"");
+			return String.format(sql,join, where_null,"","");
 		else
-			if (getValue()!=null)
-				result =   String.format(sql,where);
-			else
-				result =  String.format(sql,"");
-		//System.out.println(result);
-		return result;
+			return String.format(sql,join, 
+					String.format(where,getCondition()),
+							getValue().getText()==null?"":where_text,
+							where_dataset==null?"":where_dataset
+							
+							);
 	}
 
 	public List<QueryParam> getParameters() throws AmbitException {
-		/*
-		"select ? as idquery,idchemical,idstructure,case 1 when (q.label='Consensus') then 1 else 0 end as selected,cast(q.label as unsigned) as metric from\n"+
-		"quality_chemicals q join structure using(idchemical) where q.label = ? %s\n"+
-		"order by idchemical\n";	
-		 */
+		
 		List<QueryParam> params = new ArrayList<QueryParam>();
 		params.add(new QueryParam<Integer>(Integer.class, getId()));
-		if (getFieldname()==null) throw new AmbitException("No label!");
-		params.add(new QueryParam<String>(String.class,getFieldname()));
 		
-		if (getValue()!=null)
-			params.add(new QueryParam<String>(String.class, getValue()));
+		if (getValue()!=null) {
+			params.add(new QueryParam<String>(String.class, getValue().getLabel().toString()));
+			if (getValue().getText()!=null)
+				params.add(new QueryParam<String>(String.class, getValue().getText()));
+		}
+		if ((getFieldname()!=null) && (getFieldname().getID()>0)) {
+			params.add(new QueryParam<Integer>(Integer.class, getFieldname().getID()));
+		}
+				
 		return params;
 	}
 	@Override  
 	public String toString() {
 		StringBuilder b = new StringBuilder();
-		b.append(getFieldname());
-		b.append(getValue()==null?"":getValue());
+		b.append("Consensus labels");
 		if (getValue()!=null) {
 			b.append(getCondition().toString());
 			b.append(getValue());
@@ -73,11 +103,12 @@ public class QueryStructureByQualityPairLabel extends 	AbstractStructureQuery<St
 	}
 	@Override
 	public Object retrieveValue(ResultSet rs) throws SQLException {
+		ConsensusLabel q = new ConsensusLabel();
 		if (rs.getObject(5)!=null)
-			
-			return CONSENSUS_LABELS.values()[rs.getInt(5)-1];
-		else return null;
+			q.setLabel(CONSENSUS_LABELS.values()[rs.getInt(5)-1]);
+		q.setText(rs.getString(6));
+		return q;
 	}
 	
-
+	
 }
