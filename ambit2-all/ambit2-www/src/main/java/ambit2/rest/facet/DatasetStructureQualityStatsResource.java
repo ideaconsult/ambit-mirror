9cc2c2feaf7188eae3602d9179c72c741f09780c
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2011-12-24 10:20:42 +0000

    new resource for qlabel stats  /query/struc_label summary

---
+++ ambit2-all/ambit2-www/src/main/java/ambit2/rest/facet/DatasetStructureQualityStatsResource.java
@@ -0,0 +1,43 @@
+package ambit2.rest.facet;
+
+import org.restlet.Context;
+import org.restlet.Request;
+import org.restlet.Response;
+import org.restlet.data.Status;
+import org.restlet.resource.ResourceException;
+
+import ambit2.base.data.SourceDataset;
+import ambit2.db.facets.qlabel.DatasetStructureQualityStats;
+import ambit2.rest.OpenTox;
+
+public class DatasetStructureQualityStatsResource extends FacetResource<DatasetStructureQualityStats> {
+	public static final String resource = "/label_strucs";
+	protected Integer datasetID;
+	protected Integer queryResultsID;
+	
+	@Override
+	protected DatasetStructureQualityStats createQuery(Context context,
+			Request request, Response response) throws ResourceException {
+		DatasetStructureQualityStats q = new DatasetStructureQualityStats(getResourceRef(getRequest()).toString());
+		Object id = request.getAttributes().get(OpenTox.URI.dataset.getKey());
+		if (id != null) try {
+			datasetID = Integer.parseInt(id.toString());
+			SourceDataset dataset = new SourceDataset();
+			dataset.setId(datasetID);
+			q.setValue(dataset);
+		} catch (Exception x) {
+			/*
+			if (id.toString().startsWith("R")) {
+				queryResultsID = Integer.parseInt(id.toString().substring(1));
+				StoredQuery dataset = new StoredQuery();
+				dataset.setId(queryResultsID);
+				q.setFieldname(dataset);
+				return q;
+			} else 
+			*/
+			throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST,"Invalid dataset id");
+		} 		
+		return q;
+	}
+
+}
