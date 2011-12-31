ece29e41428d41cdcdbde2289f9774bd5a02602b
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2011-12-31 17:50:08 +0000

    struc type  statistics

---
+++ ambit2-all/ambit2-db/src/main/java/ambit2/db/facets/qlabel/DatasetStrucTypeFacet.java
@@ -0,0 +1,60 @@
+package ambit2.db.facets.qlabel;
+
+import java.net.URLEncoder;
+
+import ambit2.base.data.QLabel;
+import ambit2.base.data.SourceDataset;
+import ambit2.base.interfaces.IStructureRecord;
+import ambit2.db.facets.propertyvalue.PropertyDatasetFacet;
+
+public class DatasetStrucTypeFacet  extends PropertyDatasetFacet<IStructureRecord.STRUC_TYPE, SourceDataset> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4011962497826164064L;
+
+	public DatasetStrucTypeFacet(String url) {
+		super(url);
+	}
+
+	@Override
+	public String getResultsURL(String... params) {
+		String label = getProperty()==null?null:URLEncoder.encode(getProperty().toString());
+
+		if (label==null)
+			return getDataset()==null
+				?String.format("%s/query/structype",(params.length>0)?params[0]:"")
+				:String.format("%s/dataset/%d/query/structype",
+				(params.length>0)?params[0]:"",
+				getDataset().getId());
+		else
+			return getDataset()==null
+				?String.format("%s/query/structype?%s=%s",
+						(params.length>0)?params[0]:"",
+						"search",
+						label)
+				:String.format("%s/dataset/%d/query/structype?%s=%s",
+				(params.length>0)?params[0]:"",
+				getDataset().getId(),		
+				"search",
+				label);
+	
+	}	
+	@Override
+	public String getSubcategoryTitle() {
+		return getProperty()==null?"":getProperty().toString();
+	}
+	@Override
+	public String getSubCategoryURL(String... params) {
+		return null;
+		/*
+		return dataset==null?null:
+				String.format("%s/dataset/%d/metadata",
+				(params.length>0)?params[0]:"",
+				getDataset().getId()	
+				);
+		*/
+	}
+	
+}
---
+++ ambit2-all/ambit2-db/src/main/java/ambit2/db/facets/qlabel/DatasetStrucTypeStats.java
@@ -0,0 +1,105 @@
+package ambit2.db.facets.qlabel;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import ambit2.base.data.SourceDataset;
+import ambit2.base.exceptions.AmbitException;
+import ambit2.base.facet.IFacet;
+import ambit2.base.interfaces.IStructureRecord;
+import ambit2.db.facets.AbstractFacetQuery;
+import ambit2.db.search.QueryParam;
+import ambit2.db.search.StringCondition;
+
+public class DatasetStrucTypeStats  extends AbstractFacetQuery<IStructureRecord.STRUC_TYPE,SourceDataset,StringCondition,IFacet<String>> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5183010855788077541L;
+
+	protected static final String sql = 
+		"SELECT id_srcdataset,name,type_structure,count(idstructure),src_dataset.user_name FROM structure\n"+
+		"join struc_dataset using(idstructure)\n"+
+		"join src_dataset using(id_srcdataset)\n"+
+		"%s\n"+
+		"group by id_srcdataset,user_name,type_structure with rollup\n";
+	
+	protected DatasetStrucTypeFacet record;
+	
+	public DatasetStrucTypeStats(String url) {
+		super(url);
+		record = new DatasetStrucTypeFacet(url);
+		record.setProperty(getFieldname());
+		record.setDataset(getValue());		
+	}
+	@Override
+	public double calculateMetric(IFacet<String> object) {
+		return 0;
+	}
+
+	@Override
+	public boolean isPrescreen() {
+		return false;
+	}
+
+	@Override
+	public List<QueryParam> getParameters() throws AmbitException {
+		if (getValue()==null) return null;	
+		List<QueryParam> params = new ArrayList<QueryParam>();
+		if ((getValue() !=null) && getValue().getId()>0)
+			params.add(new QueryParam<Integer>(Integer.class,getValue().getId()));
+		
+		return params;
+	}
+
+	@Override
+	public String getSQL() throws AmbitException {
+		if ((getValue() !=null) && getValue().getId()>0) return String.format(sql,"where id_srcdataset=?");
+		else return String.format(sql,"");
+	}
+
+	@Override
+	public IFacet<String> getObject(ResultSet rs) throws AmbitException {
+		if (record == null) 
+			record = new DatasetStrucTypeFacet(null);
+		
+		try {
+			SourceDataset dataset = null;
+			Object iddataset = rs.getObject("id_srcdataset");
+			String dbname = rs.getString("name");
+			if (iddataset!=null) {
+				dataset = new SourceDataset(dbname);
+				dataset.setId(rs.getInt("id_srcdataset"));
+				
+			}
+			String uname = rs.getString("user_name");
+			record.setDataset(dataset);
+			String label = rs.getString("type_structure");
+			record.setValue(String.format("[%s] %s",
+					dataset==null?"Total":dataset.getName(),label));
+			
+			record.setCount(rs.getInt(4));
+			String ts = rs.getString("type_structure");
+			for (IStructureRecord.STRUC_TYPE t : IStructureRecord.STRUC_TYPE.values())
+				if (t.toString().equals(ts)) {
+					record.setProperty(label==null?null:t);
+					break;
+				}
+			
+			return record;
+		} catch (Exception x) {
+			record.setValue(x.getMessage());
+			record.setCount(-1);
+			return record;
+		}
+	}	
+	
+	@Override
+	public String toString() {
+		if ((getFieldname()!=null) && (getValue()!=null))
+			return String.format("Struc type %s & Dataset %d",getFieldname(),getValue().getId());
+		else return "Dataset structure types";
+	}	
+}
