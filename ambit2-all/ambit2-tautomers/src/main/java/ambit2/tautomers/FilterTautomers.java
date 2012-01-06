e0b56e562e53e2782fbc0f76c58ce66da2b3b76c
Nikolay Kochev <nick@uni-plovdiv.net>
2012-01-06 14:40:48 +0000

    Preprocessing of the result tautomers now is moved inside tautomer generator. Implemented filtration based on InChI identification. Reorganization of the code in FilterTautomers.

--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/FilterTautomers.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/FilterTautomers.java
@@ -2,8 +2,15 @@ package ambit2.tautomers;
 
 import java.util.Vector;
 
+import org.openscience.cdk.CDKConstants;
+import org.openscience.cdk.aromaticity.CDKHueckelAromaticityDetector;
+import org.openscience.cdk.inchi.InChIGenerator;
+import org.openscience.cdk.inchi.InChIGeneratorFactory;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.silent.SilentChemObjectBuilder;
+import org.openscience.cdk.tools.CDKHydrogenAdder;
+import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 
 import ambit2.smarts.IsomorphismTester;
 import ambit2.smarts.SmartsHelper;
@@ -11,10 +18,15 @@ import ambit2.smarts.SmartsParser;
 
 
 public class FilterTautomers 
-{
+{	
 	public boolean FlagApplyWarningFilter = true;
 	public boolean FlagApplyExcludeFilter = true;
-	public boolean FlagApplyDuplicationFilter = true;
+	public boolean FlagApplyDuplicationFilter = true;	
+	public boolean FlagApplyDuplicationCheckIsomorphism = false;
+	
+	//This duplication check is not so good since equal InChI keys are given to the resonance Kekeule stuctures
+	public boolean FlagApplyDuplicationCheckInChI = false;  
+	
 	
 	public TautomerManager tman;
 	public IAtomContainer originalMolecule;
@@ -38,36 +50,17 @@ public class FilterTautomers
 	public Vector<IAtomContainer> filter(Vector<IAtomContainer> tautomers)
 	{	
 		getOriginalPositions();
-		
+		//System.out.println("generated " + tautomers.size() + " strctures");
 		removedTautomers.clear();
-		Vector<IAtomContainer> filteredTautomers = new Vector<IAtomContainer>();
+		Vector<IAtomContainer> filteredTautomers = new Vector<IAtomContainer>();		
+		Vector<IAtomContainer> uniqueTautomers;
 		
-		//Remove duplications
-		Vector<IAtomContainer> uniqueTautomers = new Vector<IAtomContainer>();
-		Vector<String> tCodes = new Vector<String> ();
 		
-		for (int i = 0; i < tautomers.size(); i++)
-		{
-			String tcode = TautomerManager.getTautomerCodeString(tautomers.get(i));
-			
-			boolean FlagDuplication = false;
-			
-			if (FlagApplyDuplicationFilter)
-				for (int k = 0; k < tCodes.size(); k++)
-				{
-					if (tcode.equals(tCodes.get(k)))
-					{
-						FlagDuplication = true;
-						break;
-					}
-				}
-
-			if (!FlagDuplication)
-			{
-				tCodes.add(tcode);
-				uniqueTautomers.add(tautomers.get(i));
-			}
-		}
+		//Remove duplications based on double bond positions
+		if (FlagApplyDuplicationFilter)
+			uniqueTautomers = dubplicationFilter(tautomers);
+		else
+			uniqueTautomers = tautomers;
 		
 		
 		//Filtration according to the filter rules
@@ -110,9 +103,149 @@ public class FilterTautomers
 			filteredTautomers.add(uniqueTautomers.get(i));
 		}
 		
+		
+		//Pre-processing
+		for (int i = 0; i < filteredTautomers.size(); i++)
+		{	
+			try{
+				//System.out.println("preprocess " + i);
+				preProcessStructures(filteredTautomers.get(i));
+			}
+			catch(Exception e){
+				System.out.println(e.toString());
+			}
+		}
+		
+		
+		if (FlagApplyDuplicationCheckIsomorphism)
+		{
+			Vector<IAtomContainer> filteredTautomers2 = duplicationFilterBasedOnIsomorphism(filteredTautomers);
+			filteredTautomers = filteredTautomers2;
+		}
+		
+		
+		//Remove duplications based on InChI representation
+		//Generally this option is switched-off (false) since InChI based filtering removes
+		//leaves only one resonance aromatic structure.   
+		if (FlagApplyDuplicationCheckInChI)
+		{	
+			try{
+				Vector<IAtomContainer> filteredTautomers2 = duplicationFilterBasedOnInChI(filteredTautomers);
+				filteredTautomers = filteredTautomers2;
+			}
+			catch(Exception e)
+			{	
+				System.out.println(e.toString());
+			}
+		}
+		
+		
 		return filteredTautomers;
 	}
 	
+	
+	Vector<IAtomContainer> dubplicationFilter(Vector<IAtomContainer> tautomers)
+	{	
+		Vector<IAtomContainer> uniqueTautomers0 = new Vector<IAtomContainer>();
+		Vector<String> tCodes = new Vector<String> ();
+		
+		for (int i = 0; i < tautomers.size(); i++)
+		{
+			String tcode = TautomerManager.getTautomerCodeString(tautomers.get(i));
+			//System.out.println("#" + i + "  tcode = " + tcode);
+			boolean FlagDuplication = false;
+
+			for (int k = 0; k < tCodes.size(); k++)
+			{
+				if (tcode.equals(tCodes.get(k)))
+				{
+					FlagDuplication = true;
+					break;
+				}
+			}
+
+			if (!FlagDuplication)
+			{
+				tCodes.add(tcode);
+				uniqueTautomers0.add(tautomers.get(i));
+			}
+		}
+		
+		return uniqueTautomers0;
+	}
+	
+	
+	Vector<IAtomContainer> duplicationFilterBasedOnInChI(Vector<IAtomContainer> tautomers) throws Exception
+	{
+		InChIGeneratorFactory igf = InChIGeneratorFactory.getInstance();
+		Vector<IAtomContainer> uniqueTautomers = new Vector<IAtomContainer>();
+		Vector<String> tKeys = new Vector<String> ();
+		
+		for (int i = 0; i < tautomers.size(); i++)
+		{	
+			IAtomContainer clone = getCloneForInChIGeneration(tautomers.get(i)); //the clone contains eexplicit H atoms
+			
+			InChIGenerator ig = igf.getInChIGenerator(clone);
+			String inchi = ig.getInchi();
+			String inchiKey = ig.getInchiKey();
+			System.out.println("#" + i + "  " + inchi + "   "+ inchiKey + "  " + SmartsHelper.moleculeToSMILES(tautomers.get(i)));
+			
+			
+			boolean FlagDuplication = false;
+
+			for (int k = 0; k < tKeys.size(); k++)
+			{
+				if (inchiKey.equals(tKeys.get(k)))
+				{
+					FlagDuplication = true;
+					break;
+				}
+			}
+
+			if (!FlagDuplication)
+			{
+				tKeys.add(inchiKey);
+				uniqueTautomers.add(tautomers.get(i));
+			}
+		}
+		
+		return uniqueTautomers;
+	}
+	
+	Vector<IAtomContainer> duplicationFilterBasedOnIsomorphism(Vector<IAtomContainer> tautomers)
+	{
+		
+		Vector<IAtomContainer> filtered = new Vector<IAtomContainer> ();
+		
+		for (int i = 0; i < tautomers.size(); i++)
+		{
+			if (!checkIsomorphismEquivalence(tautomers.get(i), filtered))
+					filtered.add(tautomers.get(i));
+		}
+		
+		return (filtered);
+	}
+	
+	boolean checkIsomorphismEquivalence(IAtomContainer target, Vector<IAtomContainer> structs)
+	{
+		if (structs.isEmpty())
+			return (false);
+		
+		//TODO
+		
+		return false;
+	}
+	
+	
+	IAtomContainer getCloneForInChIGeneration(IAtomContainer ac) throws Exception
+	{	
+		IAtomContainer clone = (IAtomContainer)ac.clone();
+		AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(clone);
+		CDKHydrogenAdder adder = CDKHydrogenAdder.getInstance(SilentChemObjectBuilder.getInstance());
+		adder.addImplicitHydrogens(clone);
+		AtomContainerManipulator.convertImplicitToExplicitHydrogens(clone);
+		return(clone);
+	}
 	
 	public void getOriginalPositions()
 	{
@@ -256,6 +389,36 @@ public class FilterTautomers
 		else
 			return(v);
 	}
+	
+	
+	void clearAromaticityFlags(IAtomContainer ac)
+	{
+		for (int i = 0; i < ac.getAtomCount(); i++)
+		{
+			ac.getAtom(i).setFlag(CDKConstants.ISAROMATIC, false);
+		}
+		
+		for (int i = 0; i < ac.getBondCount(); i++)
+		{
+			ac.getBond(i).setFlag(CDKConstants.ISAROMATIC, false);
+		}
+		
+	}
+	
+	void preProcessStructures(IAtomContainer ac) throws Exception
+	{
+		if (ac == null)
+			return;
+		clearAromaticityFlags(ac);
+
+		AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(ac);
+		CDKHydrogenAdder adder = CDKHydrogenAdder.getInstance(SilentChemObjectBuilder.getInstance());
+		adder.addImplicitHydrogens(ac);
+		//AtomContainerManipulator.convertImplicitToExplicitHydrogens(ac);
+
+		CDKHueckelAromaticityDetector.detectAromaticity(ac);		
+	}
+	
 	
 	
 	
