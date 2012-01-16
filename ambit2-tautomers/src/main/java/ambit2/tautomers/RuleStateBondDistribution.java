5b5908a6a89e16d291569c5526b03650b47eb97d
Nikolay Kochev <nick@uni-plovdiv.net>
2012-01-16 13:41:00 +0000

    Implemented support of triple bonds when handling rules and rule instances. Added a preliminary test rule with a triple bond.

--- ambit2-tautomers/src/main/java/ambit2/tautomers/RuleStateBondDistribution.java
+++ ambit2-tautomers/src/main/java/ambit2/tautomers/RuleStateBondDistribution.java
@@ -14,23 +14,25 @@ import org.openscience.cdk.silent.SilentChemObjectBuilder;
 public class RuleStateBondDistribution 
 {
 	int DBPositions[]; //double bond positions
+	int TBPositions[]; //triple bond positions
 	
 	public void calcDistribution(QueryAtomContainer statePattern)
 	{
 		int n = 0; 
 		SmartsToChemObject stco = new SmartsToChemObject(SilentChemObjectBuilder.getInstance());
-		Vector<Integer> v = new Vector<Integer>(); 
+		Vector<Integer> v2 = new Vector<Integer>();
+		Vector<Integer> v3 = new Vector<Integer>();
 		for (int i = 0; i < statePattern.getBondCount(); i++)
 		{	
 			if (statePattern.getBond(i) instanceof DoubleNonAromaticBond)
 			{	
-				v.add(new Integer(i));
+				v2.add(new Integer(i));
 				continue;
 			}
 			
 			if (statePattern.getBond(i) instanceof DoubleBondAromaticityNotSpecified)
 			{	
-				v.add(new Integer(i));
+				v2.add(new Integer(i));
 				continue;
 			}
 			
@@ -38,7 +40,10 @@ public class RuleStateBondDistribution
 			{	
 				OrderQueryBond bo = (OrderQueryBond)statePattern.getBond(i);
 				if (bo.getOrder() == IBond.Order.DOUBLE)
-					v.add(new Integer(i));
+					v2.add(new Integer(i));
+				
+				if (bo.getOrder() == IBond.Order.TRIPLE)
+					v3.add(new Integer(i));
 				continue;
 			}
 			
@@ -46,22 +51,36 @@ public class RuleStateBondDistribution
 			{
 				IBond bo = stco.smartsExpressionToBond((SmartsBondExpression)statePattern.getBond(i));
 				if (bo != null)
+				{	
 					if (bo.getOrder() == IBond.Order.DOUBLE)
-						v.add(new Integer(i));
+						v2.add(new Integer(i));
+					
+					if (bo.getOrder() == IBond.Order.TRIPLE)
+						v3.add(new Integer(i));
+				}
 				continue;
 			}
 		}
 		
-		DBPositions = new int[v.size()];
-		for (int i = 0; i <v.size(); i++)
-			DBPositions[i] = v.get(i).intValue();
+		DBPositions = new int[v2.size()];
+		for (int i = 0; i <v2.size(); i++)
+			DBPositions[i] = v2.get(i).intValue();
+		
+		TBPositions = new int[v3.size()];
+		for (int i = 0; i <v3.size(); i++)
+			TBPositions[i] = v3.get(i).intValue();
+		
 	}
 	
 	public String toString()
 	{
-		String s = "";		
+		String s = "DB: ";		
 		for (int i = 0; i < DBPositions.length; i++)
 			s += DBPositions[i] + " ";
+		
+		s+= "   TB: ";
+		for (int i = 0; i < TBPositions.length; i++)
+			s += TBPositions[i] + " ";
 		return(s);
 	}
 }
