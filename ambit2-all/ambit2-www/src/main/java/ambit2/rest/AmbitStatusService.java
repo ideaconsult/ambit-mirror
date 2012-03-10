5da395e905d3f1a4e0a3309406497ea7e02e4acd
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2012-03-10 11:42:16 +0000

    error status rendering

--- ambit2-all/ambit2-www/src/main/java/ambit2/rest/AmbitStatusService.java
+++ ambit2-all/ambit2-www/src/main/java/ambit2/rest/AmbitStatusService.java
@@ -21,6 +21,92 @@ public class AmbitStatusService extends StatusService {
 		setContactEmail("jeliazkova.nina@gmail.com");
 		
 	}
+	
+	
+
+
+	@Override
+	public Representation getRepresentation(Status status, Request request,
+			Response response) {
+		try {
+			boolean wrapInHTML = true;
+			
+			if ((status.getThrowable() !=null) && (status.getThrowable() instanceof RResourceException)) 
+				wrapInHTML = ((RResourceException)status.getThrowable()).getVariant().equals(MediaType.TEXT_HTML);
+			else {
+				Form headers = (Form) request.getAttributes().get("org.restlet.http.headers"); 
+				String acceptHeader = headers.getValues("accept");
+				if (acceptHeader!=null)
+					wrapInHTML = acceptHeader.contains("text/html");
+			}
+			
+			if (wrapInHTML) {
+				StringWriter w = new StringWriter();
+				
+				AmbitResource.writeHTMLHeader(w, status.getName(), request,
+						request.getOriginalRef()==null?request.getResourceRef():request.getOriginalRef(),
+						null);
+				
+				StringWriter details = new StringWriter();
+
+				if (status.getThrowable()!= null) {
+					status.getThrowable().printStackTrace(new PrintWriter(details) {
+						@Override
+						public void print(String s) {
+							super.print(String.format("%s<br>", s));
+							
+						}
+					});
+				} 
+				
+				
+				w.write(
+						String.format(		
+						"<div class=\"ui-widget \" style=\"margin-top: 20px; padding: 0 .7em;\">\n"+
+						"<div class=\"ui-widget-header ui-corner-top\"><p>Error code <a href='%s' target='help'>%d</a>&nbsp;%s</p></div>\n"+
+						"<div class=\"ui-widget-content ui-corner-bottom \">\n"+
+						"<p>%s</p><p>"+
+						"<a href=\"javascript:toggleDiv('%s');\" style=\"background-color: #fff; padding: 5px 10px;\">Details</a>\n"+	
+						"</p>\n"+
+						"<div class=\"ui-widget\" style='display: none;' id='details'><p>%s</p></div>\n"+
+						"</div></div>\n",
+						status.getUri(),
+						status.getCode(),
+						status.getName(),
+						status.getDescription(),
+						"details",
+						details
+						)
+				);
+				
+				AmbitResource.writeHTMLFooter(w, status.getName(), request);
+				return new StringRepresentation(w.toString(),MediaType.TEXT_HTML);
+			} else {
+				if ((status.getThrowable() !=null) && (status.getThrowable() instanceof RResourceException)) 
+					return ((RResourceException)status.getThrowable()).getRepresentation();
+			}
+			/*
+			w.write(String.format("ERROR :<br>Code: %d<br>Name: %s<br>URI: %s<br>Description: %s<br>",
+					status.getCode(),
+					status.getName(),
+					status.getUri(),
+					status.getDescription()
+					));
+	*/
+
+		} catch (Exception x) {
+			
+		}
+		if (status.getThrowable()==null)
+			return new StringRepresentation(status.toString(),MediaType.TEXT_PLAIN);
+		else {
+	    	StringWriter w = new StringWriter();
+	    	status.getThrowable().printStackTrace(new PrintWriter(w));
+
+	    	return new StringRepresentation(w.toString(),MediaType.TEXT_PLAIN);	
+		}
+	}
+	/*
 
 	@Override
 	public Representation getRepresentation(Status status, Request request,
@@ -66,14 +152,7 @@ public class AmbitStatusService extends StatusService {
 				if ((status.getThrowable() !=null) && (status.getThrowable() instanceof RResourceException)) 
 					return ((RResourceException)status.getThrowable()).getRepresentation();
 			}
-			/*
-			w.write(String.format("ERROR :<br>Code: %d<br>Name: %s<br>URI: %s<br>Description: %s<br>",
-					status.getCode(),
-					status.getName(),
-					status.getUri(),
-					status.getDescription()
-					));
-	*/
+
 
 		} catch (Exception x) {
 			
@@ -87,6 +166,7 @@ public class AmbitStatusService extends StatusService {
 	    	return new StringRepresentation(w.toString(),MediaType.TEXT_PLAIN);	
 		}
 	}
+	*/
 	@Override
 	public Status getStatus(Throwable throwable, Request request,
 			Response response) {
