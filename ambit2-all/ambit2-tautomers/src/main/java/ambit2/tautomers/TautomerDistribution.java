711b1729142cccfebd2255aeb454f0b052933855
Nikolay Kochev <nick@uni-plovdiv.net>
2012-03-10 22:33:46 +0000

    Added new class. It will be developed further in the future. Implemented new utilities for automatic testing.

---
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/TautomerDistribution.java
@@ -0,0 +1,17 @@
+package ambit2.tautomers;
+
+/**
+ * 
+ * @author nick2
+ *
+ * This class is calculates the distribution of different tautomers 
+ * by applying weighting scheme and for each rule in 
+ * accordance to particular conditions
+ * 
+ * This class currently is under development
+ */
+
+public class TautomerDistribution 
+{
+	
+}
