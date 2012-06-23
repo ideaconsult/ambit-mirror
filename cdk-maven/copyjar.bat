193ec3b0b26790141aa7b1d270dd385d88ca0331
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2012-06-23 16:26:09 +0000

    cdk-maven project updated no updates for cdk-jchempaint, use  cdk-jchempaint:26

--- cdk-maven/copyjar.bat
+++ cdk-maven/copyjar.bat
@@ -5,12 +5,12 @@ rem remove the .jar extension,  result is "cdk-core" or "cdk-core-sources"
 set str=%str:.jar=%
 rem remove the "-sources" string , e.g. "cdk-core-sources" result is "cdk-core". This is the target folder
 set dir=%str:-sources=%
-rem construct the target file name, result is e.g. "cdk-core\dist\cdk-core-1.4.10.jar" or "cdk-core\dist\cdk-core-sources-1.4.10.jar"
-set str="%dir%\dist\%str%-1.4.10.jar
-rem reverse version and "-sources" to match Maven format; e.g. "cdk-core\dist\cdk-core-1.4.10-sources.jar"
-set str=%str:-sources-1.4.10.jar=-1.4.10-sources.jar%
+rem construct the target file name, result is e.g. "cdk-core\dist\cdk-core-1.4.11.jar" or "cdk-core\dist\cdk-core-sources-1.4.11.jar"
+set str="%dir%\dist\%str%-1.4.11.jar
+rem reverse version and "-sources" to match Maven format; e.g. "cdk-core\dist\cdk-core-1.4.11-sources.jar"
+set str=%str:-sources-1.4.11.jar=-1.4.11-sources.jar%
 rem echo.%str%
 rem copy the file
 @echo on
 rem del "%str%"
-copy "..\cdk-src+libs-1.4.10\dist\jar\%1" "%str%"
+copy "..\cdk-src+libs-1.4.11\dist\jar\%1" "%str%"
