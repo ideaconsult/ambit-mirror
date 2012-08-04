49f293d599079bc6382fa7dd3881808919e51f94
Nikolay Kochev <nick@uni-plovdiv.net>
2012-08-04 18:14:23 +0000

    Implemented common functionality and some specific for NCI processing.

--- ambit2-tautomers/src/test/java/ambit2/tautomers/test/AutomaticTautomerTests.java
+++ ambit2-tautomers/src/test/java/ambit2/tautomers/test/AutomaticTautomerTests.java
@@ -12,10 +12,13 @@ public class AutomaticTautomerTests
 	{	
 		String option = null;
 		String value = null;		
-	};
+	};	
 	
 	
+	public static final int LPM_PROCESS_NCI = 1;
 	
+	int lineProcessMode = 0;
+		
 	String endLine = "\r\n";
 	String outFileName = "";
 	String inFileName = "";
@@ -25,8 +28,31 @@ public class AutomaticTautomerTests
 	String inFileName5 = "";
 	
 	String command = "";
+	String dbFileName = "";
 	
 	RandomAccessFile outFile = null;
+	
+	int nInputStr = 0;
+	
+	
+	public static void main(String[] args)
+	{
+		AutomaticTautomerTests att = new AutomaticTautomerTests();
+				
+		try {
+			att.handleArguments(new String[] {
+					"-i","D:/Projects/data012-tautomers/NCISMA99.sdz",
+					"-nInpStr","10",
+					//"-db","D:/Projects/data012-tautomers/nci.smi",
+					"-c","process-nci",
+					"-o","D:/Projects/data012-tautomers/nci.smi",
+					"-nDBStr","1"
+			});		
+		} 
+		catch (Exception x) {
+			x.printStackTrace();
+		}
+	}
 	
 		
 	public int handleArguments(String[] args) throws Exception
@@ -38,8 +64,10 @@ public class AutomaticTautomerTests
 		}	
 		
 		Vector<CmdOption> options = extractOptions(args);
-		//for (int i = 0; i < v.size(); i++)
-		//	System.out.println(v.get(i).option + "  " +v.get(i).value);
+		
+		System.out.println("AutomaticTautomerTests  started with options:");
+		for (int i = 0; i < options.size(); i++)
+			System.out.println("  " + options.get(i).option + "  " + options.get(i).value);
 		
 		CmdOption opt;
 		
@@ -85,8 +113,64 @@ public class AutomaticTautomerTests
 				outFileName = opt.value; 
 		}
 		
+		opt = getOption("db", options);
+		if (opt != null)
+		{
+			if (opt.value != null)
+				dbFileName = opt.value; 
+		}	
+		
+		opt = getOption("c", options);
+		if (opt != null)
+		{
+			if (opt.value != null)
+				command = opt.value; 
+		}	
+		
+		opt = getOption("nInpStr", options);
+		if (opt != null)
+		{
+			if (opt.value != null)
+			{	
+				try
+				{
+					Integer intObj = new Integer(0);
+					nInputStr = Integer.parseInt(opt.value);
+				}
+				catch(Exception e)
+				{
+					System.out.println("Incorrect nInpStr value: "+ e.toString());
+				}
+			}	
+		}
+		
+		handleCommand();
+		
 		return (0);
 	}
+	
+	public int handleCommand() throws Exception
+	{
+		if (command == null)
+			return(0);
+		
+		
+		if (command.equals("process-nci"))
+		{
+			System.out.println("Processing nci file:");
+			openOutputFile();
+			lineProcessMode = LPM_PROCESS_NCI;
+			iterateInputFile();
+			closeOutputFile();
+			return(0);
+		}
+		
+		
+		System.out.println("Unknown command: " + command);
+		
+		return(-1);
+	}	
+	
 	
 	public void printHelp()
 	{
@@ -95,6 +179,10 @@ public class AutomaticTautomerTests
 		System.out.println("-i            input file");
 		System.out.println("-i<n>         additional input files n = 2,3,4,5");
 		System.out.println("-o            output file");
+		System.out.println("-nInpStr      the number of used input structures");
+		
+		System.out.println("-c            command: ");
+		System.out.println("                 process-nci           nci row file is processed");
 	}	
 	
 	
@@ -225,10 +313,15 @@ public class AutomaticTautomerTests
 			while (f.getFilePointer() < length)
 			{	
 				n++;
+				
+				if (nInputStr > 0)
+					if (n > nInputStr) 
+						break;
+				
 				String line = f.readLine();
 				System.out.println("line " + n + "  " + line);
 				
-				//processLine(line.trim());
+				processLine(line.trim());
 			}
 			
 			f.close();
@@ -237,5 +330,42 @@ public class AutomaticTautomerTests
 		{	
 			System.out.println(e.getMessage());
 		}
+	}
+	
+	int processLine(String line)
+	{	
+		if (lineProcessMode == LPM_PROCESS_NCI)
+		{
+			processNCILine(line);
+			return(0);
+		}
+		
+		return 0;
+	}
+	
+	
+	//------------- process line functions ------------------
+	
+	int processNCILine(String line)
+	{
+		//System.out.println(line);
+		Vector<String> tokens = filterTokens(line.split(" "));
+		//TODO
+		
+		output(line + endLine);
+		return (0);
+	}
+	
+	
+	
+	//--------------------------------------------------------
+	
+	public Vector<String> filterTokens(String tokens[])
+	{
+		Vector<String> v = new Vector<String>();
+		for (int i = 0; i < tokens.length; i++)
+			if (!tokens[i].equals(""))
+				v.add(tokens[i]);
+		return v;
 	}
 }
