cee61ecadc6cecaeabf5a7b687f82801af3e603f
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2012-08-05 19:23:26 +0000

    Wrapper for Waffles machine learning tool

---
+++ ambit2-waffles/src/main/java/ambit2/waffles/learn/options/WafflesLearnCommand.java
@@ -0,0 +1,17 @@
+package ambit2.waffles.learn.options;
+
+public enum WafflesLearnCommand {
+	autotune,
+	train,
+	predict,
+	predictdistribution,
+	test,
+	transduce,
+	transacc,
+	splittest,
+	crossvalidate,
+	precisionrecall,
+	sterilize,
+	trainrecurrent
+	
+}
---
+++ ambit2-waffles/src/main/resources/ambit2/bin/waffles/README.txt
@@ -0,0 +1,3 @@
+This folder contains binaries of Waffles machine learning command line C++ library.
+
+Sources and documentation available at http://waffles.sourceforge.net/
