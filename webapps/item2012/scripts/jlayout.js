f9d922278b166462d7bab0a0329bc4a1b8fc3dd8
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2012-11-15 16:31:55 +0000

    yet more layout beauties

--- webapps/item2012/scripts/jlayout.js
+++ webapps/item2012/scripts/jlayout.js
@@ -1,10 +1,11 @@
 function initLayout() {
 //layout
 		var myLayout = $('body').layout({
-			closable:false,
-			resizable				:false,
-			slidable				:true,
-			livePaneResizing		:true,
+			//applyDefaultStyles		: true,
+			closable				: false,
+			resizable				: false,
+			slidable				: true,
+			livePaneResizing		: true,
 			west__closable			: true,
 			west__slidable			: true,
 			west__togglerLength_closed: '100%',
@@ -13,11 +14,14 @@ function initLayout() {
 			north__spacing_closed	:0,
 			south__spacing_open		:0,
 			south__spacing_closed	:0,
-			south__size				:64,
-			south__minSize			:64,
-			south__maxSize			:64
+			east__spacing_open		:0,
+			east__spacing_closed	:0,
+			south__size				:48,
+			south__minSize			:48,
+			south__maxSize			:48,
+			north__size				:48,
+			west__size				:200,
+			east__size				:200
 		});
-		myLayout.sizePane("north", 48);
-		myLayout.sizePane("west", 180);
-		myLayout.options.west.resizable = false;
+		myLayout.close('west');
 }						
