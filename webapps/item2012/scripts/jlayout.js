4eb538db23a01068c3df7bd9c38700a7e67368f0
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2012-11-16 08:12:40 +0000

    Search form validation and proper formatting via CSS

--- webapps/item2012/scripts/jlayout.js
+++ webapps/item2012/scripts/jlayout.js
@@ -25,3 +25,41 @@ function initLayout() {
 		});
 		myLayout.close('west');
 }						
+
+
+function searchFormValidation(formName) {
+	$(formName).validate({
+		rules : {
+			'search': {
+				required : true
+			},		
+			'option': {
+				required : true
+			},
+			'threshold': {
+			},
+			'funcgroups': {
+			},
+			'pagesize': {
+				required : true,
+				number	 : true,
+				range: [1, 100]
+			}
+		},
+		messages : {
+			'search'  : {
+				required: "Please enter a search query (CAS, name, SMILES, InChI or SMARTS in case of <i>Substructure</i> search), or click the 'Submit molecule' link to transfer the structure from JME."
+			},
+			'option' : "Please select an option",
+			'threshold'  : {
+				required: "Please select the Tanimoto similarity threshold."
+			},
+			'funcgroups'     : {
+				required: "Please select a predefined functional group."
+			},
+			'pagesize'  : {
+				required: "Please provide number of hits."
+			}			
+		}
+	});
+}
