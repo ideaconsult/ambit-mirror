9186c0b050620ce567f49740aeec0404a9bb5f7b
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2012-12-12 11:35:19 +0000

    JME works

--- ambit2-all/ambit2-www/src/main/webapp/jme/jme.js
+++ ambit2-all/ambit2-www/src/main/webapp/jme/jme.js
@@ -1,18 +1,18 @@
 var smiles = "";
 var jme = "0 0";
-var formname = "form";
+var form = "form";
 
 function startEditor(baseRef) {
   // use here fully qualified IP address (number!) i.e.
   // window.open('http://123.456.789.1/jme_window.html',... 
-  formname="form";	
+  form ="form";	
   window.open(baseRef+'/jme/jme_window.html','JME','width=500,height=450,scrollbars=no,resizable=yes');
 }
 
-function startEditor(baseRef,form) {
+function startEditor(baseRef,formName) {
 	  // use here fully qualified IP address (number!) i.e.
 	  // window.open('http://123.456.789.1/jme_window.html',...
-	  formname = form;
+	  form = formName==undefined?'form':formName;
 	  window.open(baseRef+'/jme/jme_window.html','JME','width=500,height=450,scrollbars=no,resizable=yes');
 }
 
@@ -23,12 +23,13 @@ function fromEditor(smiles,jme) {
     alert ("no molecule submitted");
     return;
   }
-  document["form"].type.value = "smiles"; 
-  document["form"].search.value = smiles; 
+  document[form].type.value = "smiles"; 
+  document[form].search.value = smiles; 
+  console
 }
 
 function processMolecule() {
-  smiles = document["form"].smiles.value;
+  smiles = document[form].search.value;
   if (smiles == "") {alert("Nothing to process!"); return;}
   alert('submitting '+smiles+' for processing!');
   // in actual application remove line above and add something like this
