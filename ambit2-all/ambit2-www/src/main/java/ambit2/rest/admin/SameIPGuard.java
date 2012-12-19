dfb936e34d8ef031d5bfb2dafcebe9ada97616b9
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2012-12-19 08:15:45 +0000

    another simple guard

--- ambit2-all/ambit2-www/src/main/java/ambit2/rest/admin/SameIPGuard.java
+++ ambit2-all/ambit2-www/src/main/java/ambit2/rest/admin/SameIPGuard.java
@@ -8,25 +8,20 @@ import java.util.logging.Logger;
 import org.restlet.Request;
 import org.restlet.Response;
 import org.restlet.data.Form;
-import org.restlet.security.Authorizer;
 import org.xbill.DNS.Address;
 
 /**
- * A crude IP based guard, succeeds only if client and server resolve to the same IP address.
+ * A crude IP based guard, succeeds only if client IP is in the list.
  * @author nina
  *
  */
-public class SameIPGuard extends Authorizer {
-	String[] ipAllowed = null;
-	protected Logger logger;
+public class SameIPGuard extends SimpleGuard {
+
+	public SameIPGuard(Logger logger) {
+		this(new String[] {"127.0.0.1","/0:0:0:0:0:0:0:1"},logger);
+	}
 	public SameIPGuard(String[] ipAllowed, Logger logger) {
-		super();
-		if (ipAllowed==null)
-			this.ipAllowed = new String[] {"127.0.0.1","/0:0:0:0:0:0:0:1"};
-		else
-			this.ipAllowed = ipAllowed;
-		Arrays.sort(ipAllowed);
-		this.logger = logger;
+		super(ipAllowed,logger);
 	}
 	@Override
 	protected boolean authorize(Request request, Response response) {
@@ -40,6 +35,8 @@ public class SameIPGuard extends Authorizer {
 				if (accept.indexOf("uri-list")>=0) return true;
 				if (accept.indexOf("text/n3")>=0) return true;
 				if (accept.indexOf("application/rdf+xml")>=0) return true;
+				if (accept.indexOf("chemical")>=0) return true;
+				if (accept.indexOf("text/csv")>=0) return true;
 			}
 			
 			InetAddress[] ipclient;
@@ -54,7 +51,7 @@ public class SameIPGuard extends Authorizer {
 			
 			for (InetAddress cl : ipclient) {
 				logger.log(Level.INFO,cl.toString());
-				if (Arrays.binarySearch(ipAllowed,cl.toString()) >=0) {
+				if (Arrays.binarySearch(allowed,cl.toString()) >=0) {
 					
 					return true;
 				}
