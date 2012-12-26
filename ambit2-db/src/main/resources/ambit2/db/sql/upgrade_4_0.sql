925f14c331db3e01fcbed8289f074deddfb3da96
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2012-12-26 09:56:43 +0000

    removing the last foreign key to users table as per https://sourceforge.net/tracker/?func=detail&aid=3526592&group_id=191756&atid=938657

--- ambit2-db/src/main/resources/ambit2/db/sql/upgrade_4_0.sql
+++ ambit2-db/src/main/resources/ambit2/db/sql/upgrade_4_0.sql
@@ -770,4 +770,12 @@ ALTER TABLE `src_dataset` ADD INDEX `Index_7`(`stars`);
 ALTER TABLE `models` ADD COLUMN `stars` INTEGER UNSIGNED NOT NULL DEFAULT 5 COMMENT 'stars rating' AFTER `user_name`,
 	ADD INDEX `Index_12`(`stars`);
 insert into version (idmajor,idminor,comment) values (6,3,"AMBIT2 schema");
+
+ -- -------------------------
+-- Ambit schema 6.4
+-- Additional fields in the query table
+-- -------------------------
+ALTER TABLE `query` ADD COLUMN `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `idtemplate`;
+ALTER TABLE `sessions` DROP FOREIGN KEY `FK_sessions_1`;
+insert into version (idmajor,idminor,comment) values (6,4,"AMBIT2 schema");
  
