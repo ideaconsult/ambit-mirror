d299bd7020d576449d5a9bb7e773eed655b8c99b
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2013-01-08 13:26:16 +0000

    preparing cdk-1.4.16 maven distribution

--- cdk-maven/dist.bat
+++ cdk-maven/dist.bat
@@ -1,2 +1,2 @@
 @echo off
-for /f %%a IN ('dir /b ..\cdk-src+libs-1.4.11\dist\jar\*.jar') do call copyjar.bat %%a
+for /f %%a IN ('dir /b ..\cdk-src+libs-1.4.16\dist\jar\*.jar') do call copyjar.bat %%a
