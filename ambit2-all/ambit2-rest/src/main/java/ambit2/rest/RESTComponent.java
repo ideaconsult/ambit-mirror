8374a26e38ffddf9c83c3b2ae5aca2855ead9545
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2013-02-17 14:09:42 +0000

    let's switch on riap://

--- ambit2-all/ambit2-rest/src/main/java/ambit2/rest/RESTComponent.java
+++ ambit2-all/ambit2-rest/src/main/java/ambit2/rest/RESTComponent.java
@@ -1,18 +1,21 @@
 package ambit2.rest;
 
 import org.restlet.Application;
+import org.restlet.Component;
 import org.restlet.Context;
 import org.restlet.data.Protocol;
-import org.restlet.ext.wadl.WadlComponent;
 
-public class RESTComponent extends WadlComponent {
+public class RESTComponent extends Component {
 
 	public RESTComponent(Context context,Application[] applications) {
 		super();
 		this.getClients().add(Protocol.FILE);
 		this.getClients().add(Protocol.HTTP);
 		this.getClients().add(Protocol.HTTPS);
-
+		this.getClients().add(Protocol.RIAP);
+		
+		getServers().add(Protocol.RIAP);
+		
 		for (Application application: applications) {
 			application.setContext(context==null?getContext().createChildContext():context);
 		    getDefaultHost().attach(application);
