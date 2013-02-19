3f09dc551e1ffcdd0559f1e0e92177e409804598
Nikolay Kochev <nick@uni-plovdiv.net>
2013-02-19 15:25:13 +0000

    Implemented automatic generation of structure sets for tautomer rules statistics.

--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/RuleStructureFactory.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/RuleStructureFactory.java
@@ -6,9 +6,13 @@ import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.silent.SilentChemObjectBuilder;
 import org.openscience.cdk.smiles.SmilesParser;
 import org.openscience.cdk.AtomContainer;
+import org.openscience.cdk.Molecule;
 import org.openscience.cdk.Bond;
 
 import java.util.HashMap;
+import java.util.ArrayList;
+import ambit2.smarts.ChemObjectFactory;
+import ambit2.smarts.SmartsHelper;
 
 
 /**
@@ -31,13 +35,37 @@ public class RuleStructureFactory
 		smilesParser  = new SmilesParser(SilentChemObjectBuilder.getInstance());
 	}
 	
-	public void makeRuleStrcutureFileFor(String smi1, String smi2, int atachPoint, String outFile)
+	public void makeRuleStrcutures(String smi1, int pos1, String smi2, int pos2, 
+							ArrayList<String> smiles, String outFile) throws Exception 
 	{
+		ChemObjectFactory cof = new ChemObjectFactory(SilentChemObjectBuilder.getInstance());
+		ArrayList<ArrayList<String>> smiSets = new ArrayList<ArrayList<String>>(); 
+				
+		for (int i = 0; i < smiles.size(); i++)
+		{	  
+			String base = smiles.get(i);
+			System.out.println(base);
+			IAtomContainer mol1 = cof.connectStructures(smi1, pos1, base, 0, IBond.Order.SINGLE);
+			IAtomContainer mol2 = cof.connectStructures(smi2, pos2, base, 0, IBond.Order.SINGLE);
+			String target1 = SmartsHelper.moleculeToSMILES(new Molecule(mol1));
+			String target2 = SmartsHelper.moleculeToSMILES(new Molecule(mol2));
+			ArrayList<String> list = new ArrayList<String>();
+			list.add(target1.trim());
+			list.add(target2.trim());
+			smiSets.add(list);
+		}
 		
+		cof.saveSmilesTuplesToFile(smiSets, outFile);
 	}
 	
 	
-	
-	
+	public void makeRuleStrcutures(String smi1, int pos1, String smi2, int pos2, String inputSmilesFile, String outFile) throws Exception 
+	{
+		ChemObjectFactory cof = new ChemObjectFactory(SilentChemObjectBuilder.getInstance());
+		ArrayList<String> smiles = cof.loadSmilesStringsFromFile(inputSmilesFile);		
+		makeRuleStrcutures(smi1, pos1, smi2, pos2, smiles, outFile);
+	}
+
+
 	
 }
