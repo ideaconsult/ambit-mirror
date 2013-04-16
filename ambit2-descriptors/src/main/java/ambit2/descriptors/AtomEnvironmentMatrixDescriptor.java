aba1b06c8da9d78d07e209512ff719e6fccc333b
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2013-04-16 11:28:48 +0000

    Atom environment matrix descriptor

---
+++ ambit2-descriptors/src/main/java/ambit2/descriptors/AtomEnvironmentMatrixDescriptor.java
@@ -0,0 +1,367 @@
+/**
+ * <b>Filename</b> AtomEnvironmentDescriptor.java 
+ * @author Nina Jeliazkova nina@acad.bg
+ * <b>Created</b> 2005-6-15
+ * <b>Project</b> ambit
+ */
+package ambit2.descriptors;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.TreeMap;
+
+import org.openscience.cdk.atomtype.IAtomTypeMatcher;
+import org.openscience.cdk.atomtype.SybylAtomTypeMatcher;
+import org.openscience.cdk.config.AtomTypeFactory;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.graph.PathTools;
+import org.openscience.cdk.graph.matrix.AdjacencyMatrix;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IAtomType;
+import org.openscience.cdk.qsar.DescriptorSpecification;
+import org.openscience.cdk.qsar.DescriptorValue;
+import org.openscience.cdk.qsar.IMolecularDescriptor;
+import org.openscience.cdk.qsar.result.IDescriptorResult;
+import org.openscience.cdk.qsar.result.IntegerArrayResult;
+import org.openscience.cdk.qsar.result.IntegerArrayResultType;
+import org.openscience.cdk.silent.SilentChemObjectBuilder;
+
+import ambit2.base.data.Property;
+
+/**
+ * @author Nina Jeliazkova <br>
+ * <b>Modified</b> 2013-4-16
+ */
+public class AtomEnvironmentMatrixDescriptor implements IMolecularDescriptor {
+    //parameters
+	public enum _param {
+		maxLevel {
+			@Override
+			public Class getType() {
+				return Integer.class;
+			}
+		},
+		atomTypeFactory {
+			@Override
+			public Class getType() {
+				return AtomTypeFactory.class;
+			}
+		},
+		atomTypeMatcher {
+			@Override
+			public Class getType() {
+				return IAtomTypeMatcher.class;
+			}
+		};
+		public abstract Class getType();
+	}
+	private int maxLevel = 7;
+	
+	private IAtomTypeMatcher atm = null;
+	private AtomTypeFactory factory = null;
+	//internal data
+	protected boolean empty = true;
+	protected IAtomType[] atomTypes = null;
+	protected TreeMap<String,Integer> map = null;
+	protected String[] descriptorNames;
+    /**
+     * 
+     */
+    public AtomEnvironmentMatrixDescriptor() {
+    }
+
+    /**
+     * 
+     * @see org.openscience.cdk.qsar.IDescriptor#getSpecification()
+     */
+    public DescriptorSpecification getSpecification() {
+        // TODO Define specification !!!!
+		return new DescriptorSpecification(
+				String.format(Property.AMBIT_DESCRIPTORS_ONTOLOGY,"AtomEnvironmentMatrix"),
+				this.getClass().getName(),
+				"$Id: AtomTypeMatrixDescriptor.java,v 1.3 2013/04/16 11:41:0 jeliazkova.nina@gmail.com",
+				"http://ambit.sourceforge.net");        
+    }
+    public IDescriptorResult getDescriptorResultType() {
+    	return new IntegerArrayResultType(1);
+    }
+	/**
+	 *  Gets the parameterNames attribute of the AtomHybridizationDescriptor object
+	 *
+	 *@return    The parameterNames value
+	 */
+	public String[] getParameterNames() {
+		String[] params = new String[_param.values().length];
+		for (int i=0; i < params.length; i++) 
+			params[i] = _param.values()[i].toString();
+		return params;
+	}
+
+	/**
+	 * 
+	 * @see org.openscience.cdk.qsar.IDescriptor#getParameterType(java.lang.String)
+	 */
+    public Object getParameterType(String arg0) {
+        Object[] paramTypes = new Object[3];
+        paramTypes[0] = new Integer(1);
+        //TODO
+        paramTypes[1] = "AtomTypeFactory";
+        paramTypes[2] = "AtomTypeMatcher";
+		return paramTypes;
+    }
+
+	/**
+	 *  Sets the parameters attribute of the AtomEnvironmentDescriptor object
+	 *@param  params - parameters, as below
+	 *params[0]  MaxLevel, optional , default 3
+	 *params[1]  perceiveCyclicAtoms, optional , default false
+	 *params[2]  AtomTypeFactory factory, optional, default AtomTypeFactory.getInstance("org/openscience/cdk/dict/data/sybyl-atom-types.owl")
+	 *params[3]  AtomTypeMatcher atm, optional , default HybridizationStateATMatcher
+	 */
+    public void setParameters(Object[] allParams) throws CDKException {
+        
+        AtomTypeFactory afactory = null;
+        IAtomTypeMatcher amatcher = null;
+        
+        for (int i = 0; i < allParams.length; i++)  {
+        	switch (i) {
+        	case 0: {
+        		if (!(allParams[i] instanceof Integer)) 
+	    			throw new CDKException(String.format("The %d parameter must be of type Integer",i+1));
+        		maxLevel = ((Integer) allParams[i]).intValue();
+        		break;
+        	}
+	    	case 1: {	
+	    		if (!(allParams[i] instanceof Boolean)) 
+	    			throw new CDKException(String.format("The %d parameter must be of type AtomTypeFactory",i+1));
+	    		afactory = (AtomTypeFactory)allParams[i];
+	    		break;
+	    	}
+			case 2: {	
+				if (!(allParams[i] instanceof Boolean)) 
+					throw new CDKException(String.format("The %d parameter must be of type IAtomTypeMatcher",i+1));
+				amatcher = (IAtomTypeMatcher) allParams[i];
+				break;
+			}
+			default: {
+				throw new CDKException(String.format("%s only expects %d parameters",getClass().getName(),allParams.length));
+			}
+        	}
+        };
+				
+		try {
+			descriptorNames =  initAtomTypes(afactory,amatcher);
+		} catch (Exception x) {
+		    throw new CDKException(x.getMessage());
+		}
+    }
+
+    /**
+     * 
+     * @see org.openscience.cdk.qsar.IDescriptor#getParameters()
+     */
+    public Object[] getParameters() {
+		Object[] params = new Object[6];
+		params[0] = new Integer(maxLevel);
+		params[1] = factory; //TODO should it be  factory.clone() ?
+		params[2] = atm; //TODO should it be  atm.clone() ?
+		return params;
+    }
+
+	/**
+	 *  This method calculates the atom environment of an atom.
+	 *
+	 *@param  container         Parameter is the atom container.
+	 *@return                   int[] 
+	 *@exception  CDKException  Description of the Exception
+	 */
+    public DescriptorValue calculate(IAtomContainer container)  {
+    	IntegerArrayResult r = null;
+    	try {
+	    	Hashtable<String,Integer> sparseMatrix = doCalculation(container);
+	    	r = new IntegerArrayResult();
+	    	for (int i=0; i < descriptorNames.length;i++) {
+	    		Integer c = sparseMatrix.get(descriptorNames[i]);
+	    		r.add(c==null?0:c.intValue());
+	    	}
+	        return new DescriptorValue(
+	                getSpecification(), 
+	                getParameterNames(), 
+	                getParameters(), 
+	                r,
+	                getDescriptorNames()
+	                );
+    	} catch (Exception e) {
+    	        return new DescriptorValue(
+    	                getSpecification(), 
+    	                getParameterNames(), 
+    	                getParameters(), 
+    	                r,
+    	                new String[]{getClass().getName()},
+    	                e);    		 
+    	}
+    }
+    public String[] getDescriptorNames() {
+    	return descriptorNames;
+    }
+    
+ 
+    /**
+     * to hold counts of all atom types defined by factory for maxlevel
+     * and one additional entry for undefined atom types 
+     * @return
+     */
+    public int getAtomFingerprintSize() {
+        return (atomTypes.length +1)*(maxLevel)+2;
+        
+    }
+    public String atomTypeToString(int index) {
+        IAtomType aType = getAtomType(index);
+		String atomS=null;
+		if (aType == null) 
+		    atomS = "Misc";
+		else atomS = aType.getAtomTypeName();
+		return atomS;
+    }
+
+
+    private String getKeyLevel0(IAtomType atomType) {
+    	return "L0_"+(atomType==null?"X":atomType.getAtomTypeName());
+    }
+    private String getKeyLevel(IAtomType atomType1,IAtomType atomType2, int level) {
+    	String key1 = atomType1==null?"X":atomType1.getAtomTypeName();
+    	String key2 = atomType2==null?"X":atomType2.getAtomTypeName();
+   	    if (key1.compareTo(key2)>0)
+		   return String.format("L%d_%s_%s",level,key2,key1);
+    	else return String.format("L%d_%s_%s",level,key1,key2);
+    }
+    /**
+     * 
+     * @param arg0
+     * @param result   result[i] contains the level at which ith atom is from the current atom (one set by setParameters)
+     * @throws CDKException
+     */
+    public Hashtable<String,Integer> doCalculation(IAtomContainer atomContainer) throws CDKException {
+    	Hashtable<String,Integer> sparseMatrix = new Hashtable<String, Integer>();
+        if ((atm == null) || (factory == null)) descriptorNames = initAtomTypes(null,null);
+
+        //maxLevel is how many bonds from the atom we will count atom types
+		 int natoms = atomContainer.getAtomCount();
+		 //do atom type matching
+		 atomTypes = factory.getAllAtomTypes();
+		 /*
+		 System.out.println("AtomTypes");
+		 for (IAtomType at :atomTypes) { System.out.print(at.getAtomTypeName());System.out.print("\t");}
+		 System.out.println("End AtomTypes");
+		 */
+		 int[] atomIndex = new int[natoms]; //array of atom type integers
+		 for (int i = 0; i < natoms; i++) 
+		    try {
+		            IAtomType a = atm.findMatchingAtomType(atomContainer,atomContainer.getAtom(i));
+		            if ( a != null) {
+		                Object mappedType = map.get(a.getAtomTypeName());
+		                if (mappedType != null) { 
+		                    atomIndex[i] = ((Integer) mappedType).intValue();
+		                } else {
+		                    atomIndex[i] = -1;
+		                }    
+		            } else { //not found 
+		            	atomIndex[i] = -1;
+		            }
+		            String key = getKeyLevel0(atomIndex[i]>=0?atomTypes[atomIndex[i]]:null);
+		            Integer value = sparseMatrix.get(key);
+		            if (value==null) sparseMatrix.put(key, 1); else sparseMatrix.put(key, value + 1);
+		            
+		    } catch (Exception x) {
+		            throw new CDKException(x.getMessage() + "\ninitConnectionMatrix");
+		    }                
+	    //compute bond distances between all atoms
+	    int[][] aMatrix = PathTools.computeFloydAPSP(AdjacencyMatrix.getMatrix(atomContainer));
+	    //assign values to the results arrays for all atoms
+
+	    for (int i = 0; i < natoms; i++) {
+	    	IAtomType key1 = atomIndex[i]>=0?atomTypes[atomIndex[i]]:null;
+	    	for (int j=i; j < natoms; j++) {
+	    		IAtomType key2 = atomIndex[j]>=0?atomTypes[atomIndex[j]]:null;
+	    	   if (aMatrix[i][j] > 0) { //j is not atom i and bonds less or equal to maxlevel
+	    		   for (int level = 1; level <= 8; level++) {
+	    			   String key = getKeyLevel(key1, key2, level);
+	    			   if (level==8) {
+	    				   if (aMatrix[i][j] > 7) {
+	    					   Integer value = sparseMatrix.get(key);
+			           	   	   if (value==null) sparseMatrix.put(key, 1); else sparseMatrix.put(key, value + 1);
+	    				   } else {}
+	    			   } else if (aMatrix[i][j] == level) {
+	    				   Integer value = sparseMatrix.get(key);
+		           	   	   if (value==null) sparseMatrix.put(key, 1); else sparseMatrix.put(key, value + 1);
+	    			   }
+	    		   }
+			   }	    	    
+	    	}
+	    }
+
+		return sparseMatrix;
+	}
+	
+    protected InputStream getAtomTypeFactoryStream() {
+        return this.getClass().getClassLoader().getResourceAsStream("org/openscience/cdk/dict/data/sybyl-atom-types.owl");
+    }
+    private String[] initAtomTypes(AtomTypeFactory factory,IAtomTypeMatcher atm) throws CDKException {
+        //the idea is not to create objects if they already exist...
+        if ((atm == null) || (factory == null)) {
+            if ( (this.atm == null) || (!(this.atm instanceof SybylAtomTypeMatcher))) {
+                this.atm = SybylAtomTypeMatcher.getInstance(SilentChemObjectBuilder.getInstance());
+                
+                try {
+                	InputStream ins = getAtomTypeFactoryStream();
+                    this.factory = 
+                        AtomTypeFactory.getInstance(ins,"owl",SilentChemObjectBuilder.getInstance());
+
+                } catch (Exception x) {
+                    throw new CDKException(x.getMessage());
+                }
+                empty = true;
+            } else empty = false;
+        } else {
+            this.atm = atm;
+            this.factory = factory;
+            empty = true;
+        }
+        if (empty ) {
+	        atomTypes = this.factory.getAllAtomTypes();
+	        if (map ==null) map = new TreeMap<String,Integer>();
+	        else map.clear();
+	        for (int i = 0; i < atomTypes.length; i++) { 
+	            map.put(atomTypes[i].getAtomTypeName(),new Integer(i));
+	        }	
+
+        }
+        //descriptorNames
+        List<String> dNames = new ArrayList<String>();
+    	dNames.add(getKeyLevel0(null));
+        for (int x1 = 0; x1 < atomTypes.length;x1++) {
+        	IAtomType key1  = atomTypes[x1];
+        	dNames.add(getKeyLevel0(key1));
+        	for (int x2 = x1; x2 < atomTypes.length;x2++) {
+        		IAtomType key2  = atomTypes[x2];
+        		for (int l=1; l <= 8; l++)
+        			dNames.add(getKeyLevel(key1,key2,l));
+        	}
+        	for (int l=1; l <= 8; l++)
+        		dNames.add(getKeyLevel(key1,null,l));
+        }
+        //dNames.add(getClass().getName());
+        Collections.sort(dNames);
+        return dNames.toArray(new String[dNames.size()]);
+    }
+    public IAtomType getAtomType(int index) {
+        if (atomTypes == null) return null;
+        else
+	        if (index == -1) return null;
+	        else return atomTypes[index];
+    }
+}
+
--- ambit2-descriptors/src/test/java/ambit2/descriptors/test/AtomEnvironmentGeneratorTest.java
+++ ambit2-descriptors/src/test/java/ambit2/descriptors/test/AtomEnvironmentGeneratorTest.java
@@ -2,25 +2,29 @@ package ambit2.descriptors.test;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Hashtable;
 
-import junit.framework.Assert;
-
+import org.apache.xalan.xsltc.util.IntegerArray;
 import org.junit.Test;
+import org.openscience.cdk.aromaticity.CDKHueckelAromaticityDetector;
 import org.openscience.cdk.interfaces.IAtomContainer;
-import org.openscience.cdk.nonotify.NoNotificationChemObjectBuilder;
+import org.openscience.cdk.io.iterator.IIteratingChemObjectReader;
+import org.openscience.cdk.qsar.DescriptorValue;
+import org.openscience.cdk.qsar.result.IntegerArrayResult;
+import org.openscience.cdk.silent.SilentChemObjectBuilder;
 import org.openscience.cdk.tools.CDKHydrogenAdder;
+import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 
 import ambit2.base.interfaces.IStructureRecord;
-import ambit2.core.config.AmbitCONSTANTS;
+import ambit2.core.io.MyIteratingMDLReader;
 import ambit2.core.io.RawIteratingSDFReader;
-import ambit2.core.processors.structure.AtomConfigurator;
-import ambit2.core.smiles.SmilesParserWrapper;
 import ambit2.descriptors.AtomEnvironment;
+import ambit2.descriptors.AtomEnvironmentMatrixDescriptor;
 import ambit2.descriptors.processors.AtomEnvironmentGenerator;
 import ambit2.descriptors.processors.AtomEnvironmentList;
 
 public class AtomEnvironmentGeneratorTest {
-
+	protected CDKHydrogenAdder hAdder = null;
 	   /*
 	    public void testAtomEnvironmentDescriptor() throws Exception {
 	    	AtomEnvironmentGenerator gen = new AtomEnvironmentGenerator();
@@ -57,13 +61,14 @@ public class AtomEnvironmentGeneratorTest {
 		public void test() throws Exception {
 			
 			AtomEnvironmentGenerator gen = new AtomEnvironmentGenerator();
-			gen.setMaxLevels(3);
+			gen.setMaxLevels(1);
 			InputStream in = AtomEnvironmentGeneratorTest.class.getClassLoader().getResourceAsStream("ambit2/descriptors/3d/test.sdf");
 			RawIteratingSDFReader reader = new RawIteratingSDFReader(new InputStreamReader(in));
 			while (reader.hasNext()) {
 				IStructureRecord record = reader.nextRecord();
 				record = gen.process(record);
 				AtomEnvironmentList ae = (AtomEnvironmentList) record.getProperty(gen.getProperty());
+				System.out.println();
 				System.out.println(ae);
 				
 				for (AtomEnvironment a : (AtomEnvironmentList) ae) {
@@ -75,6 +80,7 @@ public class AtomEnvironmentGeneratorTest {
 					for (int i:l1) System.out.print(String.format("%d,", i));
 					System.out.println();
 				}
+				
 			}
 			reader.close();
 			/*
@@ -83,5 +89,59 @@ public class AtomEnvironmentGeneratorTest {
 			MoleculeReader reader = new MoleculeReader();
 			reader.process(target)
 			*/
+		}	   
+	   
+	   @Test
+		public void testAtomTypeMatrix() throws Exception {
+			
+			AtomEnvironmentMatrixDescriptor gen = new AtomEnvironmentMatrixDescriptor();
+			InputStream in = AtomEnvironmentGeneratorTest.class.getClassLoader().getResourceAsStream("ambit2/descriptors/3d/test.sdf");
+			IIteratingChemObjectReader<IAtomContainer> reader = new MyIteratingMDLReader(new InputStreamReader(in),SilentChemObjectBuilder.getInstance());
+			while (reader.hasNext()) {
+				IAtomContainer mol = reader.next();
+				AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(mol);				
+                //if (useHydrogens) { //always, otherwise atom types are not recognised correctly
+                	//for some reason H atoms are added as bond references, but not in atom list - bug?
+				try {
+	    			if (hAdder == null) hAdder = CDKHydrogenAdder.getInstance(SilentChemObjectBuilder.getInstance());
+	    		    hAdder.addImplicitHydrogens(mol);
+				} catch (Exception x) {
+					
+				}
+    			CDKHueckelAromaticityDetector.detectAromaticity(mol);        
+				Hashtable<String,Integer> sparseMatrix = gen.doCalculation(mol);
+			    System.out.println("Sparse Matrix");
+			    System.out.println(sparseMatrix);
+			    System.out.println("End Sparse Matrix");
+			}
+			reader.close();
+		}	   	
+	   @Test
+		public void testAtomTypeMatrixDescriptor() throws Exception {
+			
+			AtomEnvironmentMatrixDescriptor gen = new AtomEnvironmentMatrixDescriptor();
+			InputStream in = AtomEnvironmentGeneratorTest.class.getClassLoader().getResourceAsStream("ambit2/descriptors/3d/test.sdf");
+			IIteratingChemObjectReader<IAtomContainer> reader = new MyIteratingMDLReader(new InputStreamReader(in),SilentChemObjectBuilder.getInstance());
+			while (reader.hasNext()) {
+				IAtomContainer mol = reader.next();
+				AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(mol);				
+               //if (useHydrogens) { //always, otherwise atom types are not recognised correctly
+               	//for some reason H atoms are added as bond references, but not in atom list - bug?
+				try {
+	    			if (hAdder == null) hAdder = CDKHydrogenAdder.getInstance(SilentChemObjectBuilder.getInstance());
+	    		    hAdder.addImplicitHydrogens(mol);
+				} catch (Exception x) {
+					
+				}
+   			CDKHueckelAromaticityDetector.detectAromaticity(mol);        
+   			DescriptorValue value = gen.calculate(mol);
+			    System.out.println("Value");
+			    for (int i=0; i < value.getNames().length; i++)  
+			    	if (((IntegerArrayResult)value.getValue()).get(i)>0) {
+			    		System.out.println(value.getNames()[i] + " = " + ((IntegerArrayResult)value.getValue()).get(i));		
+			    	}
+			    System.out.println("End Value");
+			}
+			reader.close();
 		}	   
 }
