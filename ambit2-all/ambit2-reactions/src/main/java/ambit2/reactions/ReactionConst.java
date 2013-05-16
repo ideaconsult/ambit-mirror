48dd63ffd2786e936862af29167e939b716c8fef
Nikolay Kochev <nick@uni-plovdiv.net>
2013-05-16 14:33:57 +0000

    Added new classes. Added SA strategy constants.

--- ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/ReactionConst.java
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/ReactionConst.java
@@ -5,4 +5,12 @@ public class ReactionConst
 	public static final String KeyWordPrefix = "$$";
 	public static final String KeyWordSeparator = "=";
 	public static final String KeyWordElementSeparator = ",";
+	
+		
+	public static final int SA_STRATEGY_DESCRIPTORS = 0;
+	public static final int SA_STRATEGY_START_MATERIALS = 1;
+	public static final int SA_STRATEGY_RETRO_SYNTHESIS = 2;
+	
+	
+	
 }
