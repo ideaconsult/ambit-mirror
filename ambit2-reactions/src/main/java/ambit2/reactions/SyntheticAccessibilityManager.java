d7fa950ef1fee2caff733a649652b9268e3dc784
Nikolay Kochev <nick@uni-plovdiv.net>
2013-05-16 19:39:51 +0000

    Moved functionality from class SimilarityManager to class SyntheticAccessibilityManager.

--- ambit2-reactions/src/main/java/ambit2/reactions/SyntheticAccessibilityManager.java
+++ ambit2-reactions/src/main/java/ambit2/reactions/SyntheticAccessibilityManager.java
@@ -3,5 +3,15 @@ package ambit2.reactions;
 
 public class SyntheticAccessibilityManager 
 {
-	
+	private int strategy = ReactionConst.SA_STRATEGY_DESCRIPTORS;
+
+	public int getStrategy()
+	{
+		return strategy;
+	}
+
+	public void setStrategy(int newStrategy)
+	{
+		strategy = newStrategy;
+	}
 }
