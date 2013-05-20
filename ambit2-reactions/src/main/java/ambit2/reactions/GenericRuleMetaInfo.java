625e4682323dd14af7badb17fd22700305ebfa84
Nikolay Kochev <nick@uni-plovdiv.net>
2013-05-20 21:21:29 +0000

    Added two generic helper classes for Rule parsing.

---
+++ ambit2-reactions/src/main/java/ambit2/reactions/GenericRuleMetaInfo.java
@@ -0,0 +1,10 @@
+package ambit2.reactions;
+
+import java.util.ArrayList;
+
+public class GenericRuleMetaInfo 
+{
+	public ArrayList<String> keyWord = new ArrayList<String>();
+	public ArrayList<String> objectFieldName = new ArrayList<String>();
+	//public ArrayList<String> objectFieldType = new ArrayList<String>();
+}
