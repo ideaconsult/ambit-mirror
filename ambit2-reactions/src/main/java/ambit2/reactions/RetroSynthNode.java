3165c0eaadda5ca212baf2b3f449227e08b854e8
Nikolay Kochev <nick@uni-plovdiv.net>
2013-05-22 16:51:07 +0000

    Implemented some principle functionality.

--- ambit2-reactions/src/main/java/ambit2/reactions/RetroSynthNode.java
+++ ambit2-reactions/src/main/java/ambit2/reactions/RetroSynthNode.java
@@ -1,7 +1,13 @@
 package ambit2.reactions;
 
+import java.util.ArrayList;
 
 public class RetroSynthNode 
-{
+{	
 	
+	public ArrayList<RetroSynthNode> generateChildrenNodes()
+	{
+		//TODO
+		return  null;
+	}
 }
