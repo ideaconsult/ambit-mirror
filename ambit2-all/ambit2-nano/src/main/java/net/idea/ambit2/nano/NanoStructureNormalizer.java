9f9000e6835bd41fe822e3999e79c232d8da73e0
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2013-06-13 18:56:07 +0000

    ambit:ambit2-nano module

---
+++ ambit2-all/ambit2-nano/src/main/java/net/idea/ambit2/nano/NanoStructureNormalizer.java
@@ -0,0 +1,119 @@
+/* NanoStructureNormalizer.java
+ * Author: nina
+ * Date: June 13, 2013
+ * Revision: 0.1 
+ * 
+ * Copyright (C) 2005-2013  Ideaconsult Ltd.
+ * 
+ * Contact: www.ideaconsult.net
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ * All we ask is that proper credit is given for our work, which includes
+ * - but is not limited to - adding the above copyright notice to the beginning
+ * of your source code files, and to any copyright notice that you may distribute
+ * with programs based on this work.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
+
+package net.idea.ambit2.nano;
+
+import java.io.IOException;
+
+import nu.xom.ParsingException;
+
+import org.bitbucket.nanojava.data.Nanomaterial;
+import org.bitbucket.nanojava.io.Deserializer;
+import org.openscience.cdk.tools.manipulator.MolecularFormulaManipulator;
+
+import ambit2.base.data.LiteratureEntry;
+import ambit2.base.data.Property;
+import ambit2.base.exceptions.AmbitException;
+import ambit2.base.interfaces.IStructureRecord;
+import ambit2.base.interfaces.IStructureRecord.MOL_TYPE;
+import ambit2.base.interfaces.IStructureRecord.STRUC_TYPE;
+import ambit2.db.processors.StructureNormalizer;
+
+/**
+ * As {@link StructureNormalizer} but supports nanomaterials
+ * @author nina
+ *
+ */
+public class NanoStructureNormalizer extends StructureNormalizer {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -796323273484515707L;
+	public NanoStructureNormalizer() {
+		super();
+	}
+	public IStructureRecord process(IStructureRecord structure)	throws AmbitException {
+		if ((structure.getFormat()!=null) && MOL_TYPE.NANO.name().equals(structure.getFormat())) {
+			//process nanomaterial
+			try {
+				return normalizeNano(structure);
+			} catch (IOException x) {
+				throw new AmbitException(x);
+			} catch (ParsingException x) {
+				throw new AmbitException(x);
+			}
+			
+		} else return super.process(structure);
+			
+		
+	}
+
+	/**
+	 * Parses the nano CML and sets the properties
+	 * @param structure
+	 * @return
+	 * @throws IOException
+	 * @throws ParsingException
+	 */
+	public static IStructureRecord normalizeNano(IStructureRecord structure)	throws IOException, ParsingException {
+		Nanomaterial nanomaterial = Deserializer.fromCMLString(structure.getContent()); 
+		structure.setFormula(nanomaterial.getChemicalComposition()==null?null:  MolecularFormulaManipulator.getHillString(nanomaterial.getChemicalComposition()));
+		LiteratureEntry ref = new LiteratureEntry("NanoMaterialMeasurement", "nano");
+
+		if (nanomaterial.getType()!=null) {
+			Property property = new Property("MaterialType",ref);
+			property.setLabel("http://example.com/nano#Material");
+			structure.setProperty(property, nanomaterial.getType());
+		}
+		if (nanomaterial.getSize()!=null) {
+			Property property = new Property("Size", nanomaterial.getSize().getUnit().toString(),ref);
+			property.setLabel("http://example.com/nano#Size");
+			structure.setProperty(property, nanomaterial.getSize().getString());
+		}
+		if (nanomaterial.getZetaPotential()!=null) {
+			Property property = new Property("zetaPotential", nanomaterial.getZetaPotential().getUnit().toString(),ref);
+			property.setLabel("http://example.com/nano#zetaPotential");
+			structure.setProperty(property,nanomaterial.getZetaPotential().getString());
+		}
+		for (int i=0;i< nanomaterial.getLabels().size();i++) {
+			ref = new LiteratureEntry(String.format("NanoMaterialLabel%d",i+1), "nano");
+			Property property = new Property("Name", ref);
+			property.setLabel(Property.opentox_Name);
+			structure.setProperty(property,nanomaterial.getLabels().get(i));	
+		}
+		structure.setType(STRUC_TYPE.NANO);
+		structure.setIdchemical(-1);
+		structure.setIdstructure(-1);
+		structure.setInchi(null);
+		structure.setInchiKey(null);
+		structure.setSmiles(null);
+		return structure;
+	}
+}
---
+++ ambit2-all/ambit2-nano/src/main/java/net/idea/ambit2/rest/nano/NanoCMLIteratingReader.java
@@ -0,0 +1,91 @@
+package net.idea.ambit2.rest.nano;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
+
+import nu.xom.Element;
+
+import org.bitbucket.nanojava.data.Nanomaterial;
+import org.bitbucket.nanojava.io.Deserializer;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IChemObjectBuilder;
+import org.openscience.cdk.io.formats.IResourceFormat;
+import org.openscience.cdk.io.formats.MDLV2000Format;
+import org.openscience.cdk.io.iterator.DefaultIteratingChemObjectReader;
+import org.openscience.cdk.silent.SilentChemObjectBuilder;
+import org.xmlcml.cml.base.CMLBuilder;
+import org.xmlcml.cml.element.CMLList;
+
+import ambit2.base.data.StructureRecord;
+import ambit2.base.interfaces.IStructureRecord;
+import ambit2.base.interfaces.IStructureRecord.MOL_TYPE;
+import ambit2.core.io.IRawReader;
+
+public class NanoCMLIteratingReader  extends DefaultIteratingChemObjectReader implements IRawReader<IStructureRecord> {
+	protected List<Nanomaterial> nanomaterials;
+	protected IStructureRecord record = new StructureRecord();
+	protected int index = -1;
+	
+	public NanoCMLIteratingReader(InputStream in) throws Exception {
+		this(in,SilentChemObjectBuilder.getInstance());
+
+    }
+	public NanoCMLIteratingReader(InputStream in,IChemObjectBuilder builder) throws Exception {
+		super();
+		setReader(in);
+
+    }	
+	public void setReader(InputStream in) throws CDKException {
+		setReader(new InputStreamReader(in));
+
+	}
+	public void setReader(Reader reader) throws CDKException {
+		try {
+	        Element root = null;
+	        root = new CMLBuilder().build(reader).getRootElement();
+	        //quick and dirty
+			nanomaterials = Deserializer.fromCML((CMLList)root);
+		} catch (Exception x) {
+			throw new CDKException(x.getMessage(),x);
+		}
+		
+	}
+
+	public IStructureRecord nextRecord() {
+		return record;
+	}
+
+	public void close() throws IOException {
+		
+	}
+
+	public IResourceFormat getFormat() {
+		 return MDLV2000Format.getInstance();
+	}
+
+	public boolean hasNext() {
+		index ++;
+		if (index>=nanomaterials.size()) {
+			record.clear();
+			return false; 
+		}
+		record.clear();
+		record.setContent(org.bitbucket.nanojava.io.Serializer.toCML(nanomaterials.get(index)).toXML());
+		record.setFormat(MOL_TYPE.NANO.name());
+		return true;
+	}
+	
+	protected IStructureRecord createRecord() {
+		if (record == null) return new StructureRecord();
+		else record.clear(); return record;
+	}
+	public Object next() {
+		return  record;
+	}
+	
+
+	
+}
