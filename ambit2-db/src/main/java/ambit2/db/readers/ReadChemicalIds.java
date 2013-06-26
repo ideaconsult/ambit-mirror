5354e3f09f955bdff9a06f0c9be0f73c0214f573
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2013-06-26 19:24:08 +0000

    Include smiles, inchi , inchikey and formula in json

---
+++ ambit2-db/src/main/java/ambit2/db/readers/ReadChemicalIds.java
@@ -0,0 +1,55 @@
+package ambit2.db.readers;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import ambit2.base.exceptions.AmbitException;
+import ambit2.base.interfaces.IStructureRecord;
+import ambit2.db.search.AbstractQuery;
+import ambit2.db.search.EQCondition;
+import ambit2.db.search.QueryParam;
+
+public class ReadChemicalIds  extends AbstractQuery<Object,IStructureRecord,EQCondition,IStructureRecord> implements IQueryRetrieval<IStructureRecord>  {
+	private static String sql = "select idchemical,inchi,smiles,formula,inchikey,label,lastmodified from chemicals where idchemical=?";
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2064999650126789112L;
+
+	@Override
+	public List<QueryParam> getParameters() throws AmbitException {
+		List<QueryParam> params = new ArrayList<QueryParam>();
+		params.add(new QueryParam<Integer>(Integer.class, getValue().getIdchemical()));
+		return params;		
+	}
+	@Override
+	public String getSQL() throws AmbitException {
+		return sql;
+	}
+	@Override
+	public IStructureRecord getObject(ResultSet rs) throws AmbitException {
+		IStructureRecord r = getValue();
+		try {
+			r.setIdchemical(rs.getInt("idchemical"));
+			r.setFormula(rs.getString("formula"));
+			r.setInchi(rs.getString("inchi"));
+			r.setInchiKey(rs.getString("inchikey"));
+			r.setSmiles(rs.getString("smiles"));
+			return r;
+		} catch (Exception x) {
+			throw new AmbitException(x);
+		}
+	}
+	@Override
+	public boolean isPrescreen() {
+		return false;
+	}
+	@Override
+	public double calculateMetric(IStructureRecord object) {
+		return 1;
+	}
+	
+	
+
+}
