caca41cd3da124ad3464bb46fb9bf66a3a3ebfcd
Nikolay Kochev <nick@uni-plovdiv.net>
2013-07-16 05:51:03 +0000

    Fixed a subtle bug in TautomerRanking. Now the temperature is set via function setT().

--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/TautomerRanking.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/TautomerRanking.java
@@ -6,18 +6,40 @@ import org.openscience.cdk.interfaces.IAtomContainer;
 
 public class TautomerRanking 
 {
-	public double T = 300; //K
+	private double T = 300; //K
 	public static final double kB = 1.3806488E-23; //  [J/K]	
 	public static final double KeVtoJ = 1.602176565E-19;    //1eV = 1.602176565e-19 J = 1.602176565 10-19 J
-	public double energyConst = KeVtoJ/(T*kB);
+	public double energyConst;
 	public double eShift = 0; //eV;
 	
+	
+	public TautomerRanking()
+	{	
+		setT(300);
+	}
+	
+	public TautomerRanking(double temperature)
+	{	
+		setT(temperature);  
+	}
 	
 	public double getProbability(double rank)
 	{
 		double p = Math.exp(-(rank-eShift)*energyConst);
 		return p;
 	}
+	
+	public void setT(double T)
+	{
+		this.T = T;
+		energyConst = KeVtoJ/(T*kB);
+	}
+	
+	public double getT()
+	{
+		return T;
+	}
+	
 	
 	public double[] getProbabilityDistribution(Vector<IAtomContainer> tautomers)
 	{
