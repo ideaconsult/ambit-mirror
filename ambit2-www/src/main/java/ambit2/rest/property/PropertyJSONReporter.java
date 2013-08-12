ca53dcb561000b45290eb352477d315840d2797d
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2013-08-12 07:35:18 +0000

    Fix for http://sourceforge.net/p/ambit/bugs/70/

--- ambit2-www/src/main/java/ambit2/rest/property/PropertyJSONReporter.java
+++ ambit2-www/src/main/java/ambit2/rest/property/PropertyJSONReporter.java
@@ -10,7 +10,6 @@ import ambit2.base.data.ILiteratureEntry._type;
 import ambit2.base.data.Property;
 import ambit2.base.data.PropertyAnnotation;
 import ambit2.base.exceptions.AmbitException;
-import ambit2.base.interfaces.ICategory;
 import ambit2.base.json.JSONUtils;
 import ambit2.db.readers.IQueryRetrieval;
 
@@ -109,28 +108,28 @@ public class PropertyJSONReporter extends PropertyURIReporter {
 			getOutput().write(String.format(
 					"\n\"%s\":{\n" + //uri
 					"\n\t\"type\":\"Feature\"," + //uri
-					"\n\t\"%s\":\"%s\"," + //title
-					"\n\t\"%s\":\"%s\"," + //units
+					"\n\t\"%s\":%s," + //title
+					"\n\t\"%s\":%s," + //units
 					"\n\t\"%s\":\"%s\"," + //nominal
 					"\n\t\"%s\":\"%s\"," + //numeric
-					"\n\t\"%s\":\"%s\"," + //sameAs
+					"\n\t\"%s\":%s," + //sameAs
 					"\n\t\"%s\":\"%s\"," + //isModelPredictionFeature
 					"\n\t\"%s\":\"%s\"," + //creator
 					"\n\t\"%s\":%d," + //order
-					"\n\t\"%s\":{\n\t\t\"URI\":\"%s\",\n\t\t\"type\":\"%s\"\n\t}," + 					//source
+					"\n\t\"%s\":{\n\t\t\"URI\":%s,\n\t\t\"type\":%s\n\t}," + 					//source
 					"\n\t\"%s\":[%s]\n\n}",
 					uri,
-					jsonFeature.title.jsonname(),JSONUtils.jsonEscape(feature.getName()),
-					jsonFeature.units.jsonname(),feature.getUnits()==null?"":feature.getUnits(),
+					jsonFeature.title.jsonname(),JSONUtils.jsonQuote(JSONUtils.jsonEscape(getAggregatedFeatureName(feature))),
+					jsonFeature.units.jsonname(),JSONUtils.jsonQuote(JSONUtils.jsonEscape(feature.getUnits())),
 					jsonFeature.isNominal.jsonname(),feature.isNominal(),
 					jsonFeature.isNumeric.jsonname(),numeric,
-					jsonFeature.sameAs.jsonname(),uriSameAs==null?null:JSONUtils.jsonEscape(uriSameAs),
+					jsonFeature.sameAs.jsonname(),JSONUtils.jsonQuote(JSONUtils.jsonEscape(uriSameAs)),
 					jsonFeature.isModelPredictionFeature.jsonname(),isModelPredictionFeature,
 					jsonFeature.creator.jsonname(),JSONUtils.jsonEscape(feature.getReference().getURL()),
 					jsonFeature.order.jsonname(),feature.getOrder(),
 					jsonFeature.source.jsonname(),
-						uriSource==null?null:JSONUtils.jsonEscape(uriSource),
-						typeSource==null?null:JSONUtils.jsonEscape(typeSource),
+						JSONUtils.jsonQuote(JSONUtils.jsonEscape(uriSource)),
+						JSONUtils.jsonQuote(JSONUtils.jsonEscape(typeSource)),
 					jsonFeature.annotation.jsonname(),
 					annotation==null?"":annotation
 					
@@ -142,6 +141,35 @@ public class PropertyJSONReporter extends PropertyURIReporter {
 			x.printStackTrace();
 		}
 		return feature;
+	}
+	
+	private String getAggregatedFeatureName(Property feature) {
+		if (feature.getName()==null) return null;
+		if (feature.getName().startsWith("http://www.opentox.org/")) {
+			if (Property.opentox_CAS.equals(feature.getName())) return Property.CAS;
+			if (Property.opentox_IupacName.equals(feature.getName())) return "IUPAC name";
+			if (Property.opentox_Name.equals(feature.getName())) return Property.Names;
+			if (Property.opentox_InChI.equals(feature.getName())) return "InChI";
+			if (Property.opentox_InChI_std.equals(feature.getName())) return "Std. InChI";
+			if (Property.opentox_InChIKey.equals(feature.getName())) return "InChI Key";
+			if (Property.opentox_InChIKey_std.equals(feature.getName())) return "Std. InChI key";
+			if (Property.opentox_SMILES.equals(feature.getName())) return "SMILES";
+			if (Property.opentox_REACHDATE.equals(feature.getName())) return "REACH registration date";
+			if (Property.opentox_EC.equals(feature.getName())) return "EC number";
+			
+			/* Other database IDs */
+			if (Property.opentox_Pubchem.equals(feature.getName())) return "PUBCHEM ID";
+			if (Property.opentox_ChEBI.equals(feature.getName())) return "CHEBI ID";
+			if (Property.opentox_ChEMBL.equals(feature.getName())) return "CHEMBL ID";
+			if (Property.opentox_ChemSpider.equals(feature.getName())) return "ChemSpider ID";
+			/* IUCLID5 Reference substance UUID */
+			if (Property.opentox_IUCLID5_UUID.equals(feature.getName())) return "IUCLID 5 Reference substance UUID";
+	
+			/* Mostly for usage in ToxBank */
+			if (Property.opentox_ToxbankWiki.equals(feature.getName())) return "ToxBank Wiki URL";
+			if (Property.opentox_CMS.equals(feature.getName())) return "COSMOS ID";
+		}
+		return feature.getName();
 	}
 
 	public static String annotation2JSON(Property feature) {
