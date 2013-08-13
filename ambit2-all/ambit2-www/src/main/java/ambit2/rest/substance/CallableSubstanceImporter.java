397369c310615f1bd0273416bf9d24e073b96a02
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2013-08-13 16:58:49 +0000

    And the substance importer itself

---
+++ ambit2-all/ambit2-www/src/main/java/ambit2/rest/substance/CallableSubstanceImporter.java
@@ -0,0 +1,143 @@
+package ambit2.rest.substance;
+
+import java.io.File;
+import java.sql.Connection;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+
+import net.idea.i5.io.I5ZReader;
+
+import org.apache.commons.fileupload.FileItem;
+import org.openscience.cdk.io.IChemObjectReaderErrorHandler;
+import org.restlet.Context;
+import org.restlet.data.Form;
+import org.restlet.data.Reference;
+import org.restlet.data.Status;
+import org.restlet.resource.ResourceException;
+
+import ambit2.base.exceptions.AmbitException;
+import ambit2.base.interfaces.IBatchStatistics;
+import ambit2.base.interfaces.IProcessor;
+import ambit2.base.interfaces.IStructureRecord;
+import ambit2.base.processors.ProcessorsChain;
+import ambit2.core.io.FileInputState;
+import ambit2.core.io.IInputState;
+import ambit2.db.processors.AbstractBatchProcessor;
+import ambit2.db.processors.BatchDBProcessor;
+import ambit2.db.processors.DBProcessorsChain;
+import ambit2.db.processors.DBSubstanceWriter;
+import ambit2.rest.task.CallableFileUpload;
+import ambit2.rest.task.CallableQueryProcessor;
+import ambit2.rest.task.TaskResult;
+
+/**
+ * 
+ * @author nina
+ *
+ * @param <USERID>
+ */
+public class CallableSubstanceImporter<USERID> extends CallableQueryProcessor<FileInputState, IStructureRecord,USERID> {
+
+	private File file;
+	protected String fileDescription;
+	protected File getFile() {
+		return file;
+	}
+
+	protected void setFile(File file,String description) {
+		this.file = file;
+		this.fileDescription = description;
+	}
+	
+	public CallableSubstanceImporter(List<FileItem> items,String fileUploadField, Reference applicationRootReference,Context context,USERID token) throws Exception {
+		super(applicationRootReference,null,  context,  token);
+		try { processForm(items, fileUploadField); } catch (Exception x) {}
+	}
+	@Override
+	protected void processForm(Reference applicationRootReference, Form form) {
+		sourceReference = null;
+	}
+	
+	
+	protected void processForm(List<FileItem> items,String fileUploadField) throws Exception {
+		CallableFileUpload upload = new CallableFileUpload(items, fileUploadField) {
+			@Override
+			public Reference createReference() {
+				return null;
+			}
+
+			@Override
+			protected void processFile(File file,String description) throws Exception {
+				setFile(file,description);
+			}
+			@Override
+			protected void processProperties(
+					Hashtable<String, String> properties) throws Exception {
+			}
+		};
+		upload.call();
+	}
+	
+
+	@Override
+	protected FileInputState createTarget(Reference reference) throws Exception {
+		if (file==null) throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST);
+		return new FileInputState(file);
+	}
+	
+	@Override
+	protected AbstractBatchProcessor createBatch(FileInputState target)
+			throws Exception {
+		final BatchDBProcessor batch = new BatchDBProcessor() {
+			@Override
+			public Iterator<String> getIterator(IInputState target)
+					throws AmbitException {
+				try {
+					File file = ((FileInputState) target).getFile();
+					I5ZReader reader = new I5ZReader(file);
+					reader.setErrorHandler(new IChemObjectReaderErrorHandler() {
+							@Override
+							public void handleError(String message, int row, int colStart, int colEnd,
+									Exception exception) {
+							}
+							@Override
+							public void handleError(String message, int row, int colStart, int colEnd) {
+							}
+							@Override
+							public void handleError(String message, Exception exception) {
+							}
+							@Override
+							public void handleError(String message) {
+							}
+						});
+					 return reader;
+				} catch (AmbitException x) {
+
+					throw x;
+				} catch (Exception x) {
+					throw new AmbitException(x);
+				}
+			}
+		};
+		return batch;
+	}
+
+	@Override
+	protected ProcessorsChain<IStructureRecord, IBatchStatistics, IProcessor> createProcessors()
+			throws Exception {
+		DBProcessorsChain chain = new DBProcessorsChain();
+		DBSubstanceWriter writer = new DBSubstanceWriter();
+		chain.add(writer);
+		return chain;
+	}
+
+	@Override
+	protected TaskResult createReference(Connection connection)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
