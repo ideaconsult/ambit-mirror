5c72eff1ca8e3861484188d7cd99945c0376a4a7
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2013-09-28 05:47:19 +0000

    the REST URI for compound relations was changed a while ago

--- ambit2-all/ambit2-www/src/main/java/ambit2/rest/task/tautomers/CallableTautomersGenerator.java
+++ ambit2-all/ambit2-www/src/main/java/ambit2/rest/task/tautomers/CallableTautomersGenerator.java
@@ -34,7 +34,7 @@ public class CallableTautomersGenerator<USERID> extends CallableModelPredictor<I
 				throw new ResourceException(Status.SERVER_ERROR_NOT_IMPLEMENTED,"Datasets not on this server are not supported!");
 			} else {
 				TaskResult task = new TaskResult(
-						String.format("%s/query/relation/HAS_TAUTOMER?dataset_uri=%s",
+						String.format("%s/query/relation/compound/HAS_TAUTOMER?dataset_uri=%s",
 						applicationRootReference,		
 						Reference.encode(sourceReference.toString()))
 						);
