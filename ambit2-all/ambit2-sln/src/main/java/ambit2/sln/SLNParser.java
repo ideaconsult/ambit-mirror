331e8bcf7dcb482244c8b64d04699a1d15c1a4a6
Nikolay Kochev <nick@uni-plovdiv.net>
2013-10-25 13:29:43 +0000

    Added 3 new classes and some general functions/flags in the SLNContainer class.

--- ambit2-all/ambit2-sln/src/main/java/ambit2/sln/SLNParser.java
+++ ambit2-all/ambit2-sln/src/main/java/ambit2/sln/SLNParser.java
@@ -5,6 +5,18 @@ public class SLNParser
 {
 	String sln;
 	SLNContainer container;
+	SLNDictionary globalDictionary = null;
+	
+	
+	public SLNParser()
+	{
+		globalDictionary = PredefinedSLNDictionary.getDictionary();
+	}
+	
+	public SLNParser(SLNDictionary globalDictionary)
+	{
+		this.globalDictionary = globalDictionary;
+	}
 	
 	
 	public SLNContainer parse(String sln)
