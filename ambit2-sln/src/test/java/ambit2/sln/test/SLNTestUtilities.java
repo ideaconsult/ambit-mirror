5edcfa71761f610628ad80c4111643bfe37de176
Nikolay Kochev <nick@uni-plovdiv.net>
2013-10-30 14:37:51 +0000

    Added class for test utilities.

---
+++ ambit2-sln/src/test/java/ambit2/sln/test/SLNTestUtilities.java
@@ -0,0 +1,17 @@
+package ambit2.sln.test;
+
+
+
+public class SLNTestUtilities 
+{
+	public static void main(String[] args) throws Exception
+	{
+		SLNTestUtilities tu = new SLNTestUtilities();
+	}
+	
+	public void testSLN(String sln)
+	{
+		//TODO
+	}
+	
+}
