7dd0f4330fcc65073506635e6848c50c59c6ddce
Nikolay Kochev <nick@uni-plovdiv.net>
2013-11-02 17:13:22 +0000

    Implemented new class for CTIDescriptor and added a helper class TestUtilities.

---
+++ ambit2-all/ambit2-descriptors/src/main/java/ambit2/descriptors/topological/CTIDescriptor.java
@@ -0,0 +1,113 @@
+package ambit2.descriptors.topological;
+
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.qsar.DescriptorSpecification;
+import org.openscience.cdk.qsar.DescriptorValue;
+import org.openscience.cdk.qsar.result.DoubleResult;
+import org.openscience.cdk.qsar.result.IDescriptorResult;
+import org.openscience.cdk.charges.GasteigerMarsiliPartialCharges;
+import org.openscience.cdk.graph.PathTools;
+import org.openscience.cdk.graph.matrix.ConnectionMatrix;
+
+import java.util.List;
+
+
+
+public class CTIDescriptor 
+{
+	public DescriptorSpecification getSpecification()
+	{
+		return new DescriptorSpecification(
+				"Charge-related Topological Index (CTI)",
+				this.getClass().getName(),
+				"$Id: CTIDescriptor.java, v 0.1 2013 Nikolay Kochev",
+				"http://ambit.sourceforge.net");
+	}
+
+	public void setParameters(Object[] params) throws CDKException
+	{	
+	}
+
+	public Object[] getParameters()
+	{
+		return null;
+	}
+
+	public String[] getDescriptorNames() 
+	{
+		return new String[] {"CTI"};
+	}
+
+	public DescriptorValue calculate(IAtomContainer container) throws Exception
+	{
+		//Get partial charger
+		//GasteigerPEPEPartialCharges partCharges = new GasteigerPEPEPartialCharges();	
+		GasteigerMarsiliPartialCharges partCharges = new GasteigerMarsiliPartialCharges();	
+		//partCharges.assignGasteigerPiPartialCharges(container, true);
+		partCharges.assignGasteigerMarsiliSigmaPartialCharges(container, true);
+		
+		//Get topological distance matrix		
+		double[][] matr = ConnectionMatrix.getMatrix(container);        
+        int[][] D = PathTools.computeFloydAPSP(matr);
+		double CTI = 0.0;
+		double L[] =  getLocalIndexes(container);
+		
+		int n = container.getAtomCount();
+		for (int i = 0; i < n; i++)		
+			for (int j = i+1; j < n; j++)
+			{	
+				if (D[i][j] > 0)
+					CTI += L[i]*L[j] / D[i][j];
+			}	
+		
+		return new DescriptorValue(getSpecification(), getParameterNames(), getParameters(),
+				new DoubleResult(CTI), getDescriptorNames());
+	}
+	
+	double [] getLocalIndexes(IAtomContainer container)
+	{
+		double L[] = new double [container.getAtomCount()];
+		for (int i = 0; i < container.getAtomCount(); i++) 
+		{
+        	IAtom a = container.getAtom(i);
+        	int implH = 0;
+        	if (a.getImplicitHydrogenCount() != null)
+        		implH = a.getImplicitHydrogenCount();
+        	
+        	int nH = implH + getExplicitHCount(container, a);
+        	int val = 1;
+        	if (a.getValency() != null)
+        		val = a.getValency();
+        	
+			L[i] = val - nH + a.getCharge() ;
+        }
+		return L;
+	}
+	
+	int getExplicitHCount(IAtomContainer container, IAtom atom)
+	{
+		int explH = 0;
+		List<IAtom> list = container.getConnectedAtomsList(atom);
+		for (IAtom a: list)
+			if (a.getSymbol().equals("H"))
+				explH++;
+		return explH;
+	}
+	
+	public IDescriptorResult getDescriptorResultType()
+	{
+		return new DoubleResult(0.0);
+	}
+
+	public String[] getParameterNames() 
+	{
+		return null;
+	}
+
+	public Object getParameterType(String name) 
+	{
+		return "";
+	}
+}
---
+++ ambit2-all/ambit2-descriptors/src/test/java/ambit2/descriptors/test/TestUtilities.java
@@ -0,0 +1,50 @@
+package ambit2.descriptors.test;
+
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.qsar.DescriptorValue;
+import org.openscience.cdk.qsar.result.DoubleResult;
+import org.openscience.cdk.qsar.result.IDescriptorResult;
+
+import ambit2.descriptors.topological.CTIDescriptor;
+import ambit2.smarts.SmartsHelper;
+
+public class TestUtilities 
+{
+	public static void main(String[] args) throws Exception
+	{
+		calcCTI("CCCCC", false);
+		calcCTI("CC(C)CC", false);
+		calcCTI("CC(C)(C)C", false);
+		calcCTI("NCCCC", false);
+		calcCTI("CNCCC", false);
+		calcCTI("CCNCC", false);
+		calcCTI("NC(C)CC", false);
+		calcCTI("CN(C)CC", false);
+		calcCTI("CC(C)NC", false);
+		calcCTI("CC(C)CN", false);
+		calcCTI("NC(C)(C)C", false);
+		
+		//calcCTI("NCCN");
+		//calcCTI("OC1=C(O)C2=C(C=C1)C3(OC(=O)C4=C3C=CC=C4)C5=C(O2)C(=C(O)C=C5)O");
+	}
+	
+	public static void calcCTI(String smiles, boolean FlagPrintAtomAttrib) throws Exception
+	{
+		IAtomContainer mol = SmartsHelper.getMoleculeFromSmiles(smiles, false);
+		CTIDescriptor descr = new CTIDescriptor();
+		DescriptorValue dValue = descr.calculate(mol);
+		IDescriptorResult res = dValue.getValue();		
+		double CTI = ((DoubleResult)res).doubleValue();
+		System.out.println(smiles + "  CTI = " + CTI);
+		
+		if (FlagPrintAtomAttrib)
+			for (int i = 0; i < mol.getAtomCount(); i++) 
+			{
+				IAtom a = mol.getAtom(i);
+				System.out.println("Atom " + i + "  L0=" 
+						+  a.getValency() + "  H=" + a.getImplicitHydrogenCount() 
+						+ "  q=" + a.getCharge());
+			}
+	}
+}
