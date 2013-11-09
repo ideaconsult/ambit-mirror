796cc46c38675d69594ded0a02ffba62e4bdd582
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2013-11-09 08:26:13 +0000

    Cleaned up code of molecular weight descriptors. A more efficient implementation would use a single descriptor class returning multiple values and  calculating all derived quantities in one go.

--- ambit2-descriptors/src/main/java/ambit2/descriptors/constitutional/AverageMolecularWeightDescriptor.java
+++ ambit2-descriptors/src/main/java/ambit2/descriptors/constitutional/AverageMolecularWeightDescriptor.java
@@ -1,55 +1,36 @@
 package ambit2.descriptors.constitutional;
 
-import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.qsar.DescriptorSpecification;
 import org.openscience.cdk.qsar.DescriptorValue;
-import org.openscience.cdk.qsar.IMolecularDescriptor;
-import org.openscience.cdk.qsar.result.DoubleResult;
-import org.openscience.cdk.qsar.result.IntegerResult;
-import org.openscience.cdk.qsar.result.IDescriptorResult;
 import org.openscience.cdk.qsar.descriptors.molecular.AtomCountDescriptor;
+import org.openscience.cdk.qsar.result.IDescriptorResult;
+import org.openscience.cdk.qsar.result.IntegerResult;
 
-import ambit2.descriptors.MolecularWeight;
+import ambit2.base.data.Property;
 
 /**
  * Average molecular weight. Todeschini , Handbook of Molecular descriptors, physico-chemical properties, p.589.
  * @author Elena Urucheva, Nikolay Kochev
  * <b>Modified</b> 2013-10-31
  */
-public class AverageMolecularWeightDescriptor implements IMolecularDescriptor
-{
+public class AverageMolecularWeightDescriptor extends DerivedMolecularWeightDescriptor {
+	
+	public AverageMolecularWeightDescriptor() {
+		super( new String[] {"AMW"});
+	}
 	public DescriptorSpecification getSpecification()
 	{
 		return new DescriptorSpecification(
-				"AverageMolecularWeightDescriptor",
+				String.format(Property.AMBIT_DESCRIPTORS_ONTOLOGY,"AverageMolecularWeightDescriptor"),
 				this.getClass().getName(),
 				"$Id: AverageMolecularWeightDescriptor.java, v 0.1 2013 Elena Urucheva, Nikolay Kochev",
 				"http://ambit.sourceforge.net");
 	}
 
-	public void setParameters(Object[] params) throws CDKException
-	{
-	
-	}
-
-	public Object[] getParameters()
-	{
-		return null;
-	}
-
-	public String[] getDescriptorNames() 
-	{
-		return new String[] {"AMW"};
-	}
-
-	public DescriptorValue calculate(IAtomContainer container)
-	{
-		MolecularWeight descr = new MolecularWeight();
-		DescriptorValue dValue = descr.calculate(container);
-		IDescriptorResult res = dValue.getValue();		
-		double weight = ((DoubleResult)res).doubleValue();
-
+	@Override
+	public double calculateDerivedValue(IAtomContainer container,
+			double weight) {
 		AtomCountDescriptor descr1 = new AtomCountDescriptor();
 		DescriptorValue dValue1 = descr1.calculate(container);
 		IDescriptorResult res1 = dValue1.getValue();
@@ -61,23 +42,7 @@ public class AverageMolecularWeightDescriptor implements IMolecularDescriptor
 		double averageWeight = 0.0;
 		if (nA > 0)
 			averageWeight = (weight/nA);
-
-		return new DescriptorValue(getSpecification(), getParameterNames(), getParameters(),
-				new DoubleResult(averageWeight), getDescriptorNames());
+		return averageWeight;
 	}
 	
-	public IDescriptorResult getDescriptorResultType()
-	{
-		return new DoubleResult(0.0);
-	}
-
-	public String[] getParameterNames() 
-	{
-		return null;
-	}
-
-	public Object getParameterType(String name) 
-	{
-		return "";
-	}
 }
