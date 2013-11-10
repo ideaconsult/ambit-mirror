637e2170f6f6419d09a4c6b6fdba4d560fe742f6
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2013-11-10 16:01:33 +0000

    schema 7.1

--- ambit2-db/src/main/resources/ambit2/db/sql/create_tables.sql
+++ ambit2-db/src/main/resources/ambit2/db/sql/create_tables.sql
@@ -157,6 +157,45 @@ CREATE TABLE `substance_relation` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Dossier to chemicals relation';
 
 -- -----------------------------------------------------
+-- Table `substance_protocolapplication` 
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `substance_protocolapplication`;
+CREATE TABLE `substance_protocolapplication` (
+  `document_prefix` varchar(6) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
+  `document_uuid` varbinary(16) NOT NULL,
+  `endpoint` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `guidance` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `substance_prefix` varchar(6) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `substance_uuid` varbinary(16) DEFAULT NULL,
+  `params` blob NOT NULL,
+  `reference` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`document_prefix`,`document_uuid`),
+  KEY `substance` (`substance_prefix`,`substance_uuid`),
+  KEY `endpoint` (`endpoint`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- -----------------------------------------------------
+-- Table `substance_experiment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `substance_experiment`;
+CREATE TABLE `substance_experiment` (
+  `idresult` int(11) NOT NULL AUTO_INCREMENT,
+  `document_prefix` varchar(6) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
+  `document_uuid` varbinary(16) NOT NULL,
+  `endpoint` varchar(45) DEFAULT NULL,
+  `conditions` text,
+  `unit` varchar(6) DEFAULT NULL,
+  `loQualifier` varchar(6) DEFAULT NULL,
+  `loValue` double DEFAULT NULL,
+  `upQualifier` varchar(6) DEFAULT NULL,
+  `upvalue` double DEFAULT NULL,
+  PRIMARY KEY (`idresult`),
+  KEY `document_id` (`document_uuid`,`document_prefix`),
+  KEY `endpoint` (`endpoint`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- -----------------------------------------------------
 -- Table `structure`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `structure` ;
@@ -1096,7 +1135,7 @@ CREATE TABLE  `version` (
   `comment` varchar(45),
   PRIMARY KEY  (`idmajor`,`idminor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-insert into version (idmajor,idminor,comment) values (7,0,"AMBIT2 schema");
+insert into version (idmajor,idminor,comment) values (7,1,"AMBIT2 schema");
 
 -- -----------------------------------------------------
 -- Sorts comma separated strings
---
+++ ambit2-db/src/main/resources/ambit2/db/sql/upgrade_7_1.sql
@@ -0,0 +1,40 @@
+-- -----------------------------------------------------
+-- Table `substance_protocolapplication` 
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `substance_protocolapplication`;
+CREATE TABLE `substance_protocolapplication` (
+  `document_prefix` varchar(6) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
+  `document_uuid` varbinary(16) NOT NULL,
+  `endpoint` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `guidance` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `substance_prefix` varchar(6) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `substance_uuid` varbinary(16) DEFAULT NULL,
+  `params` blob NOT NULL,
+  `reference` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`document_prefix`,`document_uuid`),
+  KEY `substance` (`substance_prefix`,`substance_uuid`),
+  KEY `endpoint` (`endpoint`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- -----------------------------------------------------
+-- Table `substance_experiment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `substance_experiment`;
+CREATE TABLE `substance_experiment` (
+  `idresult` int(11) NOT NULL AUTO_INCREMENT,
+  `document_prefix` varchar(6) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
+  `document_uuid` varbinary(16) NOT NULL,
+  `endpoint` varchar(45) DEFAULT NULL,
+  `conditions` text,
+  `unit` varchar(6) DEFAULT NULL,
+  `loQualifier` varchar(6) DEFAULT NULL,
+  `loValue` double DEFAULT NULL,
+  `upQualifier` varchar(6) DEFAULT NULL,
+  `upvalue` double DEFAULT NULL,
+  PRIMARY KEY (`idresult`),
+  KEY `document_id` (`document_uuid`,`document_prefix`),
+  KEY `endpoint` (`endpoint`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+insert into version (idmajor,idminor,comment) values (7,1,"AMBIT Schema: substances study records support");
