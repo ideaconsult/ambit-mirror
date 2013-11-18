05bcfd369f63d3f7b82bc49922de5db97f1d5fa6
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2013-11-18 14:45:56 +0000

    /substance/id/studysummary

---
+++ ambit2-db/src/main/java/ambit2/db/substance/study/facet/SubstanceStudyFacetQuery.java
@@ -0,0 +1,78 @@
+package ambit2.db.substance.study.facet;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import ambit2.base.data.I5Utils;
+import ambit2.base.exceptions.AmbitException;
+import ambit2.base.facet.IFacet;
+import ambit2.db.facets.AbstractFacetQuery;
+import ambit2.db.search.QueryParam;
+import ambit2.db.search.StringCondition;
+
+public class SubstanceStudyFacetQuery  extends AbstractFacetQuery<String,String,StringCondition,IFacet<String>>  {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8910197148842469398L;
+	protected String sql = 
+		"select endpointcategory,count(*) from substance_protocolapplication %s group by endpointcategory with rollup";
+	
+	private static String  substance_uuid = " substance_prefix=? and hex(substance_uuid)=?";
+	
+	protected SubstanceStudyFacet record;
+	
+	public SubstanceStudyFacetQuery(String facetURL) {
+		super(facetURL);
+		record = new SubstanceStudyFacet(facetURL);
+	}
+	@Override
+	public boolean isPrescreen() {
+		return false;
+	}
+
+	@Override
+	public double calculateMetric(IFacet<String> object) {
+		return 1;
+	}
+
+	@Override
+	public String getSQL() throws AmbitException {
+		if (getFieldname()==null) return sql;
+		else return String.format(sql,"\nwhere" + substance_uuid);
+	}
+
+	protected String[] getSubstanceUUID() {
+		if (getFieldname()==null) return null;
+		return I5Utils.splitI5UUID(getFieldname());
+	}
+	@Override
+	public List<QueryParam> getParameters() throws AmbitException {
+		
+		List<QueryParam> params1 = new ArrayList<QueryParam>();
+		if (getFieldname()!=null) {
+			String[] uuid = getSubstanceUUID();
+			params1.add(new QueryParam<String>(String.class, uuid[0]));
+			params1.add(new QueryParam<String>(String.class, uuid[1].replace("-", "").toLowerCase()));
+			return params1;
+		} else return null;		
+	}
+
+	@Override
+	public SubstanceStudyFacet getObject(ResultSet rs) throws AmbitException {
+		try {
+			
+			record.setValue(rs.getString(1));
+			record.setCount(rs.getInt(2));
+			return record;
+		} catch (Exception x) {
+			record.setValue(x.getMessage());
+			record.setCount(-1);
+			return record;
+		}
+	}
+	
+	
+}
---
+++ ambit2-www/src/main/java/ambit2/rest/substance/study/SubstanceStudyFacetResource.java
@@ -0,0 +1,34 @@
+package ambit2.rest.substance.study;
+
+import org.restlet.Context;
+import org.restlet.Request;
+import org.restlet.Response;
+import org.restlet.resource.ResourceException;
+
+import ambit2.db.substance.study.facet.SubstanceStudyFacetQuery;
+import ambit2.rest.facet.FacetResource;
+import ambit2.rest.substance.SubstanceResource;
+
+public class SubstanceStudyFacetResource extends FacetResource<SubstanceStudyFacetQuery>  {
+	public final static String resource = "studysummary";
+	@Override
+	protected SubstanceStudyFacetQuery createQuery(Context context,
+			Request request, Response response) throws ResourceException {
+		
+		Object substanceuuid = request.getAttributes().get(SubstanceResource.idsubstance);
+		/*
+		Form form = getResourceRef(request).getQueryAsForm();
+		Object key = form.getFirstValue(Annotea.BookmarkProperty.hasTopic.toString());
+		if (key != null) {
+			if (bookmark==null) bookmark = new Bookmark();
+			bookmark.setHasTopic(Reference.decode(key.toString()));
+		} 
+		*/
+		SubstanceStudyFacetQuery q = new SubstanceStudyFacetQuery(
+				String.format("%s%s/%s/study",getRootRef(),SubstanceResource.substance,substanceuuid)
+				);
+		q.setFieldname(substanceuuid==null?null:substanceuuid.toString());
+		return q;
+	}
+
+}
