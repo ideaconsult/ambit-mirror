f97ddb731d8cbb897a99882133bf82c62bb91036
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2013-12-12 14:36:25 +0000

    protocol JSON serialization modeified

--- ambit2-base/src/main/java/ambit2/base/data/study/Protocol.java
+++ ambit2-base/src/main/java/ambit2/base/data/study/Protocol.java
@@ -9,6 +9,33 @@ public class Protocol {
 	String category;
 	String endpoint;
 	List<String> guidance;
+	//hack for IUCLID categories, as i can't find the exact titles in the schema :(
+	private enum _categories {
+		PC_PARTITION_SECTION {
+			@Override
+			public String toString() {
+				return "Partition coeffcicient";
+			}
+		},
+		TO_BIODEG_WATER_SCREEN_SECTION {
+			@Override
+			public String toString() {
+				return "Biodegradation in water - screening tests";
+			}
+		},
+		oral {
+			@Override
+			public String toString() {
+				return "Acute toxicity - oral";
+			}
+		},
+		EC_FISHTOX_SECTION {
+			@Override
+			public String toString() {
+				return "Short-term toxicity to fish";
+			}
+		}				
+	}
 	
 	public static enum _fields {
 		topcategory,
@@ -64,13 +91,21 @@ public class Protocol {
 
 		b.append("\n\t");
 		b.append(JSONUtils.jsonQuote(_fields.topcategory.name()));		
-		b.append(":");
+		b.append(": ");
 		b.append(JSONUtils.jsonQuote(JSONUtils.jsonEscape(topCategory)));
 		b.append(",\n\t");
 		b.append(JSONUtils.jsonQuote(_fields.category.name()));		
-		b.append(":");		
-		b.append(JSONUtils.jsonQuote(JSONUtils.jsonEscape(category)));
-		b.append(",\n\t");
+		b.append(": {");
+			b.append(JSONUtils.jsonQuote("code"));b.append(": ");
+			b.append(JSONUtils.jsonQuote(JSONUtils.jsonEscape(category)));
+			b.append(",");
+			b.append(JSONUtils.jsonQuote("title"));b.append(": ");
+			try {
+				b.append(JSONUtils.jsonQuote(JSONUtils.jsonEscape(_categories.valueOf(category).toString())));
+			} catch (Exception x) {
+				b.append(JSONUtils.jsonQuote(JSONUtils.jsonEscape(category)));
+			}
+		b.append("},\n\t");
 		b.append(JSONUtils.jsonQuote(_fields.endpoint.name()));		
 		b.append(":");		
 		b.append(JSONUtils.jsonQuote(JSONUtils.jsonEscape(endpoint)));
