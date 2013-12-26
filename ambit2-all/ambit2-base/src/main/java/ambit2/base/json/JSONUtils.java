5bb21a878fc8583137b73f98dda5d5b01e0a0f3b
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2013-12-26 13:59:26 +0000

    json encode

--- ambit2-all/ambit2-base/src/main/java/ambit2/base/json/JSONUtils.java
+++ ambit2-all/ambit2-base/src/main/java/ambit2/base/json/JSONUtils.java
@@ -1,5 +1,10 @@
 package ambit2.base.json;
 
+/**
+ * http://www.ietf.org/rfc/rfc4627.txt
+ * @author nina
+ *
+ */
 public class JSONUtils {
 	public static String jsonEscape(String value) {
 		if(value==null) return null;
@@ -10,7 +15,23 @@ public class JSONUtils {
         .replace("\n", "\\n")
         .replace("\r", "\\r")
         .replace("\t", "\\t")
-        .replace("\"", "\\\"");
+        .replace("\"", "\\\"")
+		.replace("\u0016", "")
+		.replace("\u0019", "");
+		
+		
+		/*
+		 * 
+		 %x22 /          ; "    quotation mark  U+0022
+         %x5C /          ; \    reverse solidus U+005C
+         %x2F /          ; /    solidus         U+002F
+         %x62 /          ; b    backspace       U+0008
+         %x66 /          ; f    form feed       U+000C
+         %x6E /          ; n    line feed       U+000A
+         %x72 /          ; r    carriage return U+000D
+         %x74 /          ; t    tab             U+0009
+         %x75 4HEXDIG )  ; uXXXX                U+XXXX
+         */
 	}
 	/**
 	 * Returns null, if the value is null, or a quoted string otherwise
