c9389b19749d26f70668ae16053abc90eefa2858
Nikolay Kochev <nick@uni-plovdiv.net>
2014-01-13 13:05:33 +0000

    Added 4 basic classes.

---
+++ ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/ui/GCM_Builder.java
@@ -0,0 +1,14 @@
+package ambit2.groupcontribution.ui;
+
+public class GCM_Builder 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
