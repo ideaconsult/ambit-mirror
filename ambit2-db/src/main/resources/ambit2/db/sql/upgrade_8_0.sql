42c78aec82675dff0c52f1158ec92ad43a4f62e1
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2014-04-03 13:14:54 +0000

    ambit_users sql script update for the latest ambit2 schema

---
+++ ambit2-db/src/main/resources/ambit2/db/sql/upgrade_8_0.sql
@@ -0,0 +1,17 @@
+-- User refactoring. From now on a separate database for users AA will be used (ambit_users)
+DROP TABLE IF EXISTS `roles`;
+DROP TABLE IF EXISTS `user_roles`;
+ALTER TABLE `users` 
+	DROP COLUMN `registration_id` , 
+	DROP COLUMN `registration_date` , 
+	DROP COLUMN `registration_status` , 
+	DROP COLUMN `password` , 
+	CHANGE COLUMN `title` `title` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '""'  , 
+	CHANGE COLUMN `firstname` `firstname` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '""'  , 
+	CHANGE COLUMN `lastname` `lastname` VARCHAR(45) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '""'  , 
+	CHANGE COLUMN `webpage` `homepage` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '""'  , 
+	CHANGE COLUMN `affiliation` `institute` VARCHAR(128) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '""'  ;
+
+ALTER TABLE `users` ADD COLUMN `iduser` INT(11) NULL AUTO_INCREMENT  FIRST 
+, ADD UNIQUE INDEX `iduser_UNIQUE` (`iduser` ASC) ;
+	
