f79c924ac195135a13094925868314c45c63f7d2
Nikolay Kochev <nick@uni-plovdiv.net>
2014-04-30 13:01:09 +0000

    Updated the Reactor list of functionalities.

--- ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/reactor/Reactor.java
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/reactor/Reactor.java
@@ -4,8 +4,17 @@ public class Reactor
 {
 	/*
 	 * Reactor functionality:
-	 * 1. Apply a set of reactions to the target in various modes
-	 * 2. Organise various sets of reactions
+	 * 1. Apply a set of reactions to the target in various modes (manage the reaction/ reaction sets to be applied)
+	 * 		1b) Apply particular reaction to many targets
+	 * 2. Organize various sets of reactions (sort of reaction DB functionality)
+	 * 		2b) Organize metabolic path ways
+	 * 		3b) Organize synthetic paths
 	 * 3. Search for a specific reaction in particular set of reactions
-	 */
+	 * 4. Filter reactions to be applied on the target (various criteria ...) 
+	 * 5. Filter target sites for the reactions: Yes-list, No-list, SMARTS based, property based, ...
+	 * 6. Estimate reaction sites (rule based, property based, charges, ...
+	 * 7.  
+	 * 
+	 * 
+	 **/
 }
