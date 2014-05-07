a304ad2b36cc22bd853460df9da747f23c72f5af
Nikolay Kochev <nick@uni-plovdiv.net>
2014-05-07 05:47:18 +0000

    changed comment

--- ambit2-reactions/src/main/java/ambit2/reactions/reactor/Reactor.java
+++ ambit2-reactions/src/main/java/ambit2/reactions/reactor/Reactor.java
@@ -7,14 +7,18 @@ public class Reactor
 	 * 1. Apply a set of reactions to the target in various modes (manage the reaction/ reaction sets to be applied)
 	 * 		1b) Apply particular reaction to many targets
 	 * 2. Organize various sets of reactions (sort of reaction DB functionality)
-	 * 		2b) Organize metabolic path ways
+	 * 		2b) Organize metabolicc path ways
 	 * 		3b) Organize synthetic paths
 	 * 3. Search for a specific reaction in particular set of reactions
 	 * 4. Filter reactions to be applied on the target (various criteria ...) 
 	 * 5. Filter target sites for the reactions: Yes-list, No-list, SMARTS based, property based, ...
-	 * 6. Estimate reaction sites (rule based, property based, charges, ...
-	 * 7.  
+	 * 6. Estimate reaction sites (rule based, property based, charges, ...). 
+	 *      Here can be used some of the strategy rules for retro- synthesis 
+	 *   
 	 * 
-	 * 
+	 * Application possible functionalities:
+	 * - draw/edit reactions
+	 * - convert reactions to various formats (io)
+	 * - define / edit rules
 	 **/
 }
