ec2821e9a1b847465c17295edbe726ba657b37b9
Nikolay Kochev <nick@uni-plovdiv.net>
2014-05-07 21:30:32 +0000

    Added new class SyntheticStrategy. Implemented some principle steps in function generateChildrenNodes().

---
+++ ambit2-reactions/src/main/java/ambit2/reactions/SyntheticStrategy.java
@@ -0,0 +1,14 @@
+package ambit2.reactions;
+
+import java.util.ArrayList;
+
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+public class SyntheticStrategy 
+{
+	public ArrayList<IRetroSynthRuleInstance> applyStrategy(IAtomContainer target, ArrayList<IRetroSynthRuleInstance> ruleInstances)
+	{
+		//TODO
+		return ruleInstances;
+	}
+}
