538af35ddbbcf19a034ee487e97f290042b37725
Nikolay Kochev <nick@uni-plovdiv.net>
2014-06-02 07:02:42 +0000

    Added new file StructureGenerator

---
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/generator/StructureGenerator.java
@@ -0,0 +1,23 @@
+package ambit2.reactions.generator;
+
+/**
+ * Focused libraries and Diverse libraries
+ * 
+ * Filters:
+ * -properties
+ * -toxicity
+ * 
+ * 
+ * Scores:
+ * -properties
+ * 
+ * 
+ * @author nick
+ *
+ */
+
+
+public class StructureGenerator 
+{
+	
+}
