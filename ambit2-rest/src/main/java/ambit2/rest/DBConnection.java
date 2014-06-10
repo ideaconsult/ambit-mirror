768794f172f6f331e7dfd9872562b67dc559e339
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2014-06-10 08:17:06 +0000

    option ambit.profile Menu refactored to allow profile specific menus

--- ambit2-rest/src/main/java/ambit2/rest/DBConnection.java
+++ ambit2-rest/src/main/java/ambit2/rest/DBConnection.java
@@ -63,6 +63,12 @@ public class DBConnection {
 		}
 	}	
 	
+	public String getMenuProfile() {
+		loadProperties();
+		String prefix = properties.getProperty("ambit.profile");
+		if (prefix == null || prefix.contains("${")) prefix = "default";
+		return prefix;
+	}
 	public LoginInfo getLoginInfo() {
 		loadProperties();
 		LoginInfo li = new LoginInfo();
