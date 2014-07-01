ddfd71c1648ee792744340e1fcaf5c9a531733cb
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2014-07-01 07:56:23 +0000

    Enable substance details in substance dataset

--- ambit2-all/ambit2-www/src/main/webapp/scripts/substance/config-dataset.js
+++ ambit2-all/ambit2-www/src/main/webapp/scripts/substance/config-dataset.js
@@ -1,33 +1,5 @@
-var config_substancedataset = {
-		/*
-	"fnAccumulate": function(fId, oldVal, newVal, features) {
-		if (
-			features[fId].sameAs == "http://www.opentox.org/echaEndpoints.owl#Carcinogenicity" ||
-			features[fId].sameAs == "http://www.wikipathways.org/index.php/Pathway"				
-			) {
-			if ((oldVal === undefined) || (oldVal == null) || ("" == oldVal)) oldVal = [];
-		    if ((newVal === undefined) || ccLib.isNull(newVal)) return oldVal;
+var config_dataset = {
 
-		    newVal = {"feature" : fId,  "value" : newVal };
-
-		    if ($.inArray(newVal,oldVal)==-1)
-		          oldVal.push(newVal);
-	
-		    return oldVal;
-		} else {
-				
-		      if (ccLib.isNull(newVal))
-		          return oldVal;
-		        newVal = newVal.toString();
-		        if (ccLib.isNull(oldVal) || newVal.toLowerCase().indexOf(oldVal.toLowerCase()) >= 0)
-		          return newVal;
-		        if (oldVal.toLowerCase().indexOf(newVal.toLowerCase()) >= 0)
-		          return oldVal;
-		        return oldVal + ", " + newVal;			
-		}
-	},		
-	*/
-	
 	
 	   "baseFeatures": {
 	        "http://www.opentox.org/api/1.1#CASRN" : {
@@ -84,31 +56,30 @@ var config_substancedataset = {
 		    },
 	        "http://www.opentox.org/api/1.1#IUCLID5_UUID" : {
 	        	"title" : "UUID"
-		    },
-			"http://www.opentox.org/api/1.1#CompositionInfo" : {
-				  "visibility": "details",
-					"title": "Substances",
-					"data": "compound.URI",
-					"basic": true,
-					"render" : function(data, type, full) {
-		        return (type != "details") ? "-" : '<span class="jtox-details-composition" data-URI="' + data + '"></span>';
-		      }
-			}   		    
-	    
-
+		    },	
+		"http://www.opentox.org/api/1.1#CompositionInfo" : {
+			  "visibility": "details",
+				"title": "Substances",
+				"data": "compound.URI",
+				"basic": true,
+				"render" : function(data, type, full) {
+					return (type != "details") ? "-" : '<span class="jtox-details-composition" data-URI="' + data + '"></span>';
+				}
+		}
 	    },
 	    "groups": function createGroups(miniset, kit) {
 	    	 var groups = {
-	          "Identifiers" : [
-	             "#DetailedInfoRow",
-	             "http://www.opentox.org/api/1.1#IUCLID5_UUID"
-	          ],
-	          
-	          "Names": [
-	                    "http://www.opentox.org/api/1.1#ChemicalName",
-	                    "http://www.opentox.org/api/1.1#TradeName",
-		  	],
-			 "Substances": [ "http://www.opentox.org/api/1.1#CompositionInfo" ]
+	   	          "Identifiers" : [
+	   	               "http://www.opentox.org/api/1.1#Diagram",	   	                           
+	   	    	       "#DetailedInfoRow",
+	   	       	       "http://www.opentox.org/api/1.1#IUCLID5_UUID"
+	   	       	  ],
+ 	       	      "Names": [
+	   	       	       "http://www.opentox.org/api/1.1#ChemicalName",
+	   	       	       "http://www.opentox.org/api/1.1#TradeName",
+	   	       		],
+ 			"Substances": [ "http://www.opentox.org/api/1.1#CompositionInfo" ]
+
 	    	 };
 	    	 	for (var fId in miniset.feature) {
 	    	      	var src = miniset.feature[fId].source;
@@ -121,10 +92,18 @@ var config_substancedataset = {
 	    	    	}
 	    	    	groups["Substances"] = [ "http://www.opentox.org/api/1.1#CompositionInfo" ];
 	    	    	groups["Calculated"] = null;
+	    	        groups["Data"] = function (name, miniset) {
+	    	            var arr = [];
+	    	            for (var f in miniset.feature) {
+	    	              if (!miniset.feature[f].used && !miniset.feature[f].basic)
+	    	                arr.push(f);
+	    	            }
+	    	            return arr;
+	    	          }	    	    	
 	    	    	return groups;
-	    },
-		"handlers": {
-			"checked": onSelectedUpdate
-		}
+        },
+    	"handlers": {
+    		"checked": onSelectedUpdate
+    	}
     
 }
