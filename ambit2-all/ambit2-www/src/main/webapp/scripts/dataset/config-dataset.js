c64f5c5fb268fec88125b80ca2251643fe2f05da
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2014-07-09 14:42:11 +0000

    copied from the jToxKit demo but doesn't work well

---
+++ ambit2-all/ambit2-www/src/main/webapp/scripts/dataset/config-dataset.js
@@ -0,0 +1,36 @@
+var config_dataset = {
+  "baseFeatures": {
+		"http://www.opentox.org/api/1.1#CompositionInfo" : {
+		  "visibility": "details",
+			"title": "Composition",
+			"data": "compound.URI",
+			"column": { bVisible: false },
+			"basic": true,
+			"render" : function(data, type, full) {
+        return (type != "details") ? "-" : '<span class="jtox-details-composition" data-URI="' + data + '"></span>';
+      }
+		},
+		"http://www.opentox.org/api/1.1#Similarity" : {
+			"title": "Similarity",
+			"data" : "compound.metric", // you need to point where is the information located
+			"accumulate": true,
+			"render" : function(data, type, full) {
+				return data;
+			}
+		}
+
+  },
+  "groups": createGroups,
+	"columns": {
+  	"dataset": {
+    	'Info': { bVisible: false }
+  	},
+  	"model": { 
+    	'Info': { bVisible: false },
+    	'Algorithm': { bVisible: false }
+  	}
+	},
+	"handlers": {
+  	"checked": onSelectedUpdate
+	}
+};
