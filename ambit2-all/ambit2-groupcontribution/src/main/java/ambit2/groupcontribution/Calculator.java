28a0570b01fac0e86c16652d65c31fd1240a3782
Nikolay Kochev <nick@uni-plovdiv.net>
2014-07-10 11:47:34 +0000

    Implemented calculation functionality. Added new methods and fields in ICorrectionFactor, GroupContributionModel and Learner classes.

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/Calculator.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/Calculator.java
@@ -1,5 +1,6 @@
 package ambit2.groupcontribution;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,27 +9,50 @@ import org.openscience.cdk.interfaces.IAtomContainer;
 import ambit2.groupcontribution.correctionfactors.ICorrectionFactor;
 import ambit2.groupcontribution.groups.IGroup;
 
+
 public class Calculator 
 {
 	
 	public static double calcModelFor(IAtomContainer mol, GroupContributionModel model)
 	{
-		Map<IGroup,Integer> groups = getGroupsCount(mol, model);
-		Map<ICorrectionFactor,Integer> corrFactors = getCorrectionFactorsCount(mol, model);
+		double modelValue = 0.0;
 		
-		//TODO
-		return 0.0;
-	}
+		//Group contributions
+		Map<IGroup,Integer> groups = getGroupsCount(mol, model);			
+		Map<String, IGroup> modelGroups = model.getGroups(); 		
+		for (Map.Entry<IGroup,Integer> entry : groups.entrySet())
+		{
+			IGroup group = entry.getKey();
+			if (modelGroups.containsKey(group.getDesignation()));
+				modelValue += group.getContribution() * entry.getValue(); 
+		}
+		
+		//Correction factors
+		Map<ICorrectionFactor,Integer> corrFactors = getCorrectionFactorsCount(mol, model);	
+		for (Map.Entry<ICorrectionFactor,Integer> entry : corrFactors.entrySet())
+			modelValue +=  entry.getKey().getContribution() * entry.getValue();
+			
+		return modelValue;
+	}	
 	
 	public static Map<IGroup,Integer> getGroupsCount(IAtomContainer mol, GroupContributionModel model)
 	{
+		Map<IGroup,Integer> groups = new HashMap<IGroup,Integer>();
 		//TODO
-		return null;
+		return groups;
 	}
 	
 	public static Map<ICorrectionFactor,Integer> getCorrectionFactorsCount(IAtomContainer mol, GroupContributionModel model)
 	{
-		//TODO
-		return null;
+		Map<ICorrectionFactor,Integer> corrFactors = new HashMap<ICorrectionFactor,Integer>();
+		ArrayList<ICorrectionFactor> modelCorrFactors = model.getCorrectionFactors();
+		for (ICorrectionFactor factor : modelCorrFactors)
+		{
+			int count = factor.calculateFor(mol);
+			if (count > 0)
+				corrFactors.put(factor, new Integer(count));
+		}
+			
+		return corrFactors;
 	}
 }
