3f93672c678964c03654c8de0dfef1e15a4d82d4
Nikolay Kochev <nick@uni-plovdiv.net>
2014-07-10 08:41:34 +0000

    added DataSet class and principal io functionality.

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/io/ModelReader.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/io/ModelReader.java
@@ -1,6 +1,18 @@
 package ambit2.groupcontribution.io;
 
+import ambit2.groupcontribution.GroupContributionModel;
+
 public class ModelReader 
 {
-
+	public static GroupContributionModel readFromCfg(String cfgFileName) throws Exception
+	{
+		//TODO
+		return null;
+	}
+	
+	public static GroupContributionModel readFromXML(String xmlFileName) throws Exception
+	{
+		//TODO
+		return null;
+	}
 }
--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/io/ModelWriter.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/io/ModelWriter.java
@@ -1,5 +1,16 @@
 package ambit2.groupcontribution.io;
 
-public class ModelWriter {
+import ambit2.groupcontribution.GroupContributionModel;
 
+public class ModelWriter 
+{
+	public static void writeToCfg(GroupContributionModel model, String cfgFileName) throws Exception
+	{
+		//TODO
+	}
+	
+	public static void writeToXML(GroupContributionModel model, String xmlFileName) throws Exception
+	{
+		//TODO
+	}
 }
