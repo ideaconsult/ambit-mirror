2ee90d76d5a4367717520fe24a8a7fbfcf7be633
Nikolay Kochev <nick@uni-plovdiv.net>
2014-07-10 08:41:34 +0000

    added DataSet class and principal io functionality.

---
+++ ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/io/DataSetReader.java
@@ -0,0 +1,22 @@
+package ambit2.groupcontribution.io;
+
+
+import ambit2.groupcontribution.dataset.DataSet;
+
+public class DataSetReader 
+{
+	public static DataSet readFromCfg(String cfgFileName) throws Exception
+	{
+		//TODO
+		return null;
+	}
+	
+	
+	public static DataSet readFromSDF(String sdfFileName /* data set config field  */) throws Exception
+	{
+		//TODO
+		return null;
+	}
+	
+	
+}
