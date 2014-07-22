1e31a9da76f3a79a02552b0b23494f8efc082a2e
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2014-07-22 18:14:45 +0000

    used by internal calls only

--- ambit2-all/ambit2-apps/ambit2-user/src/main/java/ambit2/user/aa/AmbitPolicyAuthorizer.java
+++ ambit2-all/ambit2-apps/ambit2-user/src/main/java/ambit2/user/aa/AmbitPolicyAuthorizer.java
@@ -7,6 +7,7 @@ import net.idea.restnet.db.aalocal.policy.PolicyAuthorizer;
 import org.restlet.Context;
 import org.restlet.Request;
 import org.restlet.Response;
+import org.restlet.data.Form;
 import org.restlet.data.Method;
 
 public class AmbitPolicyAuthorizer extends PolicyAuthorizer {
@@ -105,6 +106,17 @@ public class AmbitPolicyAuthorizer extends PolicyAuthorizer {
 	
 	@Override
 	public boolean authorizeSpecialCases(Request request, Response response,List<String> uri) {
+		try {
+			if (Method.GET.equals(request.getMethod())) {
+				Form headers = (Form) request.getAttributes().get("org.restlet.http.headers");
+				if (headers!=null) {
+					String ac = headers.getFirstValue("accept"); 
+					if (ac != null && "application/x-java-serialized-object".equals(ac))
+						return true;
+				}
+			}
+		} catch (Exception x) {}
+		
 		if ("riap".equals(request.getResourceRef().getScheme())) return true;
 		int depth = request.getResourceRef().getSegments().size();
 		if (depth==1) return true; //home page
