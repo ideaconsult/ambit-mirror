96b2f3c8dbd38b0e414e66631a2f5eadd6fb3368
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2014-07-22 13:35:49 +0000

    update users table

--- ambit2-all/ambit2-db/src/main/resources/ambit2/db/sql/users/ambit_users.sql
+++ ambit2-all/ambit2-db/src/main/resources/ambit2/db/sql/users/ambit_users.sql
@@ -76,20 +76,22 @@ CREATE TABLE `user_registration` (
 DROP TABLE IF EXISTS `policy`;
 CREATE TABLE `policy` (
   `idpolicy` int(11) NOT NULL AUTO_INCREMENT,
-  `role_name` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'ambit_guest',
+  `role_name` varchar(16) NOT NULL DEFAULT 'ambit_guest',
   `prefix` varchar(255) NOT NULL,
   `resource` varchar(255) NOT NULL,
+  `level` smallint(6) DEFAULT '1',
   `mget` tinyint(1) NOT NULL DEFAULT '0',
   `mput` tinyint(1) NOT NULL DEFAULT '0',
   `mpost` tinyint(1) NOT NULL DEFAULT '0',
   `mdelete` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`idpolicy`),
-  UNIQUE KEY `uri` (`resource`,`prefix`,`role_name`),
+  UNIQUE KEY `uri` (`prefix`,`resource`,`role_name`),
   KEY `fkrole1_idx` (`role_name`),
   KEY `get` (`mget`),
   KEY `put` (`mput`),
   KEY `post` (`mpost`),
   KEY `delete` (`mdelete`),
+  KEY `fk_resource` (`resource`),
   CONSTRAINT `fkrole1` FOREIGN KEY (`role_name`) REFERENCES `roles` (`role_name`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
