edafb2f7969ae13306fbc8351df67f73803d4c82
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2014-08-06 20:06:28 +0000

    jToxKit updated #47

--- ambit2-www/src/main/webapp/scripts/dataset/ui-query.js
+++ ambit2-www/src/main/webapp/scripts/dataset/ui-query.js
@@ -35,6 +35,17 @@ function onDetailedRow(row, data, event) {
   } } ) );
 }
 
+function renderRelation(data, type, full) {
+  if (type != 'display')
+    return ccLib.joinDeep(data, 'relation', ',');
+    
+  var res = '';
+  for (var i = 0, il = data.length; i < il; ++i)
+    res += '<span>' + data[i].relation.substring(4).toLowerCase() + '</span><sup><a target="_blank" href="' + (full.URI + '/composition') + '" title="' + data[i].compositionName + '(' + data[i].compositionUUID + ')">?</a></sup>';
+
+  return res;
+}
+
 function createGroups(miniset, kit) {
   var groups = {
     "Identifiers" : [
