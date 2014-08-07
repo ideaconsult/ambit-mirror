fd008fa87e31c3943ba4d53c828d4e0087c77f68
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2014-08-07 05:39:11 +0000

    Relation column formatting

--- ambit2-all/ambit2-www/src/main/webapp/scripts/dataset/ui-query.js
+++ ambit2-all/ambit2-www/src/main/webapp/scripts/dataset/ui-query.js
@@ -41,7 +41,7 @@ function renderRelation(data, type, full) {
     
   var res = '';
   for (var i = 0, il = data.length; i < il; ++i)
-    res += '<span>' + data[i].relation.substring(4).toLowerCase() + '</span><sup><a target="_blank" href="' + (full.URI + '/composition') + '" title="' + data[i].compositionName + '(' + data[i].compositionUUID + ')">?</a></sup>';
+    res += '<span class="camelCase">' + data[i].relation.substring(4).toLowerCase() + '</span><sup><a target="_blank" href="' + (full.URI + '/composition') + '" title="' + data[i].compositionName + '(' + data[i].compositionUUID + ')">?</a></sup>';
 
   return res;
 }
