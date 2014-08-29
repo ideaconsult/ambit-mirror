16f51c37bfbcc7b2b04c452b15c4e698b5bbc12b
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2014-08-29 19:17:49 +0000

    jToxKit updated

--- ambit2-all/ambit2-www/src/main/webapp/scripts/jtoxkit.js
+++ ambit2-all/ambit2-www/src/main/webapp/scripts/jtoxkit.js
@@ -645,8 +645,6 @@ window.jT = window.jToxKit = {
 		else 
   		settings = jT.$.extend(settings, kit.settings);
 
-		ccLib.fireCallback(settings.onConnect, kit, service);
-		  
 		var accType = settings.plainText ? "text/plain" : (settings.jsonp ? "application/x-javascript" : "application/json");
 		
 		if (!params.data){
@@ -663,6 +661,8 @@ window.jT = window.jToxKit = {
 		// on some queries, like tasks, we DO have baseUrl at the beginning
 		if (service.indexOf("http") != 0)
 			service = settings.baseUrl + service;
+
+		ccLib.fireCallback(settings.onConnect, kit, service, params);
 			
 		// now make the actual call
 		jT.$.ajax(service, {
@@ -1629,6 +1629,7 @@ var jToxCompound = (function () {
 
       var createATab = function(grId, name) {
         var liEl = document.createElement('li');
+        liEl.title = "Select which " + name + "(s) to be displayed";
         ulEl.appendChild(liEl);
         var aEl = document.createElement('a');
         aEl.href = "#" + grId;
@@ -2186,10 +2187,9 @@ var jToxCompound = (function () {
               // finally - go and update controls if they are visible
               self.updateControls(qStart, dataset.dataEntry.length);
           }
-
-          // time to call the supplied function, if any.
-          ccLib.fireCallback(self.settings.onLoaded, self, dataset, self);
         }
+        // time to call the supplied function, if any.
+        ccLib.fireCallback(self.settings.onLoaded, self, dataset);
       });
     },
     
@@ -2487,10 +2487,10 @@ var jToxDataset = (function () {
           self.dataset = result.dataset;
           if (!self.settings.noInterface)
             jT.$(self.table).dataTable().fnAddData(result.dataset);
-          ccLib.fireCallback(self.settings.onLoaded, self, result);
         }
         else
           self.dataset = null;
+        ccLib.fireCallback(self.settings.onLoaded, self, result);
       });
     },
     
@@ -2649,8 +2649,8 @@ var jToxModel = (function () {
           self.models = result.model;
           if (!self.settings.noInterface)
             jT.$(self.table).dataTable().fnAddData(result.model);
-          ccLib.fireCallback(self.settings.onLoaded, self, result);
         }
+        ccLib.fireCallback(self.settings.onLoaded, self, result);
       });
     },
     
@@ -2668,8 +2668,8 @@ var jToxModel = (function () {
           self.algorithm = result.algorithm;
           if (!self.settings.noInterface)
             jT.$(self.table).dataTable().fnAddData(result.algorithm);
-          ccLib.fireCallback(self.settings.onLoaded, self, result);
         }
+        ccLib.fireCallback(self.settings.onLoaded, self, result);
       });
     },
     
@@ -2906,10 +2906,9 @@ var jToxSubstance = (function () {
             
             self.updateControls(qStart, result.substance.length);
           }
-
-          // time to call the supplied function, if any.
-          ccLib.fireCallback(self.settings.onLoaded, self, result);
         }
+        // time to call the supplied function, if any.
+        ccLib.fireCallback(self.settings.onLoaded, self, result);
       });
     },
     
@@ -3659,6 +3658,8 @@ var jToxStudy = (function () {
           self.querySummary(substance.URI + "/studysummary");
           self.insertComposition(substance.URI + "/composition");
         }
+        else
+          ccLib.fireCallback(self.settings.onLoaded, self, null);
       });
     },
     
