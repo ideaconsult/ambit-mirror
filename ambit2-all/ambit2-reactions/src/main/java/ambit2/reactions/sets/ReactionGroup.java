899113de7b223de0aa1dc3d141c827e6880b7471
Nikolay Kochev <nick@uni-plovdiv.net>
2014-08-30 11:40:47 +0000

    Added new package "sets" + some classes in it and in package io.

---
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/sets/ReactionGroup.java
@@ -0,0 +1,22 @@
+package ambit2.reactions.sets;
+
+import java.util.ArrayList;
+
+public class ReactionGroup 
+{
+	private String name = "";
+	private ArrayList<ReactionData> reactions = new ArrayList<ReactionData>();
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public ArrayList<ReactionData> getReactions() {
+		return reactions;
+	}
+	public void setReactions(ArrayList<ReactionData> reactions) {
+		this.reactions = reactions;
+	}
+}
