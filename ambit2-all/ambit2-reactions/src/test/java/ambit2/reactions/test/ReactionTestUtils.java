b62eedb307b5e7a96c9d3e0cbc0f8b819c7120cd
Nikolay Kochev <nick@uni-plovdiv.net>
2014-09-10 15:28:06 +0000

    helper code.

--- ambit2-all/ambit2-reactions/src/test/java/ambit2/reactions/test/ReactionTestUtils.java
+++ ambit2-all/ambit2-reactions/src/test/java/ambit2/reactions/test/ReactionTestUtils.java
@@ -1,6 +1,11 @@
 package ambit2.reactions.test;
 
+import java.util.ArrayList;
+
 import ambit2.reactions.io.ReactionReadUtils;
+import ambit2.reactions.sets.ReactionData;
+import ambit2.reactions.sets.ReactionGroup;
+import ambit2.reactions.sets.ReactionSet;
 
 public class ReactionTestUtils 
 {
@@ -12,6 +17,29 @@ public class ReactionTestUtils
 	public static void testReadReactionFromRuleFormat(String fileName) throws Exception
 	{
 		ReactionReadUtils rru = new ReactionReadUtils();
-		rru.loadReactionsFromRuleFormat(fileName);
+		ArrayList<ReactionSet> reactionSets =  rru.loadReactionsFromRuleFormat(fileName);
+		
+	}
+	
+	public static String reactionSetToString(ReactionSet rs)
+	{
+		StringBuffer sb = new StringBuffer();
+		
+		for (ReactionData rdata: rs.getReactions())
+			sb.append(rdata.getName() + "  " + rdata.getSmirks() + "  " + rdata.getInfo());
+		
+		for (ReactionGroup group : rs.getReactionGroups())
+			;
+		
+		return (sb.toString());
+	}
+	
+	public static String reactionGroupToString(ReactionGroup rg)
+	{
+		StringBuffer sb = new StringBuffer();
+		for (ReactionData rdata: rg.getReactions())
+			sb.append(rdata.getName() + "  " + rdata.getSmirks() + "  " + rdata.getInfo());
+		
+		return (sb.toString());
 	}
 }
