eb7e5960544de7d0a602e6fe61ca54257417fc20
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2014-09-14 12:02:20 +0000

    quick query for missing fingerprints

--- ambit2-db/src/main/java/ambit2/db/search/structure/FingerprintsByStatus.java
+++ ambit2-db/src/main/java/ambit2/db/search/structure/FingerprintsByStatus.java
@@ -10,9 +10,10 @@ import net.idea.modbcum.i.query.QueryParam;
 import ambit2.base.data.StructureRecord;
 import ambit2.base.interfaces.IStructureRecord;
 import ambit2.db.search.EQCondition;
+import ambit2.db.search.StringCondition;
 import ambit2.descriptors.processors.BitSetGenerator.FPTable;
 
-public class FingerprintsByStatus extends AbstractStructureQuery<FPTable, String, EQCondition> {
+public class FingerprintsByStatus extends AbstractStructureQuery<FPTable, String, StringCondition> {
 
 	/**
 	 * 
@@ -20,17 +21,19 @@ public class FingerprintsByStatus extends AbstractStructureQuery<FPTable, String
 	private static final long serialVersionUID = -8554160549010780854L;
 	
 	public final static String sql =
-		"select idchemical from chemicals where idchemical not in (select idchemical from %s where status = ?)\n";
+		"select idchemical from chemicals where idchemical not in (select idchemical from %s where status %s ? or status = 'error')\n";
 
 	public FingerprintsByStatus(FPTable table) {
 		super();
 		setFieldname(table==null?FPTable.fp1024:table);
+		setCondition(StringCondition.getInstance("="));
+		setValue("valid");
 	}	
 	public FingerprintsByStatus() {
 		this(FPTable.fp1024);
 	}
 	public String getSQL() throws AmbitException {
-		return String.format(sql,getFieldname().getTable());
+		return String.format(sql,getFieldname().getTable(),getCondition().getSQL());
 	}
 	public List<QueryParam> getParameters() throws AmbitException {
 		List<QueryParam> params = new ArrayList<QueryParam>();
