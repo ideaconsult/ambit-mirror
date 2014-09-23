013b058f4c965ded3044c36949e77e6d8e142088
Nikolay Kochev <nick@uni-plovdiv.net>
2014-09-23 08:23:47 +0000

    Added new types of groups + new group implementation

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/groups/IGroup.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/groups/IGroup.java
@@ -4,7 +4,16 @@ package ambit2.groupcontribution.groups;
 public interface IGroup 
 {
 	public enum Type{
-		ATOM,BOND,B_GROUP,D_GROUP,L_GROUP
+		ATOM,
+		BOND,
+		B_GROUP,
+		D_GROUP,
+		L_GROUP, 
+		ATOM_TOP_LAYER_1,
+		ATOM_TOP_LAYER_2, 
+		ATOM_TOP_LAYER_3,
+		ATOM_TOP_LAYER_12,  //This is layers 1 and 2 together 
+		ATOM_TOP_LAYER_123, //This is layers 1, 2 and 3 together
 	}
 	   
 	public double getContribution();
