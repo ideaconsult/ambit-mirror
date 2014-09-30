bd737a1e724b659a485be30f84b2caee2ecd7b4b
Nikolay Kochev <nick@uni-plovdiv.net>
2014-09-30 13:14:58 +0000

    Implemented automatic test for speed and bit-set comparison of ambit and cdk variant of PubChem FP

--- ambit2-descriptors/src/test/java/ambit2/descriptors/test/TestUtilities.java
+++ ambit2-descriptors/src/test/java/ambit2/descriptors/test/TestUtilities.java
@@ -1,7 +1,10 @@
 package ambit2.descriptors.test;
 
+import java.io.File;
 import java.io.FileReader;
+import java.io.RandomAccessFile;
 import java.util.BitSet;
+import java.util.Date;
 
 import org.junit.Test;
 import org.openscience.cdk.aromaticity.CDKHueckelAromaticityDetector;
@@ -16,11 +19,15 @@ import org.openscience.cdk.qsar.result.IDescriptorResult;
 import org.openscience.cdk.silent.SilentChemObjectBuilder;
 import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 
+
+
 import ambit2.core.io.MyIteratingMDLReader;
 import ambit2.descriptors.geometrical.CGIDescriptor;
 import ambit2.descriptors.topological.CTIDescriptor;
 import ambit2.descriptors.fingerprints.PubChemFingerprinter;
 import ambit2.smarts.SmartsHelper;
+
+import org.openscience.cdk.fingerprint.PubchemFingerprinter;
 
 public class TestUtilities 
 {
@@ -49,7 +56,16 @@ public class TestUtilities
 		//calcCTI("NCCN");
 		//calcCTI("OC1=C(O)C2=C(C=C1)C3(OC(=O)C4=C3C=CC=C4)C5=C(O2)C(=C(O)C=C5)O");
 		
-		calcPubChemDescriptor("CCCCCCCCCN");
+		//calcPubChemDescriptor("CCCCCCCCCN");
+		comparePubChemFP("/Users/nick/Projects/nci-basic.smi");
+		
+		//calcPubChemDescriptor("CCCC");
+		
+		//compareFP("");
+		//compareFP("COC(=O)C1CCC(C)=CC1");
+		//compareFP("CN1C(=O)C(=O)N2C=3C=CC=CC=3(C=C2(C1(=O)))");
+		//compareFP("CCN(CC)CCNC=3C=CC(C)=C2C=3(C(=O)C1=CC=CC=C1S2)");
+		
 	}
 	
 	public static void calcCTI(String smiles, boolean FlagPrintAtomAttrib) throws Exception
@@ -168,9 +184,84 @@ public class TestUtilities
 		return(buf.toString());
 	}
 	
+	public static void comparePubChemFP(String smiFile) throws Exception
+	{
+		File file = new File(smiFile);
+		RandomAccessFile f = new RandomAccessFile(file,"r");			
+		long length = f.length();
+		
+		PubChemFingerprinter ambitFP = new PubChemFingerprinter();
+		PubchemFingerprinter cdkFP = new PubchemFingerprinter();
+		long ambitTime = 0;
+		long cdkTime = 0;
+		long curTime = 0;
+		int nStr = 0;
+		int nDiff = 0;
+		
+		while (f.getFilePointer() < length)
+		{	
+			String line = f.readLine();
+			line = line.trim();
+			if (line.isEmpty())
+				continue;
+			IAtomContainer mol = SmartsHelper.getMoleculeFromSmiles(line);			
+			nStr++;
+			curTime = System.nanoTime();
+			BitSet bs0 = ambitFP.getFingerprint(mol);
+			ambitTime += (System.nanoTime() - curTime);
+			
+			/*
+			curTime = System.nanoTime();
+			BitSet bs1 = cdkFP.getFingerprint(mol);
+			cdkTime += (System.nanoTime() - curTime);
+			
+			int diffBits = compareBitSets(bs0, bs1);
+			if ( diffBits > 0)
+			{	
+				nDiff++;
+				System.out.println("--> " + diffBits + "   " + line);
+			}
+			*/
+				
+			
+			if (nStr % 100 == 0)
+				System.out.println("nStr = " + nStr + "  nDiff = " + nDiff 
+						+ "  ambitTime = " + ambitTime/1.0e9 + "  cdkTime = " + cdkTime/1.0e9 + "  cdk/ambit = " + 1.0*cdkTime/ambitTime);
+		}	
+		f.close();
+	}
+	
+	public static int compareBitSets(BitSet bs0, BitSet bs1)
+	{
+		int nDiff = 0;
+		int maxBit = bs0.length();
+		if (maxBit < bs1.length())
+			maxBit = bs1.length();
+		for (int i = 0; i <= maxBit; i++)
+		{
+			if (bs0.get(i) != bs1.get(i))
+				nDiff++;
+		}
+		return nDiff;
+	}
+	
+	public static void compareFP(String smiles) throws Exception
+	{
+		IAtomContainer mol = SmartsHelper.getMoleculeFromSmiles(smiles);
+		PubChemFingerprinter ambitFP = new PubChemFingerprinter();
+		PubchemFingerprinter cdkFP = new PubchemFingerprinter();
+		BitSet bs0 = ambitFP.getFingerprint(mol);
+		BitSet bs1 = cdkFP.getFingerprint(mol);
+		System.out.println(smiles);
+		System.out.println("ambit: " + bs0);
+		System.out.println("cdk:   " + bs1);
+		bs1.xor(bs0);
+		System.out.println("   diff:   " + bs1);
+		
+	}
+	
 	
 	@Test
-	public void test() {
-		
+	public void test() {		
 	}
 }
