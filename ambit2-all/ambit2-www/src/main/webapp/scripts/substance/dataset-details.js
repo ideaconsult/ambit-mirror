130f3a2ecbfef5b42f2c68ecde487c32eb129e85
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2014-10-04 14:33:01 +0000

    latest jToxKit version , adding proper ui-query.js version resolves https://github.com/ideaconsult/Toxtree.js/issues/80

--- ambit2-all/ambit2-www/src/main/webapp/scripts/substance/dataset-details.js
+++ ambit2-all/ambit2-www/src/main/webapp/scripts/substance/dataset-details.js
@@ -1,6 +1,8 @@
 var jTConfig = {};
 
 function onSideLoaded(result) {
+  if (!result)
+    return;
 	var tEl = $('.title', $(this.rootElement).parents('.jtox-foldable')[0])[0];
 	var set = (result.model || result.dataset);
 	$(tEl).data('total', set.length);
@@ -11,6 +13,7 @@ function onSelectedUpdate(e) {
   var par = $(this).parents('.jtox-foldable')[0];
 	var tEl = $('.title', par)[0];
 	var v = $('input[type="checkbox"]:checked', par).length;
+	
 	tEl.innerHTML = jT.ui.updateCounter(tEl.innerHTML, v, $(tEl).data('total'));
 }
 
@@ -30,3 +33,59 @@ function onDetailedRow(row, data, event) {
     new jToxStudy(root, $.extend({}, this.settings, {substanceUri: data}));
   } } ) );
 }
+
+function createGroups(miniset, kit) {
+  var groups = {
+    "Identifiers" : [
+      "http://www.opentox.org/api/1.1#Diagram", 
+      "#DetailedInfoRow",
+      "http://www.opentox.org/api/1.1#CASRN", 
+      "http://www.opentox.org/api/1.1#EINECS",
+      "http://www.opentox.org/api/1.1#IUCLID5_UUID",
+      // Names
+      "http://www.opentox.org/api/1.1#ChemicalName",
+      "http://www.opentox.org/api/1.1#TradeName",
+      "http://www.opentox.org/api/1.1#IUPACName",
+      "http://www.opentox.org/api/1.1#SMILES",
+      "http://www.opentox.org/api/1.1#InChIKey",
+      "http://www.opentox.org/api/1.1#InChI",
+      "http://www.opentox.org/api/1.1#REACHRegistrationDate"
+    ]
+	};
+	for (var fId in miniset.feature) {
+	  var feat = miniset.feature[fId]; 
+  	var src = feat.source;
+  	if (!src || !src.type || src.type.toLowerCase() != 'model')
+  	  continue;
+    src = src.URI.substr(src.URI.lastIndexOf('/') + 1);
+    if (groups[src] === undefined)
+      groups[src] = [];
+    if (feat.title.indexOf('explanation') > 0)
+      feat.visibility = "details";
+    groups[src].push(fId);
+	}
+	groups["Substances"] = [ "http://www.opentox.org/api/1.1#CompositionInfo" ];
+	groups["Datasets"] = function (name, miniset) {
+    var arr = [];
+    for (var f in miniset.feature) {
+      if (!miniset.feature[f].used && !miniset.feature[f].basic) {
+        arr.push(f);
+        if (feat.title.indexOf('explanation') > 0)
+          feat.visibility = "details";
+      }
+    }
+    return arr;
+  }
+	return groups;
+}
+
+$(document).ready(function(){
+  var toggleBar = function () {
+    $('#sidebar').toggleClass('hidden');
+  };
+  $('#sidebar span.ui-icon').on('click', toggleBar);
+  $('#sidebar div.side-title').on('click', toggleBar);
+  $('#sidebar').on('mouseover', function () { $(this).removeClass('hidden'); }).on('mouseout', function () { $(this).addClass('hidden');});
+  jT.ui.installMultiSelect($('#sidebar')[0], onSelectedUpdate);
+  $('#logger').on('click', function (e) { $(this).toggleClass('hidden'); });
+});
