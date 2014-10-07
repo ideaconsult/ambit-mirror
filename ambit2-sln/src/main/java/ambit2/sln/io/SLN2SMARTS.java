ae4c133f3202ab9d8c7d1488ecaaa406236b84f0
Nikolay Kochev <nick@uni-plovdiv.net>
2014-10-07 07:59:33 +0000

    Added new sub-package 'sln.io' and two conversion classes.

---
+++ ambit2-sln/src/main/java/ambit2/sln/io/SLN2SMARTS.java
@@ -0,0 +1,73 @@
+package ambit2.sln.io;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SLN2SMARTS 
+{
+	public enum NotationType{
+		SMILES, SMARTS, SMIRKS
+	}
+	
+	private List<String> conversionErrors = new ArrayList<String>();
+	private List<String> conversionWarnings = new ArrayList<String>();
+
+	public List<String> getConversionErrors() {
+		return conversionErrors;
+	}
+	
+	public List<String> getConversionWarnings() {
+		return conversionWarnings;
+	}
+	
+	public String getAllErrors()
+	{
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < conversionErrors.size(); i++)
+			sb.append(conversionErrors.get(i) + "\n");
+		return sb.toString();
+	}
+	
+	public NotationType getCompatibleNotation(String sln)	{
+		//TODO
+		return null;
+	}
+	
+	public String slnToSmiles(String sln)
+	{
+		//TODO
+		return null;
+	}
+	
+	public String slnToSmarts(String sln)
+	{
+		//TODO
+		return null;
+	}
+	
+	public String slnToSmirks(String sln)
+	{
+		//TODO
+		return null;
+	}
+	
+	public String SmilesToSLN(String sln)
+	{
+		//TODO
+		return null;
+	}
+	
+	public String SmartsToSLN(String sln)
+	{
+		//TODO
+		return null;
+	}
+	
+	public String SmirksToSLN(String sln)
+	{
+		//TODO
+		return null;
+	}
+	
+	
+}
