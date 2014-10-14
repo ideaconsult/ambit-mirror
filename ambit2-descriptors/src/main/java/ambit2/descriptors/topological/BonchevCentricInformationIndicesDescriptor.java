68d9f7125e775247c71dc5b5180d0a0e64e91b48
Nikolay Kochev <nick@uni-plovdiv.net>
2014-10-14 10:47:58 +0000

    Added new sub-package "descriptors.utils". Implemented new utility classes GraphMatrices and TopologicalUtilities. Implemented classes BonchevCentricInformationIndicesDescriptor and EccentricBasedMadanIndicesDescriptor.

---
+++ ambit2-descriptors/src/main/java/ambit2/descriptors/topological/BonchevCentricInformationIndicesDescriptor.java
@@ -0,0 +1,213 @@
+package ambit2.descriptors.topological;
+
+
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.qsar.DescriptorSpecification;
+import org.openscience.cdk.qsar.DescriptorValue;
+import org.openscience.cdk.qsar.IMolecularDescriptor;
+import org.openscience.cdk.qsar.result.DoubleArrayResult;
+import org.openscience.cdk.qsar.result.DoubleResult;
+import org.openscience.cdk.qsar.result.IDescriptorResult;
+
+import ambit2.descriptors.utils.GraphMatrices;
+import ambit2.descriptors.utils.TopologicalUtilities;
+
+
+public class  BonchevCentricInformationIndicesDescriptor implements IMolecularDescriptor
+{
+	public String[] getDescriptorNames() 
+	{
+		return new String[] {"ICR", "DDCI", "DCCI", "CCI", "GRCII", "GDDCI", "GDCCI", "GCCI"};
+	}
+
+	public String[] getParameterNames() 
+	{
+		return null;
+	}
+
+	public Object getParameterType(String arg0) 
+	{
+		return null;
+	}
+
+	public Object[] getParameters() 
+	{
+		return null;
+	}
+
+	public DescriptorSpecification getSpecification()
+	{
+		return new DescriptorSpecification(
+				"",
+				this.getClass().getName(),
+				" .java",
+				"University of Plovdiv, 2013");
+	}
+
+	public void setParameters(Object[] arg0) throws CDKException 
+	{
+	}
+
+	public DescriptorValue calculate(IAtomContainer container)
+	{
+		if (container == null) 
+		{
+			return new DescriptorValue(getSpecification(), getParameterNames(), getParameters(),
+					new DoubleResult (Double.NaN), getDescriptorNames(),
+					new CDKException("The supplied AtomContainer was NULL"));
+		}
+
+		if (container.getAtomCount() == 0) 
+		{
+			return new DescriptorValue(getSpecification(), getParameterNames(), getParameters(),
+					new DoubleResult(Double.NaN), getDescriptorNames(),
+					new CDKException("The supplied AtomContainer did not have any atoms"));
+		}
+
+		double ICR = 0.0; //radial centric information index 
+		double DDCI = 0.0; //distance degree centric index
+		double DCCI = 0.0; //distance code centric index
+		double CCI = 0.0; //copmlete centric index
+		double GRCII = 0.0; //generalized radial centric information index
+		double GDDCI = 0.0; //generalized distance degree centric index
+		double GDCCI = 0.0; //generalized distance code centric index
+		double GCCI = 0.0; //generalized complete centrix index
+
+		int n = container.getAtomCount();
+		int [][] D = GraphMatrices.getTopDistanceMatrix(container);
+		int[] Veec = GraphMatrices.getVertexEccentricity(D);
+		int [] DDV = TopologicalUtilities.DistanceDegreeVector(container);
+
+		HashMap <Integer, Integer> vdeg = new HashMap <Integer, Integer> ();
+
+		for (int i = 0; i < n; i++)
+		{
+			Integer key = Veec[i];
+
+			if (vdeg.containsKey(key))
+			{
+				Integer value = vdeg.get(key);
+				value = value +1;
+				vdeg.put(key, value);
+			}
+			else 
+				vdeg.put(key, new Integer (1));
+			//System.out.println("vdeg1 = " +vdeg);
+
+		}
+
+		Iterator<Integer> keySetIterator = vdeg.keySet().iterator();
+
+		while(keySetIterator.hasNext())
+		{
+			Integer key = keySetIterator.next();
+
+			double sum = vdeg.get(key);
+
+			ICR -= (sum/n)*((Math.log10(sum/n)/Math.log10(2)));
+		}
+
+
+
+		HashMap <Integer, Integer> vdeg1 = new HashMap <Integer, Integer> ();
+
+		for (int i = 0; i < n; i++)
+		{
+			Integer key = Veec[i];
+			Integer key1 = DDV[i];
+
+			if (vdeg1.containsKey(key1))
+			{
+				{
+					Integer value = vdeg1.get(key1);
+					value = value +1;
+					vdeg1.put(key1, value);
+				}
+				if ( vdeg1.containsKey(key))
+				{
+					Integer value = vdeg1.get(key);
+					value = value +1;
+					vdeg1.put(key, value);
+				}
+			}
+			else 
+				vdeg1.put(key1, new Integer (1));
+			System.out.println("vdeg1 = " +vdeg1);
+		}
+
+		Iterator<Integer> keySetIterator1 = vdeg1.keySet().iterator();
+
+		while(keySetIterator1.hasNext())
+		{
+			Integer key = keySetIterator1.next();
+
+			double sum = vdeg1.get(key);
+
+			DDCI -= (sum/n)*((Math.log10(sum/n)/Math.log10(2)));
+		}
+		
+		
+
+
+		
+		HashMap <Integer, Integer> vdeg2 = new HashMap <Integer, Integer> ();
+
+		for (int i = 0; i < n; i++)
+		{
+			Integer key = Veec[i];
+			Integer key1 = DDV[i];
+
+			if (vdeg2.containsKey(key1))
+			{
+				{
+					Integer value = vdeg2.get(key1);
+					value = value +1;
+					vdeg2.put(key1, value);
+				}
+				if ( vdeg2.containsKey(key))
+				{
+					Integer value = vdeg2.get(key);
+					value = value +1;
+					vdeg2.put(key, value);
+				}
+			}
+			else 
+				vdeg1.put(key1, new Integer (1)); 
+			//System.out.println("vdeg2 = " +vdeg2);
+		}
+
+		Iterator<Integer> keySetIterator2 = vdeg2.keySet().iterator();
+
+		while(keySetIterator2.hasNext())
+		{
+			Integer key = keySetIterator2.next();
+
+			double sum = vdeg2.get(key);
+
+			DCCI -= (sum/n)*((Math.log10(sum/n)/Math.log10(2)));
+		}
+
+
+		DoubleArrayResult result = new DoubleArrayResult();
+		result.add(ICR);
+		result.add(DDCI);
+		result.add(DCCI);
+
+
+		return new DescriptorValue(getSpecification(), getParameterNames(), getParameters(),
+				result, getDescriptorNames());
+	}
+
+	public IDescriptorResult getDescriptorResultType() 
+	{
+		return new DoubleArrayResult(0);
+	}
+
+}
+
+
---
+++ ambit2-descriptors/src/main/java/ambit2/descriptors/utils/TopologicalUtilities.java
@@ -0,0 +1,351 @@
+package ambit2.descriptors.utils;
+
+import java.util.List;
+import java.io.IOException;
+
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IBond;
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+import ambit2.descriptors.IsotopeFactory;
+
+
+public class TopologicalUtilities 
+{
+	public static String MP_ATOM_TOP_DEGREES = "MP_ATOM_TOP_DEGREES";
+	
+	
+	public static int[] calcAtomTopDegrees(IAtomContainer container) 
+	{
+		return calcAtomTopDegrees(container, false);
+	}
+	
+	public static int[] calcAtomTopDegrees(IAtomContainer container, boolean ExlcudeExplicitH) 
+	{
+		if (container == null) 
+			return null;
+
+		if (container.getAtomCount() == 0)
+			return new int[0];
+		
+		int n = container.getAtomCount();
+		int m = container.getBondCount();
+		
+		int [] dv = new int [n];
+		
+		for (int i = 0; i < n; i++)
+			dv[i] = 0;
+		
+		for (int k = 0; k < m; k++)
+		{
+			IBond b = container.getBond(k);
+			IAtom at0 = b.getAtom(0);
+			IAtom at1 = b.getAtom(1);
+			
+			if (ExlcudeExplicitH)
+			{
+				if (at0.getSymbol().equals("H"))
+					continue;
+				if (at1.getSymbol().equals("H"))
+					continue;
+			}
+			
+			int ind0 = container.getAtomNumber(at0);
+			int ind1 = container.getAtomNumber(at1);
+			
+			dv [ind0]++;
+			dv [ind1]++;
+		}
+		
+		return dv;
+	}
+		
+	public static int[] getAtomTopDegrees(IAtomContainer container) 
+	{
+		int[] degrees = (int[])container.getProperty(MP_ATOM_TOP_DEGREES);
+		
+		if (degrees == null)
+			degrees = calcAtomTopDegrees(container); 
+			
+		return degrees;
+	}
+	
+	public static double [] getMadanChemicalDegree (IAtomContainer container) throws IOException
+	{
+		int n = container.getAtomCount();
+		double [] madanDegree = new double [n];		
+				
+		IsotopeFactory isoFact = IsotopeFactory.getInstance(container.getBuilder());
+		
+		for (int i = 0; i < n; i++)
+		{	
+			List<IAtom> list = container.getConnectedAtomsList(container.getAtom(i));			
+			madanDegree[i] = 0;
+
+			for (int m = 0; m < list.size(); m ++)
+			{
+				IAtom atom = list.get(m);
+				madanDegree [i] += isoFact.getMajorIsotope(atom.getSymbol()).getExactMass() / 12.0107;
+			}
+		}	
+		return madanDegree;
+	}
+	
+	public static double [] getRelativeAtomicWeights (IAtomContainer container) throws IOException
+	{
+		int n = container.getAtomCount();
+		double [] relAtWeight = new double [n];		
+				
+		IsotopeFactory isoFact = IsotopeFactory.getInstance(container.getBuilder());
+		
+		for (int i = 0; i < n; i++)
+		{	
+				IAtom atom = container.getAtom(i);
+				relAtWeight [i] = isoFact.getMajorIsotope(atom.getSymbol()).getExactMass() / 12.0107;
+		}
+			
+		return relAtWeight;
+	}
+		
+	
+	public static double[] DecimalAdjVector_A10 (IAtomContainer container) 
+	{
+		int [][]dA  =  GraphMatrices.getAdjacencyMatrix(container);
+
+		int n = container.getAtomCount();
+		double a10[] = new double[n];
+		double aij;
+
+		for (int i = 0; i < n; i++)
+		{
+			a10[i] = 0.0;
+			double pow2 = 1.0;  //2^0
+			for (int j = n-1; j >= 0; j--)
+			{
+				aij = dA [i][j];
+				if(aij == 1)
+					a10[i] += pow2;
+
+				pow2 *= 2; //pow2 = 2^(n-j)
+			}
+		}
+
+		return a10;
+	}
+
+	public static int DecimalAdjVector_A1 (IAtomContainer container) 
+	{
+		double [] a10 = DecimalAdjVector_A10(container);		
+
+		int n = container.getAtomCount();
+		int A1 = 0;
+		int sum = 0;
+
+		for (int i = 0; i < n; i++)
+			sum += a10[i];
+		A1 = sum;
+
+		return A1;
+	}
+
+	public static int DecimalAdjVector_A2 (IAtomContainer container) 
+	{
+		double [] a10 = DecimalAdjVector_A10(container);
+		int[] dv =  getAtomTopDegrees(container);
+
+		int n = container.getAtomCount();
+		int A2 = 0;
+		int sum = 0;
+
+		for (int i = 0; i < n; i++)
+		{
+			sum +=dv[i]* a10[i];
+			A2 = sum;
+		}
+
+		return A2;
+	}
+
+	public static int DecimalAdjVector_A3 (IAtomContainer container) 
+	{
+					
+		int d[] =  dVector (container);
+		int n = container.getAtomCount();
+		int A3 = 0;
+		int sum = 0;
+		for (int i = 0; i < n; i++)
+		{
+			sum+=d[i];
+		}
+		A3 = sum;
+
+		return A3;
+	}
+	
+	
+	private static int[] dVector (IAtomContainer container)
+	{		
+		double a10 [] =  DecimalAdjVector_A10(container);
+		int[][] dD  =  GraphMatrices.getTopDistanceMatrix(container);	
+
+		int n = container.getAtomCount();
+		int d[] = new int[n];
+		for (int i = 0; i < d.length; i++)
+			d[i] = 0;
+
+		int DM=0;
+
+		for (int i = 0; i < dD.length; i++)
+		{
+			for (int j = 0; j < dD.length; j++)
+			{
+				DM= dD[i][j];
+				d[i] += DM*a10[i];
+			}
+		}
+
+		return d;
+	}
+	
+	public static double [] AtomClusteringCoefficient (IAtomContainer container) 
+	{
+		int[][] A = GraphMatrices.getAdjacencyMatrix(container);
+		int[] dv = getAtomTopDegrees(container);
+
+		int n = container.getAtomCount();
+		double acc[] = new double[n];
+
+		for (int i = 0; i < n; i++)
+		{
+			if (dv[i] <= 1)  //delta1 = 0 or deltai = 1
+				acc[i] = 0;
+			else
+			{
+				int bi = getAtomNeighborsInterconnectivity(i, A);
+				acc[i] = 2.0*bi /(1.0*dv[i]*(dv[i]-1));
+			}
+		}
+
+		return acc;
+	}
+
+	//check return result type 'int' ?
+	static int getAtomNeighborsInterconnectivity(int i, int[][] A)
+	{		
+		int sum = 0;
+
+		for ( i = 0; i < A.length; i++)
+			for (int j = 0; j < A.length; j++)
+			{
+				if (A[i][j] == 0)
+					continue;
+
+				for (int m = 0; m < A.length; m++)
+				{
+					if (m != i)
+						sum+= A[j][m] * A[m][i];
+				}
+			}
+
+		int bi = sum/2;
+
+		return bi;
+	}
+	
+	
+	public static double OverallDegreeOfClustering (IAtomContainer container)
+	{
+		double acc [] =  AtomClusteringCoefficient(container);
+
+		int n = container.getAtomCount();
+		double sum = 0.0;
+		for (int i = 0; i < n; i++)
+			sum += acc[i];
+
+		if (n > 0)
+			sum = sum /n;
+
+		return sum;
+	}
+	
+	public static int[] DistanceDegreeVector (IAtomContainer container)  //DS[i]
+	{
+		int [][]D  =  GraphMatrices.getTopDistanceMatrix(container);
+		int n = container.getAtomCount();
+		int ddv[] = new int[n];
+
+		for(int i = 0; i < n; i++)
+		{
+			int sum = 0;
+			for(int j = 0 ; j < n; j++)
+				sum = sum + D[i][j];
+			
+			ddv[i] = sum;
+		}
+
+		return ddv;
+	}
+
+	public static int[] SquareDistanceDegreeVector (IAtomContainer container)
+	{
+		int [][]D  =  GraphMatrices.getTopDistanceMatrix(container);
+
+		int n = container.getAtomCount();
+		int sddv[] = new int[n];
+
+		for(int i = 0; i < n; i++)
+		{
+			int sum = 0;
+			for(int j = 0 ; j < n; j++)
+				sum = sum + D[i][j]*D[i][j];
+			
+			sddv[i] = sum;
+		}
+
+		return sddv;
+	}
+	
+	public static double [] ClosenessCentrality (IAtomContainer container)
+	{		
+
+		int n = container.getAtomCount();
+		double CC[] = new double[n];
+		
+		for (int i = 0; i < n; i++)
+		{
+			CC[i] = 0.0;
+			double A = container.getAtomCount();
+			int vdd [] =  DistanceDegreeVector(container);
+			
+			CC[i]=(A-1)/vdd[i];
+		}
+		return CC;
+	}
+	
+	public static double[] D_t (IAtomContainer container)
+	{		
+			
+		int[] dv = getAtomTopDegrees(container);
+		int ddv[] = DistanceDegreeVector(container);
+		
+		int n = container.getAtomCount();
+		double t[] = new double[n];
+		double sum = 0.0;
+		for(int i = 0; i < n; i++)
+		{
+			t[i] = 0.0;
+			
+		sum = ddv[i] - dv[i];
+		
+		t[i] = sum;
+		}
+		return t;
+	}
+
+	
+	
+	
+	
+	
+}
+	
