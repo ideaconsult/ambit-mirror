b7e097553dec92be4a74dd77101913326c63a74e
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2014-12-01 18:33:32 +0000

    /bundle/id/study instead of /bundle/id/studysummary returns the same format as /query/study

---
+++ ambit2-apps/ambit2-www/src/main/java/ambit2/rest/bundle/BundleSubstanceStudyResource.java
@@ -0,0 +1,60 @@
+package ambit2.rest.bundle;
+
+import java.util.Map;
+
+import net.idea.modbcum.i.IQueryRetrieval;
+import net.idea.modbcum.r.QueryReporter;
+import net.idea.restnet.i.freemarker.IFreeMarkerApplication;
+
+import org.restlet.Context;
+import org.restlet.Request;
+import org.restlet.Response;
+import org.restlet.data.Reference;
+import org.restlet.data.Status;
+import org.restlet.resource.ResourceException;
+
+import ambit2.base.data.substance.SubstanceEndpointsBundle;
+import ambit2.db.substance.study.facet.SubstanceByCategoryFacet;
+import ambit2.db.update.bundle.effects.BundleStudyFacetQuery;
+import ambit2.rest.OpenTox;
+import ambit2.rest.facet.AmbitFacetResource;
+import ambit2.rest.substance.study.StudySummaryJSONReporter;
+
+public class BundleSubstanceStudyResource<Q extends IQueryRetrieval<SubstanceByCategoryFacet>> extends AmbitFacetResource<SubstanceByCategoryFacet,Q>  {
+	public final static String resource = "/study";
+	
+	public BundleSubstanceStudyResource() {
+		super();
+		setHtmlbyTemplate(false);
+	}
+	@Override
+	protected Q createQuery(Context context,
+			Request request, Response response) throws ResourceException {
+		
+		Object idbundle = request.getAttributes().get(OpenTox.URI.bundle.getKey());
+		if (idbundle!=null) {
+			try {
+				SubstanceEndpointsBundle bundle = new SubstanceEndpointsBundle(new Integer(Reference.decode(idbundle.toString())));
+				BundleStudyFacetQuery q = new BundleStudyFacetQuery(
+						String.format("%s%s/%s/study",getRootRef(),OpenTox.URI.bundle.getURI(),idbundle)
+						);
+				q.setFieldname(bundle);
+				return (Q)q;
+			} catch (Exception x) {	}
+		}
+		throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST);
+	}
+	@Override
+	protected QueryReporter createJSONReporter(Request request, String jsonp) {
+		return new StudySummaryJSONReporter(request,jsonp);
+	};
+	
+	@Override
+	public void configureTemplateMap(Map<String, Object> map, Request request,
+			IFreeMarkerApplication app) {
+		super.configureTemplateMap(map, request, app);
+		map.put("facet_title","Substance study summary");
+		map.put("facet_group","Structure type");
+		map.put("facet_count","Number of structures");
+	}
+}
