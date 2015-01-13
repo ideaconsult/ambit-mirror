722ec60f9c68a6ff3073d64dc55ee99276131707
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2015-01-13 14:29:00 +0000

    code formatting

--- ambit2-all/ambit2-base/src/main/java/ambit2/base/data/substance/SubstanceProperty.java
+++ ambit2-all/ambit2-base/src/main/java/ambit2/base/data/substance/SubstanceProperty.java
@@ -13,66 +13,86 @@ import com.google.common.base.Objects;
 
 public class SubstanceProperty extends Property {
 	protected boolean extendedURI = false;
+
 	public boolean isExtendedURI() {
 		return extendedURI;
 	}
+
 	public void setExtendedURI(boolean extendedURI) {
 		this.extendedURI = extendedURI;
 	}
+
 	protected String topcategory;
 	protected String endpointcategory;
-	
+
 	protected String identifier;
+
 	public String getIdentifier() {
 		return identifier;
 	}
+
 	public void setIdentifier(String identifier) {
 		this.identifier = identifier;
 	}
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 4436949404781828869L;
-	public SubstanceProperty(String topcategory,String endpointCategory,String name, String reference) {
-		this(topcategory,endpointCategory,name,null,LiteratureEntry.getInstance(reference,""));
+
+	public SubstanceProperty(String topcategory, String endpointCategory,
+			String name, String reference) {
+		this(topcategory, endpointCategory, name, null, LiteratureEntry
+				.getInstance(reference, ""));
 	}
-	
-	public SubstanceProperty(String topcategory,String endpointCategory,String name, String units, ILiteratureEntry reference) {
-		super(name,units,reference);
+
+	public SubstanceProperty(String topcategory, String endpointCategory,
+			String name, String units, ILiteratureEntry reference) {
+		super(name, units, reference);
 		setEnabled(true);
 		setTopcategory(topcategory);
 		setEndpointcategory(endpointCategory);
 		setLabel(String.format("http://www.opentox.org/echaEndpoints.owl#%s",
-				endpointCategory==null?"UNKNOWN_TOXICITY":endpointcategory.replace("_SECTION", "")));
+				endpointCategory == null ? "UNKNOWN_TOXICITY"
+						: endpointcategory.replace("_SECTION", "")));
 	}
+
 	@Override
 	public String getRelativeURI() {
 		try {
-		return String.format("/property/%s/%s%s%s/%s%s%s",
-				URLEncoder.encode(topcategory==null?"TOX":topcategory,"UTF-8"),
-				URLEncoder.encode(						
-						endpointcategory==null?Protocol._categories.UNKNOWN_TOXICITY_SECTION.name():endpointcategory,
-						"UTF-8"),
-				getName()==null?"":"/",		
-				URLEncoder.encode(getName(),"UTF-8"),
-				identifier==null?
-						UUID.nameUUIDFromBytes((getName() + getTitle()).toString().getBytes()).toString()
-						:identifier,
-				extendedURI?"/":"",
-				extendedURI?URLEncoder.encode(UUID.nameUUIDFromBytes(reference.getTitle().getBytes()).toString()):""
-						);
+			return String
+					.format("/property/%s/%s%s%s/%s%s%s",
+							URLEncoder.encode(topcategory == null ? "TOX"
+									: topcategory, "UTF-8"),
+							URLEncoder
+									.encode(endpointcategory == null ? Protocol._categories.UNKNOWN_TOXICITY_SECTION
+											.name() : endpointcategory, "UTF-8"),
+							getName() == null ? "" : "/",
+							URLEncoder.encode(getName(), "UTF-8"),
+							identifier == null ? UUID.nameUUIDFromBytes(
+									(getName() + getTitle()).toString()
+											.getBytes()).toString()
+									: identifier,
+							extendedURI ? "/" : "",
+							extendedURI ? URLEncoder.encode(UUID
+									.nameUUIDFromBytes(
+											reference.getTitle().getBytes())
+									.toString()) : "");
 		} catch (UnsupportedEncodingException x) {
 			return "/property";
 		}
 	}
+
 	@Override
-    public int hashCode() {
-		return Objects.hashCode(topcategory,endpointcategory,getTitle(),getName());
-    }	
+	public int hashCode() {
+		return Objects.hashCode(topcategory, endpointcategory, getTitle(),
+				getName());
+	}
 
 	public String getTopcategory() {
 		return topcategory;
 	}
+
 	public void setTopcategory(String topcategory) {
 		this.topcategory = topcategory;
 	}
@@ -80,6 +100,7 @@ public class SubstanceProperty extends Property {
 	public String getEndpointcategory() {
 		return endpointcategory;
 	}
+
 	public void setEndpointcategory(String endpointcategory) {
 		this.endpointcategory = endpointcategory;
 	}
