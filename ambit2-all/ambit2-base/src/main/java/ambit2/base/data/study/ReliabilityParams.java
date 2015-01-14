c43629f7c97064d80e27ea89f1b720594bf4d954
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2015-01-14 13:01:10 +0000

    fix reliability  json parsing

--- ambit2-all/ambit2-base/src/main/java/ambit2/base/data/study/ReliabilityParams.java
+++ ambit2-all/ambit2-base/src/main/java/ambit2/base/data/study/ReliabilityParams.java
@@ -1,5 +1,7 @@
 package ambit2.base.data.study;
 
+import java.util.Hashtable;
+
 import com.google.common.base.Objects;
 
 /**
@@ -14,7 +16,21 @@ public class ReliabilityParams<VALUE> extends Params<VALUE> {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	    
+	private static Hashtable<String, String> phrasegroup_Z05 = phrasegroup_Z05();
+	private static Hashtable<String, String> phrasegroup_Z05() {
+	    	Hashtable<String, String> p = new Hashtable<String,String>();
+	    	p.put("1895", "experimental result");
+	    	p.put("1896", "experimental study planned");
+	    	p.put("1885", "estimated by calculation");
+	    	p.put("2303", "read-across based on grouping of substances (category approach)");
+	    	p.put("2304", "read-across from supporting substance (structural analogue or surrogate)");
+	    	p.put("14", "(Q)SAR");
+	    	p.put("1342", "other:");
+	    	p.put("1173", "no data");
+	    	p.put("NOT_SPECIFIED", ""); 
+	    	return p;
+	    }
 
 	@Override
     public int hashCode() {
@@ -74,7 +90,8 @@ public class ReliabilityParams<VALUE> extends Params<VALUE> {
 	}
 
 	public void setStudyResultType(VALUE studyResultType) {
-		put(_FIELDS_RELIABILITY.r_studyResultType.name(),studyResultType);
+	        Object value = phrasegroup_Z05.get(studyResultType);
+		put(_FIELDS_RELIABILITY.r_studyResultType.name(),value==null?studyResultType:(VALUE)value);
 	}
 
 
