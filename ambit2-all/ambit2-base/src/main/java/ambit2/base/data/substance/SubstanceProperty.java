bc2a434e650c91cb75cd01ce89a9179a29c972d2
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2015-01-17 12:47:41 +0000

    correct implementation of SubstanceProperty equals method

--- ambit2-all/ambit2-base/src/main/java/ambit2/base/data/substance/SubstanceProperty.java
+++ ambit2-all/ambit2-base/src/main/java/ambit2/base/data/substance/SubstanceProperty.java
@@ -12,98 +12,97 @@ import ambit2.base.data.study.Protocol;
 import com.google.common.base.Objects;
 
 public class SubstanceProperty extends Property {
-	protected boolean extendedURI = false;
+    protected boolean extendedURI = false;
 
-	public boolean isExtendedURI() {
-		return extendedURI;
-	}
+    public boolean isExtendedURI() {
+	return extendedURI;
+    }
 
-	public void setExtendedURI(boolean extendedURI) {
-		this.extendedURI = extendedURI;
-	}
+    public void setExtendedURI(boolean extendedURI) {
+	this.extendedURI = extendedURI;
+    }
 
-	protected String topcategory;
-	protected String endpointcategory;
+    protected String topcategory;
+    protected String endpointcategory;
 
-	protected String identifier;
+    protected String identifier;
 
-	public String getIdentifier() {
-		return identifier;
-	}
+    public String getIdentifier() {
+	return identifier;
+    }
 
-	public void setIdentifier(String identifier) {
-		this.identifier = identifier;
-	}
+    public void setIdentifier(String identifier) {
+	this.identifier = identifier;
+    }
 
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 4436949404781828869L;
+    private static final long serialVersionUID = 4436949404781828869L;
 
-	public SubstanceProperty(String topcategory, String endpointCategory,
-			String name, String reference) {
-		this(topcategory, endpointCategory, name, null, LiteratureEntry
-				.getInstance(reference, ""));
-	}
+    public SubstanceProperty(String topcategory, String endpointCategory, String name, String reference) {
+	this(topcategory, endpointCategory, name, null, LiteratureEntry.getInstance(reference, ""));
+    }
 
-	public SubstanceProperty(String topcategory, String endpointCategory,
-			String name, String units, ILiteratureEntry reference) {
-		super(name, units, reference);
-		setEnabled(true);
-		setTopcategory(topcategory);
-		setEndpointcategory(endpointCategory);
-		setLabel(String.format("http://www.opentox.org/echaEndpoints.owl#%s",
-				endpointCategory == null ? "UNKNOWN_TOXICITY"
-						: endpointcategory.replace("_SECTION", "")));
-	}
+    public SubstanceProperty(String topcategory, String endpointCategory, String name, String units,
+	    ILiteratureEntry reference) {
+	super(name, units, reference);
+	setEnabled(true);
+	setTopcategory(topcategory);
+	setEndpointcategory(endpointCategory);
+	setLabel(String.format("http://www.opentox.org/echaEndpoints.owl#%s",
+		endpointCategory == null ? "UNKNOWN_TOXICITY" : endpointcategory.replace("_SECTION", "")));
+    }
 
-	@Override
-	public String getRelativeURI() {
-		try {
-			String name = getName();
-			return String
-					.format("/property/%s/%s%s%s/%s%s%s",
-							URLEncoder.encode(topcategory == null ? "TOX"
-									: topcategory, "UTF-8"),
-							URLEncoder
-									.encode(endpointcategory == null ? Protocol._categories.UNKNOWN_TOXICITY_SECTION
-											.name() : endpointcategory, "UTF-8"),
-							name == null ? "" : "/",
-							name == null ? "": URLEncoder.encode(name, "UTF-8"),
-							identifier == null ? UUID.nameUUIDFromBytes(
-									(name + getTitle()).toString()
-											.getBytes()).toString()
-									: identifier,
-							extendedURI ? "/" : "",
-							extendedURI ? URLEncoder.encode(UUID
-									.nameUUIDFromBytes(
-											reference.getTitle().getBytes())
-									.toString()) : "");
-		} catch (UnsupportedEncodingException x) {
-			return "/property";
-		}
+    @Override
+    public String getRelativeURI() {
+	try {
+	    String name = getName();
+	    return String.format("/property/%s/%s%s%s/%s%s%s", URLEncoder.encode(topcategory == null ? "TOX"
+		    : topcategory, "UTF-8"), URLEncoder.encode(
+		    endpointcategory == null ? Protocol._categories.UNKNOWN_TOXICITY_SECTION.name() : endpointcategory,
+		    "UTF-8"), name == null ? "" : "/", name == null ? "" : URLEncoder.encode(name, "UTF-8"),
+		    identifier == null ? UUID.nameUUIDFromBytes((name + getTitle()).toString().getBytes()).toString()
+			    : identifier, extendedURI ? "/" : "",
+		    extendedURI ? URLEncoder.encode(UUID.nameUUIDFromBytes(reference.getTitle().getBytes()).toString())
+			    : "");
+	} catch (UnsupportedEncodingException x) {
+	    return "/property";
 	}
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hashCode(topcategory, endpointcategory, getTitle(),
-				getName());
-	}
+    @Override
+    public int hashCode() {
+	return Objects.hashCode(topcategory, endpointcategory,getName(), identifier, getTitle());
+    }
 
-	public String getTopcategory() {
-		return topcategory;
-	}
+    @Override
+    public boolean equals(Object o) {
+	if (this == o)
+	    return true;
+	if (o == null || getClass() != o.getClass())
+	    return false;
+	SubstanceProperty that = (SubstanceProperty) o;
+	return Objects.equal(this.topcategory, that.topcategory) && Objects.equal(this.endpointcategory, that.endpointcategory)
+		&& Objects.equal(this.getName(), that.getName()) && Objects.equal(this.identifier, that.identifier) 
+		&& Objects.equal(this.getTitle(), that.getTitle()) ;
+		
+    }
 
-	public void setTopcategory(String topcategory) {
-		this.topcategory = topcategory;
-	}
+    public String getTopcategory() {
+	return topcategory;
+    }
 
-	public String getEndpointcategory() {
-		return endpointcategory;
-	}
+    public void setTopcategory(String topcategory) {
+	this.topcategory = topcategory;
+    }
 
-	public void setEndpointcategory(String endpointcategory) {
-		this.endpointcategory = endpointcategory;
-	}
+    public String getEndpointcategory() {
+	return endpointcategory;
+    }
+
+    public void setEndpointcategory(String endpointcategory) {
+	this.endpointcategory = endpointcategory;
+    }
 
 }
