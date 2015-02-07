a2dc8502304d75f5d4963d58bc3e172671612f4f
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2015-02-07 13:27:04 +0000

    JSON serialisation and tests

--- ambit2-base/src/main/java/ambit2/base/data/PropertyAnnotations.java
+++ ambit2-base/src/main/java/ambit2/base/data/PropertyAnnotations.java
@@ -2,16 +2,29 @@ package ambit2.base.data;
 
 import java.util.ArrayList;
 
-
 public class PropertyAnnotations extends ArrayList<PropertyAnnotation> {
 
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = -1776802858368833193L;
+    private static final long serialVersionUID = -1776802858368833193L;
 
-	public PropertyAnnotations() {
-		
-	}
+    public PropertyAnnotations() {
 
+    }
+
+    public String toJSON() {
+	StringBuilder b = new StringBuilder();
+	String acomma = "";
+	for (int i=0; i < size(); i++)
+	    try {
+		b.append(acomma);
+		get(i).toJSON(b);
+		acomma = ",";
+	    } catch (Exception x) {
+
+		return null;
+	    }
+	return b.toString();
+    }
 }
