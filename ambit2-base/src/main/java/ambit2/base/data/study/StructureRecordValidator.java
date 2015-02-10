f777cc49648051d5003cd189adc7d28de7ba633d
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2015-02-10 08:39:57 +0000

    SubstanceRecord validator

---
+++ ambit2-base/src/main/java/ambit2/base/data/study/StructureRecordValidator.java
@@ -0,0 +1,122 @@
+package ambit2.base.data.study;
+
+import java.util.UUID;
+import java.util.logging.Level;
+
+import net.idea.modbcum.p.DefaultAmbitProcessor;
+import ambit2.base.data.SubstanceRecord;
+import ambit2.base.interfaces.IStructureRecord;
+import ambit2.base.relation.composition.CompositionRelation;
+
+public class StructureRecordValidator extends DefaultAmbitProcessor<IStructureRecord, IStructureRecord> {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 201323883830157691L;
+    protected boolean fixErrors = false;
+    protected String prefix = "XLSX";
+
+    public String getPrefix() {
+	return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+	this.prefix = prefix;
+    }
+
+    public StructureRecordValidator() {
+	this(false);
+    }
+
+    public StructureRecordValidator(boolean fixErrors) {
+	super();
+	setFixErrors(fixErrors);
+    }
+
+    public boolean isFixErrors() {
+	return fixErrors;
+    }
+
+    public void setFixErrors(boolean fixErrors) {
+	this.fixErrors = fixErrors;
+    }
+
+    @Override
+    public IStructureRecord process(IStructureRecord record) throws Exception {
+	if (record instanceof SubstanceRecord)
+	    return validate((SubstanceRecord) record);
+	return record;
+    }
+
+    public IStructureRecord validate(SubstanceRecord record) throws Exception {
+	if (record.getCompanyUUID() == null)
+	    logger.log(Level.SEVERE, "Missing Substance UUID");
+	if (record.getMeasurements() == null) {
+	    logger.log(Level.WARNING, "No measurements");
+	} else
+	    for (ProtocolApplication papp : record.getMeasurements()) {
+		validate(record, papp);
+	    }
+	if (record.getRelatedStructures() == null) {
+	    logger.log(Level.WARNING, "No composition");
+	} else {
+	    for (CompositionRelation rel : record.getRelatedStructures()) {
+		validate(record, rel);
+	    }
+	}
+	return record;
+    }
+
+    public IStructureRecord validate(SubstanceRecord record,
+	    ProtocolApplication<Protocol, IParams, String, IParams, String> papp) throws Exception {
+	if (papp.getDocumentUUID() == null) {
+	    logger.log(Level.SEVERE, "Missing measurement UUID");
+	    if (isFixErrors()) {
+		papp.setDocumentUUID(generateUUIDfromString(prefix, null));
+		logger.log(Level.WARNING, "Generated measurement UUID");
+	    }
+	}
+	if (papp.getProtocol() == null)
+	    logger.log(Level.SEVERE, "Missing protocol");
+	else {
+	    if (papp.getProtocol().getCategory() == null) {
+		logger.log(Level.SEVERE, "Missing protocol category");
+		if (isFixErrors()) {
+		    papp.getProtocol().setCategory(Protocol._categories.UNKNOWN_TOXICITY_SECTION.name());
+		    papp.getProtocol().setTopCategory(Protocol._categories.UNKNOWN_TOXICITY_SECTION.getTopCategory());
+		}
+	    }
+	}
+	if (papp.getInterpretationResult() == null)
+	    logger.log(Level.WARNING, "Missing interpretation result");
+	if (papp.getInterpretationCriteria() == null)
+	    logger.log(Level.WARNING, "Missing interpretation criteria");
+
+	if (papp.getEffects() == null)
+	    logger.log(Level.SEVERE, "Missing results");
+	else
+	    for (EffectRecord<String, IParams, String> effect : papp.getEffects()) {
+		if (effect.getEndpoint()==null)  logger.log(Level.SEVERE, "Missing endpoint name");
+		if (effect.getConditions()==null || effect.getConditions().isEmpty())  logger.log(Level.WARNING, "Missing conditions");
+		if (effect.getLoValue()==null && effect.getUpValue()==null && effect.getTextValue()==null)  logger.log(Level.SEVERE, "Missing result value");
+		if ((effect.getLoValue()!=null || effect.getUpValue()!=null) && effect.getUnit()==null)  logger.log(Level.WARNING, "Value without units");
+	    }
+	return record;
+    }
+
+    public IStructureRecord validate(SubstanceRecord record, CompositionRelation rel) throws Exception {
+	if (rel.getCompositionUUID() == null) {
+	    logger.log(Level.SEVERE, "Missing composition UUID");
+	    if (isFixErrors()) {
+		rel.setCompositionUUID(record.getCompanyUUID());
+		logger.log(Level.WARNING, "Generated composition UUID");
+	    }
+	}
+	return record;
+    }
+
+    public static String generateUUIDfromString(String prefix, String id) {
+	return prefix + "-" + (id == null ? UUID.randomUUID() : UUID.nameUUIDFromBytes(id.getBytes()));
+    }
+}
