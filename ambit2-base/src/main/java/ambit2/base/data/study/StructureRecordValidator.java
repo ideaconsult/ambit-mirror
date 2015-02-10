aa16769182e2c991fbae4db41acf6df6b4d709fb
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2015-02-10 10:24:51 +0000

    more validation

--- ambit2-base/src/main/java/ambit2/base/data/study/StructureRecordValidator.java
+++ ambit2-base/src/main/java/ambit2/base/data/study/StructureRecordValidator.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 import java.util.logging.Level;
 
 import net.idea.modbcum.p.DefaultAmbitProcessor;
+import ambit2.base.data.I5Utils;
+import ambit2.base.data.Property;
 import ambit2.base.data.SubstanceRecord;
 import ambit2.base.interfaces.IStructureRecord;
 import ambit2.base.relation.composition.CompositionRelation;
@@ -16,6 +18,15 @@ public class StructureRecordValidator extends DefaultAmbitProcessor<IStructureRe
     private static final long serialVersionUID = 201323883830157691L;
     protected boolean fixErrors = false;
     protected String prefix = "XLSX";
+    protected String filename;
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
 
     public String getPrefix() {
 	return prefix;
@@ -26,11 +37,12 @@ public class StructureRecordValidator extends DefaultAmbitProcessor<IStructureRe
     }
 
     public StructureRecordValidator() {
-	this(false);
+	this(UUID.randomUUID().toString(),false);
     }
 
-    public StructureRecordValidator(boolean fixErrors) {
+    public StructureRecordValidator(String filename, boolean fixErrors) {
 	super();
+	setFilename(filename);
 	setFixErrors(fixErrors);
     }
 
@@ -58,13 +70,44 @@ public class StructureRecordValidator extends DefaultAmbitProcessor<IStructureRe
 	    for (ProtocolApplication papp : record.getMeasurements()) {
 		validate(record, papp);
 	    }
-	if (record.getRelatedStructures() == null) {
+	if (record.getRelatedStructures() == null || record.getRelatedStructures().isEmpty()) {
 	    logger.log(Level.WARNING, "No composition");
 	} else {
+	    IStructureRecord referenceStructure = null;
+	    double proportion = 0;
 	    for (CompositionRelation rel : record.getRelatedStructures()) {
 		validate(record, rel);
+		switch (rel.getRelationType()) {
+		case HAS_CONSTITUENT : {
+		    if (referenceStructure==null || (proportion < rel.getRelation().getTypical_value())) {
+			referenceStructure = rel.getSecondStructure();
+			proportion =  rel.getRelation().getTypical_value()==null?0:rel.getRelation().getTypical_value();
+		    }
+		    break;
+		}
+		}
+	    }
+	    if ((record.getReferenceSubstanceUUID()==null) && referenceStructure!=null) {
+		String uuid = generateUUIDfromString(prefix, null);
+		record.setReferenceSubstanceUUID(uuid);
+		referenceStructure.setProperty(Property.getI5UUIDInstance(), uuid);
+	    }
+
+	}
+	if (record.getOwnerName()==null) {
+	    logger.log(Level.WARNING, "Missing substance owner name");
+	    if (fixErrors) {
+		record.setOwnerName(filename);
+		logger.log(Level.WARNING, "Assigned file name to substance owner name");
 	    }
 	}
+	if (record.getOwnerUUID()==null) {
+	    logger.log(Level.WARNING, "Missing substance owner UUID");
+	    if (fixErrors) {
+		record.setOwnerUUID(generateUUIDfromString(prefix, record.getOwnerName()));
+		logger.log(Level.WARNING, "Generated substance owner UUID");
+	    }
+	}    
 	return record;
     }
 
