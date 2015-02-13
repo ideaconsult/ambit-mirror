be572c7104a733bd192c165dbafcfd1592d55503
Nikolay Kochev <nick@uni-plovdiv.net>
2015-02-13 08:45:34 +0000

    Added output writers in TautomerAnalysis. Made fields public in class Rule.

--- ambit2-all/ambit2-tautomers/src/test/java/ambit2/tautomers/test/TestTautomerAnalysis.java
+++ ambit2-all/ambit2-tautomers/src/test/java/ambit2/tautomers/test/TestTautomerAnalysis.java
@@ -1,6 +1,7 @@
 package ambit2.tautomers.test;
 
 import ambit2.tautomers.tools.TautomerAnalysis;
+import ambit2.tautomers.tools.TautomerAnalysis.Task;
 
 public class TestTautomerAnalysis 
 {
@@ -13,8 +14,14 @@ public class TestTautomerAnalysis
 	public static void generateRuleCouples() throws Exception
 	{
 		TautomerAnalysis ta = new TautomerAnalysis();
+		
 		ta.filePath = "D:/Projects/Nina/Tautomers/test-druglike-str/";
 		ta.inputFileName = "TOX21S_v4b_CID_structures_v02.smi";
+		ta.outFileType = "csv";
+		ta.outFilePrefix = "out";
+		ta.setMoleculeFilter("#Mol=[1,100]");
+		ta.task = Task.GEN_RULE_COUPLES;
+		
 		ta.process();
 	}
 
