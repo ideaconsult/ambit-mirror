04a10cda70b3e7a77ad5f92ceb95796fc3e86785
Nikolay Kochev <nick@uni-plovdiv.net>
2015-02-13 11:47:52 +0000

    Finished function generatePairForEachRuleInstance(). Added function getAtoms() in IRuleInstance. Added testTautomerPairs().

--- ambit2-tautomers/src/test/java/ambit2/tautomers/test/TestTautomerAnalysis.java
+++ ambit2-tautomers/src/test/java/ambit2/tautomers/test/TestTautomerAnalysis.java
@@ -1,5 +1,10 @@
 package ambit2.tautomers.test;
 
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+import ambit2.smarts.SmartsHelper;
+import ambit2.tautomers.TautomerManager;
+import ambit2.tautomers.TautomerUtils;
 import ambit2.tautomers.tools.TautomerAnalysis;
 import ambit2.tautomers.tools.TautomerAnalysis.Task;
 
@@ -8,7 +13,8 @@ public class TestTautomerAnalysis
 	
 	public static void main(String[] args) throws Exception
 	{
-		generateRuleCouples();
+		testTautomerPairs("NC=CCCCCCC=O");
+		//generateRuleCouples();
 	}
 	
 	public static void generateRuleCouples() throws Exception
@@ -24,6 +30,14 @@ public class TestTautomerAnalysis
 		ta.task = Task.GEN_RULE_PAIR;
 		
 		ta.process();
+	}
+	
+	public static void testTautomerPairs(String smi) throws Exception
+	{
+		System.out.println("testTautomerPairs  for " + smi);
+		IAtomContainer mol = SmartsHelper.getMoleculeFromSmiles(smi);
+		TautomerManager tman = new TautomerManager();
+		TautomerUtils.generatePairForEachRuleInstance(tman, mol);
 	}
 
 }
