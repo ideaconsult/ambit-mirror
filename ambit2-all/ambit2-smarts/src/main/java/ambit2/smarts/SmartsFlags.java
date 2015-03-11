39c6dad00f17d0feaf61379b54b9064e844a136c
Nikolay Kochev <nick@uni-plovdiv.net>
2015-03-11 13:54:31 +0000

    made flag fields public.

--- ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/SmartsFlags.java
+++ ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/SmartsFlags.java
@@ -3,13 +3,13 @@ package ambit2.smarts;
 public class SmartsFlags 
 {
 	//These are flags for SMARTS mapping
-	boolean mNeedNeighbourData;
-	boolean mNeedValenceData;
-	boolean mNeedRingData;    //data with ring sizes for each atom
-	boolean mNeedRingData2;	  //data with ring 'internal formal numbers' for each atom
-	boolean mNeedExplicitHData;
-	boolean mNeedParentMoleculeData;
-	boolean hasRecursiveSmarts;
+	public boolean mNeedNeighbourData;
+	public boolean mNeedValenceData;
+	public boolean mNeedRingData;    //data with ring sizes for each atom
+	public boolean mNeedRingData2;	  //data with ring 'internal formal numbers' for each atom
+	public boolean mNeedExplicitHData;
+	public boolean mNeedParentMoleculeData;
+	public boolean hasRecursiveSmarts;
 	
 	
 	
