e30b490e186331dda4a7db8a8151b104f233a851
Nikolay Kochev <nick@uni-plovdiv.net>
2015-03-12 15:17:51 +0000

    Implemented basic parsing of JSON energy rules.

--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/rules/EnergyCorrection.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/rules/EnergyCorrection.java
@@ -1,10 +1,21 @@
 package ambit2.tautomers.rules;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class EnergyCorrection 
 {
 	public String correctionName = null;
 	public double energy = 0.0;
-	public HashMap<Integer, AtomCondition> atomConditiions = new HashMap<Integer, AtomCondition>();
+	public HashMap<Integer, AtomCondition> atomConditions = new HashMap<Integer, AtomCondition>();
+	
+	public List<String> errors = null;
+	
+	void addError(String err)
+	{
+		if (errors == null)
+			errors = new ArrayList<String>();
+		errors.add(err);
+	}
 }
