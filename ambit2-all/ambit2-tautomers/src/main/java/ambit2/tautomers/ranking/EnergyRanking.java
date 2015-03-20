c96ed8956cb8360db8cc59fa9c860475bf7ae37a
Nikolay Kochev <nick@uni-plovdiv.net>
2015-03-20 13:07:01 +0000

    Implemented loading of default ranking rules.

--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/ranking/EnergyRanking.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/ranking/EnergyRanking.java
@@ -1,26 +1,36 @@
 package ambit2.tautomers.ranking;
 
+import java.net.URL;
+import java.util.List;
+
+import ambit2.tautomers.rules.EnergyRule;
+import ambit2.tautomers.rules.JsonRuleParser;
+
 public class EnergyRanking 
 {
+	public List<EnergyRule> rules = null;
 	
-	public EnergyRanking()
+	
+	public EnergyRanking() throws Exception
 	{
 		loadDefaultEnergyRules();
 	}
 	
 	
-	public EnergyRanking(String jsonRulesFile)
+	public EnergyRanking(String jsonRulesFile) throws Exception
 	{
 		loadEnergyRules(jsonRulesFile);
 	}
 	
 	
-	public void loadDefaultEnergyRules()
+	public void loadDefaultEnergyRules() throws Exception
 	{
-		//TODO
+		JsonRuleParser jrp = new JsonRuleParser();
+		URL resource = jrp.getClass().getClassLoader().getResource("ambit2/tautomers/energy-rules.json");
+		rules =  JsonRuleParser.readRuleSetFromJSON(resource.getFile());
 	}
 	
-	public void loadEnergyRules(String jsonRulesFile)
+	public void loadEnergyRules(String jsonRulesFile) throws Exception
 	{
 		//TODO
 	}
