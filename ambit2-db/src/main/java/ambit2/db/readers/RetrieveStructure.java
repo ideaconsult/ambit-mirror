4c0b43f781076c2d640ce6daca9fe1e2bb777352
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2015-03-25 20:31:11 +0000

    typo fixed

--- ambit2-db/src/main/java/ambit2/db/readers/RetrieveStructure.java
+++ ambit2-db/src/main/java/ambit2/db/readers/RetrieveStructure.java
@@ -62,7 +62,7 @@ public class RetrieveStructure extends AbstractStructureRetrieval<IStructureReco
 	*/
 	protected String sql_prefered = 
     	"select structure.idstructure,idchemical,uncompress(structure) as ustructure,\n"+
-    	"format,type_structure,atomproperties,preference from structure,1 as selected\n"+
+    	"format,type_structure,atomproperties,preference,1 as selected from structure\n"+
     	"where structure.%s =?\n"+
     	"order by idchemical,preference,idstructure ";	
 	public RetrieveStructure() {
