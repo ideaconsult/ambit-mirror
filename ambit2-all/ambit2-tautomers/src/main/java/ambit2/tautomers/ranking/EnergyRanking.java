37acfbf6ae1e586fb027680cdba8b963f657072f
Nikolay Kochev <nick@uni-plovdiv.net>
2015-04-01 13:56:17 +0000

    Improved the energy ranking: now several energy rules are possible for a given tautomer rule.

--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/ranking/EnergyRanking.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/ranking/EnergyRanking.java
@@ -57,50 +57,53 @@ public class EnergyRanking
 			if (stateCheck != 0)
 				continue;
 			
-			EnergyRule eRule = getEnergyRuleByName(ri.rule.name);
-			if (eRule == null)
+			List<EnergyRule> eRules = getEnergyRuleByName(ri.rule.name);
+			if (eRules == null)
 				continue;
 			
 			//System.out.println("Rule: " + ri.rule.name);
 			
-			if (ri.curState == eRule.state)
+			for (EnergyRule eRule : eRules)  //It is possible to have several energy rules for a given tautomer rule
 			{	
-				boolean FlagBaseEnergy = true;
-				
-				//Checking the conditions for energy corrections
-				for (EnergyCorrection eCorrection : eRule.energyCorrections)
+				if (ri.curState == eRule.state)
 				{	
-					boolean FlagApplyCorrection = true;
-					for (Entry<Integer, AtomCondition> entry : eCorrection.atomConditions.entrySet())
-					{
-						int ruleAtomIndex = entry.getKey();
-						AtomCondition cond = entry.getValue();
-						
-						IAtom atom = ri.atoms.get(ruleAtomIndex);  //This atom corresponds to incStep.struct (not to the tautomer which is a clone of incStep.struct) 
-						int tautomerAtomIndex = incStep.struct.getAtomNumber(atom);  //The correct index is taken from incStep.struct
-						
-						if (!cond.checkConditionForAtom(tautomer, tautomerAtomIndex, isoTester))
-						{
-							FlagApplyCorrection = false;
-							break; //no more conditions are checked
-						}
-					}
-					
-					if (FlagApplyCorrection)
+					boolean FlagBaseEnergy = true;
+
+					//Checking the conditions for energy corrections
+					for (EnergyCorrection eCorrection : eRule.energyCorrections)
 					{	
-						e_rank += eCorrection.energy;
-						FlagBaseEnergy = false;  //correction energy is used instead of the base rule energy
-						break;   //no more corrections are checked
-						//System.out.println("correction: " + eCorrection.correctionName + "  " + eCorrection.energy);
-					}	
+						boolean FlagApplyCorrection = true;
+						for (Entry<Integer, AtomCondition> entry : eCorrection.atomConditions.entrySet())
+						{
+							int ruleAtomIndex = entry.getKey();
+							AtomCondition cond = entry.getValue();
+
+							IAtom atom = ri.atoms.get(ruleAtomIndex);  //This atom corresponds to incStep.struct (not to the tautomer which is a clone of incStep.struct) 
+							int tautomerAtomIndex = incStep.struct.getAtomNumber(atom);  //The correct index is taken from incStep.struct
+
+							if (!cond.checkConditionForAtom(tautomer, tautomerAtomIndex, isoTester))
+							{
+								FlagApplyCorrection = false;
+								break; //no more conditions are checked
+							}
+						}
+
+						if (FlagApplyCorrection)
+						{	
+							e_rank += eCorrection.energy;
+							FlagBaseEnergy = false;  //correction energy is used instead of the base rule energy
+							break;   //no more corrections are checked
+							//System.out.println("correction: " + eCorrection.correctionName + "  " + eCorrection.energy);
+						}	
+					}
+
+					if (FlagBaseEnergy)
+						e_rank += eRule.stateEnergy;
 				}
-				
-				if (FlagBaseEnergy)
-					e_rank += eRule.stateEnergy;
-			}
-			else
-			{
-				//This state is assigned energy 0.0
+				else
+				{
+					//This state is assigned energy 0.0
+				}
 			}
 		}
 		
@@ -130,62 +133,71 @@ public class EnergyRanking
 		{
 			RuleInstance ri = (RuleInstance) ruleInstances.get(i);	
 			
-			EnergyRule eRule = getEnergyRuleByName(ri.rule.name);
-			if (eRule == null)
+			List<EnergyRule> eRules = getEnergyRuleByName(ri.rule.name);
+			if (eRules == null)
 				continue;
 			
 			//System.out.println("Rule: " + ri.rule.name);
 			
-			if (ri.curState == eRule.state)
-			{	
-				boolean FlagBaseEnergy = true;
-				
-				//Checking the conditions for energy corrections
-				for (EnergyCorrection eCorrection : eRule.energyCorrections)
-				{	
-					boolean FlagApplyCorrection = true;
-					for (Entry<Integer, AtomCondition> entry : eCorrection.atomConditions.entrySet())
-					{
-						int ruleAtomIndex = entry.getKey();
-						AtomCondition cond = entry.getValue();
-						
-						IAtom atom = ri.atoms.get(ruleAtomIndex);   
-						int tautomerAtomIndex = tautomer.getAtomNumber(atom);  
-						
-						if (!cond.checkConditionForAtom(tautomer, tautomerAtomIndex, isoTester))
-						{
-							FlagApplyCorrection = false;
-							break; //no more conditions are checked
-						}
-					}
-					
-					if (FlagApplyCorrection)
-					{	
-						e_rank += eCorrection.energy;
-						FlagBaseEnergy = false;  //correction energy is used instead of the base rule energy
-						break;   //no more corrections are checked
-						//System.out.println("correction: " + eCorrection.correctionName + "  " + eCorrection.energy);
-					}	
-				}
-				
-				if (FlagBaseEnergy)
-					e_rank += eRule.stateEnergy;
-			}
-			else
+			for (EnergyRule eRule : eRules)  //It is possible to have several energy rules for a given tautomer rule
 			{
-				//This state is assigned energy 0.0
+				if (ri.curState == eRule.state)
+				{	
+					boolean FlagBaseEnergy = true;
+
+					//Checking the conditions for energy corrections
+					for (EnergyCorrection eCorrection : eRule.energyCorrections)
+					{	
+						boolean FlagApplyCorrection = true;
+						for (Entry<Integer, AtomCondition> entry : eCorrection.atomConditions.entrySet())
+						{
+							int ruleAtomIndex = entry.getKey();
+							AtomCondition cond = entry.getValue();
+
+							IAtom atom = ri.atoms.get(ruleAtomIndex);   
+							int tautomerAtomIndex = tautomer.getAtomNumber(atom);  
+
+							if (!cond.checkConditionForAtom(tautomer, tautomerAtomIndex, isoTester))
+							{
+								FlagApplyCorrection = false;
+								break; //no more conditions are checked
+							}
+						}
+
+						if (FlagApplyCorrection)
+						{	
+							e_rank += eCorrection.energy;
+							FlagBaseEnergy = false;  //correction energy is used instead of the base rule energy
+							break;   //no more corrections are checked
+							//System.out.println("correction: " + eCorrection.correctionName + "  " + eCorrection.energy);
+						}	
+					}
+
+					if (FlagBaseEnergy)
+						e_rank += eRule.stateEnergy;
+				}
+				else
+				{
+					//This state is assigned energy 0.0
+				}
 			}
 		}
 		
 		return e_rank;
 	}
 	
-	EnergyRule getEnergyRuleByName(String rname)
+	List<EnergyRule> getEnergyRuleByName(String rname)
 	{
+		List<EnergyRule> found_rules = new ArrayList<EnergyRule>();
 		for (EnergyRule r : rules)
 			if (r.ruleName.equals(rname))
-				return r;
-		return null;
+			{	
+				found_rules.add(r);
+			}
+		if (found_rules.isEmpty())
+			return null;
+		else 
+			return found_rules;
 	}
 	
 }
