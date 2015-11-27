f6000ce5a410452f57f0b6d4a922c3fc411a465e
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2015-11-27 11:13:31 +0000

    cleaning the trunk, this is already moved to https://github.com/vedina/opentox-aa-cli

--- opentox-opensso/src/main/java/org/opentox/aa/IOpenToxUser.java
+++
@@ -1,8 +0,0 @@
-package org.opentox.aa;
-
-public interface IOpenToxUser {
-	String getUsername();
-	void setUserName(String userName);
-	String getPassword();
-	void setPassword(String password);
-}
--- opentox-opensso/src/main/java/org/opentox/aa/policy/IPolicyHandler.java
+++
@@ -1,8 +0,0 @@
-package org.opentox.aa.policy;
-
-public interface IPolicyHandler {
-	int getProcessed();
-	void handlePolicy(String policyID) throws Exception ;
-	void handlePolicy(String policyID,String content) throws Exception ;
-	void handleOwner(String owner) throws Exception;
-}
