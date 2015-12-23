37db0e390acd40963432deedbe8c84edd502034d
Nikolay Kochev <nick@uni-plovdiv.net>
2015-12-23 08:01:11 +0000

    added class ISALocation

---
+++ ambit2-all/ambit2-apps/ambit2-export/src/main/java/ambit2/export/isa/base/ISALocation.java
@@ -0,0 +1,15 @@
+package ambit2.export.isa.base;
+
+public class ISALocation  /* implements IDataLocation */
+{
+	enum Layer {
+		
+	}
+	
+	
+	public static ISALocation parseString(String isaLocString) throws Exception
+	{
+		return null;
+	}
+	
+}
