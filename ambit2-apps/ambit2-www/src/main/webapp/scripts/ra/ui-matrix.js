868de9df373e29cef7dfe5e797cb3f9020387d81
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2015-12-27 08:07:03 +0000

    Fixes https://sourceforge.net/p/ambit/bugs/83/ This is client side issue, query option was lost during jToxkit upgrade https://github.com/ideaconsult/Toxtree.js/issues/174

--- ambit2-apps/ambit2-www/src/main/webapp/scripts/ra/ui-matrix.js
+++ ambit2-apps/ambit2-www/src/main/webapp/scripts/ra/ui-matrix.js
@@ -651,7 +651,7 @@ var jToxBundle = {
     if (panId == 'xinitial') {
       $('.jtox-toolkit', panel).show();
       self.edit.matrixEditable = false;
-      queryUri = self.bundleUri + '/dataset';
+      queryUri = self.bundleUri + '/dataset?mergeDatasets=true';
     }
     else {
       var queryPath = (panId == 'xfinal') ? '/matrix/final' : '/matrix/working';
@@ -730,7 +730,7 @@ var jToxBundle = {
           }
         });
         $(checkAll).on('change', function (e) {
-          var qUri = "/query/study?bundle_uri=" + bUri;
+          var qUri = "/query/study?mergeDatasets=true&bundle_uri=" + bUri;
           if (!this.checked)
             qUri += "&selected=substances&filterbybundle=" + bUri;
           self.endpointKit.loadEndpoints(qUri);
