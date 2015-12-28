fede96a3188300195e5011d0545b99a72694fcdf
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2015-12-28 14:53:01 +0000

    qmap updates (initial refactoring allowing to work with substances)

--- ambit2-all/ambit2-apps/ambit2-www/src/main/java/ambit2/rest/similarity/space/QMapSpaceResource.java
+++ ambit2-all/ambit2-apps/ambit2-www/src/main/java/ambit2/rest/similarity/space/QMapSpaceResource.java
@@ -25,31 +25,38 @@ import ambit2.rest.OpenTox;
 import ambit2.rest.StringConvertor;
 import ambit2.rest.query.QueryResource;
 
-public class QMapSpaceResource  extends QueryResource<IQueryRetrieval<QMapSpace>, QMapSpace> {
+public class QMapSpaceResource extends
+		QueryResource<IQueryRetrieval<QMapSpace>, QMapSpace> {
 	public final static String resource = "/toxmatch";
-	
+
 	public QMapSpaceResource() {
 		super();
 		setHtmlbyTemplate(true);
 	}
+
 	@Override
 	public String getTemplateName() {
 		return "toxmatch.ftl";
 	}
+
 	@Override
 	public IProcessor<IQueryRetrieval<QMapSpace>, Representation> createConvertor(
 			Variant variant) throws AmbitException, ResourceException {
 		String filenamePrefix = getRequest().getResourceRef().getPath();
 		if (variant.getMediaType().equals(MediaType.TEXT_URI_LIST)) {
-				QMapURIReporter r = new QMapURIReporter(getRequest());
-				return new StringConvertor(r,MediaType.TEXT_URI_LIST,filenamePrefix);
+			QMapURIReporter r = new QMapURIReporter(getRequest());
+			return new StringConvertor(r, MediaType.TEXT_URI_LIST,
+					filenamePrefix);
 		} else if (variant.getMediaType().equals(MediaType.APPLICATION_JSON)) {
-					return new OutputWriterConvertor(new QMapSpaceJSONReporter(getRequest()),MediaType.APPLICATION_JSON);
-		} else if (variant.getMediaType().equals(MediaType.APPLICATION_JAVASCRIPT)) {
-			return new OutputWriterConvertor(new QMapSpaceJSONReporter(getRequest()),MediaType.APPLICATION_JSON);
-		}			
-		throw new ResourceException(Status.CLIENT_ERROR_UNSUPPORTED_MEDIA_TYPE);		
-	}	
+			return new OutputWriterConvertor(new QMapSpaceJSONReporter(
+					getRequest()), MediaType.APPLICATION_JSON);
+		} else if (variant.getMediaType().equals(
+				MediaType.APPLICATION_JAVASCRIPT)) {
+			return new OutputWriterConvertor(new QMapSpaceJSONReporter(
+					getRequest()), MediaType.APPLICATION_JSON);
+		}
+		throw new ResourceException(Status.CLIENT_ERROR_UNSUPPORTED_MEDIA_TYPE);
+	}
 
 	@Override
 	protected IQueryRetrieval<QMapSpace> createQuery(Context context,
@@ -57,39 +64,51 @@ public class QMapSpaceResource  extends QueryResource<IQueryRetrieval<QMapSpace>
 		QueryQMapSpace query = new QueryQMapSpace();
 		Form form = request.getResourceRef().getQueryAsForm();
 		try {
-			query.setThreshold_a(Integer.parseInt(form.getFirstValue("thresholda")));
-		} catch (Exception x) {query.setThreshold_a(0);}
+			query.setThreshold_a(Integer.parseInt(form
+					.getFirstValue("thresholda")));
+		} catch (Exception x) {
+			query.setThreshold_a(0);
+		}
 		Object qmapURI = OpenTox.params.qmap_uri.getFirstValue(form);
-		if (qmapURI!=null) try {
-			int id = (Integer)OpenTox.URI.qmap.getId(Reference.decode(qmapURI.toString().trim()),request.getRootRef());
-			query.setValue(new QMap(id));
-			return query;
-		} catch (Exception x) { /* ignore the filter	*/}
+		if (qmapURI != null)
+			try {
+				int id = (Integer) OpenTox.URI.qmap.getId(
+						Reference.decode(qmapURI.toString().trim()),
+						request.getRootRef());
+				query.setValue(new QMap(id));
+				return query;
+			} catch (Exception x) { /* ignore the filter */
+			}
 
-		
-		//filter by dataset
+		// filter by dataset
 		Object datasetURI = OpenTox.params.dataset_uri.getFirstValue(form);
-		if (datasetURI!=null) try {
-			int id = (Integer)OpenTox.URI.dataset.getId(Reference.decode(datasetURI.toString().trim()),request.getRootRef());
-			QMap map = new QMap();
-			map.setDataset(new SourceDataset());
-			map.getDataset().setID(id);
-			query.setValue(map);
-		} catch (Exception x) { /* ignore the filter	*/}
+		if (datasetURI != null)
+			try {
+				int id = (Integer) OpenTox.URI.dataset.getId(
+						Reference.decode(datasetURI.toString().trim()),
+						request.getRootRef());
+				QMap map = new QMap();
+				map.setDataset(new SourceDataset());
+				map.getDataset().setID(id);
+				query.setValue(map);
+			} catch (Exception x) { /* ignore the filter */
+			}
 
 		Object propertyURI = OpenTox.params.feature_uris.getFirstValue(form);
-		if (propertyURI!=null) try {
-			int id = (Integer)OpenTox.URI.feature.getId(Reference.decode(propertyURI.toString().trim()),request.getRootRef());
-			if (query.getValue()==null) query.setValue(new QMap());
-			Property p = new Property(null);
-			p.setId(id);
-			query.getValue().setProperty(p);
-		} catch (Exception x) { /* ignore the filter	*/}
+		if (propertyURI != null)
+			try {
+				int id = (Integer) OpenTox.URI.feature.getId(
+						Reference.decode(propertyURI.toString().trim()),
+						request.getRootRef());
+				if (query.getValue() == null)
+					query.setValue(new QMap());
+				Property p = new Property(null);
+				p.setId(id);
+				query.getValue().setProperty(p);
+			} catch (Exception x) { /* ignore the filter */
+			}
 
-		
 		return query;
 	}
-
-
 
 }
