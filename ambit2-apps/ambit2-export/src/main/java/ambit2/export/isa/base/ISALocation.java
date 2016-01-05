0ab65e67fec0081e7065080dca574d77802dc231
Nikolay Kochev <nick@uni-plovdiv.net>
2016-01-05 13:04:09 +0000

    Implemented enum Layer and added functions parseString() and toString().

--- ambit2-apps/ambit2-export/src/main/java/ambit2/export/isa/base/ISALocation.java
+++ ambit2-apps/ambit2-export/src/main/java/ambit2/export/isa/base/ISALocation.java
@@ -1,15 +1,59 @@
 package ambit2.export.isa.base;
 
+
 public class ISALocation  /* implements IDataLocation */
 {
-	enum Layer {
+	public static final String splitter = ".";
+	
+	public static enum Layer 
+	{
+		INVESTIGATION, STUDY, ASSAY, MATERIAL, DATA_FILE, 
+		UNDEFINED;
 		
+		public static Layer fromString(String s)
+		{	 
+			try
+			{
+				Layer layer = Layer.valueOf(s) ;
+				return (layer);
+			}
+			catch (Exception e)
+			{
+				return UNDEFINED;
+			}
+		}
 	}
+	
+	public Layer layer = null;
+	public int sublayerNum = 0;
+	public String sublayerID = null;
 	
 	
 	public static ISALocation parseString(String isaLocString) throws Exception
 	{
-		return null;
+		if (isaLocString == null)
+			return null;
+		
+		ISALocation isaLoc = new ISALocation();		
+		String tokens[] = isaLocString.split(splitter);
+		
+		if (tokens.length == 0)
+			throw new Exception("ISA layer is not specified!");
+		
+		isaLoc.layer = Layer.fromString(tokens[0]);
+		if (isaLoc.layer == Layer.UNDEFINED)
+			throw new Exception("ISA layer is not correct!");
+		
+		
+		return isaLoc;
+	}
+	  
+	
+	public String toString()
+	{
+		StringBuffer sb = new StringBuffer();
+		//TODO
+		return sb.toString();
 	}
 	
 }
