81b39e6a441270a6aedb05eba0bacf6ffde40e62
Nikolay Kochev <nick@uni-plovdiv.net>
2016-01-05 14:49:33 +0000

    Added basic fields and some functionality in toString();

--- ambit2-apps/ambit2-export/src/main/java/ambit2/export/isa/base/ISALocation.java
+++ ambit2-apps/ambit2-export/src/main/java/ambit2/export/isa/base/ISALocation.java
@@ -1,5 +1,12 @@
 package ambit2.export.isa.base;
 
+/**
+ * 
+ * @author nick
+ * ISA Location is specified as string notation using following syntax
+ * layer.[layer position].[process number/id].[element name/number].[sub-element name/number]
+ */
+
 
 public class ISALocation  /* implements IDataLocation */
 {
@@ -24,9 +31,22 @@ public class ISALocation  /* implements IDataLocation */
 		}
 	}
 	
+	//The indices/numbers/ are 1-based
+	//value -1 is used for undefined/unused
+	//value 0 is used to specified for default index/number/
+	
 	public Layer layer = null;
-	public int sublayerNum = 0;
-	public String sublayerID = null;
+	public int layerPosIndex = -1;    
+	public String layerPosID = null;
+	
+	public int processNum = -1;
+	public String processID = null;
+	
+	public String elementName = null;
+	public int elementNum = -1;
+	
+	public String subElementName = null;
+	public int subElementNum = -1;
 	
 	
 	public static ISALocation parseString(String isaLocString) throws Exception
@@ -45,14 +65,47 @@ public class ISALocation  /* implements IDataLocation */
 			throw new Exception("ISA layer is not correct!");
 		
 		
+		
+		
 		return isaLoc;
 	}
-	  
+	
+	protected static void parseLayerPos(ISALocation isaLoc, String token) throws Exception
+	{
+		//TODO
+	}
+	 
+	protected static void parseProcess(ISALocation isaLoc, String token) throws Exception
+	{
+		//TODO
+	}
+	
+	protected static void parseElement(ISALocation isaLoc, String token) throws Exception
+	{
+		//TODO
+	}
+	
+	
 	
 	public String toString()
 	{
 		StringBuffer sb = new StringBuffer();
-		//TODO
+		if (layer == null)
+			return ("null");
+		
+		sb.append(layer.toString());
+		
+		if (layer == Layer.STUDY || layer == Layer.ASSAY)
+		{
+			if (processNum != -1)
+				sb.append("process[" + (processNum+1) + "]");
+		}
+		
+		if (elementName != null)
+		{
+			
+		}
+		
 		return sb.toString();
 	}
 	
