8b212369c2015ca065113cf61ec460e0f7c9c89c
Nikolay Kochev <nick@uni-plovdiv.net>
2016-01-07 16:00:00 +0000

    Implemented function putString. Added new test function testISAJsonMapper1_0().

--- ambit2-all/ambit2-apps/ambit2-export/src/main/java/ambit2/export/isa/v1_0/ISAJsonMapper1_0.java
+++ ambit2-all/ambit2-apps/ambit2-export/src/main/java/ambit2/export/isa/v1_0/ISAJsonMapper1_0.java
@@ -1,12 +1,18 @@
 package ambit2.export.isa.v1_0;
 
+import java.lang.reflect.Field;
+import java.util.logging.Logger;
+
 import ambit2.export.isa.base.IISADataMapper;
 import ambit2.export.isa.base.ISALocation;
+import ambit2.export.isa.base.ISALocation.Layer;
+import ambit2.export.isa.json.ISAJsonExporter;
 import ambit2.export.isa.v1_0.objects.Investigation;
 
 public class ISAJsonMapper1_0 implements IISADataMapper
 {
-
+	protected final static Logger logger = Logger.getLogger("ISAJsonMapper1_0");
+	
 	protected Investigation investigation = null;
 	
 	@Override
@@ -63,7 +69,54 @@ public class ISAJsonMapper1_0 implements IISADataMapper
 	@Override
 	public void putString(String s, ISALocation location) throws Exception 
 	{
-		// TODO Auto-generated method stub
-		
+		switch (location.layer)
+		{
+		case INVESTIGATION:
+			putDataInObject(s, location, investigation);
+			break;
+			
+		case STUDY:
+			break;
+			
+		case ASSAY:
+			break;
+		}
 	}
+	
+	/** 
+	 * Data is put into the Object using element and subElement information
+	 **/	
+	protected void putDataInObject(String data, ISALocation location, Object obj) throws Exception
+	{
+		Class cls = obj.getClass();
+		Field field = null;
+		try{
+			field = cls.getDeclaredField(location.elementName);
+			
+		}
+		catch (Exception e)
+		{
+			logger.info("Location element not found: " + location.toString());
+			return;
+		}
+		
+		String fType = field.getType().getName();	
+		if (fType.equals("java.lang.String"))
+		{	
+			field.set(obj, data);
+		}
+		else
+		{
+			//TODO
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
