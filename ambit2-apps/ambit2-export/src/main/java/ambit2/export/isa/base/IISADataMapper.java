917475fcb8fea7a307f0c4281d0a44bf92cd34c6
Nikolay Kochev <nick@uni-plovdiv.net>
2016-01-07 12:18:56 +0000

    Added new interface IISADataMapper and class ISAJsonMapper1_0

---
+++ ambit2-apps/ambit2-export/src/main/java/ambit2/export/isa/base/IISADataMapper.java
@@ -0,0 +1,9 @@
+package ambit2.export.isa.base;
+
+public interface IISADataMapper 
+{	
+	public void setTargetDataObject(Object target) throws Exception;
+	public void putInteger(Integer k, ISALocation location) throws Exception;
+	public void putDouble(Double d, ISALocation location) throws Exception;
+	public void putString(String s, ISALocation location) throws Exception;
+}
---
+++ ambit2-apps/ambit2-export/src/main/java/ambit2/export/isa/v1_0/ISAJsonMapper1_0.java
@@ -0,0 +1,37 @@
+package ambit2.export.isa.v1_0;
+
+import ambit2.export.isa.base.IISADataMapper;
+import ambit2.export.isa.base.ISALocation;
+
+public class ISAJsonMapper1_0 implements IISADataMapper
+{
+
+	@Override
+	public void setTargetDataObject(Object target) throws Exception 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void putInteger(Integer k, ISALocation location) throws Exception 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void putDouble(Double d, ISALocation location) throws Exception 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void putString(String s, ISALocation location) throws Exception 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
