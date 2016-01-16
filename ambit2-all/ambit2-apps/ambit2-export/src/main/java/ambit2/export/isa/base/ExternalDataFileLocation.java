0eafb2ebaa2d096327d8890e27fcd96ad51d6504
Nikolay Kochev <nick@uni-plovdiv.net>
2016-01-16 16:29:04 +0000

    Added new class ExternalDataFileLocation. Added new function ExternalDataFileManager.storeData()

---
+++ ambit2-all/ambit2-apps/ambit2-export/src/main/java/ambit2/export/isa/base/ExternalDataFileLocation.java
@@ -0,0 +1,13 @@
+package ambit2.export.isa.base;
+
+public class ExternalDataFileLocation 
+{
+	public String fileName = null;
+	public int index = 0;
+	
+	public String getLocationAsIdentifier()
+	{
+		//TODO
+		return null;
+	}
+}
