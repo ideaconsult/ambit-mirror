826ef7c0851109ca8d24e37c76904efe5317a46e
Nikolay Kochev <nick@uni-plovdiv.net>
2016-01-26 14:42:09 +0000

    Implemented functions: getLocationAsIdentifier() and saveBufferAsFile()

--- ambit2-all/ambit2-apps/ambit2-export/src/main/java/ambit2/export/isa/base/ExternalDataFileLocation.java
+++ ambit2-all/ambit2-apps/ambit2-export/src/main/java/ambit2/export/isa/base/ExternalDataFileLocation.java
@@ -2,12 +2,15 @@ package ambit2.export.isa.base;
 
 public class ExternalDataFileLocation 
 {
+	public static final String splitter = ","; 
+	
 	public String fileName = null;
-	public int index = 0;
+	public int recordIndex = -1;  
+	public int elementIndex = 0;
+	
 	
 	public String getLocationAsIdentifier()
 	{
-		//TODO
-		return null;
+		return "" + fileName + splitter + recordIndex + splitter + elementIndex;
 	}
 }
