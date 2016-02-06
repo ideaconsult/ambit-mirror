578b2e9fcfbf81a7a06d1c97b7f475df1881703f
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2016-02-06 07:09:34 +0000

    package-info.java is the recommended way for package level javadoc since Java 5

---
+++ ambit2-all/ambit2-apps/ambit2-www/src/main/java/ambit2/rest/substance/property/package-info.java
@@ -0,0 +1,5 @@
+  /**
+Substance property
+ **/
+//@Annotation1(...)
+package ambit2.rest.substance.property;
---
+++ ambit2-all/ambit2-base/src/main/java/ambit2/base/data/substance/package-info.java
@@ -0,0 +1,5 @@
+/**
+* Chemical substances, with complex composition (including nanomaterials)
+*/
+//@Annotation1(...)
+package ambit2.base.data.substance;
