51cc2374ea77e922453ec38bfbbb67ee2191dc2c
Nikolay Kochev <nick@uni-plovdiv.net>
2016-02-15 14:40:21 +0000

    Added function ISAJsonUtils1_0.getComment(). Improvements in the publication handling.

--- ambit2-all/ambit2-apps/ambit2-export/src/main/java/ambit2/export/isa/v1_0/ISAJsonUtils1_0.java
+++ ambit2-all/ambit2-apps/ambit2-export/src/main/java/ambit2/export/isa/v1_0/ISAJsonUtils1_0.java
@@ -1,6 +1,7 @@
 package ambit2.export.isa.v1_0;
 
 import ambit2.export.isa.base.ISAConst;
+import ambit2.export.isa.v1_0.objects.Comment;
 import ambit2.export.isa.v1_0.objects.Investigation;
 import ambit2.export.isa.v1_0.objects.Study;
 
@@ -16,5 +17,13 @@ public class ISAJsonUtils1_0
 			study.description = study.description + ISAConst.addSeparator + content; 
 	}
 	
+	
+	public static Comment getComment(String name, String value)
+	{
+		Comment c = new Comment();
+		c.name = name;
+		c.value = value;
+		return c;
+	};
 	
 }
