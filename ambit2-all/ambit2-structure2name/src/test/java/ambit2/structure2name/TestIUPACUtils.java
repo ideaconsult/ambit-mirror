c2a1eac1894dba3da34f613fe60246ede6d63d62
Nikolay Kochev <nick@uni-plovdiv.net>
2016-03-12 11:40:35 +0000

    Added TestIUPACUtils.

---
+++ ambit2-all/ambit2-structure2name/src/test/java/ambit2/structure2name/TestIUPACUtils.java
@@ -0,0 +1,15 @@
+package ambit2.structure2name;
+
+
+
+public class TestIUPACUtils 
+{
+	public static void main(String[] args) throws Exception {
+		test0();
+	}
+	
+	public static void test0()
+	{
+		//TODO
+	}
+}
