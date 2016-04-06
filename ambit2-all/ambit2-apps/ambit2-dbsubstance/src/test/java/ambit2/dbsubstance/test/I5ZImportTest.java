166083e717b663c67f2e47023ab18e9e3007146f
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2016-04-06 10:48:31 +0000

    Fixed bug resulting in empty structures when build2d of the normalizer is turned on and it is turned on when importing structures from i5z files (this was not an issue with cdk 1.4.x) related to https://sourceforge.net/p/ambit/bugs/95/ test updated

--- ambit2-all/ambit2-apps/ambit2-dbsubstance/src/test/java/ambit2/dbsubstance/test/I5ZImportTest.java
+++ ambit2-all/ambit2-apps/ambit2-dbsubstance/src/test/java/ambit2/dbsubstance/test/I5ZImportTest.java
@@ -14,55 +14,76 @@ import ambit2.db.processors.test.DbUnitTest;
 import ambit2.dbsubstance.DBSubstanceImport;
 
 /**
-<pre>
-  -c,--config <file>                    Config file (DB connection
-                                       parameters)
- -h,--help                             This help
- -i,--input <file>                     Input file or folder
- -j,--json <file>                      JSON config file
- -m,--clearMeasurements <value>        true|false
- -o,--output <file>                    Output file
- -p,--parser <type>                    File parser mode :
-                                       i5z|xlsx|xls|nanowiki
- -r,--maxReferenceSubstances <value>   Maximum reference substances in
-                                       *.i5z archive
- -s,--isSplitRecord <value>            true|false
- -t,--clearComposotion <value>         true|false
- -x,--structureMatch <value>           Match structure by
-                                       uuid|cas|einecs|smiles|inchi
-</pre>
- *
+ * <pre>
+ *   -c,--config <file>                    Config file (DB connection
+ *                                        parameters)
+ *  -h,--help                             This help
+ *  -i,--input <file>                     Input file or folder
+ *  -j,--json <file>                      JSON config file
+ *  -m,--clearMeasurements <value>        true|false
+ *  -o,--output <file>                    Output file
+ *  -p,--parser <type>                    File parser mode :
+ *                                        i5z|xlsx|xls|nanowiki
+ *  -r,--maxReferenceSubstances <value>   Maximum reference substances in
+ * .i5z archive
+ *  -s,--isSplitRecord <value>            true|false
+ *  -t,--clearComposotion <value>         true|false
+ *  -x,--structureMatch <value>           Match structure by
+ *                                        uuid|cas|einecs|smiles|inchi
+ * </pre>
+ * 
  */
 public class I5ZImportTest extends DbUnitTest {
 	@Test
 	public void test() throws Exception {
-		
+
 		setUpDatabaseFromResource("ambit2/db/processors/test/empty-datasets.xml");
 		String resource_i5 = "net/idea/i5/_5/substance/i5z/IUC4-efdb21bb-e79f-3286-a988-b6f6944d3734.i5z";
 		InputStream in = net.idea.i5._5.ambit2.I5AmbitProcessor.class
 				.getClassLoader().getResourceAsStream(resource_i5);
 		Assert.assertNotNull(in);
 		File file = fromResourcestream(in, ".i5z");
+		file.deleteOnExit();
 		System.out.println(file);
 
 		String resource_config = "ambit2/db/conf/test.properties";
 
-		in = DbUnitTest.class.getClassLoader().getResourceAsStream(resource_config);
+		in = DbUnitTest.class.getClassLoader().getResourceAsStream(
+				resource_config);
 		File fileconfig = fromResourcestream(in, ".properties");
+		fileconfig.deleteOnExit();
 		System.out.println(fileconfig);
 
-		String[] args = new String[] { "-i", file.getAbsolutePath(), "-c", fileconfig.getAbsolutePath(), "-m", "true",
-				"-t", "true" ,"-r","-1"};
-		DBSubstanceImport.main(args);
+		String[] args = new String[] { "-i", file.getAbsolutePath(), "-c",
+				fileconfig.getAbsolutePath(), "-m", "true", "-t", "true", "-r",
+				"-1", "x", "einecs" };
 		
+		DBSubstanceImport.main(args);
+
 		IDatabaseConnection c = getConnection();
-		ITable table  = c
-				.createQueryTable("EXPECTED_SUBSTANCES", "SELECT * FROM substance");
-		Assert.assertEquals(1, table.getRowCount());
-		ITable values = c.createQueryTable("EXPECTED_STRUCTURES",
-				"SELECT * FROM structure");
-		Assert.assertEquals(6, values.getRowCount());
-		c.close();
+		try {
+			ITable table = c.createQueryTable("EXPECTED_SUBSTANCES",
+					"SELECT * FROM substance");
+			Assert.assertEquals(1, table.getRowCount());
+			ITable values = c.createQueryTable("EXPECTED_STRUCTURES",
+					"SELECT * FROM structure ");
+			Assert.assertEquals(6, values.getRowCount());
+			//structures should not be empty
+			values = c.createQueryTable("EXPECTED_STRUCTURES",
+					"SELECT count(*) as c FROM structure where format='SDF' and type_structure!='NA' and uncompress(structure) regexp 'M  END' ");
+			Assert.assertEquals(4,values.getValue(0, "c"));
+
+			values = c.createQueryTable("EXPECTED_CHEMICALS",
+					"SELECT * FROM chemicals");
+			Assert.assertEquals(6, values.getRowCount());
+			values = c
+					.createQueryTable(
+							"EXPECTED_ref_subst_uuids",
+							"SELECT * FROM properties join catalog_references using(idreference) where name='I5UUID'");
+			Assert.assertEquals(7, values.getRowCount());
+		} finally {
+			c.close();
+		}
 
 	}
 
