edabd499d54f23fa0e3db89eed89ebc2924963fa
Nikolay Kochev <nick@uni-plovdiv.net>
2016-04-11 12:38:33 +0000

    refactored "center" to "terminal" atom.

--- ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/ExtendedChiralityInfo.java
+++ ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/ExtendedChiralityInfo.java
@@ -4,8 +4,9 @@ import org.openscience.cdk.interfaces.IAtom;
 
 public class ExtendedChiralityInfo 
 {
-	//Describes cases like this C1=C0=C2
-	public IAtom center1 = null;
-	public IAtom center2 = null;
+	//Describes cases like this terminal1 = focus/center = termnal2
+	
+	public IAtom terminal1 = null;
+	public IAtom terminal2 = null;
 			
 }
