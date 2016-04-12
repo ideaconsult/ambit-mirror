7b720157fa85b1e6dbf7f35a92f34a8cf5716ee9
Nikolay Kochev <nick@uni-plovdiv.net>
2016-04-12 13:01:58 +0000

    Implemented initial handling of ring data.

--- ambit2-structure2name/src/main/java/ambit2/structure2name/components/MoleculeAnalysis.java
+++ ambit2-structure2name/src/main/java/ambit2/structure2name/components/MoleculeAnalysis.java
@@ -1,15 +1,19 @@
 package ambit2.structure2name.components;
 
+import java.util.List;
+
 import org.openscience.cdk.graph.Cycles;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IRingSet;
 
+import ambit2.smarts.SmartsParser;
+
 public class MoleculeAnalysis 
 {
 	private IAtomContainer molecule = null;
 	private IRingSet ringSet = null;
-	
+	private List<int[]> atomRingIndices = null;
 	
 	public IAtomContainer getMolecule() {
 		return molecule;
@@ -31,4 +35,27 @@ public class MoleculeAnalysis
 	public void setRingSet(IRingSet ringSet) {
 		this.ringSet = ringSet;
 	}
+	
+	
+	public List<int[]> getAtomRingIndices() {
+		return atomRingIndices;
+	}
+
+	public void setAtomRingIndices(List<int[]> atomRingIndices) {
+		this.atomRingIndices = atomRingIndices;
+	}
+	
+	private void handleRingData()
+	{
+		setAtomRingIndices(SmartsParser.getRindData2(molecule, ringSet));
+	}
+	
+	public void analyse()
+	{
+		if (ringSet == null)
+			calcRingSet();
+		
+		handleRingData();
+	}
+	
 }
