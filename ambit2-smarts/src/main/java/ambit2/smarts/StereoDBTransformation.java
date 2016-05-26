e659cab1891a3fca5eb5d59a6dde61f9a965204f
Nikolay Kochev <nick@uni-plovdiv.net>
2016-05-26 12:49:06 +0000

    Added new class StereoDBTransformation

---
+++ ambit2-smarts/src/main/java/ambit2/smarts/StereoDBTransformation.java
@@ -0,0 +1,16 @@
+package ambit2.smarts;
+
+public class StereoDBTransformation 
+{
+	//Reactant 
+	public int reactDBAt1 = -1;
+	public int reactDBAt2 = -1;
+	public int reactLigand1 = -1;
+	public int reactLigand2 = -1;
+	
+	//Product
+	public int prodDBAt1 = -1;
+	public int prodDBAt2 = -1;
+	public int prodLigand1 = -1;
+	public int prodLigand2 = -1;
+}
