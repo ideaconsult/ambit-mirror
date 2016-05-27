50d7a5c221b29a438b521129950a91c67c0c65bc
Nikolay Kochev <nick@uni-plovdiv.net>
2016-05-27 22:29:21 +0000

    Added class StereoFromSmartsAtomExpression

---
+++ ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/StereoFromSmartsAtomExpression.java
@@ -0,0 +1,10 @@
+package ambit2.smarts;
+
+public class StereoFromSmartsAtomExpression 
+{
+	public static int getStereo(SmartsAtomExpression sa)
+	{
+		//TODO
+		return SmartsConst.ChC_Unspec;
+	}
+}
