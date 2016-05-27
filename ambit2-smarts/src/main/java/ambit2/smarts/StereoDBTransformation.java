b5f4a6a082bdffa32c44e74357f137888ff2497e
Nikolay Kochev <nick@uni-plovdiv.net>
2016-05-27 13:35:27 +0000

    Added fields for DB stereo

--- ambit2-smarts/src/main/java/ambit2/smarts/StereoDBTransformation.java
+++ ambit2-smarts/src/main/java/ambit2/smarts/StereoDBTransformation.java
@@ -1,5 +1,8 @@
 package ambit2.smarts;
 
+
+import ambit2.smarts.DoubleBondStereoInfo.DBStereo;
+
 public class StereoDBTransformation 
 {
 	//Reactant 
@@ -7,10 +10,12 @@ public class StereoDBTransformation
 	public int reactDBAt2 = -1;
 	public int reactLigand1 = -1;
 	public int reactLigand2 = -1;
+	public DBStereo reactDBStereo = DBStereo.UNDEFINED;
 	
 	//Product
 	public int prodDBAt1 = -1;
 	public int prodDBAt2 = -1;
 	public int prodLigand1 = -1;
 	public int prodLigand2 = -1;
+	public DBStereo prodDBStereo = DBStereo.UNDEFINED;
 }
