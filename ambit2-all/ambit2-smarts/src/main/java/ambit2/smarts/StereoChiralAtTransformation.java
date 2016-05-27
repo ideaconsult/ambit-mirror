f08045b5cccacebd7bd5461950e839525fba0dcc
Nikolay Kochev <nick@uni-plovdiv.net>
2016-05-27 13:46:42 +0000

    Added new class StereoChiralAtTransformation

---
+++ ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/StereoChiralAtTransformation.java
@@ -0,0 +1,15 @@
+package ambit2.smarts;
+
+public class StereoChiralAtTransformation 
+{
+	//Reactant
+	public int reactChiralAtom = -1;
+	public int reactLigands[] = null;
+	public int reactChirality = SmartsConst.ChC_Unspec;
+	
+	//Product
+	public int prodChiralAtom = -1;
+	public int prodLigands[] = null;
+	public int prodChirality = SmartsConst.ChC_Unspec;
+	
+}
