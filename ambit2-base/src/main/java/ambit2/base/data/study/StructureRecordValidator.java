4c3abf7860ceb8a8f6904fef7d7d5a10a30bdf8a
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2016-05-29 18:32:25 +0000

    improvements in nanomaterials support

--- ambit2-base/src/main/java/ambit2/base/data/study/StructureRecordValidator.java
+++ ambit2-base/src/main/java/ambit2/base/data/study/StructureRecordValidator.java
@@ -9,184 +9,227 @@ import ambit2.base.data.SubstanceRecord;
 import ambit2.base.interfaces.IStructureRecord;
 import ambit2.base.relation.composition.CompositionRelation;
 
-public class StructureRecordValidator extends DefaultAmbitProcessor<IStructureRecord, IStructureRecord> {
+public class StructureRecordValidator extends
+		DefaultAmbitProcessor<IStructureRecord, IStructureRecord> {
 
-    /**
+	/**
      * 
      */
-    private static final long serialVersionUID = 201323883830157691L;
-    protected boolean fixErrors = false;
-    protected String prefix = "XLSX";
-    protected String filename;
+	private static final long serialVersionUID = 201323883830157691L;
+	protected boolean fixErrors = false;
+	protected String prefix = "XLSX";
+	protected String filename;
 
-    public String getFilename() {
-        return filename;
-    }
+	public String getFilename() {
+		return filename;
+	}
 
-    public void setFilename(String filename) {
-        this.filename = filename;
-    }
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
 
-    public String getPrefix() {
-	return prefix;
-    }
+	public String getPrefix() {
+		return prefix;
+	}
 
-    public void setPrefix(String prefix) {
-	this.prefix = prefix;
-    }
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
 
-    public StructureRecordValidator() {
-	this(UUID.randomUUID().toString(),false);
-    }
+	public StructureRecordValidator() {
+		this(UUID.randomUUID().toString(), false);
+	}
 
-    public StructureRecordValidator(String filename, boolean fixErrors) {
-	super();
-	setFilename(filename);
-	setFixErrors(fixErrors);
-    }
+	public StructureRecordValidator(String filename, boolean fixErrors) {
+		super();
+		setFilename(filename);
+		setFixErrors(fixErrors);
+	}
 
-    public boolean isFixErrors() {
-	return fixErrors;
-    }
+	public boolean isFixErrors() {
+		return fixErrors;
+	}
 
-    public void setFixErrors(boolean fixErrors) {
-	this.fixErrors = fixErrors;
-    }
+	public void setFixErrors(boolean fixErrors) {
+		this.fixErrors = fixErrors;
+	}
 
-    @Override
-    public IStructureRecord process(IStructureRecord record) throws Exception {
-	if (record instanceof SubstanceRecord)
-	    return validate((SubstanceRecord) record);
-	return record;
-    }
+	@Override
+	public IStructureRecord process(IStructureRecord record) throws Exception {
+		if (record instanceof SubstanceRecord)
+			return validate((SubstanceRecord) record);
+		return record;
+	}
 
-    public IStructureRecord validate(SubstanceRecord record) throws Exception {
-	if (record.getOwnerName()==null) {
-	    logger.log(Level.WARNING, "Missing substance owner name");
-	    if (fixErrors) {
-		record.setOwnerName(filename);
-		logger.log(Level.WARNING, "Assigned file name to substance owner name");
-	    }
-	}	
-	if (record.getSubstanceUUID()==null) {
-	    logger.log(Level.WARNING, "Missing substance UUID");
-	    if (fixErrors) {
-		record.setSubstanceUUID(generateUUIDfromString(prefix, record.getOwnerName() + record.getSubstanceName()));
-		logger.log(Level.WARNING, "Generated substance UUID from substance name");
-	    }
-	}   		
-	if (record.getMeasurements() == null) {
-	    logger.log(Level.WARNING, "No measurements");
-	} else
-	    for (ProtocolApplication papp : record.getMeasurements()) {
-		validate(record, papp);
-	    }
-	if (record.getRelatedStructures() == null || record.getRelatedStructures().isEmpty()) {
-	    logger.log(Level.WARNING, "No composition");
-	} else {
-	    IStructureRecord referenceStructure = null;
-	    double proportion = 0;
-	    for (CompositionRelation rel : record.getRelatedStructures()) {
-		validate(record, rel);
-		switch (rel.getRelationType()) {
-		case HAS_CONSTITUENT : {
-		    if (referenceStructure==null || (proportion < rel.getRelation().getTypical_value())) {
-			referenceStructure = rel.getSecondStructure();
-			proportion =  rel.getRelation().getTypical_value()==null?0:rel.getRelation().getTypical_value();
-		    }
-		    break;
+	public IStructureRecord validate(SubstanceRecord record) throws Exception {
+		if (record.getOwnerName() == null) {
+			logger.log(Level.WARNING, "Missing substance owner name");
+			if (fixErrors) {
+				record.setOwnerName(filename);
+				logger.log(Level.WARNING,
+						"Assigned file name to substance owner name");
+			}
 		}
+		if (record.getSubstanceUUID() == null) {
+			logger.log(Level.WARNING, "Missing substance UUID");
+			if (fixErrors) {
+				record.setSubstanceUUID(generateUUIDfromString(prefix,
+						record.getOwnerName() + record.getSubstanceName()));
+				logger.log(Level.WARNING,
+						"Generated substance UUID from substance name");
+			}
 		}
-	    }
-	    if ((record.getReferenceSubstanceUUID()==null) && referenceStructure!=null) {
-		String uuid = generateUUIDfromString(prefix, null);
-		record.setReferenceSubstanceUUID(uuid);
-		referenceStructure.setRecordProperty(Property.getI5UUIDInstance(), uuid);
-	    }
+		if (record.getMeasurements() == null) {
+			logger.log(Level.WARNING, "No measurements");
+		} else
+			for (ProtocolApplication papp : record.getMeasurements()) {
+				validate(record, papp);
+			}
+		if (record.getRelatedStructures() == null
+				|| record.getRelatedStructures().isEmpty()) {
+			logger.log(Level.WARNING, "No composition");
+		} else {
+			IStructureRecord referenceStructure = null;
+			double proportion = 0;
+			for (CompositionRelation rel : record.getRelatedStructures()) {
+				validate(record, rel);
+				switch (rel.getRelationType()) {
+				case HAS_CONSTITUENT: {
+					Object tvo = rel.getRelation() == null ? 0 : rel
+							.getRelation().getTypical_value();
+					double tv = 0;
+					if (tvo != null)
+						tv = (Double) tvo;
+					if (referenceStructure == null || (proportion < tv)) {
+						referenceStructure = rel.getSecondStructure();
+						if (rel.getRelation() == null)
+							proportion = 0;
+						else
+							proportion = rel.getRelation().getTypical_value() == null ? 0
+									: rel.getRelation().getTypical_value();
+					}
+					break;
+				}
+				}
+			}
+			if ((record.getReferenceSubstanceUUID() == null)
+					&& referenceStructure != null) {
+				String uuid = generateUUIDfromString(prefix, null);
+				record.setReferenceSubstanceUUID(uuid);
+				referenceStructure.setRecordProperty(
+						Property.getI5UUIDInstance(), uuid);
+			}
 
-	}
-
-	if (record.getOwnerUUID()==null) {
-	    logger.log(Level.WARNING, "Missing substance owner UUID");
-	    if (fixErrors) {
-		record.setOwnerUUID(generateUUIDfromString(prefix, record.getOwnerName()));
-		logger.log(Level.WARNING, "Generated substance owner UUID");
-	    }
-	}    
-
-	return record;
-    }
-
-    public IStructureRecord validate(SubstanceRecord record,
-	    ProtocolApplication<Protocol, IParams, String, IParams, String> papp) throws Exception {
-	if (papp.getSubstanceUUID()==null) {
-	    logger.log(Level.WARNING, "Missing substance UUID in protocol application!");
-	    papp.setSubstanceUUID(record.getSubstanceUUID());
-	}
-	if (papp.getDocumentUUID() == null) {
-	    logger.log(Level.WARNING, "Missing measurement UUID");
-	    if (isFixErrors()) {
-		papp.setDocumentUUID(generateUUIDfromString(prefix, null));
-		logger.log(Level.WARNING, "Generated measurement UUID");
-	    }
-	}
-	if (papp.getProtocol() == null)
-	    logger.log(Level.WARNING, "Missing protocol");
-	else {
-	    if (papp.getProtocol().getCategory() == null) {
-		logger.log(Level.WARNING, "Missing protocol category");
-		if (isFixErrors()) {
-		    papp.getProtocol().setCategory(Protocol._categories.UNKNOWN_TOXICITY_SECTION.name());
-		    papp.getProtocol().setTopCategory(Protocol._categories.UNKNOWN_TOXICITY_SECTION.getTopCategory());
 		}
-	    }
-	    if (papp.getProtocol().getTopCategory() == null) {
-		logger.log(Level.WARNING, "Missing protocol top category");
-		if (isFixErrors()) {
-		    try {
-			String c = papp.getProtocol().getCategory();
-			if (!c.endsWith("_SECTION")) c = c + "_SECTION";
-			Protocol._categories category = Protocol._categories.valueOf(c);
-			papp.getProtocol().setCategory(category.name());
-			papp.getProtocol().setTopCategory(category.getTopCategory());
-		    } catch (Exception x) {
-			papp.getProtocol().setTopCategory(Protocol._categories.UNKNOWN_TOXICITY_SECTION.getTopCategory());
-			papp.getProtocol().setCategory(Protocol._categories.UNKNOWN_TOXICITY_SECTION.name());
-		    }
-		    
+
+		if (record.getOwnerUUID() == null) {
+			logger.log(Level.WARNING, "Missing substance owner UUID");
+			if (fixErrors) {
+				record.setOwnerUUID(generateUUIDfromString(prefix,
+						record.getOwnerName()));
+				logger.log(Level.WARNING, "Generated substance owner UUID");
+			}
 		}
-	    }
+
+		return record;
 	}
-	if (papp.getInterpretationResult() == null)
-	    logger.log(Level.WARNING, "Missing interpretation result");
-	if (papp.getInterpretationCriteria() == null)
-	    logger.log(Level.WARNING, "Missing interpretation criteria");
 
-	if (papp.getEffects() == null)
-	    logger.log(Level.WARNING, "Missing results");
-	else
-	    for (EffectRecord<String, IParams, String> effect : papp.getEffects()) {
-		if (effect.getEndpoint()==null)  logger.log(Level.WARNING, "Missing endpoint name");
-		if (effect.getConditions()==null || effect.getConditions().isEmpty())  logger.log(Level.WARNING, "Missing conditions");
-		if (effect.getLoValue()==null && effect.getUpValue()==null && effect.getTextValue()==null)  logger.log(Level.WARNING, "Missing result value");
-		if ((effect.getLoValue()!=null || effect.getUpValue()!=null) && effect.getUnit()==null)  logger.log(Level.WARNING, "Value without units");
-	    }
-	return record;
-    }
+	public IStructureRecord validate(SubstanceRecord record,
+			ProtocolApplication<Protocol, IParams, String, IParams, String> papp)
+			throws Exception {
+		if (papp.getSubstanceUUID() == null) {
+			logger.log(Level.WARNING,
+					"Missing substance UUID in protocol application!");
+			papp.setSubstanceUUID(record.getSubstanceUUID());
+		}
+		if (papp.getDocumentUUID() == null) {
+			logger.log(Level.WARNING, "Missing measurement UUID");
+			if (isFixErrors()) {
+				papp.setDocumentUUID(generateUUIDfromString(prefix, null));
+				logger.log(Level.WARNING, "Generated measurement UUID");
+			}
+		}
+		if (papp.getProtocol() == null)
+			logger.log(Level.WARNING, "Missing protocol");
+		else {
+			if (papp.getProtocol().getCategory() == null) {
+				logger.log(Level.WARNING, "Missing protocol category");
+				if (isFixErrors()) {
+					papp.getProtocol().setCategory(
+							Protocol._categories.UNKNOWN_TOXICITY_SECTION
+									.name());
+					papp.getProtocol().setTopCategory(
+							Protocol._categories.UNKNOWN_TOXICITY_SECTION
+									.getTopCategory());
+				}
+			}
+			if (papp.getProtocol().getTopCategory() == null) {
+				logger.log(Level.WARNING, "Missing protocol top category");
+				if (isFixErrors()) {
+					try {
+						String c = papp.getProtocol().getCategory();
+						if (!c.endsWith("_SECTION"))
+							c = c + "_SECTION";
+						Protocol._categories category = Protocol._categories
+								.valueOf(c);
+						papp.getProtocol().setCategory(category.name());
+						papp.getProtocol().setTopCategory(
+								category.getTopCategory());
+					} catch (Exception x) {
+						papp.getProtocol().setTopCategory(
+								Protocol._categories.UNKNOWN_TOXICITY_SECTION
+										.getTopCategory());
+						papp.getProtocol().setCategory(
+								Protocol._categories.UNKNOWN_TOXICITY_SECTION
+										.name());
+					}
 
-    public IStructureRecord validate(SubstanceRecord record, CompositionRelation rel) throws Exception {
-	if (rel.getCompositionUUID() == null) {
-	    logger.log(Level.WARNING, "Missing composition UUID");
-	    if (isFixErrors()) {
-		String uuid = generateUUIDfromString(prefix, record.getSubstanceUUID());
-		rel.setCompositionUUID(uuid);
-		logger.log(Level.WARNING, "Generated composition UUID");
-	    }
+				}
+			}
+		}
+		if (papp.getInterpretationResult() == null)
+			logger.log(Level.WARNING, "Missing interpretation result");
+		if (papp.getInterpretationCriteria() == null)
+			logger.log(Level.WARNING, "Missing interpretation criteria");
+
+		if (papp.getEffects() == null)
+			logger.log(Level.WARNING, "Missing results");
+		else
+			for (EffectRecord<String, IParams, String> effect : papp
+					.getEffects()) {
+				if (effect.getEndpoint() == null)
+					logger.log(Level.WARNING, "Missing endpoint name");
+				if (effect.getConditions() == null
+						|| effect.getConditions().isEmpty())
+					logger.log(Level.WARNING, "Missing conditions");
+				if (effect.getLoValue() == null && effect.getUpValue() == null
+						&& effect.getTextValue() == null)
+					logger.log(Level.WARNING, "Missing result value");
+				if ((effect.getLoValue() != null || effect.getUpValue() != null)
+						&& effect.getUnit() == null)
+					logger.log(Level.WARNING, "Value without units");
+			}
+		return record;
 	}
-	return record;
-    }
 
-    public static String generateUUIDfromString(String prefix, String id) {
-	return prefix + "-" + (id == null ? UUID.randomUUID() : UUID.nameUUIDFromBytes(id.getBytes()));
-    }
+	public IStructureRecord validate(SubstanceRecord record,
+			CompositionRelation rel) throws Exception {
+		if (rel.getCompositionUUID() == null) {
+			logger.log(Level.WARNING, "Missing composition UUID");
+			if (isFixErrors()) {
+				String uuid = generateUUIDfromString(prefix,
+						record.getSubstanceUUID());
+				rel.setCompositionUUID(uuid);
+				logger.log(Level.WARNING, "Generated composition UUID");
+			}
+		}
+		return record;
+	}
+
+	public static String generateUUIDfromString(String prefix, String id) {
+		return prefix
+				+ "-"
+				+ (id == null ? UUID.randomUUID() : UUID.nameUUIDFromBytes(id
+						.getBytes()));
+	}
 }
