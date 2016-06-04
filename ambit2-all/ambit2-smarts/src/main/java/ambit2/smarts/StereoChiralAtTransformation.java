97fe347e251feab5b8c721644537a70e5164ba95
Nikolay Kochev <nick@uni-plovdiv.net>
2016-06-04 18:12:43 +0000

    Implemented setting of reactant atoms mapped to the product ligands for db stereo

--- ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/StereoChiralAtTransformation.java
+++ ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/StereoChiralAtTransformation.java
@@ -4,14 +4,14 @@ public class StereoChiralAtTransformation
 {
 	//Reactant
 	public int reactChiralAtom = -1;
-	public int reactLigands[] = null;
+	public int reactLigands[] = null; //atom indices include mapped and unmapped atoms
 	public int reactTerminal1 = -1;
 	public int reactTerminal2 = -1;
 	public int reactChirality = SmartsConst.ChC_Unspec;
 	
 	//Product
 	public int prodChiralAtom = -1;
-	public int prodLigands[] = null;
+	public int prodLigands[] = null; //atom indices include mapped and unmapped atoms
 	public int prodLigandsReactMap[] = null; //if mapped reactant partners may not be in the same order as specified in reactLigand
 	public int prodTerminal1 = -1;
 	public int prodTerminal2 = -1;
--- ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/StereoDBTransformation.java
+++ ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/StereoDBTransformation.java
@@ -8,16 +8,16 @@ public class StereoDBTransformation
 	//Reactant 
 	public int reactDBAt1 = -1;
 	public int reactDBAt2 = -1;
-	public int reactLigand1 = -1;
-	public int reactLigand2 = -1;
+	public int reactLigand1 = -1;  //atom index is for mapped or unmapped atom
+	public int reactLigand2 = -1;  //atom index is for mapped or unmapped atom
 	public DBStereo reactDBStereo = DBStereo.UNDEFINED;
 	
 	//Product
 	public int prodDBAt1 = -1;
 	public int prodDBAt2 = -1;
-	public int prodLigand1 = -1;
-	public int prodLigand2 = -1;
-	public int prodLigand1ReactMap = -1; //if mapped the reactant partner is not obligatory reactLigand1	
-	public int prodLigand2ReactMap = -1; //if mapped the reactant partner is not obligatory reactLigand2
+	public int prodLigand1 = -1; //atom index is for mapped or unmapped atom
+	public int prodLigand2 = -1; //atom index is for mapped or unmapped atom
+	public int prodLigand1ReactMap = -1; //if mapped the reactant partner is not obligatory reactLigand1,2	
+	public int prodLigand2ReactMap = -1; //if mapped the reactant partner is not obligatory reactLigand1,2
 	public DBStereo prodDBStereo = DBStereo.UNDEFINED;
 }
