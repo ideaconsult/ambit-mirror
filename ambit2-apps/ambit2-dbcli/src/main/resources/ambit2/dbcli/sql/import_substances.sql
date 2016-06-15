05bae4f655423919c9611ae6c02a6cacbb5d3809
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2016-06-15 10:03:27 +0000

    ambitcli -a dbmigrate option to copy substances from another ambit database (structures are a pain, as idchemical ids are db unique)

--- ambit2-apps/ambit2-dbcli/src/main/resources/ambit2/dbcli/sql/import_substances.sql
+++ ambit2-apps/ambit2-dbcli/src/main/resources/ambit2/dbcli/sql/import_substances.sql
@@ -1,5 +1,5 @@
 -- substance table
-LOAD DATA INFILE "substance_table.tsv"
+LOAD DATA INFILE "{TMP}substance_table.tsv"
 into table substance
 FIELDS TERMINATED BY '\t' OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
@@ -8,7 +8,7 @@ publicname,content,substanceType,rs_prefix,@var_rs_uuid,owner_prefix,@var_owner_
 SET uuid = UNHEX(@var_uuid), owner_uuid=unhex(@var_owner_uuid),rs_uuid=unhex(@var_rs_uuid);
 
 -- substance_protocolapplications table
-LOAD DATA INFILE "substance_protocolapplication_table.tsv"
+LOAD DATA INFILE "{TMP}substance_protocolapplication_table.tsv"
 into table substance_protocolapplication
 FIELDS TERMINATED BY '\t' OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
@@ -18,7 +18,7 @@ reliability,isRobustStudy,isUsedforClassification,isUsedforMSDS,purposeFlag,stud
 SET document_uuid = UNHEX(@var_document_uuid), substance_uuid=unhex(@var_substance_uuid);
 
 -- substance_experiments table
-LOAD DATA INFILE "substance_experiment_table.tsv"
+LOAD DATA INFILE "{TMP}substance_experiment_table.tsv"
 into table substance_experiment
 FIELDS TERMINATED BY '\t' OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
@@ -30,7 +30,7 @@ SET document_uuid = UNHEX(@var_document_uuid), substance_uuid=unhex(@var_substan
 update substance_experiment set endpointhash= unhex(sha1(concat(ifnull(endpoint,""),ifnull(unit,""),ifnull(conditions,""))));
 
 -- substance_ids table
-LOAD DATA INFILE "substance_ids_table.tsv"
+LOAD DATA INFILE "{TMP}substance_ids_table.tsv"
 into table substance_ids
 FIELDS TERMINATED BY '\t' OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
@@ -57,6 +57,7 @@ CREATE TABLE `substance_relation_chemicals_tmp` (
   `rs_prefix` varchar(6) COLLATE utf8_bin DEFAULT NULL COMMENT 'ReferenceSubstance UUID (prefix)',
   `rs_uuid` varbinary(16) DEFAULT NULL COMMENT 'ReferenceSubstance UUID',
   `name` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT 'composition name',
+  `hidden` tinyint(4) DEFAULT '0',
   `substance_prefix` varchar(6) COLLATE utf8_bin DEFAULT NULL COMMENT 'ECB5 in I5 UUIDs like ECB5-2c94e32c-3662-4dea-ba00-43787b8a6fd3',
   `substance_uuid` varbinary(16) DEFAULT NULL COMMENT 'The UUID part of  I5 UUIDs in binary format',  
   `inchi` text CHARACTER SET latin1 COLLATE latin1_bin,
@@ -66,10 +67,10 @@ CREATE TABLE `substance_relation_chemicals_tmp` (
   `label` enum('OK','UNKNOWN','ERROR') NOT NULL DEFAULT 'UNKNOWN',
   `lastmodified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,  
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Dossier to chemicals relation';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Dossier to chemicals relation';
 
 -- import into a temporary table
-LOAD DATA INFILE "substance_relation_chemicals_table.tsv"
+LOAD DATA INFILE "{TMP}substance_relation_chemicals_table.tsv"
 into table substance_relation_chemicals_tmp
 FIELDS TERMINATED BY '\t' OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
@@ -80,12 +81,56 @@ proportion_real_lower,proportion_real_lower_value,
 proportion_real_upper,proportion_real_upper_value,
 proportion_real_unit,
 proportion_typical,proportion_typical_value,proportion_typical_unit,
-rs_prefix,@var_rs_uuid,name,
+rs_prefix,@var_rs_uuid,name,hidden,
 substance_prefix,@var_uuid,
 inchi,smiles,formula,inchikey,label,lastmodified)
 SET cmp_uuid=UNHEX(@var_cmp_uuid),rs_uuid=unhex(@var_rs_uuid),substance_uuid=unhex(@var_uuid);
 
+-- add the missing chemicals
+insert into chemicals
+SELECT null,inchi,smiles,formula,inchikey,label,lastmodified FROM substance_relation_chemicals_tmp
+where inchikey not in 
+(select inchikey from chemicals)
+group by inchikey;
 
--- then join
--- substance compositio
+-- and the chemicals without structure
+-- temporarily using the inchi field to link with substance_uuid
+insert into chemicals
+SELECT null,hex(substance_uuid),smiles,formula,inchikey,label,lastmodified FROM substance_relation_chemicals_tmp
+where inchi is null
+group by substance_uuid;
 
+-- join to get idsubstance
+-- TODO fields
+SELECT * FROM substance_relation_chemicals_tmp
+join substance on prefix=substance_prefix and uuid=substance_uuid;
+
+-- then join with chemicals having a structure and insert into substance_relation
+insert ignore into substance_relation
+SELECT 
+cmp_prefix,cmp_uuid,s.idsubstance,c.idchemical,
+relation,function,
+proportion_real_lower,proportion_real_lower_value,
+proportion_real_upper,proportion_real_upper_value,
+proportion_real_unit,
+proportion_typical,proportion_typical_value,proportion_typical_unit,
+t.rs_prefix,t.rs_uuid,t.name,0
+FROM substance_relation_chemicals_tmp t
+join chemicals c using(inchikey)
+join substance s on prefix=substance_prefix and uuid=substance_uuid
+where inchikey is not null;
+
+-- then join the chemicals WITHOUT structure and insert into substance_relation
+insert ignore into substance_relation
+SELECT 
+cmp_prefix,cmp_uuid,s.idsubstance,c.idchemical,
+relation,function,
+proportion_real_lower,proportion_real_lower_value,
+proportion_real_upper,proportion_real_upper_value,
+proportion_real_unit,
+proportion_typical,proportion_typical_value,proportion_typical_unit,
+t.rs_prefix,t.rs_uuid,t.name,0
+FROM substance_relation_chemicals_tmp t
+join chemicals c on unhex(c.inchi)=substance_uuid
+join substance s on prefix=substance_prefix and uuid=substance_uuid
+where !(c.inchi regexp "^InChI") and t.inchikey is null;
