ac03ac3593e7ef37addd93fea4bdb5783d64c7b3
Nikolay Kochev <nick@uni-plovdiv.net>
2016-06-20 13:12:51 +0000

    Fixed a subtle bug in function deleteAtomAndDoStereoTransformation for the case when the invalidated stereo element does not contain the 'deleted' atom. Function restoreDBStereo() moved to class StereoChemUtils and implemented. Implemented functions stereoElement2String() and toString()

--- ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/StereoChange.java
+++ ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/StereoChange.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IBond;
 import org.openscience.cdk.interfaces.IStereoElement;
 import org.openscience.cdk.stereo.DoubleBondStereochemistry;
@@ -44,6 +45,40 @@ public class StereoChange
 			return false;
 		
 		return true;
+	}
+	
+	public String toString(IAtomContainer target) 
+	{
+		StringBuffer sb = new StringBuffer();
+		if (ligand0Deleted)
+			sb.append("ligand 0 deleted");
+		
+		if (!addLigands0.isEmpty())
+		{
+			sb.append(addLigands0);
+			for (int i = 0; i < addLigands0.size(); i++)
+			{	
+				IBond bo  = addLigands0.get(i);
+				sb.append("  ("+ bo.getAtom(0).getSymbol() + target.getAtomNumber(bo.getAtom(0)));
+				sb.append(","+ bo.getAtom(1).getSymbol() + target.getAtomNumber(bo.getAtom(1)) + ")");
+			}	
+		}
+		
+		if (ligand1Deleted)
+			sb.append("ligand 1 deleted");
+		
+		if (!addLigands1.isEmpty())
+		{
+			sb.append(addLigands0);
+			for (int i = 0; i < addLigands1.size(); i++)
+			{	
+				IBond bo  = addLigands1.get(i);
+				sb.append("  ("+ bo.getAtom(0).getSymbol() + target.getAtomNumber(bo.getAtom(0)));
+				sb.append(","+ bo.getAtom(1).getSymbol() + target.getAtomNumber(bo.getAtom(1)) + ")");
+			}	
+		}
+		
+		return (sb.toString());
 	}
 	
 }
