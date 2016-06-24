a865d7b57da4e57a5c8b136496170099113f7153
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2016-06-24 18:28:02 +0000

    non-db test converting nanowiki RDF to AMBIT  RDF :) https://sourceforge.net/p/ambit/feature-requests/99/

--- ambit2-apps/ambit2-dbsubstance/src/test/java/ambit2/dbsubstance/test/NanoWikiRDFTest.java
+++ ambit2-apps/ambit2-dbsubstance/src/test/java/ambit2/dbsubstance/test/NanoWikiRDFTest.java
@@ -3,7 +3,9 @@ package ambit2.dbsubstance.test;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.sql.Connection;
+import java.util.zip.GZIPInputStream;
 
 import junit.framework.Assert;
 import net.idea.loom.nm.nanowiki.NanoWikiRDFReader;
@@ -13,13 +15,30 @@ import org.junit.Test;
 
 import ambit2.base.data.SubstanceRecord;
 import ambit2.base.interfaces.IStructureRecord;
+import ambit2.base.io.DownloadTool;
 import ambit2.core.io.IRawReader;
 import ambit2.core.processors.structure.key.PropertyKey;
 import ambit2.core.processors.structure.key.ReferenceSubstanceUUID;
 import ambit2.db.processors.test.DbUnitTest;
 import ambit2.db.substance.processor.DBSubstanceWriter;
 
+/**
+ * These are tests reading NanoWiki RDF and writing to database
+ * @author nina
+ *
+ */
 public class NanoWikiRDFTest extends DbUnitTest {
+
+	public static File getNanoWikiFile() throws Exception {
+		File baseDir = new File(System.getProperty("java.io.tmpdir"));
+		File file = new File(baseDir, "nanowiki3.rdf.gz");
+		if (!file.exists()) {
+			URL url = new URL("https://ndownloader.figshare.com/files/5228257");
+			DownloadTool.download(url, file);
+		}
+		return file;
+	}
+
 	@Test
 	public void test() throws Exception {
 	}
@@ -29,10 +48,9 @@ public class NanoWikiRDFTest extends DbUnitTest {
 		NanoWikiRDFReader reader = null;
 		int records = 0;
 		try {
-			reader = new NanoWikiRDFReader(new InputStreamReader(
-					new FileInputStream(new File(
-							"D:/src-other/nanowiki/backup_figshare.rdf")),
-					"UTF-8"));
+			reader = new NanoWikiRDFReader(
+					new InputStreamReader(new GZIPInputStream(
+							new FileInputStream(getNanoWikiFile()))));
 			while (reader.hasNext()) {
 				IStructureRecord record = reader.nextRecord();
 				Assert.assertTrue(record instanceof SubstanceRecord);
@@ -50,10 +68,9 @@ public class NanoWikiRDFTest extends DbUnitTest {
 		IDatabaseConnection c = getConnection();
 		IRawReader<IStructureRecord> parser = null;
 		try {
-			parser = new NanoWikiRDFReader(new InputStreamReader(
-					new FileInputStream(new File(
-							"D:/src-other/nanowiki/backup_figshare.rdf")),
-					"UTF-8"));
+			parser = new NanoWikiRDFReader(
+					new InputStreamReader(new GZIPInputStream(
+							new FileInputStream(getNanoWikiFile()))));
 			write(parser, c.getConnection(), new ReferenceSubstanceUUID(),
 					false);
 
