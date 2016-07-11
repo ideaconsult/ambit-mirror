c43ffe1c14cc9a4820e27a05d73a6ce1c290864f
Nikolay Kochev <nick@uni-plovdiv.net>
2016-07-11 22:10:36 +0000

    Implemented handling of variable type

--- ambit2-all/ambit2-apps/ambit2-export/src/test/java/ambit2/export/isa/codeutils/j2p_helpers/VariableInfo.java
+++ ambit2-all/ambit2-apps/ambit2-export/src/test/java/ambit2/export/isa/codeutils/j2p_helpers/VariableInfo.java
@@ -3,9 +3,25 @@ package ambit2.export.isa.codeutils.j2p_helpers;
 public class VariableInfo 
 {
 	public static enum  Type {
-		STRING, INT 
+		STRING, ARRAY, OBJECT; 
 	}
 	
 	public Type type = null;
 	public String name = null;
+	public String objectClass = null; //used only for type OBJECT
+	
+	public static Type getTypeFromString(String s)
+	{
+		if (s.equals("string"))
+			return Type.STRING;
+		
+		if (s.equals("array"))
+			return Type.ARRAY;
+		
+		if (s.equals("object"))
+			return Type.OBJECT;
+		
+		return null;
+	}
+	
 }
