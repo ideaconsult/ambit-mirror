fce76991e028aef8b3c91b303ac72536366d5cb1
Nikolay Kochev <nick@uni-plovdiv.net>
2016-07-11 11:52:54 +0000

    Added new class Json2Pojo.

---
+++ ambit2-apps/ambit2-export/src/test/java/ambit2/export/isa/codeutils/Json2Pojo.java
@@ -0,0 +1,9 @@
+package ambit2.export.isa.codeutils;
+
+import java.io.File;
+
+public class Json2Pojo 
+{
+	public File sourceDir = null;
+	public File targetDir = null;
+}
--- ambit2-apps/ambit2-export/src/test/java/ambit2/export/isa/codeutils/TestCodeUtils.java
+++ ambit2-apps/ambit2-export/src/test/java/ambit2/export/isa/codeutils/TestCodeUtils.java
@@ -16,13 +16,13 @@ public class TestCodeUtils
 		//Automatic generation if java classes from json schemas
 		//------------------------------------------------------
 		//Step 1
-		//CodeUtils.clearJsonFileSuffix(new File("/git-repositories/isa-api/isatools/schemas/isa_model_version_1_0_schemas/core"), 
-		//		new File("/temp2/test/test2"), "_schema");
+		CodeUtils.clearJsonFileSuffix(new File("/git-repositories/isa-api/isatools/schemas/isa_model_version_1_0_schemas/core"), 
+				new File("/temp2/test/test2"), "_schema");
 		
 		//Step 2 pojo
 		
 		//Step 3
-		javaRename ("D:/Projects/Nina/jsonschema2pojo-0.4.15/target-isa1/ambit2/export/isa/v1_0/objects");
+		//javaRename ("D:/Projects/Nina/jsonschema2pojo-0.4.15/target-isa1/ambit2/export/isa/v1_0/objects");
 		//------------------------------------------------------
 	}
 	
@@ -30,7 +30,6 @@ public class TestCodeUtils
 	public static void javaRename(String targetDir) throws Exception
 	{	
 		CodeUtils.renameJavaClass(null, new File(targetDir),"ProcessSequence", "Process");
-		
 		CodeUtils.renameJavaClass(null, new File(targetDir),"ExecutesProtocol", "Protocol");
 		
 		CodeUtils.renameJavaClass(null, new File(targetDir),"CharacteristicCategory", "MaterialAttribute");  
@@ -42,7 +41,29 @@ public class TestCodeUtils
 		
 		CodeUtils.renameJavaClass(null, new File(targetDir),"ParameterValue", "ProcessParameterValue");
 		CodeUtils.renameJavaClass(null, new File(targetDir),"Parameter", "ProtocolParameter");
+		
+		
+		
+		
+		/*
+		 *  (previous rename version)
+		 * 
+		CodeUtils.renameJavaClass(null, new File(targetDir),"ProcessSequence", "Process");
+		CodeUtils.renameJavaClass(null, new File(targetDir),"ExecutesProtocol", "Protocol");
+		
+		CodeUtils.renameJavaClass(null, new File(targetDir),"CharacteristicCategory", "MaterialAttribute");  
+		CodeUtils.renameJavaClass(null, new File(targetDir),"Characteristic", "MaterialAttributeValue");
+		 
+		CodeUtils.renameJavaClass(null, new File(targetDir),"DataFile", "Data");
+		CodeUtils.renameJavaClass(null, new File(targetDir),"DerivesFrom", "Source");
+		CodeUtils.renameJavaClass(null, new File(targetDir),"MeasurementType", "OntologyAnnotation");
+		
+		CodeUtils.renameJavaClass(null, new File(targetDir),"ParameterValue", "ProcessParameterValue");
+		CodeUtils.renameJavaClass(null, new File(targetDir),"Parameter", "ProtocolParameter");
+		 */
 	}
+	
+	
 	
 
 }
