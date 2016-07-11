8e29be54fcfb47652d9d30682d34871425e077d4
Nikolay Kochev <nick@uni-plovdiv.net>
2016-07-11 16:47:07 +0000

    Implemented function tokenize().

--- ambit2-apps/ambit2-export/src/test/java/ambit2/export/isa/codeutils/j2p_helpers/ClassNameGenerator.java
+++ ambit2-apps/ambit2-export/src/test/java/ambit2/export/isa/codeutils/j2p_helpers/ClassNameGenerator.java
@@ -1,5 +1,8 @@
 package ambit2.export.isa.codeutils.j2p_helpers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ambit2.export.isa.codeutils.Json2Pojo;
 
 public class ClassNameGenerator 
@@ -10,9 +13,16 @@ public class ClassNameGenerator
 	public String suffix = "_schema";
 	
 	public boolean FlagHandleTokens = true;
-	public char tokenSeparatos[] = new char[] {'_', '-'}; 
+	public String splitters[] = new String[] {"_", "-"}; 
 	
 	public String additionSuffixForDuplication = "_";
+	
+	
+	//work variables
+	//int numTokens = 0;
+	//int numTokensAll = 0; //Including the empty Tokens
+	List<TokenPars> tokens = new ArrayList<TokenPars>();
+	List<TokenPars> allTokens = new ArrayList<TokenPars>();
 	
 	public ClassNameGenerator(Json2Pojo j2p)
 	{
@@ -45,4 +55,111 @@ public class ClassNameGenerator
 		return jcName;
 	}
 	
+	private void tokenize(String s)
+	{
+		tokens.clear();
+		allTokens.clear();
+		
+		int pos = 0;
+		int copyPos, nextMinPos, i;
+		int currentBeginSplitter = -1;
+		int ind, splitType = -1;
+		int numSplitters = splitters.length;
+		
+		//Check for splitters in the beginning of the string
+		boolean startSplitters = false;
+		for (i=0; i < numSplitters; i++)
+		{
+			if (s.startsWith(splitters[i])){
+				pos = pos + splitters[i].length();
+				startSplitters = true;
+				currentBeginSplitter = i;
+				break;
+			}
+		}
+		
+		while (startSplitters)
+		{
+			startSplitters = false;
+			for (i=0; i < numSplitters; i++){
+				if (s.startsWith(splitters[i],pos)){
+					allTokens.add(new TokenPars(pos,pos,currentBeginSplitter,i));
+					pos = pos + splitters[i].length();
+					currentBeginSplitter = i;
+					startSplitters = true;
+					break;
+				}
+			}
+		}
+		
+		copyPos = pos;
+		
+		//Detecting all tokens
+		while (pos < s.length()) 
+		{
+			//Finding the next splitter
+			nextMinPos = s.length();
+			splitType  = -1;
+			for (i=0; i < numSplitters; i++)
+			{
+				ind = s.indexOf(splitters[i],pos);
+				if ((ind != -1)&(ind < nextMinPos))
+				{
+					nextMinPos = ind;
+					splitType = i;
+				}
+			}
+			
+			if (splitType == -1)
+			{
+				//This is the last Token reaching the string End
+				tokens.add(new TokenPars(copyPos,s.length(),currentBeginSplitter,splitType));
+				allTokens.add(new TokenPars(copyPos,s.length(),currentBeginSplitter,splitType));
+				pos = s.length();
+				break;
+			};
+			
+			boolean FlagAdd = true;
+			
+			if (nextMinPos == copyPos) 
+			{	
+				//Empty Token is added
+				if (FlagAdd)
+					allTokens.add(new TokenPars(copyPos,copyPos,currentBeginSplitter,splitType));
+				pos = nextMinPos + splitters[splitType].length();
+				if (FlagAdd)
+					copyPos = pos;
+				currentBeginSplitter = splitType;
+			}
+			else
+			{
+				//System.out.println(pos+"    "+nextMinPos);
+				if (FlagAdd)
+				{	
+					tokens.add(new TokenPars(copyPos,nextMinPos,currentBeginSplitter,splitType));
+					allTokens.add(new TokenPars(copyPos,nextMinPos,currentBeginSplitter,splitType));
+				}
+				pos = nextMinPos + splitters[splitType].length();
+				if (FlagAdd)
+					copyPos = pos;
+				currentBeginSplitter = splitType;
+			}
+		}// of while pos < s.length()
+		
+	}//End of tokenize
+	
 }
+
+class TokenPars{
+	int iBegin; //Token indexes which define the corresponding substring
+	int iEnd;
+	int splitterBegin, splitterEnd; //Splitter types
+	int indexAll; //This is the token corresponding index in the "All" Vector
+	
+	public TokenPars(int iBegin_, 	int iEnd_, 	int spliterBegin_, int spliterEnd_){
+		iBegin = iBegin_;
+		iEnd = iEnd_;
+		splitterBegin = spliterBegin_;
+		splitterEnd = spliterEnd_;
+	}
+}// End of class TokenPars
