7e5423107917069fe5219fbed6985819ca45e42a
Nikolay Kochev <nick@uni-plovdiv.net>
2016-07-13 20:40:22 +0000

    Finished the variable initialization.

--- ambit2-apps/ambit2-export/src/test/java/ambit2/export/isa/codeutils/j2p_helpers/VariableInfo.java
+++ ambit2-apps/ambit2-export/src/test/java/ambit2/export/isa/codeutils/j2p_helpers/VariableInfo.java
@@ -25,25 +25,31 @@ public class VariableInfo
 					sourceConfig.getArrayInitialization(objectClass) + ";";
 		
 		case BOOLEAN:
-			return "boolean " + name + ";";
+			return "boolean " + name + 
+					sourceConfig.getBooleanInitialization() + ";";
 		
 		case INTEGER:
-			return "int " + name + ";";
+			return "int " + name + 
+					sourceConfig.getIntegerInitialization() + ";";
 		
 		case NUMBER:
-			return "double " + name + ";";
+			return sourceConfig.number + " " + name + 
+					sourceConfig.getNumberInitialization() + ";";
 			
 		case OBJECT:
-			return objectClass + " " + name + ";";
+			return objectClass + " " + name + 
+					sourceConfig.getObjectInitialization(objectClass) + ";";
 			
 		case STRING:
 		{	
 			if (stringFormat != null)
 			{
 				if (stringFormat == StringFormat.URL_FORMAT)
-					return "URL " + name + ";";
+					return "URL " + name + 
+							sourceConfig.getURLInitialization() + ";";
 			}
-			return "String " + name + ";";
+			return "String " + name + 
+					sourceConfig.getStringInitialization() + ";";
 		}	
 			
 		}
