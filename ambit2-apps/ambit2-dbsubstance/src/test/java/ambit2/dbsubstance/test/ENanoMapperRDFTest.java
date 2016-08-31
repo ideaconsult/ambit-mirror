a3b4735e2a15b321a4e798a52ae62291329fe9f3
Egon Willighagen <egon.willighagen@gmail.com>
2016-08-31 18:53:10 +0000

    Added a roundtrip test

--- ambit2-apps/ambit2-dbsubstance/src/test/java/ambit2/dbsubstance/test/ENanoMapperRDFTest.java
+++ ambit2-apps/ambit2-dbsubstance/src/test/java/ambit2/dbsubstance/test/ENanoMapperRDFTest.java
@@ -2,14 +2,25 @@ package ambit2.dbsubstance.test;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.URL;
 
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.RDFFormat;
 import org.junit.Test;
+import org.restlet.Request;
+import org.restlet.data.MediaType;
+import org.restlet.data.Reference;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 
 import ambit2.base.data.SubstanceRecord;
 import ambit2.base.interfaces.IStructureRecord;
 import ambit2.base.io.DownloadTool;
+import ambit2.rest.substance.SubstanceRDFReporter;
 import junit.framework.Assert;
 import net.idea.loom.nm.nanowiki.ENanoMapperRDFReader;
 
@@ -33,17 +44,46 @@ public class ENanoMapperRDFTest {
 	@Test
 	public void testRead() throws Exception {
 		ENanoMapperRDFReader reader = null;
-		int records = 0;
 		try {
 			reader = new ENanoMapperRDFReader(
-				new InputStreamReader(new FileInputStream(getRDFFile()))
+				new InputStreamReader(new FileInputStream(getRDFFile())), "DEMO"
 			);
 			while (reader.hasNext()) {
 				IStructureRecord record = reader.nextRecord();
 				Assert.assertTrue(record instanceof SubstanceRecord);
 				System.out.println("Substance: " + ((SubstanceRecord) record).toJSON(""));
-				records++;
 			}
+		} finally {
+			if (reader != null) reader.close();
+		}
+	}
+
+	@Test
+	public void testRoundtrip() throws Exception {
+		ENanoMapperRDFReader reader = null;
+		Request hack = new Request();
+		hack.setRootRef(new Reference("http://localhost/ambit2"));
+		SubstanceRDFReporter r = new SubstanceRDFReporter(hack,
+				MediaType.TEXT_RDF_N3);
+		Model model = ModelFactory.createDefaultModel();
+		r.header(model, null);
+		r.setOutput(model);
+		try {
+			reader = new ENanoMapperRDFReader(
+				new InputStreamReader(new FileInputStream(getRDFFile())), "DEMO"
+			);
+			while (reader.hasNext()) {
+				IStructureRecord record = reader.nextRecord();
+				Assert.assertTrue(record instanceof SubstanceRecord);
+				r.processItem((SubstanceRecord) record);
+			}
+			r.footer(model, null);
+			File output = new File(System.getProperty("java.io.tmpdir") + "/"
+					+ "enmrdf_export.ttl");
+			System.out.println("Exported to " + output.getAbsolutePath());
+			OutputStream writer = new FileOutputStream(output);
+
+			RDFDataMgr.write(writer, model, RDFFormat.TURTLE);
 		} finally {
 			if (reader != null) reader.close();
 		}
