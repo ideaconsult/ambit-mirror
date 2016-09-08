2c1deca7a4a050982a4aea9a38ed0047190428b0
Nikolay Kochev <nick@uni-plovdiv.net>
2016-09-08 10:55:06 +0000

    Added class TestUtils.

---
+++ ambit2-tautomers/src/test/java/ambit2/tautomers/test/TestUtils.java
@@ -0,0 +1,21 @@
+package ambit2.tautomers.test;
+
+import ambit2.tautomers.tools.TautomerAnalysis;
+
+public class TestUtils {
+
+	public static void main(String[] args) 
+	{
+		testRanking(new double[] {3,6,1,9.5,3});
+
+	}
+	
+	
+	public static void testRanking(double values[])
+	{
+		int ranks[] = TautomerAnalysis.getRanks(values);
+		for (int i = 0; i < ranks.length; i++)
+			System.out.println("  " + values[i] + "   rank = " + ranks[i]);
+	}
+
+}
