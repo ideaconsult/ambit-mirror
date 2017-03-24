1174b1ed18d709619675cf0ed1db364a35c3d15f
Ognyan Pukalov <oggy@uni-plovdiv.bg>
2017-03-24 13:52:57 +0000

    MoleculeUtils - added functions for counting explicit and total hydrogen. TestUtils - added functions for testing MoleculeUtils.

--- ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/utils/MoleculeUtils.java
+++ ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/utils/MoleculeUtils.java
@@ -1,5 +1,26 @@
 package ambit2.groupcontribution.utils;
 
+import java.util.List;
+
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IAtomContainer;
+
 public class MoleculeUtils {
+
+	public static int getExplicitHCount (IAtom at, IAtomContainer ac){
+		int explicitHCount = 0;
+			List<IAtom> neighbors = ac.getConnectedAtomsList(at);
+			for (int i = 0; i < neighbors.size(); i++){
+				if(neighbors.get(i).getSymbol().equals("H")){
+					explicitHCount++;
+				}
+			}			
+		return explicitHCount;
+	}
+
+	public static int getHCount (IAtom at, IAtomContainer ac){
+		int totalHCount = at.getImplicitHydrogenCount() + getExplicitHCount(at, ac);
+		return totalHCount;
+	}
 
 }
