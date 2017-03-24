3627a62a5ed3dfdc6c3dccb3abf14edd6eff155c
Ognyan Pukalov <oggy@uni-plovdiv.bg>
2017-03-24 13:52:57 +0000

    MoleculeUtils - added functions for counting explicit and total hydrogen. TestUtils - added functions for testing MoleculeUtils.

--- ambit2-all/ambit2-groupcontribution/src/test/java/ambit2/groupcontribution/test/TestUtils.java
+++ ambit2-all/ambit2-groupcontribution/src/test/java/ambit2/groupcontribution/test/TestUtils.java
@@ -11,35 +11,37 @@ import ambit2.groupcontribution.descriptors.LDAtomHeavyNeighbours;
 import ambit2.groupcontribution.descriptors.LDAtomSymbol;
 import ambit2.groupcontribution.descriptors.LDAtomValency;
 import ambit2.groupcontribution.descriptors.LDHNum;
+import ambit2.groupcontribution.utils.MoleculeUtils;
 import ambit2.smarts.SmartsHelper;
 
 
 public class TestUtils 
 {
 
-	
 	public static void main(String[] args) throws Exception
 	{
 		//testLD("CC(NC=C)COC(S(=O)(=O)O)CCNC=O");
-		testLD("[CH4]");
+		testLD("C([H])([H])O([H])C[H]");
 	}
-	
+
 	public static void testLD(String smiles) throws Exception
 	{
-		System.out.println("Testing LDs for " + smiles);		
-		testLocalDescriptor(smiles, new LDAtomSymbol());
-		testLocalDescriptor(smiles, new LDHNum());
-		testLocalDescriptor(smiles, new LDAtomFormalCharge());			
-		testLocalDescriptor(smiles, new LDAtomHybridization());		
-		testLocalDescriptor(smiles, new LDAtomValency());
-		testLocalDescriptor(smiles, new LDAtomHeavyNeighbours());
+		//System.out.println("Testing LDs for " + smiles);		
+		//testLocalDescriptor(smiles, new LDAtomSymbol());
+		//testLocalDescriptor(smiles, new LDHNum());
+		//testLocalDescriptor(smiles, new LDAtomFormalCharge());			
+		//testLocalDescriptor(smiles, new LDAtomHybridization());		
+		//testLocalDescriptor(smiles, new LDAtomValency());
+		//testLocalDescriptor(smiles, new LDAtomHeavyNeighbours());
+		testGetExplicitHCount(moleculeBuilder(smiles));
+		testGetHCount(moleculeBuilder(smiles));
 	}
-	
+
 	public static void testLocalDescriptor(String smiles, ILocalDescriptor ld) throws Exception
 	{
 		IAtomContainer mol = SmartsHelper.getMoleculeFromSmiles(smiles);
 		AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(mol);
-		
+
 		System.out.print("  " + ld.getShortName() + ":");
 		for (IAtom at : mol.atoms())
 		{	
@@ -47,6 +49,32 @@ public class TestUtils
 			System.out.print("\t" + ld.getDesignation(descrVal));
 		}	
 		System.out.println();
+	}
+
+	public static void testGetExplicitHCount(IAtomContainer ac){
+		System.out.println("Testing getExplicitHCount: ");
+		for (IAtom at : ac.atoms()){
+			if(!at.getSymbol().equals("H")){
+				int explicitHCount = MoleculeUtils.getExplicitHCount(at, ac);
+				System.out.println(at.getSymbol() + ": " + explicitHCount);
+			}
+		}
+	}
+	
+	public static void testGetHCount(IAtomContainer ac){
+		System.out.println("Testing getHCount: ");
+		for (IAtom at : ac.atoms()){
+			if(!at.getSymbol().equals("H")){
+				int hCount = MoleculeUtils.getHCount(at, ac);
+				System.out.println(at.getSymbol() + ": " + hCount);
+			}
+		}
+	}
+	
+	public static IAtomContainer moleculeBuilder(String smiles) throws Exception{
+		IAtomContainer mol = SmartsHelper.getMoleculeFromSmiles(smiles);
+		AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(mol);
+		return mol;
 	}
 
 }
