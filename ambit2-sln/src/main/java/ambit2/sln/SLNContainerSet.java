268f14f1fdf0ab98d8d4151f835e78674aebd375
Nikolay Kochev <nick@uni-plovdiv.net>
2017-03-31 13:12:20 +0000

    added container list.

--- ambit2-sln/src/main/java/ambit2/sln/SLNContainerSet.java
+++ ambit2-sln/src/main/java/ambit2/sln/SLNContainerSet.java
@@ -1,6 +1,10 @@
 package ambit2.sln;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SLNContainerSet 
 {
+	public List<SLNContainer> containers = new ArrayList<SLNContainer>();
 	
 }
