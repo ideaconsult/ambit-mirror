48101aee50b6c75d9f10d6677db3d29376042873
Nikolay Kochev <nick@uni-plovdiv.net>
2017-03-31 11:57:48 +0000

    Fixed the parsing of atom attributes 'n' and 'r'.

--- ambit2-sln/src/test/java/ambit2/sln/test/SLNTestUtilities.java
+++ ambit2-sln/src/test/java/ambit2/sln/test/SLNTestUtilities.java
@@ -42,7 +42,7 @@ public class SLNTestUtilities
 		//tu.testSLN2SLN("CCH4[aaa<=123;tttt=23;bbb;aaaa;s=R;spin=t;c=y;hac=2;tbo=1](O)CCH(O)N");
 		//tu.testSLN2SLN("C-[type=#]C");
 		
-		tu.testSLNIsomorphism("N[f]","CCCCC");
+		tu.testSLNIsomorphism("C[n]","CCCCC");
 		//tu.testSLNIsomorphism("N[fcharge<0]","CC(C)CC[N-]");
 		//tu.testSLNIsomorphism("C[c=o1]","N[CH2]N");
 		//tu.testSLNIsomorphism("C[hc=0]","CC(C)CCC");
