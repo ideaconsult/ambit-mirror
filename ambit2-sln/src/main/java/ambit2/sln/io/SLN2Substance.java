fc3280e2edaef7791c49ef86f1baeaa14bb44355
Nikolay Kochev <nick@uni-plovdiv.net>
2017-04-22 11:59:44 +0000

    added new class SLN2Substance.

---
+++ ambit2-sln/src/main/java/ambit2/sln/io/SLN2Substance.java
@@ -0,0 +1,36 @@
+package ambit2.sln.io;
+
+import java.util.List;
+
+import ambit2.base.relation.composition.CompositionRelation;
+import ambit2.sln.SLNContainer;
+import ambit2.sln.SLNContainerSet;
+
+public class SLN2Substance 
+{
+	public List<CompositionRelation> slnToSubstanceComposition(SLNContainerSet slnContSet)
+	{
+		//TODO
+		return null;
+	}
+	
+	public SLNContainerSet substanceCompositionToSln(List<CompositionRelation> composition)
+	{
+		//TODO
+		return null;
+	}
+	
+	
+	public CompositionRelation slnToCompositionRelation(SLNContainer slnContainer)
+	{
+		//TODO
+		return null;
+	}
+	
+	public SLNContainer CompositionRelationslnTo(CompositionRelation compRel)
+	{
+		//TODO
+		return null;
+	}
+	
+}
