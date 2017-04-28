ba3fef24bafc55d79a3c2a119f1fdfc6600af305
Nikolay Kochev <nick@uni-plovdiv.net>
2017-04-28 11:00:09 +0000

    Implemented function slnToSubstanceComposition().

--- ambit2-sln/src/main/java/ambit2/sln/io/SLN2Substance.java
+++ ambit2-sln/src/main/java/ambit2/sln/io/SLN2Substance.java
@@ -1,5 +1,6 @@
 package ambit2.sln.io;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import ambit2.base.relation.composition.CompositionRelation;
@@ -13,8 +14,18 @@ public class SLN2Substance
 	
 	public List<CompositionRelation> slnToSubstanceComposition(SLNContainerSet slnContSet)
 	{
-		//TODO
-		return null;
+		if (slnContSet == null)
+			return null;
+		
+		List<CompositionRelation> composition = new ArrayList<CompositionRelation>();
+		for (int i = 0; i < slnContSet.containers.size(); i++)
+		{
+			SLNContainer slnContainer = slnContSet.containers.get(i);
+			CompositionRelation compRel = slnToCompositionRelation(slnContainer);
+			composition.add(compRel);
+		}
+		
+		return composition;
 	}
 	
 	public SLNContainerSet substanceCompositionToSln(List<CompositionRelation> composition)
