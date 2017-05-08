d1cd0e306e0435cd1cdd9cfa37a24c4c15da57b1
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2017-05-08 09:36:57 +0000

    testing https://github.com/cdk/cdk/issues/279

--- ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/smirks/Transformations.java
+++ ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/smirks/Transformations.java
@@ -1,142 +1,175 @@
 package ambit2.smarts.smirks;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.List;
 
-import org.openscience.cdk.Atom;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IBond;
+import org.openscience.cdk.io.iterator.IIteratingChemObjectReader;
+import org.openscience.cdk.smiles.SmilesGenerator;
 import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 
 import ambit2.core.data.MoleculeTools;
+import ambit2.core.io.FileInputState;
 import ambit2.smarts.CMLUtilities;
 import ambit2.smarts.SmartsConst.HandleHAtoms;
 
-public class Transformations 
-{
-	public static void fixSHValence7Atoms(IAtomContainer mol)
-	{
-		for (IAtom at: mol.atoms())
-		{
+public class Transformations {
+	public static void fixSHValence7Atoms(IAtomContainer mol) {
+		for (IAtom at : mol.atoms()) {
 			if (!at.getSymbol().equals("S"))
 				continue;
 			if (at.getImplicitHydrogenCount() == null)
 				continue;
 			if (at.getImplicitHydrogenCount() != 1)
 				continue;
-			
-			//If valence is not set bond order sum is calculated
+
+			// If valence is not set bond order sum is calculated
 			List<IBond> boList = mol.getConnectedBondsList(at);
 			int boSum = 0;
-			for (IBond bo: boList)
+			for (IBond bo : boList)
 				boSum += bo.getOrder().numeric();
-			
-			//System.out.println("***** boSum " + boSum);
+
+			// System.out.println("***** boSum " + boSum);
 			if (boSum == 6)
 				at.setImplicitHydrogenCount(0);
 		}
 	}
-	
-	public static void setAtomHNeighbours(IAtom atom, IAtomContainer target, int numH, HandleHAtoms hTransformMode)
-    {
-    	//For all transformation modes both implicit and explicit H atoms are taken into account
-    	Integer hc_im = atom.getImplicitHydrogenCount();
-    	if (hc_im == null)
-    		hc_im = 0;
-    	Integer hc_ex = (Integer) atom.getProperty(CMLUtilities.ExplicitH); //This flag is set
-    	if (hc_ex == null)
-    		hc_ex = 0;
-    	
-    	int totalH = hc_im + hc_ex;
-    	
-    	if (numH == (totalH))
-    		return; //The current H atom count is exactly numH
-    	
-    	if (numH > totalH)
-    	{
-    		//Adding H Atoms
-    		int hDiff = numH - totalH;
-    		switch (hTransformMode)
-    		{
-    		case IMPLICIT:
-    			hc_im = hc_im + hDiff;
-    			atom.setImplicitHydrogenCount(hc_im);
-    			break;
-    		case EXPLICIT:
-    			addExplcitHNeighbours(atom, target, hDiff);
-    			break;
-    		}
-    	}
-    	else //numH < totalH
-    	{	
-    		//Removing H Atoms
-    		int hDiff = totalH - numH;
-    		switch (hTransformMode)
-    		{
-    		case IMPLICIT:
-    			if (hc_im >= hDiff)
-    			{	
-    				hc_im = hc_im - hDiff;
-    				atom.setImplicitHydrogenCount(hc_im);
-    			}
-    			else
-    			{
-    				atom.setImplicitHydrogenCount(0);
-    				//Additionally removing explicit H atoms
-    				removeExplcitHNeighbours(atom, target, hDiff-hc_im);
-    			}
-    			break;
-    		case EXPLICIT:
-    			//TODO
-    			break;
-    		}
-    	}
-    }
-    
-    public static void addExplcitHNeighbours(IAtom atom, IAtomContainer target, int numH)
-    {
-    	for (int i = 0; i < numH; i++)
-    	{
-    		IAtom ha = MoleculeTools.newAtom(target.getBuilder()); 
-    		ha.setSymbol("H");
-    		target.addAtom(ha);
-    		IBond b = MoleculeTools.newBond(target.getBuilder());
+
+	public static void setAtomHNeighbours(IAtom atom, IAtomContainer target, int numH, HandleHAtoms hTransformMode) {
+		// For all transformation modes both implicit and explicit H atoms are
+		// taken into account
+		Integer hc_im = atom.getImplicitHydrogenCount();
+		if (hc_im == null)
+			hc_im = 0;
+		Integer hc_ex = (Integer) atom.getProperty(CMLUtilities.ExplicitH); // This
+																			// flag
+																			// is
+																			// set
+		if (hc_ex == null)
+			hc_ex = 0;
+
+		int totalH = hc_im + hc_ex;
+
+		if (numH == (totalH))
+			return; // The current H atom count is exactly numH
+
+		if (numH > totalH) {
+			// Adding H Atoms
+			int hDiff = numH - totalH;
+			switch (hTransformMode) {
+			case IMPLICIT:
+				hc_im = hc_im + hDiff;
+				atom.setImplicitHydrogenCount(hc_im);
+				break;
+			case EXPLICIT:
+				addExplcitHNeighbours(atom, target, hDiff);
+				break;
+			}
+		} else // numH < totalH
+		{
+			// Removing H Atoms
+			int hDiff = totalH - numH;
+			switch (hTransformMode) {
+			case IMPLICIT:
+				if (hc_im >= hDiff) {
+					hc_im = hc_im - hDiff;
+					atom.setImplicitHydrogenCount(hc_im);
+				} else {
+					atom.setImplicitHydrogenCount(0);
+					// Additionally removing explicit H atoms
+					removeExplcitHNeighbours(atom, target, hDiff - hc_im);
+				}
+				break;
+			case EXPLICIT:
+				// TODO
+				break;
+			}
+		}
+	}
+
+	public static void addExplcitHNeighbours(IAtom atom, IAtomContainer target, int numH) {
+		for (int i = 0; i < numH; i++) {
+			IAtom ha = MoleculeTools.newAtom(target.getBuilder());
+			ha.setSymbol("H");
+			target.addAtom(ha);
+			IBond b = MoleculeTools.newBond(target.getBuilder());
 			b.setAtoms(new IAtom[] { atom, ha });
 			b.setOrder(IBond.Order.SINGLE);
 			target.addBond(b);
-    	}
-    }
-    
-    public static int removeExplcitHNeighbours(IAtom atom, IAtomContainer target, int numH)
-    {
-    	List<IBond> boList = target.getConnectedBondsList(atom);
+		}
+	}
+
+	public static int removeExplcitHNeighbours(IAtom atom, IAtomContainer target, int numH) {
+		List<IBond> boList = target.getConnectedBondsList(atom);
 		int nRemovedHAtoms = 0;
-		for (IBond bo: boList)
-		{	
+		for (IBond bo : boList) {
 			if (nRemovedHAtoms >= numH)
 				break;
-			
+
 			IAtom ha = null;
 			if (bo.getAtom(0).getSymbol().equals("H"))
 				ha = bo.getAtom(0);
-			else
-			{	
+			else {
 				if (bo.getAtom(1).getSymbol().equals("H"))
 					ha = bo.getAtom(1);
 				else
 					continue;
 			}
-			
+
 			target.removeBond(bo);
 			target.removeAtom(ha);
 			nRemovedHAtoms++;
 		}
-		
-    	return nRemovedHAtoms;
-    }
-	
-	public static void process(IAtomContainer mol) throws Exception
-	{
+
+		return nRemovedHAtoms;
+	}
+
+	public static void process(IAtomContainer mol) throws Exception {
 		AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(mol);
+	}
+/**
+ * CC1CN(CC(O1)C)C2=NC3=CC=CC=C3N=C2C(C#N)S(=O)(=O)C4=CC=CC(=C4)C
+ * @param args
+ */
+	public static void main(String[] args) {
+		if (args.length > 0)
+			try {
+				try (FileInputStream in = new FileInputStream(new File(args[0]))) {
+					IIteratingChemObjectReader<IAtomContainer> reader = FileInputState.getReader(in, args[0]);
+					int max = -1;
+					try {
+						max = Integer.parseInt(args[1]);
+					} catch (Exception x) {
+					}
+					String oldsmiles = null;
+					SmilesGenerator g = SmilesGenerator.absolute();
+					int r = 0;
+					while (reader.hasNext()) {
+						if (max > 0 && (r > max))
+							break;
+						IAtomContainer mol = reader.next();
+						String smiles = mol.getProperty("SMILES");
+						if (!smiles.equals(oldsmiles)) {
+							oldsmiles = smiles;
+
+							AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(mol);
+							fixSHValence7Atoms(mol);
+							System.out.println(String.format("%s\t%s\t%s", g.create(mol), smiles, mol.getProperty("Original_Entry_ID")));
+							r++;
+						}
+					}
+
+				} catch (Exception x) {
+					x.printStackTrace();
+				}
+			} catch (Exception x) {
+				x.printStackTrace();
+			}
+		else
+			System.err.println("Please provide file name as first argument");
+
 	}
 }
--- ambit2-all/ambit2-tautomers/src/test/java/ambit2/tautomers/test/TestTautomersProcessor.java
+++ ambit2-all/ambit2-tautomers/src/test/java/ambit2/tautomers/test/TestTautomersProcessor.java
@@ -1,16 +1,16 @@
 package ambit2.tautomers.test;
 
-import junit.framework.Assert;
-
 import org.junit.Test;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IBond;
 import org.openscience.cdk.silent.SilentChemObjectBuilder;
+import org.openscience.cdk.smiles.SmilesGenerator;
 import org.openscience.cdk.smiles.SmilesParser;
 import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 
 import ambit2.tautomers.processor.StructureStandardizer;
 import ambit2.tautomers.processor.TautomerProcessor;
+import junit.framework.Assert;
 
 public class TestTautomersProcessor {
 	protected static final TautomerProcessor p = new TautomerProcessor(null);
@@ -18,25 +18,33 @@ public class TestTautomersProcessor {
 
 	@Test
 	public void test() throws Exception {
-		SmilesParser sp = new SmilesParser(
-				SilentChemObjectBuilder.getInstance());
 
-		String[] smiles = new String[] {
-				"Cc1cc2nc(NCCCO)n(CC(=O)c3cc(c(O)c(c3)C(C)(C)C)C(C)(C)C)c2cc1C",
-				"CCc1cc2c(cc1OC(C)=O)oc(C)c(-c1nc3ccccc3n1C)c2=O",
-				"CCCCCCCN(C(=O)CCl)c1cccc(C)c1",
-				"CCc1ccc(OCC(=O)Nc2nnc[nH]2)cc1",
-				"CN(C)Cc1c(nnn1-c1nonc1N)C(=O)NN=Cc1ccccc1OCc1ccc(F)cc1",
+		String[] smiles = new String[] { "Cc1cc2nc(NCCCO)n(CC(=O)c3cc(c(O)c(c3)C(C)(C)C)C(C)(C)C)c2cc1C",
+				"CCc1cc2c(cc1OC(C)=O)oc(C)c(-c1nc3ccccc3n1C)c2=O", "CCCCCCCN(C(=O)CCl)c1cccc(C)c1",
+				"CCc1ccc(OCC(=O)Nc2nnc[nH]2)cc1", "CN(C)Cc1c(nnn1-c1nonc1N)C(=O)NN=Cc1ccccc1OCc1ccc(F)cc1",
 				"CCOC(=O)C(C)Oc1cccc2c1ccn(CC(=O)Nc1ccc(OC)cc1)c2=O" };
-		
+		testSmiles(smiles);
+	}
+
+	@Test
+	public void test7() throws Exception {
+
+		String[] smiles = new String[] { "CC1CN(CC(O1)C)C2=NC3=CC=CC=C3N=C2C(C#N)S(=O)(=O)C4=CC=CC(=C4)C" };
+		testSmiles(smiles);
+	}
+	
+	public void testSmiles(String[] smiles) throws Exception {
+		SmilesParser sp = new SmilesParser(SilentChemObjectBuilder.getInstance());
+
 		st.setGenerateTautomers(true);
 		st.setGenerateInChI(true);
 		st.setGenerateSMILES_Canonical(true);
 		st.setImplicitHydrogens(true);
 		st.setNeutralise(true);
 		st.setSplitFragments(true);
-		
+
 		st.setClearIsotopes(true);
+		SmilesGenerator g = SmilesGenerator.absolute();
 		for (String smi : smiles) {
 			IAtomContainer mol = sp.parseSmiles(smi);
 			AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(mol);
@@ -47,10 +55,13 @@ public class TestTautomersProcessor {
 			 */
 
 			IAtomContainer smol = st.process(mol);
-			Assert.assertFalse(smol.getProperties().get("http://www.opentox.org/api/1.1#InChI").toString().startsWith("InChI=1S/"));
+
 			for (IBond b : smol.bonds()) {
 				Assert.assertNotSame(IBond.Order.UNSET, b.getOrder());
 			}
+			System.out.println(String.format("%s\t-->\t%s", smi, g.create(smol)));
+			Assert.assertFalse(smol.getProperties().get("http://www.opentox.org/api/1.1#InChI").toString()
+					.startsWith("InChI=1S/"));
 		}
 	}
 }
