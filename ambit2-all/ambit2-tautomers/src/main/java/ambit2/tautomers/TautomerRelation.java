d831f69f425b6970291663d1fd5c9737b139c01c
Nikolay Kochev <nick@uni-plovdiv.net>
2017-06-26 20:49:26 +0000

    In class TautomerRelation: added tautomers indices, implemented finction toString(), some refactoring

--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/TautomerRelation.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/TautomerRelation.java
@@ -9,14 +9,38 @@ public class TautomerRelation
 {
 	public IAtomContainer tautomer1 = null;
 	public IAtomContainer tautomer2 = null;
+	public int tautomer1Index = 0;
+	public int tautomer2Index = 0;
 	public List<List<Integer>> instanceAtomIndices = new ArrayList<List<Integer>>();
 	public List<Rule> rules = new ArrayList<Rule>();
-	public List<Integer> ruleStatesTautomer1 = new ArrayList<Integer>();
-	public List<Integer> ruleStatesTautomer2 = new ArrayList<Integer>();
+	public List<Integer> tautomer1RuleStates = new ArrayList<Integer>();
+	public List<Integer> tautomer2RuleStates = new ArrayList<Integer>();
 	
 	public String toString()
 	{
-		//TODO
-		return "";
+		//printing 1-based indices
+		StringBuffer sb = new StringBuffer();
+		sb.append(tautomer1Index);
+		sb.append("   ");
+		sb.append(tautomer2Index);
+		sb.append("   ");
+		for (int i = 0; i< rules.size(); i++)
+		{
+			sb.append("rule: ");
+			sb.append(rules.get(i).name);
+			sb.append(" atoms ");
+			List<Integer> atomIndices = instanceAtomIndices.get(i);
+			for (int k = 0; k < atomIndices.size(); k++)
+			{
+				sb.append(atomIndices.get(k)+1); 
+				sb.append(" ");
+			}
+			sb.append("states ");
+			sb.append(tautomer1RuleStates.get(i)+1);
+			sb.append(" ");
+			sb.append(tautomer2RuleStates.get(i)+1);
+			sb.append(" ");
+		}
+		return sb.toString();
 	}
 }
