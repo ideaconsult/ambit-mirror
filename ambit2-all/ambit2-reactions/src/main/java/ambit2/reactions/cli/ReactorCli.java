776b389522256b7d2deeb5c594d0ca27390b66bc
Nikolay Kochev <nick@uni-plovdiv.net>
2017-06-29 21:15:53 +0000

    Added new cli option 'smiles'. Implemented function runReactor()

--- ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/cli/ReactorCli.java
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/cli/ReactorCli.java
@@ -1,5 +1,7 @@
 package ambit2.reactions.cli;
 
+import java.io.File;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.HelpFormatter;
@@ -7,10 +9,17 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.PosixParser;
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+import ambit2.reactions.ReactionDataBase;
+import ambit2.reactions.reactor.Reactor;
+import ambit2.reactions.reactor.ReactorResult;
+import ambit2.reactions.reactor.ReactorStrategy;
+import ambit2.smarts.SmartsHelper;
 
 public class ReactorCli 
 {
-	private static final String title = "Reactor cli";
+	private static final String title = "Virtual Chemical Reactor";
 	
 	public static void main(String[] args) {
 		ReactorCli app = new ReactorCli();
@@ -54,8 +63,6 @@ public class ReactorCli
 	}
 	
 	
-	
-	
 	protected static Options createOptions() {
     	Options options = new Options();
     	for (_option o: _option.values()) {
@@ -72,7 +79,7 @@ public class ReactorCli
 	}
 	
 	enum _option {
-
+		
 		config {
 			@Override
 			public String getArgName() {
@@ -85,6 +92,21 @@ public class ReactorCli
 			@Override
 			public String getShortName() {
 				return "c";
+			}
+		},
+		
+		smiles {
+			@Override
+			public String getArgName() {
+				return "smiles";
+			}
+			@Override
+			public String getDescription() {
+				return "Input smiles for starting molecule";
+			}
+			@Override
+			public String getShortName() {
+				return "s";
 			}
 		},
 		
@@ -137,15 +159,65 @@ public class ReactorCli
 		case config: {
 			if ((argument == null) || "".equals(argument.trim()))
 				return;
-			
+			reactorConfigFile = argument;
+			break;
+		}
+		case smiles: {
+			if ((argument == null) || "".equals(argument.trim()))
+				return;
+			inputSmiles = argument;
 			break;
 		}
 		}
 	}
 	
-	protected int runReactor()
+	protected int runReactor() throws Exception
 	{
-		//TODO
+		if (inputSmiles == null)
+			throw new Exception("Smiles not assigned! Use -s command line option.");
+		if (reactorConfigFile == null)
+			throw new Exception("Reactor configuratio file not assigned! Use -c command line option.");
+		
+		System.out.println("input smiles: " + inputSmiles);
+		System.out.println("reactor config: " + reactorConfigFile);
+		
+		System.out.println("Setting reactor and reaction database...");
+		Reactor reactor = new Reactor();
+		ReactionDataBase reactDB = new ReactionDataBase(new File(reactorConfigFile));
+		
+		System.out.println("Configuring reaction database...");
+		reactDB.configureReactions(reactor.getSMIRKSManager());
+		reactor.setReactionDataBase(reactDB);
+		
+		System.out.println("Configuring reactor strategy ...");
+		ReactorStrategy strategy = new ReactorStrategy(new File(reactorConfigFile));  //strategy is in the same file
+		
+		strategy.FlagStoreFailedNodes = true;
+		strategy.FlagStoreSuccessNodes = true;
+		strategy.maxNumOfSuccessNodes = 0;  //if 0 then the reactor will stop after the first success node
+		
+		strategy.FlagCheckNodeDuplicationOnPush = true;
+		strategy.FlagTraceReactionPath = true;
+		strategy.FlagLogMainReactionFlow = true;
+		strategy.FlagLogReactionPath = true;
+		strategy.FlagLogNameInReactionPath = false;
+		strategy.FlagLogExplicitHToImplicit = true;
+		
+		
+		reactor.setStrategy(strategy);
+		
+		//Setup Smirks manager
+		reactor.getSMIRKSManager().setFlagProcessResultStructures(true);
+		reactor.getSMIRKSManager().setFlagClearImplicitHAtomsBeforeResultProcess(false);
+		reactor.getSMIRKSManager().setFlagAddImplicitHAtomsOnResultProcess(false);
+		reactor.getSMIRKSManager().setFlagConvertExplicitHToImplicitOnResultProcess(false);
+		
+		IAtomContainer mol = SmartsHelper.getMoleculeFromSmiles(inputSmiles, true);
+		System.out.println();
+		System.out.println("Reactor on target: " + inputSmiles);
+		System.out.println();
+		
+		ReactorResult result = reactor.react(mol);
 		return 0;
 	}
 }
