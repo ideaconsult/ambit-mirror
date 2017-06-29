5a014c2c8606f80d063f647c234107d1d1b6957d
Nikolay Kochev <nick@uni-plovdiv.net>
2017-06-29 20:29:31 +0000

    Implemented basic utilities for handling cli options

--- ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/cli/ReactorCli.java
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/cli/ReactorCli.java
@@ -10,13 +10,141 @@ import org.apache.commons.cli.PosixParser;
 
 public class ReactorCli 
 {
+	private static final String title = "Reactor cli";
+	
 	public static void main(String[] args) {
 		ReactorCli app = new ReactorCli();
     	app.run(args);
-    	
 	}
 	
-	public int run(String[] args) {
+	public String inputSmiles = null;
+	public String reactorConfigFile = null;
+	
+	public int run(String[] args) 
+	{
+		Options options = createOptions();
+		final CommandLineParser parser = new PosixParser();
+		try {
+			CommandLine line = parser.parse( options, args,false );
+			if (line.hasOption(_option.help.name())) {
+				printHelp(options, null);
+				return -1;
+			}
+
+			for (_option o: _option.values()) 
+				if (line.hasOption(o.getShortName())) try {
+					setOption(o,line.getOptionValue(o.getShortName()));
+				} catch (Exception x) {
+					printHelp(options,x.getMessage());
+					return -1;
+				}
+
+			return runReactor();	
+
+		} catch (Exception x ) {
+			printHelp(options,x.getMessage());
+			return -1;
+		} finally {
+			try { 
+				//run whatever cleanup is needed
+			} catch (Exception xx) {
+				printHelp(options,xx.getMessage());
+			}
+		}
+	}
+	
+	
+	
+	
+	protected static Options createOptions() {
+    	Options options = new Options();
+    	for (_option o: _option.values()) {
+    		options.addOption(o.createOption());
+    	}
+    	return options;
+	}
+	
+	protected static void printHelp(Options options,String message) {
+		if (message!=null) System.out.println(message);
+		System.out.println(title);
+	    HelpFormatter formatter = new HelpFormatter();
+	    formatter.printHelp( ReactorCli.class.getName(), options );
+	}
+	
+	enum _option {
+
+		config {
+			@Override
+			public String getArgName() {
+				return "config";
+			}
+			@Override
+			public String getDescription() {
+				return "Reactor configuration (.json) file";
+			}
+			@Override
+			public String getShortName() {
+				return "c";
+			}
+		},
+		
+		help {
+			@Override
+			public String getArgName() {
+				return null;
+			}
+			@Override
+			public String getDescription() {
+				return title;
+			}
+			@Override
+			public String getShortName() {
+				return "h";
+			}
+			@Override
+			public String getDefaultValue() {
+				return null;
+			}
+			public Option createOption() {
+		    	Option option   = OptionBuilder.withLongOpt(name())
+		        .withDescription(getDescription())
+		        .create(getShortName());
+		    	return option;
+			}
+		};		
+		
+		public abstract String getArgName();
+		public abstract String getDescription();
+		public abstract String getShortName();
+		public String getDefaultValue() { return null; }
+			
+		public Option createOption() {
+			String defaultValue = getDefaultValue();
+	    	Option option   = OptionBuilder.withLongOpt(name())
+	        .hasArg()
+	        .withArgName(getArgName())
+	        .withDescription(String.format("%s %s %s",getDescription(),defaultValue==null?"":"Default value: ",defaultValue==null?"":defaultValue))
+	        .create(getShortName());
+
+	    	return option;
+		}
+	}
+	
+	public void setOption(_option option, String argument) throws Exception {
+		if (argument != null)
+			argument = argument.trim();
+		switch (option) {
+		case config: {
+			if ((argument == null) || "".equals(argument.trim()))
+				return;
+			
+			break;
+		}
+		}
+	}
+	
+	protected int runReactor()
+	{
 		//TODO
 		return 0;
 	}
