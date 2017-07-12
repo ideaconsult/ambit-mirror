1a9c3dbdabd1b2336fdc894235e376b1e396812d
Nikolay Kochev <nick@uni-plovdiv.net>
2017-07-12 22:09:51 +0000

    Added new class CDKDescriptorSolver

---
+++ ambit2-all/ambit2-rules/src/main/java/ambit2/rules/conditions/solvers/CDKDescriptorSolver.java
@@ -0,0 +1,14 @@
+package ambit2.rules.conditions.solvers;
+
+import ambit2.rules.conditions.AbstractDescriptorSolver;
+
+public class CDKDescriptorSolver extends AbstractDescriptorSolver
+{
+
+	@Override
+	public Object calculateDescriptor(String descrName, Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
