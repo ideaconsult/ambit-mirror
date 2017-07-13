02aceb1c38422f86d3696c086e6c82f20707007f
Nikolay Kochev <nick@uni-plovdiv.net>
2017-07-13 11:47:07 +0000

    Defined basic functionality for GroupInfo

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/groups/GroupInfo.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/groups/GroupInfo.java
@@ -2,5 +2,14 @@ package ambit2.groupcontribution.groups;
 
 public class GroupInfo 
 {
+	public String description = null;
 	
+	// fragmentation scheme is practically described in GroupContributionModel
+	
+	// fragmentation instance (particular descriptor values)
+	
+	public String getDesignation()
+	{
+		return null;
+	}
 }
