883771779ba2544a2e1ee5fed8d6451d0c78b8a1
Nikolay Kochev <nick@uni-plovdiv.net>
2017-07-13 12:22:48 +0000

    Added new package fragmentation

---
+++ ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/fragmentation/Fragmentation.java
@@ -0,0 +1,12 @@
+package ambit2.groupcontribution.fragmentation;
+
+import ambit2.groupcontribution.GroupContributionModel;
+import ambit2.groupcontribution.dataset.DataSet;
+
+public class Fragmentation 
+{
+	public void makeFragmentation(DataSet dataset, GroupContributionModel gcm)
+	{
+		//TODO
+	}
+}
