b8043b2b85939537999f54e2b5c96727616e41a8
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2017-07-31 08:56:26 +0000

    page for data availability /query/data_availability

--- ambit2-all/ambit2-db/src/main/java/ambit2/db/substance/QueryCountProtocolApplications.java
+++ ambit2-all/ambit2-db/src/main/java/ambit2/db/substance/QueryCountProtocolApplications.java
@@ -5,6 +5,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import ambit2.base.data.I5Utils;
+import ambit2.base.data.SubstanceRecord;
+import ambit2.base.data.study.Protocol;
+import ambit2.base.data.study.ProtocolApplication;
 import ambit2.base.data.substance.SubstanceEndpointsBundle;
 import ambit2.db.substance.study.facet.ProtocolApplicationSummaryFacet;
 import ambit2.db.substance.study.facet.SubstanceByCategoryFacet;
@@ -13,8 +17,7 @@ import ambit2.db.update.dataset.QueryCount;
 import net.idea.modbcum.i.exceptions.AmbitException;
 import net.idea.modbcum.i.query.QueryParam;
 
-public class QueryCountProtocolApplications<F extends SubstanceByCategoryFacet>
-		extends QueryCount<F> {
+public class QueryCountProtocolApplications<F extends SubstanceByCategoryFacet> extends QueryCount<F> {
 
 	/**
 	 * 
@@ -22,7 +25,7 @@ public class QueryCountProtocolApplications<F extends SubstanceByCategoryFacet>
 	private static final long serialVersionUID = -6001454887114379950L;
 
 	public enum _mode_related {
-		endpoints, substances, detail,data_availability
+		endpoints, substances, detail, data_availability
 	}
 
 	public _mode_related mode = _mode_related.endpoints;
@@ -46,15 +49,16 @@ public class QueryCountProtocolApplications<F extends SubstanceByCategoryFacet>
 	}
 
 	public QueryCountProtocolApplications(String facetURL) {
-		this(facetURL,_mode_related.endpoints);
+		this(facetURL, _mode_related.endpoints);
 	}
 
-	public QueryCountProtocolApplications(String facetURL,_mode_related mode) {
+	public QueryCountProtocolApplications(String facetURL, _mode_related mode) {
 		super(facetURL);
 		setPageSize(Integer.MAX_VALUE);
 		setMode(mode);
 		facet = createFacet(facetURL);
 	}
+
 	/**
 	 * 
 	 */
@@ -65,11 +69,12 @@ public class QueryCountProtocolApplications<F extends SubstanceByCategoryFacet>
 
 	private static final String sql_bundle_byendpoints = "SELECT p.topcategory,count(*),p.endpointcategory,-1  FROM substance_protocolapplication p join bundle_endpoints b where b.topcategory=p.topcategory and b.endpointcategory=p.endpointcategory and idbundle=? %s group by p.topcategory,p.endpointcategory";
 
-	private static final String sql_bundle_bysubstances =			
-			"select p.topcategory,count(*),p.endpointcategory,-1 from substance_protocolapplication p join bundle_substance using(substance_prefix,substance_uuid)\n"+
-			"where %s idbundle=? group by p.topcategory,p.endpointcategory";
-	
-	private static final String sql_dataavailability = "SELECT topcategory,count(*),endpointcategory,-1,substance_prefix,hex(substance_uuid) as uuid FROM substance_protocolapplication group by substance_prefix,substance_uuid,topcategory,endpointcategory order by substance_prefix,substance_uuid,topcategory,endpointcategory";
+	private static final String sql_bundle_bysubstances = "select p.topcategory,count(*),p.endpointcategory,-1 from substance_protocolapplication p join bundle_substance using(substance_prefix,substance_uuid)\n"
+			+ "where %s idbundle=? group by p.topcategory,p.endpointcategory";
+
+	private static final String sql_dataavailability = "select topcategory,c,endpointcategory,-1,prefix as substance_prefix,hex(uuid) as uuid,protocol,provider,reference,name,publicname,owner_name,substancetype from substance join ("
+			+ "SELECT topcategory,count(*) c,endpointcategory,-1,substance_prefix as prefix,substance_uuid as uuid,group_concat(distinct(guidance)) as protocol,group_concat(distinct(reference_owner)) provider ,group_concat(distinct(reference)) as reference FROM substance_protocolapplication group by substance_prefix,substance_uuid,topcategory,endpointcategory "
+			+ "order by substance_prefix,substance_uuid,topcategory,endpointcategory " + ") a using (prefix,uuid)";
 	/*
 	 * select p.topcategory,p.endpointcategory,count(*) from
 	 * substance_protocolapplication p, bundle_endpoints b where
@@ -117,8 +122,7 @@ public class QueryCountProtocolApplications<F extends SubstanceByCategoryFacet>
 				throw new AmbitException("bundle not defined");
 		}
 		default: {
-			String d = ((bundle != null) && (bundle.getID() > 0)) ? "\nand "
-					: "\nwhere ";
+			String d = ((bundle != null) && (bundle.getID() > 0)) ? "\nand " : "\nwhere ";
 			if (getFieldname() != null) {
 				b.append(d);
 				b.append(w_topcategory);
@@ -129,9 +133,8 @@ public class QueryCountProtocolApplications<F extends SubstanceByCategoryFacet>
 				b.append(w_endpointcategory);
 				d = " and ";
 			}
-			return String
-					.format(((bundle != null) && (bundle.getID() > 0)) ? sql_bundle_byendpoints
-							: sql, b.toString());
+			return String.format(((bundle != null) && (bundle.getID() > 0)) ? sql_bundle_byendpoints : sql,
+					b.toString());
 		}
 		}
 
@@ -147,9 +150,10 @@ public class QueryCountProtocolApplications<F extends SubstanceByCategoryFacet>
 
 	@Override
 	protected F createFacet(String facetURL) {
-		if (mode==null) return  (F) new SubstanceByCategoryFacet(facetURL);
+		if (mode == null)
+			return (F) new SubstanceByCategoryFacet(facetURL);
 		F facet;
-	
+
 		switch (mode) {
 		case data_availability: {
 			facet = (F) new SubstanceDataAvailabilityFacet(facetURL);
@@ -174,28 +178,22 @@ public class QueryCountProtocolApplications<F extends SubstanceByCategoryFacet>
 		switch (mode) {
 		case substances: {
 			if (getFieldname() != null)
-				params.add(new QueryParam<String>(String.class, getFieldname()
-						.toString()));
+				params.add(new QueryParam<String>(String.class, getFieldname().toString()));
 			if (getValue() != null)
-				params.add(new QueryParam<String>(String.class, getValue()
-						.toString()));
+				params.add(new QueryParam<String>(String.class, getValue().toString()));
 			if ((bundle != null) && (bundle.getID() > 0))
-				params.add(new QueryParam<Integer>(Integer.class, getBundle()
-						.getID()));
+				params.add(new QueryParam<Integer>(Integer.class, getBundle().getID()));
 			else
 				throw new AmbitException("bundle not defined");
 			return params;
 		}
 		default: {
 			if ((bundle != null) && (bundle.getID() > 0))
-				params.add(new QueryParam<Integer>(Integer.class, getBundle()
-						.getID()));
+				params.add(new QueryParam<Integer>(Integer.class, getBundle().getID()));
 			if (getFieldname() != null)
-				params.add(new QueryParam<String>(String.class, getFieldname()
-						.toString()));
+				params.add(new QueryParam<String>(String.class, getFieldname().toString()));
 			if (getValue() != null)
-				params.add(new QueryParam<String>(String.class, getValue()
-						.toString()));
+				params.add(new QueryParam<String>(String.class, getValue().toString()));
 			return params;
 		}
 		}
@@ -213,7 +211,24 @@ public class QueryCountProtocolApplications<F extends SubstanceByCategoryFacet>
 			case data_availability: {
 				try {
 					SubstanceDataAvailabilityFacet ps = (SubstanceDataAvailabilityFacet) facet;
-					ps.setSubstanceID(rs.getString("substance_prefix"),rs.getString("uuid"));
+					SubstanceRecord record = new SubstanceRecord();
+
+					record.setSubstanceUUID(
+							I5Utils.getPrefixedUUID(rs.getString("substance_prefix"), rs.getString("uuid")));
+					record.setSubstanceName(rs.getString("name"));
+					record.setPublicName(rs.getString("publicname"));
+					record.setSubstancetype(rs.getString("substancetype"));
+					record.setOwnerName(rs.getString("owner_name"));
+					Protocol p = new Protocol(null,rs.getString("protocol"));
+					p.setTopCategory(rs.getString("topcategory"));
+					p.setCategory(rs.getString("endpointcategory"));
+					ProtocolApplication papp = new ProtocolApplication<Protocol, Object, String, Object, String>(p);
+					papp.setReference(rs.getString("reference"));
+					papp.setReferenceOwner(rs.getString("provider"));
+					List<ProtocolApplication> papps = new ArrayList<ProtocolApplication>();
+					papps.add(papp);
+					record.setMeasurements(papps);
+					ps.setRecord(record);
 				} catch (Exception x) {
 					x.printStackTrace();
 				}
