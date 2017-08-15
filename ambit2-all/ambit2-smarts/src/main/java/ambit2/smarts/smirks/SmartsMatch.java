3e33a7686a9d3a0e19c1a810a09f05d56391027a
Nikolay Kochev <nick@uni-plovdiv.net>
2017-08-15 17:43:14 +0000

    Added functions for matching the basic IQueryAtom classes

--- ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/smirks/SmartsMatch.java
+++ ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/smirks/SmartsMatch.java
@@ -1,23 +1,31 @@
 package ambit2.smarts.smirks;
 
+import org.openscience.cdk.CDKConstants;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IBond;
 import org.openscience.cdk.isomorphism.matchers.IQueryAtom;
 import org.openscience.cdk.isomorphism.matchers.IQueryBond;
+import org.openscience.cdk.isomorphism.matchers.smarts.AliphaticAtom;
+import org.openscience.cdk.isomorphism.matchers.smarts.AnyAtom;
+import org.openscience.cdk.isomorphism.matchers.smarts.AromaticAtom;
 import org.openscience.cdk.isomorphism.matchers.smarts.SMARTSAtom;
 import org.openscience.cdk.isomorphism.matchers.smarts.SMARTSBond;
+
+import ambit2.smarts.AliphaticSymbolQueryAtom;
+import ambit2.smarts.AromaticSymbolQueryAtom;
+import ambit2.smarts.SmartsAtomExpression;
 
 public class SmartsMatch 
 {
 	public static enum MatchMode {
-		GENERIC, EXACT
+		SPECIFIC_MATCHES_GENERIC, GENERIC_MATCHES_SPECIFIC, EXACT
 	}
 	
 	public static enum AttributeComparison {
 		NO_COMPARISON, EXACT, EXACT_WHEN_DEFINED, LESS_THAN, GREAT_THAN
 	}
 	
-	public MatchMode mode = MatchMode.GENERIC;
+	public MatchMode mode = MatchMode.EXACT;	
 	public AttributeComparison compareAtomSymbol = AttributeComparison.EXACT_WHEN_DEFINED;
 	public AttributeComparison compareAromaticity = AttributeComparison.EXACT_WHEN_DEFINED;
 	public AttributeComparison compareIsotope = AttributeComparison.EXACT_WHEN_DEFINED;
@@ -27,13 +35,66 @@ public class SmartsMatch
 	
 	public boolean match(IQueryAtom qa, IAtom ta)
 	{
-		//TODO
-		return true;
+		if (qa instanceof AliphaticSymbolQueryAtom)
+			return matchAliphaticSymbolQueryAtom((AliphaticSymbolQueryAtom)qa, ta);
+		
+		if (qa instanceof AromaticSymbolQueryAtom)
+			return matchAromaticSymbolQueryAtom((AromaticSymbolQueryAtom)qa, ta);
+		
+		if (qa instanceof SmartsAtomExpression)
+			return matchSmartsAtomExpression((SmartsAtomExpression)qa, ta);
+		
+		if (qa instanceof AliphaticAtom)
+			return matchAliphaticAtom((AliphaticAtom)qa, ta);
+		
+		if (qa instanceof AromaticAtom)
+			return matchAromaticAtom((AromaticAtom)qa, ta);
+		
+		if (qa instanceof AnyAtom)
+			return matchAnyAtom((AnyAtom)qa, ta);
+		
+		return false;
 	}
 	
 	public boolean match(IQueryBond qb, IBond tb)
 	{
 		//TODO
 		return true;
+	}
+	
+	boolean matchAliphaticSymbolQueryAtom (AliphaticSymbolQueryAtom qa, IAtom ta)
+	{
+		//TODO
+		return false;
+	}
+	
+	boolean matchAromaticSymbolQueryAtom (AromaticSymbolQueryAtom qa, IAtom ta)
+	{
+		//TODO
+		return false;
+	}
+	
+	boolean matchSmartsAtomExpression (SmartsAtomExpression qa, IAtom ta)
+	{
+		//TODO
+		return false;
+	}
+	
+	boolean matchAliphaticAtom (AliphaticAtom qa, IAtom ta)
+	{
+		//TODO
+		return false;
+	}
+	
+	boolean matchAromaticAtom (AromaticAtom qa, IAtom ta)
+	{
+		//TODO
+		return false;
+	}
+	
+	boolean matchAnyAtom (AnyAtom qa, IAtom ta)
+	{
+		//TODO
+		return false;
 	}
 }
