57dff7c1afafae9b76e50201e1847c63177967b9
Nikolay Kochev <nick@uni-plovdiv.net>
2017-08-17 15:36:18 +0000

    Added basic functions for reaction searching (with TODOs)

--- ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/smirks/ReactionSearch.java
+++ ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/smirks/ReactionSearch.java
@@ -3,15 +3,19 @@ package ambit2.smarts.smirks;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.isomorphism.matchers.IQueryAtomContainer;
+import org.openscience.cdk.silent.SilentChemObjectBuilder;
 
 import ambit2.smarts.IsomorphismTester;
 import ambit2.smarts.SMIRKSReaction;
+import ambit2.smarts.SMIRKSManager;
 import ambit2.smarts.SmartsParser;
 
 public class ReactionSearch 
 {
 	SmartsParser parser = new SmartsParser();
+	SMIRKSManager smirksMan = new SMIRKSManager(SilentChemObjectBuilder.getInstance());
 	SmartsIsomorphismTester smartsIsoTester = new SmartsIsomorphismTester();
 	IsomorphismTester isoTester = new IsomorphismTester();
 	
@@ -23,7 +27,54 @@ public class ReactionSearch
 	SMIRKSReaction queryReaction = null;
 	
 	
-	boolean matchReactant()
+	public void reset()
+	{
+		errors.clear();
+	}
+	
+	public void setQuerySmarts(String querySmarts) {
+		this.querySmarts = querySmarts;
+		query = parser.parse(querySmarts);
+		parser.setNeededDataFlags();
+		String errorMsg = parser.getErrorMessages();
+		if (!errorMsg.equals("")) {
+			errors.add(errorMsg);
+			query = null;
+			return;
+		}
+
+		if (parser.hasRecursiveSmarts) {
+			//getRecursiveAtoms();			
+		}
+	}
+	
+	
+	public void setQuerySmirks(String querySmirks) {
+		this.querySmirks = querySmirks;
+		
+		//TODO
+	}
+	
+	boolean matchComponent(IAtomContainer comp)
+	{
+		//TODO
+		return false;
+	}
+	
+	
+	boolean matchReaction(String targetSmirks)
+	{
+		//TODO
+		return false;
+	}
+	
+	boolean matchReaction(SMIRKSReaction targetReaction)
+	{
+		//TODO
+		return false;
+	}
+	
+	boolean matchReaction(List<IAtomContainer> reactants, List<IAtomContainer> product)
 	{
 		//TODO
 		return false;
