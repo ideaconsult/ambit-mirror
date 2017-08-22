a04950930717e9fa68c25089b644eae99cefbeac
Nikolay Kochev <nick@uni-plovdiv.net>
2017-08-22 18:24:39 +0000

    Added properties and implemented function getEndpintValue()

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/dataset/DataSetObject.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/dataset/DataSetObject.java
@@ -1,10 +1,33 @@
 package ambit2.groupcontribution.dataset;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openscience.cdk.interfaces.IAtomContainer;
 
 public class DataSetObject 
 {
 	public IAtomContainer molecule = null;
-	public double endpointValue = 0.0;
+	public Map<String, Object> properties = new HashMap<String, Object>();
+	public String endpointProperty = "endpoint";
+	
+	
+	public Double getEndpintValue()
+	{
+		Object o = properties.get(endpointProperty);
+		if (o != null)
+		{	
+			if (o instanceof Double)
+				return (Double) o;
+			
+			if (o instanceof Integer)
+				return ((Integer)o).doubleValue();
+			
+			if (o instanceof Boolean)
+				return ((Boolean)o) ? 1.0 : 0.0;
+		}	
+		
+		return null;
+	}
 	
 }
