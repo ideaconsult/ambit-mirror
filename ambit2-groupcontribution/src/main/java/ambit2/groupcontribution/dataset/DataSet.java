18e1d7326a550f842a7372cfa53d722ee833282f
Nikolay Kochev <nick@uni-plovdiv.net>
2017-08-22 12:49:29 +0000

    Added basic functions in DataSet. Added new class DataSetObject.

--- ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/dataset/DataSet.java
+++ ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/dataset/DataSet.java
@@ -1,6 +1,27 @@
 package ambit2.groupcontribution.dataset;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataSet 
 {
-
+	public List<DataSetObject> dataObjects = new ArrayList<DataSetObject>();
+	File datafile = null;
+	
+	
+	public DataSet(File f) throws Exception
+	{
+		datafile = f;
+		loadDataFromFile(datafile);
+		
+	}
+	
+	public void loadDataFromFile(File file) throws Exception
+	{
+		//TODO use basic Ambit io utils
+	}
+	
+	
+	
 }
---
+++ ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/dataset/DataSetObject.java
@@ -0,0 +1,10 @@
+package ambit2.groupcontribution.dataset;
+
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+public class DataSetObject 
+{
+	public IAtomContainer molecule = null;
+	public double endpointValue = 0.0;
+	
+}
