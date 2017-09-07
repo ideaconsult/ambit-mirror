5deba31371eeaaab0204ec492260c10c3c4bfabd
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2017-09-07 15:08:39 +0000

    set model name to dc:creator

--- ambit2-dragon/src/test/java/ambit2/dragon/test/VegaShellTest.java
+++ ambit2-dragon/src/test/java/ambit2/dragon/test/VegaShellTest.java
@@ -1,9 +1,12 @@
 package ambit2.dragon.test;
 
+import java.util.Map.Entry;
+
 import org.junit.Test;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.io.iterator.IIteratingChemObjectReader;
 
+import ambit2.base.data.Property;
 import ambit2.core.io.FileInputState;
 import ambit2.vega.VegaShell;
 import junit.framework.Assert;
@@ -14,7 +17,7 @@ public class VegaShellTest {
 	public void testVegaShell() throws Exception {
 		//VegaShell shell = new VegaShell("ambit2/dragon/test/test.pref");
 		VegaShell shell = new VegaShell(null);
-
+		
 		try (IIteratingChemObjectReader reader = FileInputState.getReader(
 				getClass().getClassLoader().getResourceAsStream("ambit2/dragon/test/structure.sdf"), "structure.sdf")) {
 
@@ -24,7 +27,12 @@ public class VegaShellTest {
 				IAtomContainer mol = (IAtomContainer) o;
 				mol = shell.process(mol);
 				
-				System.out.println(mol.getProperties());
+				for (Entry<Object,Object> e : mol.getProperties().entrySet()) {
+					Property p = (Property) e.getKey();
+					System.out.println(String.format("%s\n%s\n%s\n%s", p.getName(),p.getLabel(),p.getTitle(),p.getUrl(),p.getUnits()));
+					System.out.println(e.getValue());
+					System.out.println();
+				}
 				Assert.assertTrue(mol.getProperties().size() > 0);
 			}
 		}
