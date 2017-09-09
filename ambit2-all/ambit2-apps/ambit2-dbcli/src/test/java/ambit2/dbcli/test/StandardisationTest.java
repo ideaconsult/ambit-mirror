b579fe839ea89bd005dbf2bfee8bd1a44732ec22
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2017-09-09 06:52:34 +0000

    ambitcli fingerprint and simmatrix tests

--- ambit2-all/ambit2-apps/ambit2-dbcli/src/test/java/ambit2/dbcli/test/StandardisationTest.java
+++ ambit2-all/ambit2-apps/ambit2-dbcli/src/test/java/ambit2/dbcli/test/StandardisationTest.java
@@ -3,24 +3,21 @@ package ambit2.dbcli.test;
 import java.io.File;
 import java.net.URL;
 
-import junit.framework.Assert;
-
 import org.junit.Test;
 
 import ambit2.dbcli.AmbitCli;
 import ambit2.dbcli.CliOptions;
+import junit.framework.Assert;
 
 public class StandardisationTest {
 
 	@Test
 	public void test() throws Exception {
-		URL in = getClass().getClassLoader().getResource(
-				"ambit2/db/processors/txt/test.txt");
+		URL in = getClass().getClassLoader().getResource("ambit2/db/processors/txt/test.txt");
 		Assert.assertNotNull(in);
-		String out = String.format("%s/output.txt",
-				System.getProperty("java.io.tmpdir"));
-		String[] args = new String[] { "-a", "standardize", "-i", in.getFile(),
-				"-m", "post", "-o", out ,"-d","smiles=true","-d","inchi=false", "-d","tautomers=true"};
+		String out = String.format("%s/output.txt", System.getProperty("java.io.tmpdir"));
+		String[] args = new String[] { "-a", "standardize", "-i", in.getFile(), "-m", "post", "-o", out, "-d",
+				"smiles=true", "-d", "inchi=false", "-d", "tautomers=true" };
 		CliOptions options = new CliOptions();
 		if (options.parse(args))
 			try {
@@ -30,23 +27,55 @@ public class StandardisationTest {
 				(new File(out)).delete();
 			}
 	}
-	
+
 	@Test
 	public void fptest() throws Exception {
-		URL in = getClass().getClassLoader().getResource(
-				"ambit2/dbcli/test/testfp.txt");
+		URL in = getClass().getClassLoader().getResource("ambit2/dbcli/test/testfp.txt");
 		Assert.assertNotNull(in);
-		String out = String.format("%s/output",
-				System.getProperty("java.io.tmpdir"));
-		String[] args = new String[] { "-a", "fingerprint", "-i", in.getFile(),
-				"-m", "post", "-o", out ,"-d","fpclass=CircularFingerprinter","-d","inputtag_smiles=AMBIT_SMILES"};
+		String out = String.format("%s/output", System.getProperty("java.io.tmpdir"));
+		String[] args = new String[] { "-a", "fingerprint", "-i", in.getFile(), "-m", "post", "-o", out, "-d",
+				"fpclass=CircularFingerprinter", "-d", "inputtag_smiles=AMBIT_SMILES" };
 		CliOptions options = new CliOptions();
 		if (options.parse(args))
 			try {
 				AmbitCli cli = new AmbitCli(options);
 				cli.go(options.getCmd(), options.getSubcommand().name());
 			} finally {
-				(new File(out)).delete();
+				// (new File(out)).delete();
+			}
+	}
+
+	@Test
+	public void simtest() throws Exception {
+		URL in = getClass().getClassLoader().getResource("ambit2/dbcli/test/mccp10.txt");
+		Assert.assertNotNull(in);
+		String out = String.format("%s/output", System.getProperty("java.io.tmpdir"));
+		String[] args = new String[] { "-a", "fingerprint", "-i", in.getFile(), "-m", "post", "-o", out, "-d",
+				"fpclass=CircularFingerprinter", "-d", "inputtag_smiles=SMILES", "-d", "tag_tokeep=AMBIT_InChIKey" };
+		CliOptions options = new CliOptions();
+		if (options.parse(args))
+			try {
+				AmbitCli cli = new AmbitCli(options);
+				cli.go(options.getCmd(), options.getSubcommand().name());
+				simmatrix(new File(out + "org.openscience.cdk.fingerprint.CircularFingerprinter.hashed.csv"));
+			} finally {
+				// (new File(out)).delete();
+			}
+	}
+
+	public void simmatrix(File input) throws Exception {
+
+		String out = String.format("%s/simmatrix", System.getProperty("java.io.tmpdir"));
+		String[] args = new String[] { "-a", "simmatrix", "-i", input.getAbsolutePath(), "-m", "post", "-o", out, "-d",
+				"threshold=0.1", "-d", "sparse=TRUE" };
+		CliOptions options = new CliOptions();
+		if (options.parse(args))
+			try {
+				AmbitCli cli = new AmbitCli(options);
+				cli.go(options.getCmd(), options.getSubcommand().name());
+
+			} finally {
+				// (new File(out)).delete();
 			}
 	}
 }
