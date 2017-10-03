f8119f0e12e676ca8262007f1c25bbd893c58d95
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2017-10-03 16:10:36 +0000

    Move tautomers before implicit h in standardization pipeline add test

--- ambit2-all/ambit2-apps/ambit2-dbcli/src/test/java/ambit2/dbcli/test/StandardisationTest.java
+++ ambit2-all/ambit2-apps/ambit2-dbcli/src/test/java/ambit2/dbcli/test/StandardisationTest.java
@@ -12,6 +12,50 @@ import junit.framework.Assert;
 public class StandardisationTest {
 
 	@Test
+	public void test9815161() throws Exception {
+		URL in = getClass().getClassLoader().getResource("ambit2/dbcli/test/pubchem9815161.sdf");
+		Assert.assertNotNull(in);
+		String out = String.format("%s/output_sdf.txt", System.getProperty("java.io.tmpdir"));
+		System.out.println(out);
+		String[] args = new String[] { "-a", "standardize", "-i", in.getFile(), "-m", "post", "-o", out, "-d",
+				"smiles=true", "-d", "inchi=false", "-d", "tautomers=true", "-d","tag_tokeep=cdk:Title" };
+		CliOptions options = new CliOptions();
+		if (options.parse(args))
+			try {
+				AmbitCli cli = new AmbitCli(options);
+				cli.go(options.getCmd(), options.getSubcommand().name());
+			} finally {
+				(new File(out)).delete();
+			}
+
+	
+	@Test
+	public void test9815161_smi() throws Exception {
+		URL in = getClass().getClassLoader().getResource("ambit2/dbcli/test/pubchem9815161.txt");
+		Assert.assertNotNull(in);
+		String out = String.format("%s/output_smi.txt", System.getProperty("java.io.tmpdir"));
+		System.out.println(out);
+		String[] args = new String[] { "-a", "standardize", "-i", in.getFile(), "-m", "post", "-o", out, 
+				"-d","splitfragments=true",
+				"-d","smiles=true",
+				"-d","smilescanonical=false",
+				"-d","implicith=true", 
+				"-d", "inchi=true", 
+				"-d", "tautomers=true", 
+				"-d","tag_tokeep=cdk:Title",
+				"-d","neutralise=true" 
+				};
+		CliOptions options = new CliOptions();
+		if (options.parse(args))
+			try {
+				AmbitCli cli = new AmbitCli(options);
+				cli.go(options.getCmd(), options.getSubcommand().name());
+			} finally {
+				(new File(out)).delete();
+			}
+	}
+	
+	@Test
 	public void test() throws Exception {
 		URL in = getClass().getClassLoader().getResource("ambit2/db/processors/txt/test.txt");
 		Assert.assertNotNull(in);
