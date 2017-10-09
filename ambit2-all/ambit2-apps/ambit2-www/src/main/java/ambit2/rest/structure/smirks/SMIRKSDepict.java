32a4eb4984619b58847d97ee65f77b4ec8e94080
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2017-10-09 11:26:08 +0000

    Resolves broken demo/reaction page

--- ambit2-all/ambit2-apps/ambit2-www/src/main/java/ambit2/rest/structure/smirks/SMIRKSDepict.java
+++ ambit2-all/ambit2-apps/ambit2-www/src/main/java/ambit2/rest/structure/smirks/SMIRKSDepict.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.silent.SilentChemObjectBuilder;
+import org.openscience.cdk.smiles.SmilesGenerator;
 import org.openscience.cdk.smiles.SmilesParser;
 import org.openscience.cdk.tools.CDKHydrogenAdder;
 import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
@@ -26,28 +27,32 @@ import ambit2.smarts.SmartsConst;
 public class SMIRKSDepict extends CDKDepictVariants implements IAcceptable {
 	public static final String resource = "/reaction/product";
 	protected ArrayList<String> list = new ArrayList<String>();
+
 	@Override
-	protected BufferedImage getImage(String smiles, int w, int h,
-			String recordType, QueryType type) throws ResourceException {
+	protected BufferedImage getImage(String smiles, int w, int h, String recordType, QueryType type)
+			throws ResourceException {
 		try {
-			depict.setImageSize(new Dimension(w,h));
+			String smirks = getSmirks();
+			if (smirks==null || "".equals(smirks)) throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST);
+			depict.setImageSize(new Dimension(w, h));
 
 			SMIRKSManager smrkMan = new SMIRKSManager(SilentChemObjectBuilder.getInstance());
+			smrkMan.setFlagProcessResultStructures(true);
 			smrkMan.setSSMode(SmartsConst.SSM_MODE.SSM_NON_IDENTICAL_FIRST);
-			
-			SMIRKSReaction smr = smrkMan.parse(getSmirks());
+
+			SMIRKSReaction smr = smrkMan.parse(smirks);
 			SmilesParser parser = new SmilesParser(SilentChemObjectBuilder.getInstance());
 			IAtomContainer reactant = parser.parseSmiles(smiles);
-			reactant = AtomContainerManipulator.removeHydrogens(reactant);	
+			reactant = AtomContainerManipulator.removeHydrogens(reactant);
 			AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(reactant);
 			CDKHydrogenAdder adder = CDKHydrogenAdder.getInstance(SilentChemObjectBuilder.getInstance());
-			CDKHueckelAromaticityDetector.detectAromaticity(reactant); 
+			CDKHueckelAromaticityDetector.detectAromaticity(reactant);
 			adder.addImplicitHydrogens(reactant);
 			HydrogenAdderProcessor.convertImplicitToExplicitHydrogens(reactant);
 			BufferedImage image = null;
-			if (smrkMan.applyTransformation(reactant, this, smr))
-			    image = depict.getImage(reactant,null,true,false);
-			else {
+			if (smrkMan.applyTransformation(reactant, this, smr)) {
+				image = depict.getImage(reactant, null, true, false);
+			} else {
 				list.add("Not applicable");
 				image = depict.getImage(list);
 			}
@@ -55,9 +60,10 @@ public class SMIRKSDepict extends CDKDepictVariants implements IAcceptable {
 			smrkMan = null;
 			return image;
 
-		} catch (ResourceException x) {throw x; 
-		} catch (Exception x) { 
-			throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST,x.getMessage(),x); 
+		} catch (ResourceException x) {
+			throw x;
+		} catch (Exception x) {
+			throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, x.getMessage(), x);
 		}
 	}
 
