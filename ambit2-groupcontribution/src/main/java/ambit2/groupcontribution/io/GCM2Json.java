5a1b3c53518540e6d4eb29be21d5e0a1c2e6c1a6
Nikolay Kochev <nick@uni-plovdiv.net>
2017-10-13 13:15:06 +0000

    Implemented parsing of MODEL_NAME and MODEL_TYPE

--- ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/io/GCM2Json.java
+++ ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/io/GCM2Json.java
@@ -2,12 +2,14 @@ package ambit2.groupcontribution.io;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import ambit2.groupcontribution.GroupContributionModel;
+import ambit2.groupcontribution.GroupContributionModel.Type;
 import ambit2.rules.json.JSONParsingUtils;
 
 
@@ -15,7 +17,11 @@ public class GCM2Json
 {
 	private final static Logger logger = Logger.getLogger(GCM2Json.class.getName());
 	
-	public GroupContributionModel loadFromJSON(File jsonConfig) throws Exception {
+	public ArrayList<String> configErrors = new ArrayList<String>();
+	public ArrayList<String> configWarnings = new ArrayList<String>();
+	
+	public GroupContributionModel loadFromJSON(File jsonConfig) throws Exception 
+	{
 		FileInputStream fin = new FileInputStream(jsonConfig);
 		ObjectMapper mapper = new ObjectMapper();
 		JsonNode root = null;
@@ -31,10 +37,38 @@ public class GCM2Json
 			}
 		}
 
-		JSONParsingUtils jsonUtils = new JSONParsingUtils();
-		//ExcelParserConfigurator conf = new ExcelParserConfigurator();
+		//JSONParsingUtils jsonUtils = new JSONParsingUtils();
+		GroupContributionModel gcm = new GroupContributionModel();
 		
-		return null;
+		JsonNode curNode = root.path("MODEL_NAME");
+		if (!curNode.isMissingNode())
+		{	
+			if (curNode.isTextual())
+				gcm.setModelName(curNode.asText());
+			else
+				configErrors.add("MODEL_NAME is not textual!");
+		}	
+		
+		curNode = root.path("MODEL_TYPE");
+		if (!curNode.isMissingNode())
+		{	
+			if (curNode.isTextual())
+			{
+				String t = curNode.asText();
+				try {
+					Type type = Type.valueOf(t);
+					gcm.setModelType(type);
+				}
+				catch (Exception x)
+				{
+					configErrors.add("MODEL_TYPE '" + t + "' is not correct!");
+				}
+			}	
+			else
+				configErrors.add("MODEL_TYPE is not textual!");
+		}	
+		
+		return gcm;
 	}	
 	
 }
