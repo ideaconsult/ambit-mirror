2576197b3eb41b396f243460a8779e9b8fd46ad9
Nikolay Kochev <nick@uni-plovdiv.net>
2017-10-17 19:16:56 +0000

    Implemented class GCMTestConfig

---
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/GCMTestConfig.java
@@ -0,0 +1,13 @@
+package ambit2.groupcontribution;
+
+public class GCMTestConfig 
+{
+	public boolean selfTest = true;
+	public boolean externalTestSet = false;
+	public boolean crossValidation = false;
+	public int crossValidationFold = 5;
+	public boolean leaveOneOut = false;
+	public boolean bootStrap = false;
+	public boolean yScrambling = false;
+	
+}
