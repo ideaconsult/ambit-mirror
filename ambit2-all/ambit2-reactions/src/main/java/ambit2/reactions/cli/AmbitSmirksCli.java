c6009046c930b9c07e7328948c8ca7c38e65bca1
Nikolay Kochev <nick@uni-plovdiv.net>
2017-10-21 08:18:47 +0000

    Added new class AmbitSmirksCli

---
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/cli/AmbitSmirksCli.java
@@ -0,0 +1,153 @@
+package ambit2.reactions.cli;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.PosixParser;
+
+
+public class AmbitSmirksCli 
+{
+	private static final String title = "Ambit SMIRKS CLI";
+	public String smilesArg = null;
+
+	public static void main(String[] args) 
+	{
+		AmbitSmirksCli cli = new AmbitSmirksCli();
+		cli.run(args);
+	}
+
+	enum _option {
+
+		smiles {
+			@Override
+			public String getArgName() {
+				return "smiles";
+			}
+			@Override
+			public String getDescription() {
+				return "Target molecule smiles";
+			}
+			@Override
+			public String getShortName() {
+				return "s";
+			}
+		},
+
+		help {
+			@Override
+			public String getArgName() {
+				return null;
+			}
+			@Override
+			public String getDescription() {
+				return title;
+			}
+			@Override
+			public String getShortName() {
+				return "h";
+			}
+			@Override
+			public String getDefaultValue() {
+				return null;
+			}
+			public Option createOption() {
+				Option option   = OptionBuilder.withLongOpt(name())
+						.withDescription(getDescription())
+						.create(getShortName());
+				return option;
+			}
+		};		
+
+		public abstract String getArgName();
+		public abstract String getDescription();
+		public abstract String getShortName();
+		public String getDefaultValue() { return null; }
+
+		public Option createOption() {
+			String defaultValue = getDefaultValue();
+			Option option   = OptionBuilder.withLongOpt(name())
+					.hasArg()
+					.withArgName(getArgName())
+					.withDescription(String.format("%s %s %s",getDescription(),defaultValue==null?"":"Default value: ",defaultValue==null?"":defaultValue))
+					.create(getShortName());
+
+			return option;
+		}
+	}
+
+	protected static Options createOptions() {
+		Options options = new Options();
+		for (_option o: _option.values()) {
+			options.addOption(o.createOption());
+		}
+		return options;
+	}
+
+	protected static void printHelp(Options options,String message) {
+		if (message!=null) System.out.println(message);
+		System.out.println(title);
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp( AmbitSmirksCli.class.getName(), options );
+	}
+	
+	public void setOption(_option option, String argument) throws Exception {
+		if (argument != null)
+			argument = argument.trim();
+		switch (option) {
+		case smiles: {
+			if ((argument == null) || "".equals(argument.trim()))
+				return;
+			smilesArg = argument;
+			break;
+		}
+		}
+	}
+	
+	public int run(String[] args) 
+	{
+		Options options = createOptions();
+		final CommandLineParser parser = new PosixParser();
+		try {
+			CommandLine line = parser.parse( options, args,false );
+			if (line.hasOption(_option.help.name())) {
+				printHelp(options, null);
+				return -1;
+			}
+
+			for (_option o: _option.values()) 
+				if (line.hasOption(o.getShortName())) try {
+					setOption(o,line.getOptionValue(o.getShortName()));
+				} catch (Exception x) {
+					printHelp(options,x.getMessage());
+					return -1;
+				}
+
+			return runSmirks();	
+
+		} catch (Exception x ) {
+			System.out.println(x.getMessage());
+			x.printStackTrace();
+			//printHelp(options,x.getMessage());
+			return -1;
+		} finally {
+			try { 
+				//run whatever cleanup is needed
+			} catch (Exception xx) {
+				printHelp(options,xx.getMessage());
+			}
+		}
+	}
+	
+	protected int runSmirks() throws Exception
+	{	
+		//TODO
+		return 0;
+	}
+
+
+
+}
