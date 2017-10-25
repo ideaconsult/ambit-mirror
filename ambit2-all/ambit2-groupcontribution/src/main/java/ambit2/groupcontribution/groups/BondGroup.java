ba520c28e9a586ecaf1857f801a26e0907565eea
Nikolay Kochev <nick@uni-plovdiv.net>
2017-10-25 21:45:59 +0000

    Finalized basic functionality in class BondGroup

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/groups/BondGroup.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/groups/BondGroup.java
@@ -1,43 +1,45 @@
 package ambit2.groupcontribution.groups;
 
+
+
 public class BondGroup extends ValueTransformedGroup 
 {
+	private String bondDesignation = "";
+	private double contribution = 0.0;
 	private boolean missing = true;
 	
 	@Override
 	public double getContribution() {
-		// TODO Auto-generated method stub
-		return 0;
+		return contribution;
 	}
 
 	@Override
 	public void setContribution(double contribution) {
-		// TODO Auto-generated method stub
-		
+		this.contribution = contribution;
+	}
+	
+	public void setBondDesignation(String bondDesignation) {		
+		this.bondDesignation = bondDesignation;
 	}
 
 	@Override
 	public String getDesignation() {
-		// TODO Auto-generated method stub
-		return null;
+		return bondDesignation;
 	}
 
 	@Override
 	public GroupInfo getInfo() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public IGroupSet getGroupSet() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Type getType() {
-		// TODO Auto-generated method stub
-		return null;
+		return Type.BOND;
 	}
 
 	@Override
