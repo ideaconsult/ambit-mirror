ab2cd6dfb7f11e1709bf74973f0b3aba4b0cb462
Nikolay Kochev <nick@uni-plovdiv.net>
2017-11-10 12:00:15 +0000

    Added basic flags and functions in TautomerRegion

--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/rules/TautomerRegion.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/rules/TautomerRegion.java
@@ -1,12 +1,22 @@
 package ambit2.tautomers.rules;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+import ambit2.tautomers.RuleInstance;
+
 public class TautomerRegion 
 {
 	boolean useRegion = false;
+	boolean useCustomRegions = false;
+	boolean tautomerizeNitorGroup = false;
+	boolean tautomerizeAromaticSystems = true;
 	
+	List<Integer> regionAtomIndices = new ArrayList<Integer>();
 	
-
-	public boolean isUseRegion() {
+	public boolean useRegion() {
 		return useRegion;
 	}
 
@@ -14,6 +24,55 @@ public class TautomerRegion
 		this.useRegion = useRegion;
 	}
 	
+	public boolean isRuleInstanceInRegion(RuleInstance ruleInst, IAtomContainer mol)
+	{
+		//TODO
+		return true;
+	}
 	
+	public void calculateRegion(IAtomContainer mol)
+	{
+		//TODO
+	}
+	
+
+	public boolean isUseCustomRegions() {
+		return useCustomRegions;
+	}
+
+
+	public void setUseCustomRegions(boolean useCustomRegions) {
+		this.useCustomRegions = useCustomRegions;
+	}
+
+
+	public boolean isTautomerizeNitorGroup() {
+		return tautomerizeNitorGroup;
+	}
+
+
+	public void setTautomerizeNitorGroup(boolean tautomerizeNitorGroup) {
+		this.tautomerizeNitorGroup = tautomerizeNitorGroup;
+	}
+
+
+	public boolean isTautomerizeAromaticSystems() {
+		return tautomerizeAromaticSystems;
+	}
+
+
+	public void setTautomerizeAromaticSystems(boolean tautomerizeAromaticSystems) {
+		this.tautomerizeAromaticSystems = tautomerizeAromaticSystems;
+	}
+
+
+	public List<Integer> getRegionAtomIndices() {
+		return regionAtomIndices;
+	}
+
+
+	public void setRegionAtomIndices(List<Integer> regionAtomIndices) {
+		this.regionAtomIndices = regionAtomIndices;
+	}	
 	
 }
