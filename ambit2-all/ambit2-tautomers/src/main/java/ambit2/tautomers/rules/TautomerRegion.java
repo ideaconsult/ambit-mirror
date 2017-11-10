20b93ea71708cf96a0ba2cf2dd9c7ccd08fe7156
Nikolay Kochev <nick@uni-plovdiv.net>
2017-11-10 11:39:11 +0000

    Added new class TautomerRegion

---
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/rules/TautomerRegion.java
@@ -0,0 +1,19 @@
+package ambit2.tautomers.rules;
+
+public class TautomerRegion 
+{
+	boolean useRegion = false;
+	
+	
+
+	public boolean isUseRegion() {
+		return useRegion;
+	}
+
+	public void setUseRegion(boolean useRegion) {
+		this.useRegion = useRegion;
+	}
+	
+	
+	
+}
