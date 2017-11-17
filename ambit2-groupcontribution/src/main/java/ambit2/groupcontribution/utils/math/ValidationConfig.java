4b84039d1a78c749093987de979548d66b95a44c
Nikolay Kochev <nick@uni-plovdiv.net>
2017-11-17 15:16:04 +0000

    Added new classes CrossValidation and ValidationConfig.

---
+++ ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/utils/math/ValidationConfig.java
@@ -0,0 +1,11 @@
+package ambit2.groupcontribution.utils.math;
+
+public class ValidationConfig 
+{
+	public boolean selfTest = true;
+	
+	public CrossValidation singleOutVal = new CrossValidation();
+	public CrossValidation crossValVal = null;
+	
+	
+}
