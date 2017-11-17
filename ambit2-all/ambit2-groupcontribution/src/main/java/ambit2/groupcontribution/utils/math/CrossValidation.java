41c6d4ddb4870a12ab0744a354354e8168157d14
Nikolay Kochev <nick@uni-plovdiv.net>
2017-11-17 15:16:04 +0000

    Added new classes CrossValidation and ValidationConfig.

---
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/utils/math/CrossValidation.java
@@ -0,0 +1,11 @@
+package ambit2.groupcontribution.utils.math;
+
+public class CrossValidation 
+{
+	public boolean isLeaveOneOut = true;
+	public int folds = 5;
+	public boolean randomization = false;
+	public int numCycles = 1;
+	
+	
+}
