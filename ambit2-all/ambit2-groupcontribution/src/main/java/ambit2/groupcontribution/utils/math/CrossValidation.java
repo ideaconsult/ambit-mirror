bdd122d0485c99fb2cc5565e1c4784cfbdbf60da
Nikolay Kochev <nick@uni-plovdiv.net>
2017-11-19 21:06:09 +0000

    Implemented enum ObjectSelection and constructors in class CrossValidation()

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/utils/math/CrossValidation.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/utils/math/CrossValidation.java
@@ -2,10 +2,31 @@ package ambit2.groupcontribution.utils.math;
 
 public class CrossValidation 
 {
+	public static enum ObjectSelection {
+		NONE, RANDOM, RANDOM_VALUE_DISTRIBUTION
+	}
+	
 	public boolean isLeaveOneOut = true;
-	public int folds = 5;
-	public boolean randomization = false;
+	public int numFolds = 5;
+	public ObjectSelection selection = ObjectSelection.RANDOM;
 	public int numCycles = 1;
 	
+	public CrossValidation()
+	{	
+	}
+	
+	public CrossValidation(int numFolds)
+	{	
+		this.numFolds = numFolds;
+		isLeaveOneOut = false;
+	}
+	
+	public CrossValidation(int numFolds, int numCycles, ObjectSelection selection)
+	{	
+		this.numFolds = numFolds;
+		this.numCycles = numCycles;
+		this.selection = selection;
+		isLeaveOneOut = false;
+	}
 	
 }
