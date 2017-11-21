c7d89d207c581d32e464b2d4da6688e79eb0e547
Nikolay Kochev <nick@uni-plovdiv.net>
2017-11-21 14:10:01 +0000

    Added class CustomTautomerRegion

---
+++ ambit2-tautomers/src/main/java/ambit2/tautomers/rules/CustomTautomerRegion.java
@@ -0,0 +1,13 @@
+package ambit2.tautomers.rules;
+
+import org.openscience.cdk.isomorphism.matchers.IQueryAtomContainer;
+
+import ambit2.tautomers.RuleStateFlags;
+
+public class CustomTautomerRegion 
+{
+	public String smirks = null;
+	public RuleStateFlags flags = null;
+	public IQueryAtomContainer query = null;
+		
+}
