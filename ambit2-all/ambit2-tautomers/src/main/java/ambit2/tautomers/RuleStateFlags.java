db4428bdd8aa5d6dc3062f389ff4a05ca4ac567d
Nikolay Kochev <nick@uni-plovdiv.net>
2017-11-22 22:44:27 +0000

    Implemented function setCustomExcludeRegions()

--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/RuleStateFlags.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/RuleStateFlags.java
@@ -3,11 +3,11 @@ package ambit2.tautomers;
 public class RuleStateFlags 
 {
 	//These are flags for SMARTS mapping
-	boolean mNeedNeighbourData;
-	boolean mNeedValenceData;
-	boolean mNeedRingData;    //data with ring sizes for each atom
-	boolean mNeedRingData2;	  //data with ring 'internal formal numbers' for each atom
-	boolean mNeedExplicitHData;
-	boolean mNeedParentMoleculeData;
-	boolean hasRecursiveSmarts;
+	public boolean mNeedNeighbourData;
+	public boolean mNeedValenceData;
+	public boolean mNeedRingData;    //data with ring sizes for each atom
+	public boolean mNeedRingData2;	  //data with ring 'internal formal numbers' for each atom
+	public boolean mNeedExplicitHData;
+	public boolean mNeedParentMoleculeData;
+	public boolean hasRecursiveSmarts;
 }
