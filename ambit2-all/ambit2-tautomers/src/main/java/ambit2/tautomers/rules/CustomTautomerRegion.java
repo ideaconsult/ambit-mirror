6700ecfa917af41ef36122adb5c8ef2e1071f551
Nikolay Kochev <nick@uni-plovdiv.net>
2017-11-22 22:07:23 +0000

    Implemented function getNitroGroupPositions()

--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/rules/CustomTautomerRegion.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/rules/CustomTautomerRegion.java
@@ -19,8 +19,50 @@ public class CustomTautomerRegion
 	
 	public static List<IAtom[]> getNitroGroupPositions(IAtomContainer target)
 	{
-		//TODO
-		return null;
+		List<IAtom[]> pos = new ArrayList<IAtom[]>();
+		for (IAtom at : target.atoms())
+		{
+			if (!at.getSymbol().equals("N"))
+				continue;
+			
+			TopLayer tl = (TopLayer)at.getProperty(TopLayer.TLProp);
+			if (tl.atoms.size() < 3)
+				continue;
+			
+			int nOAtoms = 0;
+			IAtom atoms[] = new IAtom[3];
+			atoms[0] = at;
+			
+			for (int i = 0; i < tl.atoms.size(); i++)
+			{
+				IAtom a0 = tl.atoms.get(i);
+				if (a0.getSymbol().equals("O"))
+				{	
+					if ((at.getFormalCharge() == 0) && (a0.getFormalCharge() == 0) 
+							&& (tl.bonds.get(i).getOrder() ==IBond.Order.DOUBLE))
+					{
+						//N=O
+						nOAtoms++;
+						atoms[nOAtoms] = a0;
+					}
+					else
+						if ((at.getFormalCharge() == +1) && (a0.getFormalCharge() == -1) 
+								&& (tl.bonds.get(i).getOrder() ==IBond.Order.SINGLE))
+						{
+							//[N+]-[O-]
+							nOAtoms++;
+							atoms[nOAtoms] = a0;
+						}	
+				}
+				
+				if (nOAtoms == 2)
+					break;
+			}
+			if (nOAtoms == 2)
+				pos.add(atoms);
+		}
+		
+		return pos;
 	}
 	
 	public static List<IAtom[]> getNitroxidePositions(IAtomContainer target)
@@ -48,6 +90,7 @@ public class CustomTautomerRegion
 						IAtom atoms[] = new IAtom[2];
 						atoms[0] = at;
 						atoms[1] = a0;
+						pos.add(atoms);
 						break;
 					}
 					else
@@ -58,6 +101,7 @@ public class CustomTautomerRegion
 							IAtom atoms[] = new IAtom[2];
 							atoms[0] = at;
 							atoms[1] = a0;
+							pos.add(atoms);
 							break;
 						}	
 				}
