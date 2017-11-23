2384518d736f92d20212f6dd207cab65acc0bf1c
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2017-11-23 10:42:06 +0000

    suppoert for read/write the updated field

--- ambit2-db/src/main/java/ambit2/db/substance/study/UpdateSubstanceStudyBundle.java
+++ ambit2-db/src/main/java/ambit2/db/substance/study/UpdateSubstanceStudyBundle.java
@@ -25,8 +25,8 @@ public class UpdateSubstanceStudyBundle extends UpdateSubstanceStudy {
 	private static final String[] create_sql_bundle = {
 		"INSERT INTO bundle_substance_protocolapplication (idbundle,document_prefix,document_uuid,topcategory,endpointcategory,endpoint,guidance," +
 		"substance_prefix,substance_uuid,params,interpretation_result,interpretation_criteria,reference,reference_year,reference_owner," +
-		"reliability,isRobustStudy,isUsedforClassification,isUsedforMSDS,purposeFlag,studyResultType,investigation_uuid)\n" +
-		"values(?,?,unhex(replace(?,'-','')),?,?,?,?,?,unhex(replace(?,'-','')),?,?,?,?,?,?,?,?,?,?,?,?,unhex(replace(?,'-',''))) on duplicate key update\n"+
+		"reliability,isRobustStudy,isUsedforClassification,isUsedforMSDS,purposeFlag,studyResultType,investigation_uuid,updated)\n" +
+		"values(?,?,unhex(replace(?,'-','')),?,?,?,?,?,unhex(replace(?,'-','')),?,?,?,?,?,?,?,?,?,?,?,?,unhex(replace(?,'-','')),?) on duplicate key update\n"+
 		"substance_prefix=values(substance_prefix),substance_uuid=values(substance_uuid),topcategory=values(topcategory),\n"+
 		"endpointcategory=values(endpointcategory),endpoint=values(endpoint),guidance=values(guidance),params=values(params)," +
 		"interpretation_result=values(interpretation_result),interpretation_criteria=values(interpretation_criteria)," +
