21a61b36f6e437608c5a83004f429d05b07ff59b
Nina Jeliazkova <jeliazkova.nina@gmail.com>
2017-11-24 12:28:54 +0000

    db version 9.3

--- ambit2-db/src/main/java/ambit2/db/DBVersion.java
+++ ambit2-db/src/main/java/ambit2/db/DBVersion.java
@@ -26,7 +26,7 @@ package ambit2.db;
 
 public interface DBVersion {
 	int AMBITDB_VERSION_MAJOR = 9;
-	int AMBITDB_VERSION_MINOR = 2;
+	int AMBITDB_VERSION_MINOR = 3;
 	int getMajor();
 	int getMinor();
 	
