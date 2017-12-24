c0b3fae08173499e5ef2c8ca31f71c5578b10055
Nikolay Kochev <nick@uni-plovdiv.net>
2017-12-24 22:19:10 +0000

    Implemented functions: firstLayer() and getSortedCodesIndices()

--- ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/AtomEquivalenceInfo.java
+++ ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/AtomEquivalenceInfo.java
@@ -5,14 +5,15 @@ import java.util.List;
 
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IBond;
 
 public class AtomEquivalenceInfo 
 {
-	public static final int SHIFT_ATOM_ELEMENT = 100000000;
-	public static final int SHIFT_POS_CHARGE   = 1000000;
-	public static final int SHIFT_NEG_CHARGE   = 100000;
+	public static final int SHIFT_ATOM_ELEMENT = 10000000;
+	public static final int SHIFT_BOND_TYPE    = 1000000;
+	public static final int SHIFT_POS_CHARGE   = 100000;
+	public static final int SHIFT_NEG_CHARGE   = 10000;
 	public static final int SHIFT_ISOTOPE      = 1000;
-	public static final int SHIFT_BOND_TYPE    = 100;
 	public static final int SHIFT_PREV_LAYER   = 1;
 	
 	
@@ -20,45 +21,101 @@ public class AtomEquivalenceInfo
 	public List<Integer> atomLayersCode = new ArrayList<Integer>();
 	boolean isTerminalAtom = false;
 	int curLayerNum = 0;
-	int nProcessedAtoms = 0;
+	int nProcessedBonds = 0;
 	public List<IAtom> curLayerAtoms = new ArrayList<IAtom>();
 
 	public void initialize(IAtom at)
 	{
-		atomLayersCode.add(getAtomCode(at));
+		atomLayersCode.add(getAtomCode(at, null));
 		curLayerAtoms.add(at);
-		nProcessedAtoms = 1;
 	}
 	
 	public void initialize(IAtom at, int numLayers, IAtomContainer mol)
 	{
-		atomLayersCode.add(getAtomCode(at));
+		atomLayersCode.add(getAtomCode(at, null));
 		curLayerAtoms.add(at);
 		for (int i = 0; i < numLayers; i++)
 			nextLayer(mol);
 	}
 	
-	public void nextLayer(IAtomContainer mol)
+	public void firstLayer(IAtomContainer mol)
 	{
-		if (nProcessedAtoms >= mol.getAtomCount())
-			return;
+		IAtom at = curLayerAtoms.get(0);
+		TopLayer tl = (TopLayer)at.getProperty(TopLayer.TLProp);
 		
 		List<IAtom> newLayerAtoms = new ArrayList<IAtom>();
+		List<Integer> codes = new ArrayList<Integer>();
+		
+		if (tl.atoms.size() == 1)
+		{
+			IAtom a = tl.atoms.get(0);
+			IBond b = tl.bonds.get(0);
+			atomLayersCode.add(getAtomCode(a,b) + curLayerNum);
+			newLayerAtoms.add(a);
+		}
+		else
+		{	
+			for (int k = 0; k < tl.atoms.size(); k++)
+			{
+				IAtom a = tl.atoms.get(k);
+				IBond b = tl.bonds.get(k);
+				codes.add(getAtomCode(a,b) + curLayerNum);
+			}
+
+			int sort[] = getSortedCodesIndices(codes);
+			for (int k = 0; k < sort.length; k++)
+			{
+				int index = sort[k];
+				atomLayersCode.add(codes.get(index));
+				newLayerAtoms.add(tl.atoms.get(index));
+			}
+		}
+		
+		curLayerNum++;
+		curLayerAtoms.clear();
+		curLayerAtoms.addAll(newLayerAtoms);
+	}	
+	
+	public void nextLayer(IAtomContainer mol)
+	{
+		if (nProcessedBonds >= mol.getBondCount())
+			return;
+		
+		//Special treatment for the first layer
+		if (curLayerNum == 0)
+		{
+			firstLayer(mol);
+			return;
+		}	
+		
+		List<IAtom> newLayerAtoms = new ArrayList<IAtom>();
+		List<IBond> curLayerInnerBonds = new ArrayList<IBond>();
+		
 		for (int i = 0; i < curLayerAtoms.size(); i++)
 		{
 			IAtom at = curLayerAtoms.get(i);
-			TopLayer topLayer = (TopLayer)at.getProperty(TopLayer.TLProp);
+			TopLayer tl = (TopLayer)at.getProperty(TopLayer.TLProp);
+			if (tl.atoms.size() == 1)
+					continue; // it is a terminal atom from the current layer
 			
+			for (int k = 0; k < tl.atoms.size(); k++)
+			{
+				//TODO
+			}
 			//TODO
 		}
 		
+		curLayerNum++;
 		curLayerAtoms.clear();
 		curLayerAtoms.addAll(newLayerAtoms);
 	}
 	
-	public static Integer getAtomCode(IAtom atom)
+	public static Integer getAtomCode(IAtom atom, IBond bond)
 	{
-		Integer n = atom.getAtomicNumber()*SHIFT_ATOM_ELEMENT;
+		int n = atom.getAtomicNumber() * SHIFT_ATOM_ELEMENT;
+		if (bond != null)
+			n = n + bond.getOrder().numeric() * SHIFT_BOND_TYPE; 
+		
 		//TODO handle charges and isotopes
 		return n;
 	}
@@ -98,6 +155,54 @@ public class AtomEquivalenceInfo
 				for (int k = 0; k <= i; k++)
 				{
 					if (codes[indices[k]] > codes[indices[k+1]])
+					{
+						tmp = indices[k];
+						indices[k] = indices[k+1];
+						indices[k+1] = tmp;
+					}
+				}
+			break;
+		}
+		
+		return indices;
+	}
+	
+	
+	public static int[] getSortedCodesIndices(List<Integer> codes)
+	{
+		int n = codes.size();
+		int indices[] = new int[n];
+		
+		switch (n)
+		{
+		case 0:
+			return null;
+		case 1:
+			indices[0] = 0;
+			break;
+		case 2:
+			if (codes.get(0) > codes.get(1))
+			{
+				indices[0] = 1;
+				indices[1] = 0;
+			}
+			else
+			{
+				indices[0] = 0;
+				indices[1] = 1;
+			}
+			break;
+			
+		default: //n >= 3 
+			//initialize indices
+			for (int i = 0; i < n; i++)
+				indices[i] = i;
+			//bubble algorithm
+			int tmp;
+			for (int i = (n-2); i >= 0; i--)
+				for (int k = 0; k <= i; k++)
+				{
+					if (codes.get(indices[k]) > codes.get(indices[k+1]))
 					{
 						tmp = indices[k];
 						indices[k] = indices[k+1];
