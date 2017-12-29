e88113be434616bd7870dc3004766236084080a0
Nikolay Kochev <nick@uni-plovdiv.net>
2017-12-29 21:22:55 +0000

    Added reaction score to strategy

--- ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/retrosynth/SyntheticStrategy.java
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/retrosynth/SyntheticStrategy.java
@@ -5,13 +5,19 @@ import java.util.ArrayList;
 import org.openscience.cdk.interfaces.IAtomContainer;
 
 import ambit2.reactions.rules.IRetroSynthRuleInstance;
+import ambit2.reactions.rules.scores.ReactionScore;
 
 public class SyntheticStrategy 
 {
+	public ReactionScore reactionScore = new ReactionScore();
+	
+	
+	/*
 	public ArrayList<IRetroSynthRuleInstance> applyStrategy(IAtomContainer target, ArrayList<IRetroSynthRuleInstance> ruleInstances)
 	{
 		//Currently the strategy does nothing
 		//TODO
 		return ruleInstances;
 	}
+	*/
 }
