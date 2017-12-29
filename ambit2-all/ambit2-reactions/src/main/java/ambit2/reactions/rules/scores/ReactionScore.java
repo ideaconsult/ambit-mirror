f7b08adde5bd3b8ea93463a0f7cc58232477bf75
Nikolay Kochev <nick@uni-plovdiv.net>
2017-12-29 20:46:29 +0000

    Added class ReactionScore

---
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/rules/scores/ReactionScore.java
@@ -0,0 +1,31 @@
+package ambit2.reactions.rules.scores;
+
+public class ReactionScore 
+{
+	public double basicScoreWeight = 0.0;
+	public double classcScoreWeight = 0.0;
+	public double transformScoreWeight = 0.0;
+	public double conditionsScoreWeight = 0.0;
+	public double experimentalConditionsScoreWeight = 0.0;
+	public double yieldScoreWeight = 0.0;
+	public double productComplexityWeight = 0.0; 
+	public double productSimilarityWeight = 0.0;
+	public double productStabilityWeight = 0.0;
+	public double reactionCenterPositionWeight = 0.0;
+	public double reactionCenterComplexityWeight = 0.0;
+	public double electronWithdrawingLevelWeight = 0.0;
+	
+	/*
+	 * "BASIC_SCORE_WEIGHT" : 0.3,
+		"CLASS_SCORE_WEIGHT" : 0.2,
+		"TRANSFORM_SCORE_WEIGHT" : 0.3,
+		"CONDITIONS_SCORE_WEIGHT" : 0.7,
+		"EXPERIMENTAL_CONDITIONS_SCORE_WEIGHT" : 0.0,
+		"PRODUCT_COMPLEXITY_WEIGHT" : 0.5,
+		"PRODUCT_SIMILARITY_WEIGHT" : 0.3,
+		"PRODUCT_STABILITY_WEIGHT" : 0.3,		
+		"REACTION_CENTER_POSITION_WEIGHT"  : 0.5,
+		"REACTION_CENTER_COMPLEXITY_WEIGHT" : 0.5,
+		"ELECTRON_WITHDRAWING_LEVEL_WEIGHT" : 0.0,
+	 */
+}
