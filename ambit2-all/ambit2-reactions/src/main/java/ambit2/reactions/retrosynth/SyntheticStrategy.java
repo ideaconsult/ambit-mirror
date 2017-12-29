4652887b4d08ee08352d65922cd826aefecfc7c8
Nikolay Kochev <nick@uni-plovdiv.net>
2017-12-29 21:39:32 +0000

    Added config variables

--- ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/retrosynth/SyntheticStrategy.java
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/retrosynth/SyntheticStrategy.java
@@ -1,23 +1,31 @@
 package ambit2.reactions.retrosynth;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.openscience.cdk.interfaces.IAtomContainer;
 
 import ambit2.reactions.rules.IRetroSynthRuleInstance;
 import ambit2.reactions.rules.scores.ReactionScore;
+import ambit2.rules.weight.DescriptorWeight;
 
 public class SyntheticStrategy 
 {
 	public ReactionScore reactionScore = new ReactionScore();
+	public List<DescriptorWeight> productComplexityDescirptors = new ArrayList<DescriptorWeight>();
+	public List<DescriptorWeight> productSimilarityDescirptors = new ArrayList<DescriptorWeight>();
+	public Map<String, Double> reactionClassScores = new HashMap<String, Double>();
+	public Map<String, Double> trbasformTypeScores = new HashMap<String, Double>();
 	
 	
-	/*
+	
 	public ArrayList<IRetroSynthRuleInstance> applyStrategy(IAtomContainer target, ArrayList<IRetroSynthRuleInstance> ruleInstances)
 	{
 		//Currently the strategy does nothing
 		//TODO
 		return ruleInstances;
 	}
-	*/
+	
 }
