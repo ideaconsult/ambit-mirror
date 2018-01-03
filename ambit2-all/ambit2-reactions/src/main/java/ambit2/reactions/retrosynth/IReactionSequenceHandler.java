b3134a24c613ea4d8acf8b033b3e2ed84d836173
Nikolay Kochev <nick@uni-plovdiv.net>
2018-01-03 21:49:22 +0000

    Added new interface IReactionSequenceHandler

---
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/retrosynth/IReactionSequenceHandler.java
@@ -0,0 +1,13 @@
+package ambit2.reactions.retrosynth;
+
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+public interface IReactionSequenceHandler 
+{
+	public ReactionSequence getReactionSequence();
+	
+	public void setReactionSequence(ReactionSequence sequence);
+	
+	public void addMoleculeToLevel(IAtomContainer mol);
+	
+}
