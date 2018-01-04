cf34fc5a4fdefe647cb7bd97f08fe8d9dc7e3cee
Nikolay Kochev <nick@uni-plovdiv.net>
2018-01-04 23:35:01 +0000

    Added RSEvent and EventType. Refactoring: addMoleculeToLevelEvent--> registerEvent

--- ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/retrosynth/IReactionSequenceHandler.java
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/retrosynth/IReactionSequenceHandler.java
@@ -1,13 +1,23 @@
 package ambit2.reactions.retrosynth;
 
-import org.openscience.cdk.interfaces.IAtomContainer;
+
 
 public interface IReactionSequenceHandler 
 {
+	public static class RSEvent {
+		public EventType type = null;
+		public int level = 0;
+		public Object params[] = null;
+	}
+	
+	public static enum EventType {
+		ADD_MOLECULE, REMOVE_MOLECULE
+	}
+	
 	public ReactionSequence getReactionSequence();
 	
 	public void setReactionSequence(ReactionSequence sequence);
 	
-	public void addMoleculeToLevelEvent(IAtomContainer mol, int levelIndex);
+	public void registerEvent(RSEvent event);
 	
 }
