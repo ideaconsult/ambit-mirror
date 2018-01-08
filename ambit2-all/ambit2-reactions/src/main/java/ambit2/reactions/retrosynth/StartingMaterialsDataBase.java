78795d88007e2e5df419322abcd27d5ce9f6a454
Nikolay Kochev <nick@uni-plovdiv.net>
2018-01-08 22:48:48 +0000

    Reorganized code. Added class StartMaterialData and inchi options setup

--- ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/retrosynth/StartingMaterialsDataBase.java
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/retrosynth/StartingMaterialsDataBase.java
@@ -1,14 +1,32 @@
 package ambit2.reactions.retrosynth;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.openscience.cdk.inchi.InChIGeneratorFactory;
 import org.openscience.cdk.interfaces.IAtomContainer;
+
+import net.sf.jniinchi.INCHI_OPTION;
 
 
 public class StartingMaterialsDataBase 
 {		
-	//It can be composed of several data sets
+	public enum AccessType{
+		MEMORY, FILE, DATABASE
+	}
 	
+	public static class StartMaterialData {
+		public String smiles = null;
+		public int id = 0;
+	}
+	
+	Map<String,StartMaterialData> materials = new HashMap<String,StartMaterialData>();
+	List<INCHI_OPTION> inchiOptions = new ArrayList<INCHI_OPTION>();
+	InChIGeneratorFactory inchiGeneratorFactory = null;
+	
+	/*
 	ArrayList<String> errors = new ArrayList<String>(); 
 	
 	//Flags that configure the search process
@@ -26,16 +44,42 @@ public class StartingMaterialsDataBase
 	String sourceStructures = null;
 	String sourceDescriptors = null;
 	String sourceHashCodes = null;
+	*/
 	
 	public StartingMaterialsDataBase()
 	{	
 	}
 	
+	public boolean isStartingMaterial(String inchiKey)
+	{
+		return materials.containsKey(inchiKey);
+	}
+	
+	public boolean isStartingMaterial(IAtomContainer str)
+	{
+		//TODO
+		return false;
+	}
+	
+	void inchiSetup() throws Exception
+	{
+		inchiOptions.add(INCHI_OPTION.FixedH);
+		inchiOptions.add(INCHI_OPTION.SAbs);
+		inchiOptions.add(INCHI_OPTION.SAsXYZ);
+		inchiOptions.add(INCHI_OPTION.SPXYZ);
+		inchiOptions.add(INCHI_OPTION.FixSp3Bug);
+		inchiOptions.add(INCHI_OPTION.AuxNone);
+		inchiGeneratorFactory = InChIGeneratorFactory.getInstance();
+	}
+	
+		
+	/*
 	public StartingMaterialsDataBase(String sourceStructures, String sourceDescriptors, String sourceHashCodes)
 	{	
 		setSources(sourceStructures, sourceDescriptors, sourceHashCodes);
 		initialize();
 	}
+	
 	
 	public void setSources(String sourceStructures, String sourceDescriptors, String sourceHashCodes)
 	{
@@ -47,12 +91,6 @@ public class StartingMaterialsDataBase
 	public void initialize()
 	{	
 		//TODO
-	}
-	
-	public boolean isStartingMaterial(IAtomContainer str)
-	{
-		//TODO
-		return false;
 	}
 	
 	public ArrayList<Integer> seachMolecularDescriptors()
@@ -80,10 +118,8 @@ public class StartingMaterialsDataBase
 		return 0;
 	}
 	
+	*/
 	
 	
-	public enum AccessType{
-		MEMORY, FILE, DATABASE
-	}
 	
 }
