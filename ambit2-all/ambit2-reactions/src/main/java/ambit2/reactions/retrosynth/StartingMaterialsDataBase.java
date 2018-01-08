6f97ef24a907ae8229257dfce5531314398718a6
Nikolay Kochev <nick@uni-plovdiv.net>
2018-01-08 23:37:10 +0000

    Added functions: loadStartingMaterials() and createStartingMaterialsFile()

--- ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/retrosynth/StartingMaterialsDataBase.java
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/retrosynth/StartingMaterialsDataBase.java
@@ -1,5 +1,7 @@
 package ambit2.reactions.retrosynth;
 
+import java.io.File;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,7 +21,7 @@ public class StartingMaterialsDataBase
 	
 	public static class StartMaterialData {
 		public String smiles = null;
-		public int id = 0;
+		public String id = null;
 	}
 	
 	Map<String,StartMaterialData> materials = new HashMap<String,StartMaterialData>();
@@ -50,6 +52,11 @@ public class StartingMaterialsDataBase
 	{	
 	}
 	
+	public StartingMaterialsDataBase(File file)
+	{	
+		loadStartingMaterials(file);
+	}
+	
 	public boolean isStartingMaterial(String inchiKey)
 	{
 		return materials.containsKey(inchiKey);
@@ -72,7 +79,66 @@ public class StartingMaterialsDataBase
 		inchiGeneratorFactory = InChIGeneratorFactory.getInstance();
 	}
 	
-		
+	void loadStartingMaterials(File file)
+	{
+		try
+		{	
+			RandomAccessFile f = new RandomAccessFile(file,"r");			
+			long length = f.length();
+			
+			while (f.getFilePointer() < length)
+			{	
+				String line = f.readLine();
+				line = line.trim();
+				if (line.equals(""))
+					continue;
+				String tokens[] = line.split("\t");
+				if (tokens.length >=3)
+				{
+					StartMaterialData smd = new StartMaterialData();
+					smd.id = tokens[0];
+					smd.smiles = tokens[1];
+					materials.put(tokens[2], smd);
+				}
+				else
+				{
+					//error
+				}
+			}
+			
+			f.close();
+		}
+		catch (Exception e)
+		{	
+		}
+	}
+	
+	public static void createStartingMaterialsFile(File sourceFile, File outputFile, 
+				Map<String,Integer> column)
+	{
+		try
+		{	
+			RandomAccessFile sf = new RandomAccessFile(sourceFile,"r");			
+			long length = sf.length();
+			
+			while (sf.getFilePointer() < length)
+			{	
+				String line = sf.readLine();
+				line = line.trim();
+				if (line.equals(""))
+					continue;
+				
+				//TODO
+			}
+			
+			sf.close();
+		}
+		catch (Exception e)
+		{	
+		}
+	}
+	
+	
 	/*
 	public StartingMaterialsDataBase(String sourceStructures, String sourceDescriptors, String sourceHashCodes)
 	{	
@@ -119,7 +185,5 @@ public class StartingMaterialsDataBase
 	}
 	
 	*/
-	
-	
 	
 }
