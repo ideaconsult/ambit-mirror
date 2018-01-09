4ab846de622f82ba0489c64976c2d5381894caa5
Nikolay Kochev <nick@uni-plovdiv.net>
2018-01-09 22:29:23 +0000

    Added writer/read file utils

--- ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/io/ReactionWriteUtils.java
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/io/ReactionWriteUtils.java
@@ -1,5 +1,53 @@
 package ambit2.reactions.io;
 
-public class ReactionWriteUtils {
+import java.io.FileWriter;
+import java.io.RandomAccessFile;
 
+public class ReactionWriteUtils 
+{
+	public static FileWriter createWriter(String fname) throws Exception
+	{
+		FileWriter writer = null;
+		try {
+			writer = new FileWriter(fname);
+			
+		}catch (Exception x) {
+			//in case smth's wrong with the writer file, close it and throw an error
+			try {writer.close(); } catch (Exception xx) {}
+			throw x;
+		} finally { }
+		
+		return writer;
+	}
+	
+	public static  void closeWriter(FileWriter writer)
+	{
+		try { 
+			writer.close();
+		} catch (Exception x) {}
+	}
+	
+	public static RandomAccessFile  createReader(String fname) throws Exception
+	{
+		RandomAccessFile reader = null; 	
+		
+		try {
+			reader = new RandomAccessFile(fname,"r");
+			
+		}catch (Exception x) {
+			//in case smth's wrong with the writer file, close it and throw an error
+			try {reader.close(); } catch (Exception xx) {}
+			throw x;
+		} finally { }
+		
+		return reader;
+	}
+	
+	public static  void closeReader(RandomAccessFile reader)
+	{
+		try { 
+			reader.close();
+		} catch (Exception x) {}
+	}
+	
 }
