93b0b9e90614cd82cf4525baa62e3489864faeb7
Nikolay Kochev <nick@uni-plovdiv.net>
2018-01-12 11:19:05 +0000

    Added basic fields in class ReactionSequenceScoreSchema

--- ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/rules/scores/ReactionSequenceScoreSchema.java
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/rules/scores/ReactionSequenceScoreSchema.java
@@ -2,5 +2,13 @@ package ambit2.reactions.rules.scores;
 
 public class ReactionSequenceScoreSchema 
 {
-
+	public double moleculeComplexityWeight = 0.0;
+	public double moleculeSimilarityWeight = 0.0;
+	public double levelWeight = 0.0;
+	public double reactionScoreWeight = 0.0;
+	
+	/*
+	 	"LEVEL_WEIGHT" : 0.3,
+		"REACTION_SCORE_WEIGHT" : 0.7
+	 */
 }
