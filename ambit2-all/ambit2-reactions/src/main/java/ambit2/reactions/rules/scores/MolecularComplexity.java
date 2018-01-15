d35f6c7e2d686dfcf20fe179441634ca23ac61ea
Nikolay Kochev <nick@uni-plovdiv.net>
2018-01-15 15:44:46 +0000

    Implemented functions: calcAtomComplexities(), calcMolecularComplexity01(), getters and setters

--- ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/rules/scores/MolecularComplexity.java
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/rules/scores/MolecularComplexity.java
@@ -1,12 +1,15 @@
 package ambit2.reactions.rules.scores;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openscience.cdk.interfaces.IAtomContainer;
 
 public class MolecularComplexity 
 {
 	protected IAtomContainer target = null;
 	protected AtomComplexity atomComplexityCalculator = new AtomComplexity();
-	
+	protected List<Double> atomComplexities = new ArrayList<Double>();
 	
 	public IAtomContainer getTarget() {
 		return target;
@@ -14,6 +17,7 @@ public class MolecularComplexity
 
 	public void setTarget(IAtomContainer target) {
 		this.target = target;
+		nullify();
 	}
 
 	public AtomComplexity getAtomComplexityCalculator() {
@@ -23,22 +27,53 @@ public class MolecularComplexity
 	public void setAtomComplexityCalculator(AtomComplexity atomComplexityCalculator) {
 		this.atomComplexityCalculator = atomComplexityCalculator;
 	}
+	
+	public List<Double> getAtomComplexities() {
+		return atomComplexities;
+	}
+
+	public void setAtomComplexities(List<Double> atomComplexities) {
+		this.atomComplexities = atomComplexities;
+	}
+	
+	void nullify()
+	{
+		atomComplexities = null;
+	}
 
 	/**
-	 * Proudfood paper, CM = sum(CA_i) 
+	 * Proudfood paper, CM = sum{CA_i} 
 	 * @return
 	 */
 	public double calcMolecularComplexity01()
 	{
-		return 0.0;
+		if (target == null)
+			return 0.0;
+		calcAtomComplexities();
+		double cM = 0.0;
+		for (int i = 0; i < atomComplexities.size(); i++)
+			cM += atomComplexities.get(i);
+		return cM;
 	}
 	
 	/**
-	 * Proudfood paper, CM* = log(sum(2^CA_i))
+	 * Proudfood paper, CM* = log(sum{2^CA_i})
 	 * @return
 	 */
 	public double calcMolecularComplexity02()
 	{
 		return 0.0;
+	}
+	
+	void calcAtomComplexities()
+	{	
+		if (atomComplexities != null)
+			return;
+		atomComplexities = new ArrayList<Double>();
+		for (int i = 0; i < target.getAtomCount(); i++)
+		{
+			double c = atomComplexityCalculator.calcAtomComplexity(target.getAtom(i), target);
+			atomComplexities.add(c);
+		}
 	}
 }
