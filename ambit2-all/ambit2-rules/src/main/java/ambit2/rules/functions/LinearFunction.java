d8ce7854c3e21a06171a59c895d9e1d399d27b89
Nikolay Kochev <nick@uni-plovdiv.net>
2018-01-21 23:13:13 +0000

    Added new interface IFunction and functions LinearFunction and NestedFunctions

---
+++ ambit2-all/ambit2-rules/src/main/java/ambit2/rules/functions/LinearFunction.java
@@ -0,0 +1,32 @@
+package ambit2.rules.functions;
+
+public class LinearFunction implements IFunction
+{
+	protected double params[] =  {1.0, 0.0};
+	
+	@Override
+	public String getName() {
+		return "Linear";
+	}
+
+	@Override
+	public double getFunctionValue(double argument) {
+		return params[0]*argument + params[1];
+	}
+
+	@Override
+	public double[] getParams() {
+		return params;
+	}
+
+	@Override
+	public void setParams(double[] params) {
+		if (params != null)
+			if (params.length == 2)
+			{
+				this.params[0] = params[0];
+				this.params[1] = params[1];
+			}
+	}
+	
+}
---
+++ ambit2-all/ambit2-rules/src/main/java/ambit2/rules/functions/NestedFunctions.java
@@ -0,0 +1,39 @@
+package ambit2.rules.functions;
+
+import java.util.List;
+
+public class NestedFunctions implements IFunction
+{
+	protected List<IFunction> functions = null;
+	
+	@Override
+	public String getName() {
+		return "Nested functions";
+	}
+
+	@Override
+	public double getFunctionValue(double argument) {
+		double value = argument;
+		for (int i=0; i < functions.size(); i++)
+			value = functions.get(0).getFunctionValue(value);
+		return value;
+	}
+
+	@Override
+	public double[] getParams() {
+		return null;
+	}
+
+	@Override
+	public void setParams(double[] params) {
+	}
+
+	public List<IFunction> getFunctions() {
+		return functions;
+	}
+
+	public void setFunctions(List<IFunction> functions) {
+		this.functions = functions;
+	}
+
+}
