a8ce6fa0b10d3995c937d1f634b64b40c139199c
Nikolay Kochev <nick@uni-plovdiv.net>
2018-02-22 21:12:03 +0000

    Added descriptors list. reorganized the synthetic accessibility strategy.

--- ambit2-reactions/src/main/java/ambit2/reactions/syntheticaccessibility/SyntheticAccessibilityStrategy.java
+++ ambit2-reactions/src/main/java/ambit2/reactions/syntheticaccessibility/SyntheticAccessibilityStrategy.java
@@ -1,22 +1,17 @@
 package ambit2.reactions.syntheticaccessibility;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ambit2.reactions.rules.scores.ReactionSequenceScoreSchema;
+import ambit2.rules.weight.DescriptorWeight;
 
 public class SyntheticAccessibilityStrategy 
 {
-	public static enum SynthAccessMethod {
-		DESCRIPTORS, 
-		START_MATERIALS,
-		RETRO_SYNTHESIS,
-		COMBINED_METHOD,
-		UNKNOWN,
-	}
 	
-	public SynthAccessMethod synthAccessMethod = SynthAccessMethod.RETRO_SYNTHESIS;
+	public List<DescriptorWeight> descirptors = new ArrayList<DescriptorWeight>();
+	public double startMatrialeSimilarityWeight = 0.0;
+	public double retroSynthesisWeight = 0.0;
 	public ReactionSequenceScoreSchema reactionSequenceScoreSchema = null;
 	
-	//weights used for COMBINED_METHOD
-	public double combinedDescrWeight = 0.5;
-	public double combinedStartMatWeight = 0.0;
-	public double combinedRetroSynthWeight = 0.5;
 }
