a978fa240f7542c706bcddebb102297afd258c76
Nikolay Kochev <nick@uni-plovdiv.net>
2018-03-12 23:56:57 +0000

    Implemented functions configure() and match()

--- ambit2-smarts/src/main/java/ambit2/smarts/groups/GroupMatch.java
+++ ambit2-smarts/src/main/java/ambit2/smarts/groups/GroupMatch.java
@@ -1,19 +1,60 @@
 package ambit2.smarts.groups;
 
+import java.util.List;
+
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.isomorphism.matchers.IQueryAtomContainer;
+
+import ambit2.smarts.IsomorphismTester;
+import ambit2.smarts.QuerySequenceElement;
+import ambit2.smarts.SmartsParser;
+
 public class GroupMatch 
 {
+	IsomorphismTester isoTester = null;
+	SmartsParser parser = null;
 	String smarts = null;
+	IQueryAtomContainer smartsQuery = null;
+	List<QuerySequenceElement> sequence = null;
 	String error = null;
 	
-	public GroupMatch(String smarts)
+	public GroupMatch(String smarts, SmartsParser parser, IsomorphismTester isoTester)
 	{
 		this.smarts = smarts;
+		this.parser = parser;
+		this.isoTester = isoTester;
 		configure();
 	}
 	
-	void configure()
+	public void configure()
 	{
-		//TODO
+		smartsQuery = parser.parse(smarts);
+		
+		if (parser.getErrors().isEmpty())
+		{	
+			// parser.setNeededDataFlags();  //TODO 
+			isoTester.setQuery(smartsQuery);
+			sequence = isoTester.transferSequenceToOwner();
+		}
+		else
+			error = parser.getErrorMessages();
+	}
+	
+	
+	public boolean match(IAtomContainer target)
+	{
+		/*
+		SmartsParser.prepareTargetForSMARTSSearch(
+				flags.mNeedNeighbourData, 
+				flags.mNeedValenceData, 
+				flags.mNeedRingData, 
+				flags.mNeedRingData2, 
+				flags.mNeedExplicitHData , 
+				flags.mNeedParentMoleculeData, mol);	
+			*/	
+		
+		isoTester.setSequence(smartsQuery, sequence);
+		return isoTester.hasIsomorphism(target);
 	}
 	
 	public String getError()
