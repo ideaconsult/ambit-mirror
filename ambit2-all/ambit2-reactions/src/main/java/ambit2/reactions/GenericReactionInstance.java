dd8f15c0651811deaa7b10dd83750ca2ca59d49c
Nikolay Kochev <nick@uni-plovdiv.net>
2018-03-12 22:21:34 +0000

    Added condtion status member variables. Implemented function calculateConditionsStatus()

--- ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/GenericReactionInstance.java
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/GenericReactionInstance.java
@@ -6,6 +6,7 @@ import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 
 import ambit2.reactions.rules.scores.ReactionScore;
+import ambit2.rules.conditions.ICondition;
 
 public class GenericReactionInstance 
 {
@@ -16,6 +17,8 @@ public class GenericReactionInstance
 	//public List<IAtom> instanceAtomIndices = null;
 	public IAtomContainer target = null;
 	public IAtomContainer products = null;
+	public boolean conditionsStatus[] = null;
+	public boolean applicationConditionsStatus[] = null;
 	
 	public GenericReactionInstance()
 	{	
@@ -40,6 +43,34 @@ public class GenericReactionInstance
 	public void generateProducts()
 	{
 		//TODO
+	}
+	
+	public void calculateConditionsStatus()
+	{
+		if (reaction == null)
+			return;
+		
+		if (reaction.conditions != null)
+		{	
+			int n = reaction.conditions.size();
+			conditionsStatus = new boolean[n];
+			for (int i = 0; i < n ; i++)
+			{
+				ICondition cond = reaction.conditions.get(i);
+				conditionsStatus[i] = cond.isTrue(this);
+			}
+		}
+		
+		if (reaction.applicationConditions != null)
+		{	
+			int n = reaction.applicationConditions.size();
+			applicationConditionsStatus = new boolean[n];
+			for (int i = 0; i < n ; i++)
+			{
+				ICondition cond = reaction.applicationConditions.get(i);
+				applicationConditionsStatus[i] = cond.isTrue(this);
+			}
+		}
 	}
 	
 }
