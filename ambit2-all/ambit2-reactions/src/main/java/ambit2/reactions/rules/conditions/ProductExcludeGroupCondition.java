2cd271f43e829c0f816a654c3d20a86c1dbf33c0
Nikolay Kochev <nick@uni-plovdiv.net>
2018-03-13 00:05:29 +0000

    Implemented basic codes in ProductExcludeGroupCondition

--- ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/rules/conditions/ProductExcludeGroupCondition.java
+++ ambit2-all/ambit2-reactions/src/main/java/ambit2/reactions/rules/conditions/ProductExcludeGroupCondition.java
@@ -1,6 +1,33 @@
 package ambit2.reactions.rules.conditions;
 
-public class ProductExcludeGroupCondition 
+import ambit2.rules.conditions.ICondition;
+import ambit2.smarts.IsomorphismTester;
+import ambit2.smarts.SmartsParser;
+import ambit2.smarts.groups.GroupMatch;
+
+public class ProductExcludeGroupCondition implements ICondition
 {
+	GroupMatch groupMatch = null;
+	
+	public ProductExcludeGroupCondition(String smarts, SmartsParser parser, IsomorphismTester isoTester)
+	{
+		groupMatch = new GroupMatch(smarts, parser, isoTester);
+	}
+	
+	@Override
+	public boolean isTrue(Object target) {
+		//TODO
+		return false;
+	}
+
+	@Override
+	public boolean isNegated() {
+		return false;
+	}
+
+	@Override
+	public void setIsNegated(boolean isNeg) {
+		//does not have any effect
+	}
 	
 }
