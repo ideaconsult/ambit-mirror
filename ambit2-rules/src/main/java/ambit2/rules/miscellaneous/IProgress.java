46148d1964ab4f09bf2bb463d911ffce61e86a14
Nikolay Kochev <nick@uni-plovdiv.net>
2018-03-16 10:24:22 +0000

    refactoring: typo

--- ambit2-rules/src/main/java/ambit2/rules/miscellaneous/IProgress.java
+++ ambit2-rules/src/main/java/ambit2/rules/miscellaneous/IProgress.java
@@ -2,8 +2,8 @@ package ambit2.rules.miscellaneous;
 
 public interface IProgress 
 {
-	public double getPercetage();
-	public void setPercetage(double percentage);
+	public double getPercentage();
+	public void setPercentage(double percentage);
 	public String getInfo();	
 	public void setInfo(String info);
 	
