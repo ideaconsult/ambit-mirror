d29f1bce9e8ae5f6a773e825409979cef7cc36a9
ntk73 <nick@uni-plovdiv.net>
2018-10-31 14:40:18 +0200

    Added functions setStructure() and generateZwitterions()

--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/zwitterion/ZwitterionManager.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/zwitterion/ZwitterionManager.java
@@ -1,6 +1,12 @@
 package ambit2.tautomers.zwitterion;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
+
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+import ambit2.tautomers.TautomerConst;
 
 
 
@@ -9,15 +15,32 @@ public class ZwitterionManager
 	protected static final Logger logger = Logger
 			.getLogger(ZwitterionManager.class.getName());
 	
+	IAtomContainer originalMolecule = null;
+	IAtomContainer molecule = null;
 	
-		
+	List<String> errors = new ArrayList<String>();
+	int numOfRegistrations = 0;
+	int status = TautomerConst.STATUS_NONE;
+	
 	public ZwitterionManager() 
 	{
 		init();
 	}
 	
 	void init() 
-	{
-		//TODO
+	{	
 	}
+	
+	public void setStructure(IAtomContainer str) throws Exception {
+		molecule = str;
+		originalMolecule = str;
+	}	
+	
+	public List<IAtomContainer> generateZwitterions() throws Exception {
+		status = TautomerConst.STATUS_STARTED;
+		numOfRegistrations = 0;
+		//TODO
+		
+		return null;
+	}	
 }
