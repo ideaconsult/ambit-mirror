92ee79ea31c19f328e5e79dc78a8a922638737ae
ntk73 <nick@uni-plovdiv.net>
2018-10-31 23:28:16 +0200

    Added methods in IBasicCenter

--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/zwitterion/AminoGroupState.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/zwitterion/AminoGroupState.java
@@ -8,4 +8,22 @@ public class AminoGroupState implements IBasicCenter
 	public int numHAtoms = 0;
 	public int numHeavyAtoms = 0;
 	public int charge = 0;
+	
+	State state = State.NEUTRAL;
+	
+	@Override
+	public State getState() {
+		return state;
+	}
+	
+	@Override
+	public void setState(State state) {
+		this.state = state;
+		// TODO 		
+	}
+	
+	@Override
+	public void shiftState() {
+		//TODO 
+	}	
 }
--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/zwitterion/IBasicCenter.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/zwitterion/IBasicCenter.java
@@ -1,9 +1,12 @@
 package ambit2.tautomers.zwitterion;
 
 public interface IBasicCenter 
-{
-	
+{	
 	public static enum State {
 		NEUTRAL, CATION
 	}
+	
+	public State getState();
+	public void setState(State state);
+	public void shiftState();
 }
