d3585d45396bcd02a3d6b14b1e35806911dd0833
ntk73 <nick@uni-plovdiv.net>
2018-10-31 14:20:34 +0200

    Added new package zwitterion and new class ZwitterionManager

---
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/zwitterion/ZwitterionManager.java
@@ -0,0 +1,23 @@
+package ambit2.tautomers.zwitterion;
+
+import java.util.logging.Logger;
+
+
+
+public class ZwitterionManager 
+{
+	protected static final Logger logger = Logger
+			.getLogger(ZwitterionManager.class.getName());
+	
+	
+		
+	public ZwitterionManager() 
+	{
+		init();
+	}
+	
+	void init() 
+	{
+		//TODO
+	}
+}
