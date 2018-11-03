f838c8c86c21cc96032187ed892cdb81d44b475b
ntk73 <nick@uni-plovdiv.net>
2018-11-03 23:51:00 +0200

    Implemented searchAllAcidicAndBasicCenters()

--- ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/zwitterion/ZwitterionManager.java
+++ ambit2-all/ambit2-tautomers/src/main/java/ambit2/tautomers/zwitterion/ZwitterionManager.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 
 import ambit2.tautomers.TautomerConst;
@@ -19,16 +20,18 @@ public class ZwitterionManager
 	IAtomContainer molecule = null;
 	List<IAcidicCenter> initialAcidicCenters = new ArrayList<IAcidicCenter>();
 	List<IBasicCenter> initialBasicCenters = new ArrayList<IBasicCenter>();	
-	List<IAcidicCenter> acidicCenters = new ArrayList<IAcidicCenter>();
-	List<IBasicCenter> basicCenters = new ArrayList<IBasicCenter>();
-	
-	//TODO add flags for S and P based acids
-	
+	List<IAcidicCenter> acidicCenters = null;
+	List<IBasicCenter> basicCenters = null;
+		
 	List<String> errors = new ArrayList<String>();
 	int numOfRegistrations = 0;
 	int status = TautomerConst.STATUS_NONE;
 		
-	
+	public boolean FlagUseCarboxylicGroups = true;
+	public boolean FlagUseSulfonicGroups = true;
+	public boolean FlagUseSulfinicGroups = true;
+	public boolean FlagUsePhosphoricGroups = true;
+		
 	
 	public ZwitterionManager() 
 	{
@@ -58,7 +61,27 @@ public class ZwitterionManager
 	
 	void searchAllAcidicAndBasicCenters()
 	{
-		//TODO groups search and filtration
+		for (IAtom atom : molecule.atoms())
+		{
+			switch (atom.getAtomicNumber())
+			{
+			case 6:
+				if (FlagUseCarboxylicGroups)
+				{
+					IAcidicCenter c = CarboxylicGroup.getCenter(molecule, atom);
+					if (c != null)
+						initialAcidicCenters.add(c);
+				}
+				break;
+			case 7:
+				IBasicCenter c = AminoGroup.getCenter(molecule, atom);
+				if (c != null)
+					initialBasicCenters.add(c);
+				break;
+			}
+		}
 		
+		acidicCenters = initialAcidicCenters;
+		basicCenters = initialBasicCenters;
 	}
 }
