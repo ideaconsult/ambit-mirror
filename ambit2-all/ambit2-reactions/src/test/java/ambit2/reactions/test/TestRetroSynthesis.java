7143759687394cb4b8c106eee798d3e0847054a5
ngn <jeliazkova.nina@gmail.com>
2018-12-08 17:58:35 +0200

    test refactored to avoid error "no runnable methods" T128

--- ambit2-all/ambit2-reactions/src/test/java/ambit2/reactions/test/TestRetroSynthesis.java
+++ ambit2-all/ambit2-reactions/src/test/java/ambit2/reactions/test/TestRetroSynthesis.java
@@ -1,39 +1,47 @@
 package ambit2.reactions.test;
 
+import org.junit.Test;
 import org.openscience.cdk.interfaces.IAtomContainer;
 
 import ambit2.reactions.retrosynth.RetroSynthesis;
 import ambit2.reactions.retrosynth.RetroSynthesisResult;
 import ambit2.smarts.SmartsHelper;
-public class TestRetroSynthesis 
-{
+
+public class TestRetroSynthesis {
 	RetroSynthesis retroSyn;
-	
-	
-	public static void main(String[] args) throws Exception 
-	{
-		
+
+	public static void main(String[] args) throws Exception {
+
 		TestRetroSynthesis trs = new TestRetroSynthesis();
 		trs.retroSyn = new RetroSynthesis();
-		//System.out.println("Retro Synthesis Knowledge base:\n" + trs.retroSyn.getReactionKnowledgeBase().toString());
-		
-		
-		//trs.test("CCNS");
-		trs.test("c1[n+](S)ccn1O");
+		// System.out.println("Retro Synthesis Knowledge base:\n" +
+		// trs.retroSyn.getReactionKnowledgeBase().toString());
+
+		// trs.test("CCNS");
+		trs.testSmi("c1[n+](S)ccn1O");
 	}
-	
-	
-	public void test(String smi) throws Exception
-	{	
+
+	@Test
+	public void test() throws Exception {
+		retroSyn = new RetroSynthesis();
+		// System.out.println("Retro Synthesis Knowledge base:\n" +
+		// trs.retroSyn.getReactionKnowledgeBase().toString());
+
+		// trs.test("CCNS");
+		testSmi("c1[n+](S)ccn1O");
+	}
+
+	public void testSmi(String smi) throws Exception {
 		System.out.println("Testing Retro Synthesis for " + smi);
 		IAtomContainer mol = SmartsHelper.getMoleculeFromSmiles(smi);
-		//System.out.println("Atom attributes: \n" + SmartsHelper.getAtomsAttributes(mol));
-		
+		// System.out.println("Atom attributes: \n" +
+		// SmartsHelper.getAtomsAttributes(mol));
+
 		System.out.println();
 		retroSyn.setStructure(mol);
-		RetroSynthesisResult result = retroSyn.run(); 
-		
+		RetroSynthesisResult result = retroSyn.run();
+
 		System.out.println("Retro Synthesis result:\n" + result.toString());
 	}
-	
+
 }
