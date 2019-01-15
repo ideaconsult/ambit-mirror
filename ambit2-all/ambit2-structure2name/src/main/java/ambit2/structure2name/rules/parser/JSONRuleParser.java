279ff01abbb6d62e95c40aaf4d2ca8f55386f059
Nikolay Kochev <nick@uni-plovdiv.net>
2019-01-15 17:19:50 +0200

    Added function loadIUPACRuleDataBase()

--- ambit2-all/ambit2-structure2name/src/main/java/ambit2/structure2name/rules/parser/JSONRuleParser.java
+++ ambit2-all/ambit2-structure2name/src/main/java/ambit2/structure2name/rules/parser/JSONRuleParser.java
@@ -1,6 +1,29 @@
 package ambit2.structure2name.rules.parser;
 
+import java.io.FileInputStream;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ambit2.structure2name.IUPACRuleDataBase;
+
 public class JSONRuleParser 
 {
-	
+	public IUPACRuleDataBase loadIUPACRuleDataBase (String jsonFileName) throws Exception 
+	{
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode rootNode = null;
+
+		try (FileInputStream fin = new FileInputStream(jsonFileName)) {
+			rootNode = mapper.readTree(fin);
+		} catch (Exception x) {
+			throw x;
+		} finally {
+
+		}
+		
+		//TODO
+		
+		return null;
+	}	
 }
