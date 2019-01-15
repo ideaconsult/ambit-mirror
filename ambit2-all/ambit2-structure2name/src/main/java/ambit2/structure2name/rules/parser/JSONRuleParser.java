e76f19f1652cfd82504903f1a7f5d6e68b2027d5
ntk73 <nick@uni-plovdiv.net>
2019-01-15 20:57:05 +0200

    Added JSON error handling code. Added basic alkane info in section CARBONS

--- ambit2-all/ambit2-structure2name/src/main/java/ambit2/structure2name/rules/parser/JSONRuleParser.java
+++ ambit2-all/ambit2-structure2name/src/main/java/ambit2/structure2name/rules/parser/JSONRuleParser.java
@@ -1,6 +1,8 @@
 package ambit2.structure2name.rules.parser;
 
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,6 +11,28 @@ import ambit2.structure2name.rules.IUPACRuleDataBase;
 
 public class JSONRuleParser 
 {
+	private List<String> errors = null;
+
+	void addError(String err) {
+		if (errors == null)
+			errors = new ArrayList<String>();
+		errors.add(err);
+	}
+
+	public List<String> getErrors() {
+		return errors;
+	}
+
+	public String getAllErrors() {
+		if (errors == null)
+			return "";
+
+		StringBuffer sb = new StringBuffer();
+		for (String err : errors)
+			sb.append(err + "\n");
+		return sb.toString();
+	}
+	
 	public IUPACRuleDataBase loadIUPACRuleDataBase (String jsonFileName) throws Exception 
 	{
 		ObjectMapper mapper = new ObjectMapper();
