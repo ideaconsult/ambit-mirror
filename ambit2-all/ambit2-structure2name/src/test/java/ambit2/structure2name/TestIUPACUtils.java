3a1b478de82e1d15f5d5cce1b8673c2ac0e1a71f
ntk73 <nick@uni-plovdiv.net>
2019-01-29 17:07:41 +0200

    Implemented function testIUPACNameGen()

--- ambit2-all/ambit2-structure2name/src/test/java/ambit2/structure2name/TestIUPACUtils.java
+++ ambit2-all/ambit2-structure2name/src/test/java/ambit2/structure2name/TestIUPACUtils.java
@@ -1,12 +1,16 @@
 package ambit2.structure2name;
 
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+import ambit2.smarts.SmartsHelper;
 import ambit2.structure2name.rules.IUPACRuleDataBase;
 
 public class TestIUPACUtils 
 {
 	public static void main(String[] args) throws Exception 
 	{
-		testIUPACRuleDB();
+		//testIUPACRuleDB();
+		testIUPACNameGen("CCCCC");
 	}
 	
 	public static void testIUPACRuleDB() throws Exception
@@ -16,5 +20,17 @@ public class TestIUPACUtils
 		{
 			System.out.println(rdb.carbonData[i].toString());
 		}
+	}
+	
+	public static void testIUPACNameGen(String smiles) throws Exception
+	{
+		System.out.println("Testing IUPACNameGenerator for " + smiles);
+		IAtomContainer mol = SmartsHelper.getMoleculeFromSmiles(smiles);
+		IUPACNameGenerator gen = new IUPACNameGenerator();
+		
+		String name = gen.generateIUPACName(mol);
+		String s = gen.getComponentDataAsString();
+		System.out.println(s);
+		
 	}
 }
