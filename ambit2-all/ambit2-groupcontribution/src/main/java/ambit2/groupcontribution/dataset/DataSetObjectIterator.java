573c807887cf54101dd0775e74699feb1303fa5a
ntk73 <nick@uni-plovdiv.net>
2019-06-29 21:59:06 +0300

    Implemented DataSetObjectIterator contructor and functions init() and close()

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/dataset/DataSetObjectIterator.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/dataset/DataSetObjectIterator.java
@@ -1,10 +1,39 @@
 package ambit2.groupcontribution.dataset;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Iterator;
+
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.io.iterator.IIteratingChemObjectReader;
 
 public class DataSetObjectIterator implements Iterator<DataSetObject> 
 {
-
+	File dataFile = null;
+	File localPropertiesFile = null;
+	
+	IIteratingChemObjectReader<IAtomContainer> molReader = null;
+	
+	public DataSetObjectIterator(File dataFile, File localPropertiesFile) throws Exception
+	{
+		this.dataFile = dataFile;
+		this.localPropertiesFile = localPropertiesFile;
+		init();
+	}
+	
+	protected void init() throws Exception
+	{
+		if (!dataFile.exists()) 
+			throw new FileNotFoundException(dataFile.getAbsolutePath());
+		
+		//Setup molReader
+		InputStream in = new FileInputStream(dataFile);
+		molReader = DataSet.getReader(in,dataFile.getName());
+	}
+	
+	
 	@Override
 	public boolean hasNext() {
 		// TODO 
@@ -15,6 +44,13 @@ public class DataSetObjectIterator implements Iterator<DataSetObject>
 	public DataSetObject next() {
 		// TODO 
 		return null;
+	}
+	
+	public void close()
+	{
+		try {
+			molReader.close(); 
+		} catch (Exception x) {}
 	}
 
 }
