39283bbdc2ce52d5a81db6b7c66976e690189a65
ntk73 <nick@uni-plovdiv.net>
2019-07-03 22:36:54 +0300

    Implemented function init():initialization of CSV reader and fucntion close()

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/dataset/LocalPropertiesIterator.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/dataset/LocalPropertiesIterator.java
@@ -1,21 +1,43 @@
 package ambit2.groupcontribution.dataset;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Iterator;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 public class LocalPropertiesIterator implements Iterator<LocalProperties> 
 {
-	File file = null;
+	protected File file = null;
+	protected CSVParser parser = null;
+	protected Iterator<CSVRecord> iterator = null;
+	protected CSVFormat format = null;
 	
 	public LocalPropertiesIterator(File file) throws Exception
 	{
 		this.file = file;
+		format = CSVFormat.DEFAULT.withDelimiter(',').withQuote('\"');
+		init();
+	}
+	
+	public LocalPropertiesIterator(File file, CSVFormat format) throws Exception
+	{
+		this.file = file;
+		this.format = format;
 		init();
 	}
 	
 	protected void init() throws Exception
-	{
-		//TODO
+	{	
+		InputStream in = new FileInputStream(file);
+		Reader reader = new InputStreamReader(in);
+		parser = format.withHeader().parse(reader);
+		iterator = parser.iterator();
 	}
 	
 	@Override
@@ -28,6 +50,12 @@ public class LocalPropertiesIterator implements Iterator<LocalProperties>
 	public LocalProperties next() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public void close() throws Exception
+	{
+		if (parser != null)
+			parser.close();
 	}
 	
 }
