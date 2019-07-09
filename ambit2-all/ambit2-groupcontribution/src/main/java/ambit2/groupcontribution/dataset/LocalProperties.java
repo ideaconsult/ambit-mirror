e566508f15a37ab7878b16c2f27912aa67459324
ntk73 <nick@uni-plovdiv.net>
2019-07-09 12:27:53 +0300

    Implemented functions: indicesToAtoms() and indicesToChemLocPropObject()

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/dataset/LocalProperties.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/dataset/LocalProperties.java
@@ -2,13 +2,45 @@ package ambit2.groupcontribution.dataset;
 
 import java.util.Map;
 
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IAtomContainer;
+
 
 public class LocalProperties 
 {
 	public static enum Type {
-		ATOM, BOND, ATOM_PAIR
+		ATOM, BOND, ATOM_PAIR, ATOM_TRIPLE
 	}
 	
 	public Type type = Type.ATOM;
 	public Map<Object, double[]> properties = null;
+	
+	
+	public static IAtom[] indicesToAtoms(int ind[], IAtomContainer mol)
+	{
+		IAtom atoms[] = new IAtom[ind.length];
+		for (int i = 0; i < ind.length; i++)
+			atoms[i] = mol.getAtom(ind[i]);
+		return null;
+	}
+	
+	public static Object indicesToChemLocPropObject(int ind[], IAtomContainer mol, Type locPropType)
+	{
+		Object obj = null;
+		switch (locPropType)
+		{
+		case ATOM:
+			obj = mol.getAtom(ind[0]);
+			break;
+		case BOND:
+			obj = mol.getBond(mol.getAtom(ind[0]), mol.getAtom(ind[1]));
+			break;	
+		case ATOM_PAIR:
+		case ATOM_TRIPLE:	
+			obj = indicesToAtoms(ind, mol);
+			break;	
+		}
+		
+		return obj;
+	}
 }
