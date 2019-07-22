5b1879f86530857805ecd8a5ecc757dde4b84ab3
n10705013 <jeliazkova.nina@gmail.com>
2019-07-22 22:44:32 +0300

    T555 context is only available in the component start() method not in the constructor

--- ambit2-apps/ambit2-www/src/main/java/ambit2/rest/AmbitComponent.java
+++ ambit2-apps/ambit2-www/src/main/java/ambit2/rest/AmbitComponent.java
@@ -1,31 +1,42 @@
 package ambit2.rest;
 
+import javax.servlet.ServletContext;
+
 import org.restlet.Application;
 import org.restlet.Context;
 
 /**
- * This is used as a servlet component instead of the core one, to be able to attach protocols 
+ * This is used as a servlet component instead of the core one, to be able to
+ * attach protocols
+ * 
  * @author nina
  *
  */
 public class AmbitComponent extends RESTComponent {
 
+	public AmbitComponent() {
+		this(null);
+	}
 
-		public AmbitComponent() {
-			this(null);
-		}
-		public AmbitComponent(Context context,Application[] applications) {
-			super(context,applications);
-			
-		
-		}
-		public AmbitComponent(Context context,boolean standalone) {
-			this(context,new Application[]{new AmbitApplication(true)});
-		}
-		public AmbitComponent(Context context) {
-			this(context,new Application[]{new AmbitApplication()});
-		}
-		
-		
+	public AmbitComponent(Context context, Application[] applications) {
+		super(context, applications);
+
+	}
+
+	public AmbitComponent(Context context, boolean standalone) {
+		this(context, new Application[] { new AmbitApplication(true) });
+	}
+
+	public AmbitComponent(Context context) {
+		this(context, new Application[] { new AmbitApplication() });
+	}
+	
+	
+	@Override
+	public synchronized void start() throws Exception {
+		//ServletContext ctx = (ServletContext) getContext().getServerDispatcher().getContext().getAttributes().get("org.restlet.ext.servlet.ServletContext");
+		//System.out.println(ctx.getContextPath());
+		super.start();
+	}
 
 }
