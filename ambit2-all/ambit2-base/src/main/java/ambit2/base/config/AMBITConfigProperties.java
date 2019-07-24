e908ee1ce4f41919cd69f1759aef519aef43e43d
n10705013 <jeliazkova.nina@gmail.com>
2019-07-24 12:11:31 +0300

    T555 refactoring - moved parent class to modbcum-i package

--- ambit2-all/ambit2-base/src/main/java/ambit2/base/config/AMBITConfigProperties.java
+++ ambit2-all/ambit2-base/src/main/java/ambit2/base/config/AMBITConfigProperties.java
@@ -1,136 +1,24 @@
 package ambit2.base.config;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
 
-public class AMBITConfigProperties {
+import net.idea.modbcum.i.config.ConfigProperties;
+
+public class AMBITConfigProperties extends ConfigProperties {
 	
 	public static final String ambitProperties = "ambit2/rest/config/ambit2.pref";
 	public static final String templateProperties = "ambit2/rest/config/ambit2.assay.properties";
 	public static final String configProperties = "ambit2/rest/config/config.prop";
 	public static final String loggingProperties = "ambit2/rest/config/logging.prop";
 	
-	protected String AMBIT_CONFIG_OVERRIDE_VAR = "AMBIT_CONFIG_OVERRIDE_VAR";
-	public String getConfigOverrideVar() {
-		return AMBIT_CONFIG_OVERRIDE_VAR;
-	}
-
-	public void setConfigOverrideVar(String configOverrideVar) {
-		this.AMBIT_CONFIG_OVERRIDE_VAR = configOverrideVar;
-	}
-
-	protected ConcurrentHashMap<String, Properties> properties = new ConcurrentHashMap<String, Properties>();
-	protected File overridePath = null;
-
-	public File getConfigOverrideDir(String override_var) {
-		try {
-			String path = System.getenv(override_var);
-			if (path != null) {
-				File dir = new File(path);
-				if (dir.exists() && dir.isDirectory())
-					return dir;
-			}
-			return null;
-		} catch (Exception x) {
-			return null;
-		}
-
-	}
 
 	public AMBITConfigProperties() {
-		this(null);
+		super(null);
 	}
 
 
 	public AMBITConfigProperties(File overridePath) {
-		this.overridePath = overridePath;
+		super(overridePath);
 	}
 
-	public synchronized Properties getProperties(String config) {
-		try {
-			Properties p = properties.get(config);
-			if (p == null) {
-				// Load the internal config as defaults
-				Properties defaults = new Properties();
-				try (InputStream in = this.getClass().getClassLoader().getResourceAsStream(config)) {
-					defaults.load(in);
-				}
-				if (getConfigOverrideVar() !=null && defaults.get(getConfigOverrideVar()) != null) {
-					File dir = getConfigOverrideDir(defaults.get(getConfigOverrideVar()).toString());
-					if (dir != null && !"".equals(dir.getAbsolutePath()))
-						overridePath = dir;
-				}
-				if (overridePath != null) {
-					p = new Properties(defaults);
-					String[] segments = config.split("/");
-
-					File file = new File(overridePath, segments[segments.length - 1]);
-					if (file.exists())
-						try (InputStream in = new FileInputStream(file)) {
-							p.load(in);
-							//no overriding of the var
-							p.remove(AMBIT_CONFIG_OVERRIDE_VAR);
-						}
-				} else
-					p = defaults;
-				properties.put(config, p);
-
-			}
-			return p;
-
-		} catch (Exception x) {
-			return null;
-		}
-	}
-
-	protected synchronized String getProperty(String name, String config) {
-		try {
-			Properties p = getProperties(config);
-			return p.getProperty(name);
-		} catch (Exception x) {
-			return null;
-		}
-	}
-
-	public synchronized String getPropertyWithDefault(String name, String config, String defaultValue) {
-		try {
-			String value = getProperty(name, config);
-			if (value == null)
-				return defaultValue;
-			else if (value != null && value.startsWith("${"))
-				return defaultValue;
-			else
-				return value;
-		} catch (Exception x) {
-			return defaultValue;
-		}
-	}
-
-	public synchronized Long getPropertyWithDefaultLong(String name, String config, Long defaultValue) {
-		try {
-			String value = getProperty(name, config);
-			if (value == null)
-				return defaultValue;
-			else if (value != null && value.startsWith("${"))
-				return defaultValue;
-			else
-				return Long.parseLong(value);
-		} catch (Exception x) {
-			return defaultValue;
-		}
-	}
-
-	public synchronized boolean getBooleanPropertyWithDefault(String name, String config, boolean defaultValue) {
-		try {
-			String attach = getProperty(name, config);
-			if (attach != null && attach.startsWith("${"))
-				return defaultValue;
-			return attach == null ? defaultValue : Boolean.parseBoolean(attach);
-		} catch (Exception x) {
-			return defaultValue;
-		}
-	}
 }
