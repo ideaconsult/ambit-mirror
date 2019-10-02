c19c698630bf9ff930c25b0b812a86ceee16420f
ntk73 <nick@uni-plovdiv.net>
2019-10-02 22:03:19 +0300

    Implemented basic fields in classes

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/Substituent.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/Substituent.java
@@ -1,6 +1,16 @@
 package ambit2.groupcontribution.nmr;
 
+import ambit2.smarts.groups.GroupMatch;
+
 public class Substituent 
 {
-
+	public Substituent()
+	{	
+	}
+	
+	public String info = "";
+	public String smarts = "";
+	public double chemShifts[] = null;
+	
+	public GroupMatch groupMatch = null;
 }
--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/nmr_1h/HAtomEnvironment.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/nmr_1h/HAtomEnvironment.java
@@ -1,6 +1,17 @@
 package ambit2.groupcontribution.nmr.nmr_1h;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ambit2.groupcontribution.nmr.Substituent;
+
 public class HAtomEnvironment 
 {
+	public String name = null;
+	public String info = "";
+	public double chemShift0 = 0.0;
+	public String substituentPositions[] = null;
+	
+	public List<Substituent> substituents = new ArrayList<Substituent>(); 
 	
 }
