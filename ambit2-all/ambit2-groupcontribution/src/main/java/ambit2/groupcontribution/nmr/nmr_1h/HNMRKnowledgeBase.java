5b81cb1770a4a08d1fef097a485374f2ae6dfcc5
ntk73 <nick@uni-plovdiv.net>
2019-10-02 22:52:56 +0300

    Added basic fields and function configure()

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/nmr_1h/HNMRKnowledgeBase.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/nmr_1h/HNMRKnowledgeBase.java
@@ -1,5 +1,30 @@
 package ambit2.groupcontribution.nmr.nmr_1h;
 
-public class HNMRKnowledgeBase {
+import java.util.ArrayList;
+import java.util.List;
 
+public class HNMRKnowledgeBase 
+{
+	public List<HAtomEnvironment> hAtomEnvironments = new ArrayList<HAtomEnvironment>();
+	
+	public List<String> errors = new ArrayList<String>();
+	
+	public HNMRKnowledgeBase()
+	{	
+	}
+	
+	public void configure()
+	{
+		for (int i = 0; i < hAtomEnvironments.size(); i++)
+		{
+			HAtomEnvironment hae = hAtomEnvironments.get(i);
+			configure(hae);
+		}
+	}
+	
+	public void configure(HAtomEnvironment hae)
+	{
+		//TODO
+	}
+	
 }
--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/nmr_1h/HNMRPredefinedKnowledgeBase.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/nmr_1h/HNMRPredefinedKnowledgeBase.java
@@ -2,7 +2,7 @@ package ambit2.groupcontribution.nmr.nmr_1h;
 
 public class HNMRPredefinedKnowledgeBase 
 {
-	public static HNMRKnowledgeBase getHNMRKnoledgeBase()
+	public static HNMRKnowledgeBase getHNMRKnowledgeBase()
 	{
 		//TODO
 		return null;
