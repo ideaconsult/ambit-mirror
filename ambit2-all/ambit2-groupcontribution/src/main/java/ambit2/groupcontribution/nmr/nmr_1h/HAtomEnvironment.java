7ee1cc902fed1bac4b037e44f03bceced13dff8c
ntk73 <nick@uni-plovdiv.net>
2019-10-06 20:32:24 +0300

    Added fields in HAtomEnvironment

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/nmr_1h/HAtomEnvironment.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/nmr_1h/HAtomEnvironment.java
@@ -4,14 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ambit2.groupcontribution.nmr.Substituent;
+import ambit2.smarts.groups.GroupMatch;
 
 public class HAtomEnvironment 
 {
 	public String name = null;
+	public String smarts = null;
 	public String info = "";
 	public double chemShift0 = 0.0;
-	public String substituentPositions[] = null;
-	
+	public String substituentPosDesignations[] = null;
+	public int substituentPosAtomIndex[] = null;
+		
 	public List<Substituent> substituents = new ArrayList<Substituent>(); 
 	
+	public GroupMatch groupMatch = null;
 }
