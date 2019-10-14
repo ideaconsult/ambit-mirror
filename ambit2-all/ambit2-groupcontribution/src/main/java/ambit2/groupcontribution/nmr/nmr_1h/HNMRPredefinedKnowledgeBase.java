22fa126b99f333217f8265cbc8dd645afbc6dd74
ntk73 <nick@uni-plovdiv.net>
2019-10-14 23:25:05 +0300

    Added functionality for predefined knowledge base

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/nmr_1h/HNMRPredefinedKnowledgeBase.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/nmr_1h/HNMRPredefinedKnowledgeBase.java
@@ -1,10 +1,32 @@
 package ambit2.groupcontribution.nmr.nmr_1h;
 
+import java.util.List;
+
 public class HNMRPredefinedKnowledgeBase 
 {
+	public static final String ALKANES[] = 
+	{
+		"$$NAME= ALKANES $$SMARTS= [C;^3]",
+		"$$INFO= ",
+		"$$BASIC_SHIFT = 0.83",
+		""
+	};
+
+	
+	
 	public static HNMRKnowledgeBase getHNMRKnowledgeBase()
 	{
 		//TODO
 		return null;
 	}
+	
+	
+	public static HAtomEnvironment parseHAtomEnvironment(String lines[], List<String> errors)
+	{
+		
+		//TODO	
+		return null;
+	}
+	
+		
 }	
