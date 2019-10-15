e4dd131d34311e3dec73cda05effbaf571a867e5
ntk73 <nick@uni-plovdiv.net>
2019-10-15 22:59:34 +0300

    Implemented functions parserLine() and parserKeyWord()

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/nmr_1h/HNMRPredefinedKnowledgeBase.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/nmr_1h/HNMRPredefinedKnowledgeBase.java
@@ -2,9 +2,11 @@ package ambit2.groupcontribution.nmr.nmr_1h;
 
 import java.util.List;
 
+
 public class HNMRPredefinedKnowledgeBase 
 {
-	public static final String KeyWordPrefix = "$$";
+	public static final String keyWordPrefix = "$$";
+	public static final String keyWordSeparator = "=";
 	
 	public static final String ALKANES[] = 
 	{
@@ -31,17 +33,63 @@ public class HNMRPredefinedKnowledgeBase
 		
 		for (int i = 0; i < lines.length; i++)
 		{
-			parseLine(haEnv, errorPrefix + " line " + (i+1), errors);
+			parseLine(lines[i], haEnv, errorPrefix + " line " + (i+1), errors);
 		}
 		
+		postProcess (haEnv, errors);
 		return haEnv;
 	}
 	
-	public static int parseLine(HAtomEnvironment haEnv, String errorPrefix, List<String> errors)
+	
+	public static int parseLine(String line, HAtomEnvironment haEnv, String errorPrefix, List<String> errors)
 	{
-		//TODO
+		int res = line.indexOf(keyWordPrefix, 0);
+		int curPos = res;
+		
+		while (res != -1)
+		{	
+			res = line.indexOf(keyWordPrefix, curPos + keyWordPrefix.length());
+			String keyword;
+			if (res == -1)
+				keyword = line.substring(curPos);
+			else
+			{	
+				keyword = line.substring(curPos,res);
+				curPos = res;	
+			}
+			
+			parseKeyWord(keyword, haEnv, errorPrefix, errors);
+		}	
 		return 0;
 	}
 	
 		
+	public static void parseKeyWord(String keyword, HAtomEnvironment haEnv, String errorPrefix, List<String> errors)
+	{
+		int sepPos = keyword.indexOf(keyWordSeparator);		
+		if (sepPos == -1)
+		{	
+			errors.add(errorPrefix + " Incorrect key word syntax: " + keyword);
+			return;
+		}
+		
+		String key = keyword.substring(keyWordPrefix.length(), sepPos).trim();		
+		String keyValue = keyword.substring(sepPos+1).trim();
+		
+		
+		if (key.equals("NAME"))	
+		{	
+			haEnv.name = keyValue;
+			return;
+		}
+		
+		errors.add(errorPrefix + " Unknow key word: " + key);
+	}
+	
+	public static void postProcess(HAtomEnvironment haEnv, List<String> errors)
+	{
+		//TODO
+	}
+	
+	
 }	
