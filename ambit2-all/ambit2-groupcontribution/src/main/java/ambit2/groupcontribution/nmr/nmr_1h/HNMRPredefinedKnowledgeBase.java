31929eb795f809e54e8b59ec0993c23cc0d5b754
ntk73 <nick@uni-plovdiv.net>
2019-10-15 21:53:18 +0300

    Implemented function parserHAtomEnvironment(). Added function parseLine()

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/nmr_1h/HNMRPredefinedKnowledgeBase.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/nmr_1h/HNMRPredefinedKnowledgeBase.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class HNMRPredefinedKnowledgeBase 
 {
+	public static final String KeyWordPrefix = "$$";
+	
 	public static final String ALKANES[] = 
 	{
 		"$$NAME= ALKANES $$SMARTS= [C;^3]",
@@ -11,21 +13,34 @@ public class HNMRPredefinedKnowledgeBase
 		"$$BASIC_SHIFT = 0.83",
 		""
 	};
-
 	
 	
 	public static HNMRKnowledgeBase getHNMRKnowledgeBase()
 	{
-		//TODO
-		return null;
+		HNMRKnowledgeBase knowledgeBase = new HNMRKnowledgeBase();
+		
+		parseHAtomEnvironment (ALKANES, "Alkanes", knowledgeBase.errors);
+		
+		return knowledgeBase;
 	}
 	
 	
-	public static HAtomEnvironment parseHAtomEnvironment(String lines[], List<String> errors)
+	public static HAtomEnvironment parseHAtomEnvironment(String lines[], String errorPrefix, List<String> errors)
 	{
+		HAtomEnvironment haEnv = new HAtomEnvironment();
 		
-		//TODO	
-		return null;
+		for (int i = 0; i < lines.length; i++)
+		{
+			parseLine(haEnv, errorPrefix + " line " + (i+1), errors);
+		}
+		
+		return haEnv;
+	}
+	
+	public static int parseLine(HAtomEnvironment haEnv, String errorPrefix, List<String> errors)
+	{
+		//TODO
+		return 0;
 	}
 	
 		
