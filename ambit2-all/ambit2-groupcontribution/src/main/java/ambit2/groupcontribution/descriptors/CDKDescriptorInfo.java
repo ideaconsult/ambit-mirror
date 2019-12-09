030493caddceec60fa1071d0326a7770808c8c01
ntk73 <nick@uni-plovdiv.net>
2019-12-09 22:03:47 +0200

    Added new class CDKDescriptorInfo

---
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/descriptors/CDKDescriptorInfo.java
@@ -0,0 +1,9 @@
+package ambit2.groupcontribution.descriptors;
+
+import org.openscience.cdk.qsar.IMolecularDescriptor;
+
+public class CDKDescriptorInfo 
+{
+	public String name = null;
+	public IMolecularDescriptor descr = null;
+}
