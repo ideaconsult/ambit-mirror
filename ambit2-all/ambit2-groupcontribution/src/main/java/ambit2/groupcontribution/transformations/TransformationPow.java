2ce7457b799591c4d92f0b13db43dc99eff36db5
ntk73 <nick@uni-plovdiv.net>
2019-12-10 15:27:41 +0200

    Added new class TransformationPow

---
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/transformations/TransformationPow.java
@@ -0,0 +1,38 @@
+package ambit2.groupcontribution.transformations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransformationPow implements IValueTransformation 
+{
+	double power = 1;
+	
+	public TransformationPow (double power) {
+		this.power = power;
+	}
+	
+	@Override
+	public String getTransformationName() {
+		return "POW(" + power + ")";
+	}
+
+	@Override
+	public List<Double> getParameters() {
+		List<Double> params = new ArrayList<Double>(); 
+		params.add(power);
+		return params;
+	}
+
+	@Override
+	public List<String> getParameterNames() {
+		List<String> paramNames = new ArrayList<String>(); 
+		paramNames.add("power");
+		return paramNames;
+	}
+
+	@Override
+	public double transform(double value) throws Exception {
+		return Math.pow(value, power);
+	}
+	
+}
