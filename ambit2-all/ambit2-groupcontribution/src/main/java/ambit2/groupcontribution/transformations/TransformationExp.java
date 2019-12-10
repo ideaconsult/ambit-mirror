6c65a39b31a7df00cf70a25dbac30ade9b9aa4be
ntk73 <nick@uni-plovdiv.net>
2019-12-10 15:07:48 +0200

    Added class TransformExp

---
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/transformations/TransformationExp.java
@@ -0,0 +1,28 @@
+package ambit2.groupcontribution.transformations;
+
+import java.util.List;
+
+public class TransformationExp implements IValueTransformation 
+{
+
+	@Override
+	public String getTransformationName() {
+		return "EXP";
+	}
+
+	@Override
+	public List<Double> getParameters() {
+		return null;
+	}
+
+	@Override
+	public List<String> getParameterNames() {
+		return null;
+	}
+
+	@Override
+	public double transform(double value) throws Exception {
+		return Math.exp(value);
+	}
+
+}
