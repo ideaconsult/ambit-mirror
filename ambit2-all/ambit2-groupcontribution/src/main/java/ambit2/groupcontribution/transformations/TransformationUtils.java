ab7ebcabf197bccb84659779e2b4c20c77927189
ntk73 <nick@uni-plovdiv.net>
2019-12-10 15:45:04 +0200

    Initial version of function parseTransformation()

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/transformations/TransformationUtils.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/transformations/TransformationUtils.java
@@ -1,10 +1,44 @@
 package ambit2.groupcontribution.transformations;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TransformationUtils 
 {
-	public static IValueTransformation parse(String s)
+	public List<String> errors = new ArrayList<String>();
+		
+	public IValueTransformation parseTransformation(String s)
 	{
-		//TODO
+		errors.clear();
+		String transformationName = null;
+		String paramsString = null;
+		
+		int ind = s.indexOf("(");
+		
+		if (ind == -1)
+		{
+			transformationName = s;
+		}
+		else
+		{	
+			if (ind == 0)
+			{
+				errors.add("Incorrect transformation: string starts with '('");
+				return null;
+			}
+			else
+			{
+				if (!s.endsWith(")"))
+				{
+					errors.add("Incorrect transformation: missing closing bracket ')'");
+					return null;
+				}
+				
+				transformationName = s.substring(0, ind);
+				paramsString = s.substring(ind+1, s.length()-1);
+			}
+		}
+		
 		return null;
 	}
 }
