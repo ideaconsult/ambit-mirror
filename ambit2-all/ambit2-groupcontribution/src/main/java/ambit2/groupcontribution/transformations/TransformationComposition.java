a7669652d8a3e1793e8e6b4bbc5a489b970f1476
ntk73 <nick@uni-plovdiv.net>
2019-12-11 20:54:29 +0200

    Added new class TransformationComposition

---
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/transformations/TransformationComposition.java
@@ -0,0 +1,48 @@
+package ambit2.groupcontribution.transformations;
+
+import java.util.List;
+
+public class TransformationComposition implements IValueTransformation 
+{
+	public static final String composeSeparator = ">";
+	
+	IValueTransformation transformations[] = null;
+	
+	public TransformationComposition(IValueTransformation transformations[])
+	{
+		this.transformations = transformations;
+	}
+	
+	@Override
+	public String getTransformationName() {
+		StringBuffer sb = new StringBuffer(); 
+		for (int i = 0; i < transformations.length; i++)
+		{
+			sb.append(transformations[i].getTransformationName());
+			if (i < (transformations.length-1) )
+				sb.append(composeSeparator);
+		}
+		return sb.toString();
+	}
+
+	@Override
+	public List<Double> getParameters() {
+		return null;
+	}
+
+	@Override
+	public List<String> getParameterNames() {
+		return null;
+	}
+
+	@Override
+	public double transform(double value) throws Exception {
+		double resValue = value;
+		for (int i = 0; i < transformations.length; i++)
+		{
+			resValue = transformations[i].transform(resValue);
+		}
+		return resValue;
+	}
+	
+}
