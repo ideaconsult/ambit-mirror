38293a1434a29c0491a7559afdc4d40f6ec609e8
ntk73 <nick@uni-plovdiv.net>
2019-12-11 20:34:13 +0200

    Added new parameter shift

--- ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/transformations/TransformationLn.java
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/transformations/TransformationLn.java
@@ -1,27 +1,56 @@
 package ambit2.groupcontribution.transformations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransformationLn implements IValueTransformation 
 {
-
+	Double shift = null;
+	
+	public TransformationLn() {
+	}
+	
+	public TransformationLn(double shift) {
+		this.shift = shift;
+	}
+	
 	@Override
 	public String getTransformationName() {
-		return "LN";
+		if (shift != null)
+			return "LN(" + shift + ")";
+		else
+			return "LN";
 	}
 
 	@Override
 	public List<Double> getParameters() {
-		return null;
+		if (shift != null)
+		{
+			List<Double> params = new ArrayList<Double>(); 
+			params.add(shift);
+			return params;
+		}
+		else
+			return null;
 	}
 	
 	@Override
 	public List<String> getParameterNames() {
-		return null;
+		if (shift != null)
+		{
+			List<String> paramNames = new ArrayList<String>(); 
+			paramNames.add("shift");
+			return paramNames;
+		}
+		else
+			return null;
 	}
 
 	@Override
 	public double transform(double value) throws Exception {
-		return Math.log(value);
+		if (shift != null)
+			return Math.log(value + shift);
+		else
+			return Math.log(value);
 	}
 }
