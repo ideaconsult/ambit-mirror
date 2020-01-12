dc492f0fcafc07236dc2a6acb461725dedac5634
vedina <jeliazkova.nina@gmail.com>
2020-01-12 10:16:51 +0200

    print cleanup

--- ambit2-all/ambit2-rendering/src/test/java/ambit2/rendering/RendererTest.java
+++ ambit2-all/ambit2-rendering/src/test/java/ambit2/rendering/RendererTest.java
@@ -9,11 +9,9 @@ import java.io.File;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
-
-import junit.framework.Assert;
-import net.idea.modbcum.i.exceptions.AmbitException;
 
 import org.junit.Test;
 import org.openscience.cdk.graph.PathTools;
@@ -36,7 +34,6 @@ import org.openscience.cdk.renderer.generators.BasicSceneGenerator;
 import org.openscience.cdk.renderer.generators.IGenerator;
 import org.openscience.cdk.renderer.generators.IGeneratorParameter;
 import org.openscience.cdk.renderer.generators.SelectAtomGenerator;
-import org.openscience.cdk.renderer.generators.standard.StandardGenerator;
 import org.openscience.cdk.renderer.selection.IChemObjectSelection;
 import org.openscience.cdk.renderer.selection.SingleSelection;
 import org.openscience.cdk.renderer.visitor.AWTDrawVisitor;
@@ -47,6 +44,8 @@ import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 import ambit2.core.data.MoleculeTools;
 import ambit2.core.smiles.SmilesParserWrapper;
 import ambit2.core.smiles.SmilesParserWrapper.SMILES_PARSER;
+import junit.framework.Assert;
+import net.idea.modbcum.i.exceptions.AmbitException;
 
 /**
  * 
@@ -79,7 +78,7 @@ import ambit2.core.smiles.SmilesParserWrapper.SMILES_PARSER;
  * parameter: BasicAtomGenerator$KekuleStructure -> false
  * parameter: BasicAtomGenerator$ShowEndCarbons -> false
  * parameter: BasicAtomGenerator$ShowExplicitHydrogens -> true
- * </pre>
+ *         </pre>
  */
 public class RendererTest {
 
@@ -93,59 +92,57 @@ public class RendererTest {
 		StructureDiagramGenerator g = new StructureDiagramGenerator(mol);
 		g.generateCoordinates();
 
-
 		IAtomContainerHighlights p = new IAtomContainerHighlights() {
 
-					@Override
-					public void setEnabled(boolean value) {
+			@Override
+			public void setEnabled(boolean value) {
 
-					}
+			}
 
-					@Override
-					public void open() throws Exception {
-					}
+			@Override
+			public void open() throws Exception {
+			}
 
-					@Override
-					public void close() throws Exception {
-					}
+			@Override
+			public void close() throws Exception {
+			}
 
-					@Override
-					public IChemObjectSelection process(IAtomContainer mol)
-							throws AmbitException {
-						IAtom anAtom = null;
-						for (IAtom atom : mol.atoms()) {
-							if (atom.getSymbol().equals("N")) {
-								anAtom = atom;
-							}
-						}						
-						final IAtomContainer selectedMol = MoleculeTools
-								.newMolecule(SilentChemObjectBuilder.getInstance());
-						selectedMol.addAtom(anAtom);
-						selectedMol.addBond(mol.getBond(0));
-						selectedMol.addAtom(mol.getBond(0).getAtom(0));
-						selectedMol.addAtom(mol.getBond(0).getAtom(1));						
-						return new SingleSelection<IChemObject>(selectedMol);
+			@Override
+			public IChemObjectSelection process(IAtomContainer mol) throws AmbitException {
+				IAtom anAtom = null;
+				for (IAtom atom : mol.atoms()) {
+					if (atom.getSymbol().equals("N")) {
+						anAtom = atom;
 					}
+				}
+				final IAtomContainer selectedMol = MoleculeTools.newMolecule(SilentChemObjectBuilder.getInstance());
+				selectedMol.addAtom(anAtom);
+				selectedMol.addBond(mol.getBond(0));
+				selectedMol.addAtom(mol.getBond(0).getAtom(0));
+				selectedMol.addAtom(mol.getBond(0).getAtom(1));
+				return new SingleSelection<IChemObject>(selectedMol);
+			}
 
-					@Override
-					public boolean isEnabled() {
-						return true;
-					}
+			@Override
+			public boolean isEnabled() {
+				return true;
+			}
 
-					@Override
-					public long getID() {
-						return 0;
-					}
-				};
-		BufferedImage img = t.getImage(mol,p,true, false);
+			@Override
+			public long getID() {
+				return 0;
+			}
+		};
+		BufferedImage img = t.getImage(mol, p, true, false);
 
 		File file = new File("test.png");
 		if (file.exists())
 			file.delete();
 		ImageIO.write(img, "png", file);
 		Assert.assertTrue(file.exists());
-		System.out.println(file.getAbsolutePath());
-
+		// System.out.println(file.getAbsolutePath());
+		if (file.exists())
+			file.delete();
 	}
 
 	@Test
@@ -159,48 +156,43 @@ public class RendererTest {
 	}
 
 	public void testsdf2image(String filename, String imgfile) throws Exception {
+		try (InputStream in = RendererTest.class.getClassLoader().getResourceAsStream(filename)) {
 
-		InputStream in = RendererTest.class.getClassLoader()
-				.getResourceAsStream(filename);
+			Assert.assertNotNull(in);
+			IteratingSDFReader reader = new IteratingSDFReader(in, SilentChemObjectBuilder.getInstance());
 
-		Assert.assertNotNull(in);
-		IteratingSDFReader reader = new IteratingSDFReader(in,
-				SilentChemObjectBuilder.getInstance());
+			while (reader.hasNext()) {
+				IChemObject mol = reader.next();
+				Assert.assertTrue(mol instanceof IAtomContainer);
+				AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms((IAtomContainer) mol);
+				/**
+				 * if generating 2D coordinates via StructureDiagramGenerator, the problem goes
+				 * away, but it is still valid use case to generate a structure diagram from
+				 * coordinates retrieved from a file
+				 */
+				// StructureDiagramGenerator sdg = new
+				// StructureDiagramGenerator((IAtomContainer)mol);
+				// sdg.generateCoordinates(new Vector2d(0,1));
 
-		while (reader.hasNext()) {
-			IChemObject mol = reader.next();
-			Assert.assertTrue(mol instanceof IAtomContainer);
-			AtomContainerManipulator
-					.percieveAtomTypesAndConfigureAtoms((IAtomContainer) mol);
-			/**
-			 * if generating 2D coordinates via StructureDiagramGenerator, the
-			 * problem goes away, but it is still valid use case to generate a
-			 * structure diagram from coordinates retrieved from a file
-			 */
-			// StructureDiagramGenerator sdg = new
-			// StructureDiagramGenerator((IAtomContainer)mol);
-			// sdg.generateCoordinates(new Vector2d(0,1));
-
-			BufferedImage img = paint((IAtomContainer) mol);
-			File file = new File(imgfile);
-			// file.deleteOnExit();
-			if (file.exists())
+				BufferedImage img = paint((IAtomContainer) mol);
+				File file = new File(imgfile);
+				// file.deleteOnExit();
+				if (file.exists())
+					file.delete();
+				ImageIO.write(img, "png", file);
+				Assert.assertTrue(file.exists());
+				// System.out.println(file.getAbsolutePath());
 				file.delete();
-			ImageIO.write(img, "png", file);
-			Assert.assertTrue(file.exists());
-			System.out.println(file.getAbsolutePath());
+			}
 		}
-		in.close();
 	}
 
 	protected BufferedImage paint(IAtomContainer mol) throws Exception {
 		Rectangle drawArea = new Rectangle(0, 0, 200, 200);
-		BufferedImage img = new BufferedImage(drawArea.width, drawArea.height,
-				BufferedImage.TYPE_INT_ARGB);
+		BufferedImage img = new BufferedImage(drawArea.width, drawArea.height, BufferedImage.TYPE_INT_ARGB);
 		int alpha = 128;
 		final Graphics2D g = img.createGraphics();
-		AlphaComposite ac = AlphaComposite
-				.getInstance(AlphaComposite.SRC, 1.0f);
+		AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.SRC, 1.0f);
 		g.setComposite(ac);
 		g.setColor(new Color(255, 255, 255, 255));
 		g.fillRect(0, 0, drawArea.width, drawArea.height);
@@ -208,8 +200,7 @@ public class RendererTest {
 		List<IGenerator<IAtomContainer>> generators = new ArrayList<IGenerator<IAtomContainer>>();
 		generators.add(new BasicSceneGenerator() {
 			@Override
-			public IRenderingElement generate(IAtomContainer ac,
-					RendererModel model) {
+			public IRenderingElement generate(IAtomContainer ac, RendererModel model) {
 				// TODO Auto-generated method stub
 				return super.generate(ac, model);
 			}
@@ -224,13 +215,10 @@ public class RendererTest {
 		});
 		// generators.add(new BasicAtomGenerator());
 
-		IRenderer renderer = new AtomContainerRenderer(generators,
-				new AWTFontManager());
+		IRenderer renderer = new AtomContainerRenderer(generators, new AWTFontManager());
 		final RendererModel rendererModel = renderer.getRenderer2DModel();
-		rendererModel.set(BasicBondGenerator.DefaultBondColor.class, new Color(
-				255, 0, 0, alpha));
-		rendererModel.set(BasicSceneGenerator.BackgroundColor.class, new Color(
-				0, 0, 255, alpha));
+		rendererModel.set(BasicBondGenerator.DefaultBondColor.class, new Color(255, 0, 0, alpha));
+		rendererModel.set(BasicSceneGenerator.BackgroundColor.class, new Color(0, 0, 255, alpha));
 
 		renderer.setup(mol, drawArea);
 		renderer.paint(mol, new AWTDrawVisitor(g));
@@ -248,15 +236,13 @@ public class RendererTest {
 		generators.add(new SelectAtomGenerator());
 
 		// the renderer needs to have a toolkit-specific font manager
-		IRenderer renderer = new AtomContainerRenderer(generators,
-				new AWTFontManager());
+		IRenderer renderer = new AtomContainerRenderer(generators, new AWTFontManager());
 
 		// dump all parameters
 		for (Object generator : renderer.getGenerators()) {
 			IGenerator g = (IGenerator) generator;
 			for (Object parameter : g.getParameters()) {
-				System.out.println("parameter: "
-						+ parameter.getClass().getName().substring(40) + " -> "
+				System.out.println("parameter: " + parameter.getClass().getName().substring(40) + " -> "
 						+ ((IGeneratorParameter) parameter).getValue());
 			}
 		}
@@ -270,18 +256,15 @@ public class RendererTest {
 		// "NC(=O)C%32=C(N)C(N1C=CC=CC1)=C(C(N)=O)C%31%35C3=C%30C=2c%29c%10C=9C=2C=%26C3=C%34C%27=C8c7c%33c6c(c%28C=5C%31C%30=C4c%29c%11C%12=C4C=5C=%13c%28c%14c6c%15c7C%16=C8C%17=C(C=9C%18C%20c%10c%11C%19C%24=C%12C=%13C=%23C%14=C%15C%25=C%16C(=C%17%18)C%21(C(C(N)=O)=C(C(N)=C(C(N)=O)C%19%20%21)N%22C=CC=CC%22)C%25C=%23%24)C=%26%27)C%32%35C%33%34";
 
 		String smiles = "c1ccccc1";
-		SmilesParserWrapper w = SmilesParserWrapper
-				.getInstance(SMILES_PARSER.CDK);
+		SmilesParserWrapper w = SmilesParserWrapper.getInstance(SMILES_PARSER.CDK);
 		IAtomContainer a = w.parseSmiles(smiles);
 		for (int i = 0; i < a.getAtomCount(); i++) {
 			a.getAtom(i).setID(Integer.toString(i + 1));
 		}
 
-		int[][] aMatrix = PathTools.computeFloydAPSP(AdjacencyMatrix
-				.getMatrix(a));
+		int[][] aMatrix = PathTools.computeFloydAPSP(AdjacencyMatrix.getMatrix(a));
 		/*
-		 * System.out.println("%%MatrixMarket matrix coordinate real general");
-		 * System
+		 * System.out.println("%%MatrixMarket matrix coordinate real general"); System
 		 * .out.println(String.format("%d %d %d",a.getAtomCount(),a.getAtomCount
 		 * (),a.getBondCount()));
 		 */
@@ -296,28 +279,16 @@ public class RendererTest {
 				if (v > 0)
 					count++;
 			}
-		System.out.println("%%MatrixMarket matrix coordinate real general");
-		System.out.println(String.format("%d %d %d", aMatrix.length,
-				aMatrix.length, count));
+		Logger.getGlobal().log(java.util.logging.Level.FINEST, "%%MatrixMarket matrix coordinate real general");
+		Logger.getGlobal().log(java.util.logging.Level.FINEST,
+				String.format("%d %d %d", aMatrix.length, aMatrix.length, count));
 
 		for (int i = 0; i < aMatrix.length; i++)
 			for (int j = i + 1; j < aMatrix[i].length; j++)
 				if (aMatrix[i][j] > 0) {
 					double v = getVal(aMatrix[i][j]);
-					System.out.println(String.format("%s %s %f", i + 1, j + 1,
-							v));
+					Logger.getGlobal().log(java.util.logging.Level.FINEST, String.format("%s %s %f", i + 1, j + 1, v));
 				}
-		System.out.println(max);
-
-		/*
-		 * System.out.println("%%MatrixMarket matrix coordinate real general");
-		 * System
-		 * .out.println(String.format("%d %d %d",a.getAtomCount(),a.getAtomCount
-		 * (),a.getBondCount())); for (int i=0; i < a.getBondCount(); i++) {
-		 * System.out.println(String.format("%s %s %d",
-		 * a.getBond(i).getAtom(0).getID(), a.getBond(i).getAtom(1).getID(), 1
-		 * )); }
-		 */
 	}
 
 	private static final double coeff = Math.sqrt(3);
