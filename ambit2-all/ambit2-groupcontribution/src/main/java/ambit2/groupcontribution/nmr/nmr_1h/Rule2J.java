2894faf2337cb14bbdf213a178d36f563c6696bc
ntk73 <nick@uni-plovdiv.net>
2020-03-10 16:16:06 +0200

    Added new classes for JJ coupling

---
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/nmr_1h/Rule2J.java
@@ -0,0 +1,9 @@
+package ambit2.groupcontribution.nmr.nmr_1h;
+
+public class Rule2J 
+{
+	public String smarts = null;	
+	public Double coupling = null;
+	public Double couplingIntervalLeftVal = null;
+	public Double couplingIntervalRightVal = null;	
+}
---
+++ ambit2-all/ambit2-groupcontribution/src/main/java/ambit2/groupcontribution/nmr/nmr_1h/Rule3J.java
@@ -0,0 +1,10 @@
+package ambit2.groupcontribution.nmr.nmr_1h;
+
+public class Rule3J {
+	public String smarts1 = null;
+	public String smarts2 = null;
+	public int bondOrder = 0;	
+	public Double coupling = null;
+	public Double couplingIntervalLeftVal = null;
+	public Double couplingIntervalRightVal = null;
+}
