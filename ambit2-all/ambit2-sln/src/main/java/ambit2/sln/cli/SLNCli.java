88f55d769d7ae032ed16dcff1d5d6e3b09380aec
ntk73 <nick@uni-plovdiv.net>
2020-11-16 22:22:25 +0200

    Added basic cli functionality: options, help

--- ambit2-all/ambit2-sln/src/main/java/ambit2/sln/cli/SLNCli.java
+++ ambit2-all/ambit2-sln/src/main/java/ambit2/sln/cli/SLNCli.java
@@ -1,10 +1,171 @@
 package ambit2.sln.cli;
 
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+
+
+
 public class SLNCli {
 
-	public static void main(String[] args) {
-		
+	private static final String title = "SLN";
+
+	public String inputFileName = null;
+	public String outputFileName = null;
+	public String inputSmiles = null;
+
+	public static void main(String[] args) 
+	{
+
 
 	}
+
+	protected static Options createOptions() {
+		Options options = new Options();
+		for (_option o: _option.values()) {
+			options.addOption(o.createOption());
+		}
+		return options;
+	}
+
+	protected static void printHelp(Options options,String message) {
+		if (message!=null) System.out.println(message);
+		System.out.println(title);
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp( SLNCli.class.getName(), options );
+	}
+
+	enum _option {
+
+		config {
+			@Override
+			public String getArgName() {
+				return "config";
+			}
+			@Override
+			public String getDescription() {
+				return "GCM configuration (.json) file";
+			}
+			@Override
+			public String getShortName() {
+				return "c";
+			}
+		},
+
+		smiles {
+			@Override
+			public String getArgName() {
+				return "smiles";
+			}
+			@Override
+			public String getDescription() {
+				return "Input molecule smiles";
+			}
+			@Override
+			public String getShortName() {
+				return "s";
+			}
+		},
+
+		input {
+			@Override
+			public String getArgName() {
+				return "input";
+			}
+			@Override
+			public String getDescription() {
+				return "Input molecule file";
+			}
+			@Override
+			public String getShortName() {
+				return "i";
+			}
+		},
+
+		output {
+			@Override
+			public String getArgName() {
+				return "output";
+			}
+			@Override
+			public String getDescription() {
+				return "Output file name";
+			}
+			@Override
+			public String getShortName() {
+				return "o";
+			}
+		},
+
+		help {
+			@Override
+			public String getArgName() {
+				return null;
+			}
+			@Override
+			public String getDescription() {
+				return title;
+			}
+			@Override
+			public String getShortName() {
+				return "h";
+			}
+			@Override
+			public String getDefaultValue() {
+				return null;
+			}
+			public Option createOption() {
+				Option option   = OptionBuilder.withLongOpt(name())
+						.withDescription(getDescription())
+						.create(getShortName());
+				return option;
+			}
+		};	
+
+		public abstract String getArgName();
+		public abstract String getDescription();
+		public abstract String getShortName();
+		public String getDefaultValue() { return null; }
+
+		public Option createOption() {
+			String defaultValue = getDefaultValue();
+			Option option   = OptionBuilder.withLongOpt(name())
+					.hasArg()
+					.withArgName(getArgName())
+					.withDescription(String.format("%s %s %s",getDescription(),defaultValue==null?"":"Default value: ",defaultValue==null?"":defaultValue))
+					.create(getShortName());
+
+			return option;
+		}
+	}
+
+	public void setOption(_option option, String argument) throws Exception 
+	{
+		if (argument != null)
+			argument = argument.trim();
+		switch (option) {
+		case smiles: {
+			if ((argument == null) || "".equals(argument.trim()))
+				return;
+			inputSmiles = argument;
+			break;
+		}
+		case input: {
+			if ((argument == null) || "".equals(argument.trim()))
+				return;
+			inputFileName = argument;
+			break;
+		}
+		case output: {
+			if ((argument == null) || "".equals(argument.trim()))
+				return;
+			outputFileName = argument;
+			break;
+		}
+
+		}	
+	}
+
 
 }
