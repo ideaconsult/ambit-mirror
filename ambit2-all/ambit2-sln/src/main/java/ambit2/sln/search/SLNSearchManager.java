872d4c5dc32a9e12fd1116211bcdf54542378f21
ntk73 <nick@uni-plovdiv.net>
2020-12-28 08:57:55 +0200

    Implemented basic functionality in SLNSearchManager

--- ambit2-all/ambit2-sln/src/main/java/ambit2/sln/search/SLNSearchManager.java
+++ ambit2-all/ambit2-sln/src/main/java/ambit2/sln/search/SLNSearchManager.java
@@ -1,24 +1,92 @@
 package ambit2.sln.search;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+import ambit2.sln.SLNContainer;
+import ambit2.sln.SLNParser;
 import ambit2.smarts.IsomorphismTester;
 
 public class SLNSearchManager 
 {
+	boolean FlagAutoResetErrors = true;
+	
+	
 	IsomorphismTester isoTester = new IsomorphismTester();
-	List<String> errors = null;
+	SLNParser slnParser = new SLNParser();
+	List<String> errors = new ArrayList<String>();
+	
+	String slnQueryString = null;
+	SLNContainer query = null;
+	
+	
 	
 	public void setQuery(String slnQuery)
 	{
-		//TODO
+		if (FlagAutoResetErrors)
+			reset();
+		
+		slnQueryString = slnQuery;
+		query = slnParser.parse(slnQuery);
+		
+		if (!slnParser.getErrorMessages().equals(""))
+		{
+			errors.add(slnParser.getErrorMessages());			
+			return;
+		}
 	}
 	
-	public String getAllErrors()
+	public void reset()
+	{
+		errors.clear();
+	}
+	
+	public String getAllErrorsAsString()
 	{
 		//TODO
 		return("");
 	}
+
+	public List<String> getErrors() {
+		return errors;
+	}
+
+	public void setErrors(List<String> errors) {
+		this.errors = errors;
+	}
+	
+	
+	public SLNContainer getQuerySLNContainer() {
+		return query;
+	}
+
+	public void setQuerySLNContainer(SLNContainer query) {
+		if (FlagAutoResetErrors)
+			reset();
+		this.query = query;
+	}
+	
+		
+	public boolean isFlagAutoResetErrors() {
+		return FlagAutoResetErrors;
+	}
+
+	public void setFlagAutoResetErrors(boolean flagAutoResetErrors) {
+		FlagAutoResetErrors = flagAutoResetErrors;
+	}
+
+	
+	boolean mappingIn(IAtomContainer target) throws Exception
+	{
+		isoTester.setQuery(query);
+		return (isoTester.hasIsomorphism(target));
+	}
+	
+	
+	
+	
 	
 	
 }
