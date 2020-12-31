2837273958ff8f350863fff19376fad13bc2b67a
ntk73 <nick@uni-plovdiv.net>
2020-12-31 09:01:03 +0200

    Implemented basic functionality in class GenericDictionaryObject. Class AtomDictionaryObject made to inherit GenericDictionaryObject

--- ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/AtomDictionaryObject.java
+++ ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/AtomDictionaryObject.java
@@ -1,39 +1,20 @@
 package ambit2.sln.dictionary;
 
 import ambit2.sln.SLNAtom;
-import ambit2.sln.SLNBond;
-import ambit2.sln.SLNContainer;
 
-public class AtomDictionaryObject implements ISLNDictionaryObject
+public class AtomDictionaryObject extends GenericDictionaryObject
 {
-	String sln = null;
-	SLNAtom atom = null;
+	
+	public AtomDictionaryObject(String sln, SLNAtom atom) {
+		this.sln = sln;
+		this.atom = atom;
+		makeContainerFromAtom();
+	}
+	
 	
 	@Override
 	public Type getObjectType() {
 		return Type.ATOM;
-	}
-
-	@Override
-	public String getSLNString() {
-		return sln;
-	}
-
-	@Override
-	public SLNContainer getSLNContainer() {		
-		return null;
-	}
-
-	
-	@Override
-	public SLNAtom getSLNAtom() {
+	}	
 		
-		return atom;
-	}
-
-	@Override
-	public SLNBond getSLNBond() {
-		return null;
-	}
-	
 }
--- ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/GenericDictionaryObject.java
+++ ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/GenericDictionaryObject.java
@@ -1,6 +1,41 @@
 package ambit2.sln.dictionary;
 
+import org.openscience.cdk.silent.SilentChemObjectBuilder;
+
+import ambit2.sln.SLNAtom;
+import ambit2.sln.SLNBond;
+import ambit2.sln.SLNContainer;
+
+
 public abstract class GenericDictionaryObject implements ISLNDictionaryObject
 {
+	String sln = null;
+	SLNAtom atom = null;
+	SLNBond bond = null;
+	SLNContainer container = null;
 	
+	@Override
+	public String getSLNString() {
+		return sln;
+	}
+
+	@Override
+	public SLNContainer getSLNContainer() {		
+		return container;
+	}
+	
+	@Override
+	public SLNAtom getSLNAtom() {		
+		return atom;
+	}
+
+	@Override
+	public SLNBond getSLNBond() {
+		return bond;
+	}
+	
+	void makeContainerFromAtom() {
+		 container = new SLNContainer(SilentChemObjectBuilder.getInstance());
+		 container.addAtom(atom);
+	}
 }
