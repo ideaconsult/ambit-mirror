993d653038cc9ac7cd52acb83217224ae4650e0e
ntk73 <nick@uni-plovdiv.net>
2021-01-04 12:08:16 +0200

    Added field tokens

--- ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/MarkushAtomDictionaryObject.java
+++ ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/MarkushAtomDictionaryObject.java
@@ -1,5 +1,8 @@
 package ambit2.sln.dictionary;
 
+import java.util.ArrayList;
+
 public class MarkushAtomDictionaryObject {
 
+	public ArrayList<MarkushExpressionToken>  tokens = new ArrayList<MarkushExpressionToken>();
 }
