738a58f52c0c0f31d2d3e3347304f3c69172b6ad
ntk73 <nick@uni-plovdiv.net>
2021-01-04 12:25:08 +0200

    Added logical operation handling and slnContainerIndex

--- ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/MarkushExpressionToken.java
+++ ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/MarkushExpressionToken.java
@@ -1,11 +1,53 @@
 package ambit2.sln.dictionary;
 
+import ambit2.sln.SLNConst;
+
 public class MarkushExpressionToken 
 {
 	public int type;
+	public int slnContainerIndex = -1;
 	
 	public MarkushExpressionToken(int type)
 	{	
 		this.type = type;
 	}
+	
+	public MarkushExpressionToken(int type, int slnContainerIndex)
+	{	
+		this.type = type;
+		this.slnContainerIndex = slnContainerIndex;
+	}
+	
+	
+	public boolean isLogicalOperation()
+	{
+		if(type >= SLNConst.LO)
+			return(true);
+		return(false);
+	}
+
+	public int getLogOperation()
+	{
+		return(type - SLNConst.LO);
+	}
+
+	/*
+	private String logOperationToString()
+	{
+		int lo = getLogOperation();
+		switch (lo)
+		{
+		case SLNConst.LO_NOT:
+			return "!";			
+		case SLNConst.LO_AND:
+			return "&";
+		case SLNConst.LO_OR:
+			return "|";
+		case SLNConst.LO_ANDLO:
+			return ";";
+		}
+		return "*";
+	}
+	*/
+	
 }
