7304c617dce89ad08f1e2117aa0fa87f76da9814
ntk73 <nick@uni-plovdiv.net>
2021-01-04 14:47:33 +0200

    Added constructor and method getObjectType()

--- ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/MacroAtomDictionaryObject.java
+++ ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/MacroAtomDictionaryObject.java
@@ -1,5 +1,21 @@
 package ambit2.sln.dictionary;
 
-public class MacroAtomDictionaryObject {
+import ambit2.sln.SLNContainer;
+
+public class MacroAtomDictionaryObject extends GenericDictionaryObject 
+{
+
+	public MacroAtomDictionaryObject(String name, String sln, SLNContainer container, int[] valences) {
+		this.name = name;
+		this.sln = sln;
+		this.container = container;
+		this.valences = valences;
+		
+	}
+	
+	@Override
+	public Type getObjectType() {
+		return Type.MACRO_ATOM;
+	}
 
 }
