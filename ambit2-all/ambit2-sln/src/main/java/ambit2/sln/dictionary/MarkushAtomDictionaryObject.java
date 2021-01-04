b974a2de4e2274b174517db5b28edf8e39b99d65
ntk73 <nick@uni-plovdiv.net>
2021-01-04 12:58:06 +0200

    SLN containers replaced with macro atoms

--- ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/MarkushAtomDictionaryObject.java
+++ ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/MarkushAtomDictionaryObject.java
@@ -1,11 +1,10 @@
 package ambit2.sln.dictionary;
 
 import java.util.ArrayList;
-import ambit2.sln.SLNContainer;
 
 public class MarkushAtomDictionaryObject {
 
 	public ArrayList<MarkushExpressionToken>  tokens = new ArrayList<MarkushExpressionToken>();
-	public ArrayList<SLNContainer>  containers = new ArrayList<SLNContainer>();
+	public ArrayList<MacroAtomDictionaryObject>  macroAtoms = new ArrayList<MacroAtomDictionaryObject>();
 	
 }
