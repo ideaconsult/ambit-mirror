7c214038820dc04614ae2fcb69c4a1026f2d8c21
ntk73 <nick@uni-plovdiv.net>
2021-01-04 12:28:37 +0200

    Added field containers

--- ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/MarkushAtomDictionaryObject.java
+++ ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/MarkushAtomDictionaryObject.java
@@ -1,8 +1,11 @@
 package ambit2.sln.dictionary;
 
 import java.util.ArrayList;
+import ambit2.sln.SLNContainer;
 
 public class MarkushAtomDictionaryObject {
 
 	public ArrayList<MarkushExpressionToken>  tokens = new ArrayList<MarkushExpressionToken>();
+	public ArrayList<SLNContainer>  containers = new ArrayList<SLNContainer>();
+	
 }
