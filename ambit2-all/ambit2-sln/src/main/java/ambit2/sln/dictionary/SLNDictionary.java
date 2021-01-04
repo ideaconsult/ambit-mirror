a94363ec04ef0a96ffeefe8c216f2939394bd25e
ntk73 <nick@uni-plovdiv.net>
2021-01-04 18:29:38 +0200

    Added list with dictionary objects names

--- ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/SLNDictionary.java
+++ ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/SLNDictionary.java
@@ -1,16 +1,24 @@
 package ambit2.sln.dictionary;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-
+import java.util.List;
 
 
 public class SLNDictionary 
 {
 	private HashMap<String,ISLNDictionaryObject> objects = new HashMap<String,ISLNDictionaryObject>();
+	private List<String> names = new ArrayList<String>();
 	
-	
+		
 	public ISLNDictionaryObject getDictionaryObject(String name)
 	{	
+		return objects.get(name);
+	}
+	
+	public ISLNDictionaryObject getDictionaryObject(int index)
+	{	
+		String name = names.get(index);
 		return objects.get(name);
 	}
 	
@@ -22,6 +30,11 @@ public class SLNDictionary
 	public boolean containsObject(String name, ISLNDictionaryObject.Type type)
 	{
 		return objects.containsKey(name);
+	}
+	
+	public List<String> getNames() 
+	{
+		return names;
 	}
 	
 }
