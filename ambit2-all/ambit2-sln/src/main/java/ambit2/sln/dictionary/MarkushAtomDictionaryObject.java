264480fb534f65f9d32dee63f86803d9e6d79c0d
ntk73 <nick@uni-plovdiv.net>
2021-01-12 16:26:05 +0200

    Implemented constructors and method getObjectType()

--- ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/MarkushAtomDictionaryObject.java
+++ ambit2-all/ambit2-sln/src/main/java/ambit2/sln/dictionary/MarkushAtomDictionaryObject.java
@@ -1,10 +1,31 @@
 package ambit2.sln.dictionary;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class MarkushAtomDictionaryObject {
 
-	public ArrayList<MarkushExpressionToken>  tokens = new ArrayList<MarkushExpressionToken>();
-	public ArrayList<MacroAtomDictionaryObject>  macroAtoms = new ArrayList<MacroAtomDictionaryObject>();
+
+public class MarkushAtomDictionaryObject extends GenericDictionaryObject 
+{
+	
+	public List<MarkushExpressionToken>  tokens = null; //When tokens = null this is a simple Markush atom
+	public List<MacroAtomDictionaryObject>  macroAtoms = new ArrayList<MacroAtomDictionaryObject>();
+	
+	public MarkushAtomDictionaryObject(String name, String sln) {
+		this.name = name;
+		this.sln = sln;
+	}
+	
+	public MarkushAtomDictionaryObject(String name, String sln, List<MacroAtomDictionaryObject>  macroAtoms) {
+		this.name = name;
+		this.sln = sln;
+		this.macroAtoms = macroAtoms;
+	}
+	
+	
+	@Override
+	public Type getObjectType() {
+		return Type.MARKUSH_ATOM;
+	}
 	
 }
