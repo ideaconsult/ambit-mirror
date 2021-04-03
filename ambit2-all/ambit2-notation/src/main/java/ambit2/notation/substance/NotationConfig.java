6a2d7eb4093259728197596f79b1168abb24bc34
ntk73 <nick@uni-plovdiv.net>
2021-04-03 22:10:40 +0300

    Added function loadFromJSON()

--- ambit2-all/ambit2-notation/src/main/java/ambit2/notation/substance/NotationConfig.java
+++ ambit2-all/ambit2-notation/src/main/java/ambit2/notation/substance/NotationConfig.java
@@ -1,5 +1,44 @@
 package ambit2.notation.substance;
 
-public class NotationConfig {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ambit2.rules.json.JSONParsingUtils;
+
+
+
+public class NotationConfig 
+{
+
+	public static NotationConfig loadFromJSON(File jsonConfig) throws FileNotFoundException, IOException, JsonProcessingException {
+		FileInputStream fin = new FileInputStream(jsonConfig);
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode root = null;
+
+		try {
+			root = mapper.readTree(fin);
+		} catch (JsonProcessingException x) {
+			throw x;			
+		} catch (IOException x) {
+			throw x;
+		} finally {
+			try {
+				fin.close();
+			} catch (Exception x) {
+			}
+		}
+		
+		JSONParsingUtils jsonUtils = new JSONParsingUtils(); 
+		NotationConfig notCfg = new NotationConfig(); 
+		
+		//TODO
+		
+		return notCfg;
+	}	
 }
