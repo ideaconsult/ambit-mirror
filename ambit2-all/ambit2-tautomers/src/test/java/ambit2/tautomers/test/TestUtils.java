916a9a92b3f137c7cffb2f5169d8b16744d23149
ntk73 <nick@uni-plovdiv.net>
2021-04-12 17:33:43 +0300

    Added function testPartialCharges()

--- ambit2-all/ambit2-tautomers/src/test/java/ambit2/tautomers/test/TestUtils.java
+++ ambit2-all/ambit2-tautomers/src/test/java/ambit2/tautomers/test/TestUtils.java
@@ -1,13 +1,26 @@
 package ambit2.tautomers.test;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+import org.openscience.cdk.charges.AtomTypeCharges;
+import org.openscience.cdk.charges.GasteigerMarsiliPartialCharges;
+import org.openscience.cdk.charges.IChargeCalculator;
+import org.openscience.cdk.charges.InductivePartialCharges;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+import ambit2.smarts.SmartsHelper;
 import ambit2.tautomers.tools.TautomerAnalysis;
 
 public class TestUtils {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws Exception
 	{
-		testRanking(new double[] {2,3,3,0,45,1,3});
-
+		//testRanking(new double[] {2,3,3,0,45,1,3});
+		
+		testPartialCharges("CCC(O)=O");
 	}
 	
 	
@@ -41,6 +54,35 @@ public class TestUtils {
 			System.out.println("  " + values[i] + "   rank = " + ranks[i]);
 		
 		System.out.println("\nNumber of distinct values = " + TautomerAnalysis.getNumberOfDistinctValues(values));
+	}
+	
+	
+	public static void testPartialCharges(String smi) throws Exception
+	{
+		System.out.println("Test partial charges for " + smi);
+		DecimalFormat df = new DecimalFormat("0.000", new DecimalFormatSymbols(new Locale("en-US")));
+		
+		IAtomContainer mol = SmartsHelper.getMoleculeFromSmiles(smi, true);
+		
+		
+		//IChargeCalculator chargeCalc = new GasteigerPEPEPartialCharges(); - not working --> only 0.0 charges
+		//IChargeCalculator chargeCalc = new InductivePartialCharges(); - not working --> Exception
+		//MMFF94PartialCharges present in CDK 2.3
+		
+		//GasteigerPEPEPartialCharges  gastPC = new GasteigerPEPEPartialCharges();		
+		//gastPC.assignGasteigerPiPartialCharges(mol, true);		
+		IChargeCalculator chargeCalc = new GasteigerMarsiliPartialCharges();
+				
+		
+		
+		chargeCalc.calculateCharges(mol);
+		
+		for (int i = 0; i < mol.getAtomCount(); i++)
+		{	
+			IAtom at = mol.getAtom(i);
+			System.out.println("" + (i+1) + "  " + at.getSymbol() + "  " + df.format(at.getCharge()));
+		}		
+		
 	}
 
 }
