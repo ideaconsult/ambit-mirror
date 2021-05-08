7acb51cda9320baf57b8566520fb9574e7098f7d
ntk73 <nick@uni-plovdiv.net>
2021-05-08 23:48:14 +0300

    Added new classes NotationSection and NotationElement

---
+++ ambit2-all/ambit2-notation/src/main/java/ambit2/notation/NotationElement.java
@@ -0,0 +1,10 @@
+package ambit2.notation;
+
+public class NotationElement 
+{
+	public String name = null;
+	public String info = null;
+	public boolean flagActive = true; 
+	public int order = -1;
+	public Object dataObj = null;
+}
---
+++ ambit2-all/ambit2-notation/src/main/java/ambit2/notation/NotationSection.java
@@ -0,0 +1,14 @@
+package ambit2.notation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotationSection 
+{
+	public String name = null;
+	public String info = null;
+	public boolean flagActive = true; 
+	public int order = -1;
+	
+	public List<NotationElement> dataElements = new ArrayList<NotationElement>();
+}
