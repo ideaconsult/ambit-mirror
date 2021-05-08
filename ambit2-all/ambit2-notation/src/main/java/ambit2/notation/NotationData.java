a26a7834ae1f88013d9a4f41b05fb18a87484255
ntk73 <nick@uni-plovdiv.net>
2021-05-09 00:47:31 +0300

    Added new class NotationData

---
+++ ambit2-all/ambit2-notation/src/main/java/ambit2/notation/NotationData.java
@@ -0,0 +1,16 @@
+package ambit2.notation;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class NotationData 
+{
+	public NotationConfig config = null;
+	public Map<String, Object> elementData = new HashMap<String, Object>();
+	
+	public List<String> checkData() {
+		//TODO
+		return null;
+	}
+}
