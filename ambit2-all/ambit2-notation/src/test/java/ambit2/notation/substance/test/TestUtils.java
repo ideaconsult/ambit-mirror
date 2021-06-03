4d248a2e17d7b2bb60d5bb01be9e785c3d069cae
ntk73 <nick@uni-plovdiv.net>
2021-06-03 11:26:26 +0300

    Fixed test failire: added at leas one test

--- ambit2-all/ambit2-notation/src/test/java/ambit2/notation/substance/test/TestUtils.java
+++ ambit2-all/ambit2-notation/src/test/java/ambit2/notation/substance/test/TestUtils.java
@@ -2,10 +2,18 @@ package ambit2.notation.substance.test;
 
 import java.io.File;
 
+import org.junit.Test;
+
 import ambit2.notation.NotationConfig;
 
 public class TestUtils 
 {
+	
+	@Test
+	public void fakeTest() {
+		// there should be at least one test
+	}
+	
 	public static void testNotationConfigFromJSONFile(String fileName) throws Exception 
 	{
 		NotationConfig notCfg = NotationConfig.loadFromJSON(new File (fileName));
