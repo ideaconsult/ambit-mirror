2e3f82bfab9b0b2ce1d0b034de895dd283095a73
ntk73 <nick@uni-plovdiv.net>
2021-11-10 23:20:46 +0200

    Added enum NotationElementType. Implemented function checkData()

--- ambit2-all/ambit2-notation/src/main/java/ambit2/notation/NotationElement.java
+++ ambit2-all/ambit2-notation/src/main/java/ambit2/notation/NotationElement.java
@@ -2,9 +2,14 @@ package ambit2.notation;
 
 public class NotationElement 
 {
+	public static enum NotationElementType {
+		INTEGER, DOUBLE, STRING
+	}
+	
 	public String name = null;
 	public String info = null;
 	public boolean flagActive = true; 
 	public int order = -1;
-	public Object dataObj = null;
+	public NotationElementType elementType = NotationElementType.STRING; 
+	//public Object dataObj = null;
 }
