772dc02aaf522e392c67d37cdc8421561b86f148
Nikolay Kochev <nick@uni-plovdiv.net>
2022-05-25 00:19:47 +0300

    Added basic functions for JSON config of aNotationElement

--- ambit2-all/ambit2-notation/src/main/java/ambit2/notation/NotationElement.java
+++ ambit2-all/ambit2-notation/src/main/java/ambit2/notation/NotationElement.java
@@ -1,5 +1,9 @@
 package ambit2.notation;
 
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
 public class NotationElement 
 {
 	public static enum NotationElementType {
@@ -12,4 +16,43 @@ public class NotationElement
 	public int order = -1;
 	public NotationElementType elementType = NotationElementType.STRING; 
 	//public Object dataObj = null;
+	
+	
+	public static NotationElement extractNotationElement(JsonNode node, int notSecIndex, List<String> errors)
+	{
+		NotationElement notElement = new NotationElement();
+		
+		//TODO
+		
+		return notElement;
+	}
+	
+	public String toJSONString(String offset) 
+	{
+		StringBuffer sb = new StringBuffer();
+		sb.append(offset + "{\n");
+		int nFields = 0;
+		
+		if (name != null) {
+			if (nFields > 0)
+				sb.append(",\n");
+			sb.append(offset + "\t\"NAME\" : \"" + name + "\"");
+			nFields++;
+		}
+		
+		if (info != null) {
+			if (nFields > 0)
+				sb.append(",\n");
+			sb.append(offset + "\t\"INFO\" : \"" + info + "\"");
+			nFields++;
+		}
+		
+		
+		sb.append(offset + "\n");
+		
+		sb.append("offset + }\n"); // end of JSON
+		return sb.toString();
+	}
+	
+	
 }
