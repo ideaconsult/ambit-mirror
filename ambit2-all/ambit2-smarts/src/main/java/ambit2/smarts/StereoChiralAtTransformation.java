8c416fa429d570e55be3927fcd8ff5919cbc9228
Nikolay Kochev <nick@uni-plovdiv.net>
2023-07-05 12:18:29 +0300

    Improved SMIRKSReaction.toString(): added info about stereo double bond and chiral atom transformations. Implemented function StereoChiralAtTransformation.toString()

--- ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/StereoChiralAtTransformation.java
+++ ambit2-all/ambit2-smarts/src/main/java/ambit2/smarts/StereoChiralAtTransformation.java
@@ -16,4 +16,40 @@ public class StereoChiralAtTransformation
 	public int prodTerminal1 = -1;
 	public int prodTerminal2 = -1;
 	public int prodChirality = SmartsConst.ChC_Unspec;
+	
+	public String toString()
+	{
+		StringBuffer sb = new StringBuffer();
+		sb.append("  reactChiralAtom = " + reactChiralAtom);
+		if (reactLigands != null) {
+			sb.append("  reactLigands= ");
+			for (int i = 0; i < reactLigands.length; i++)
+				sb.append(" " + reactLigands[i]);
+		}		
+		if (reactTerminal1 != -1)
+			sb.append("  reactTerminal1 = " + reactTerminal1);
+		if (reactTerminal2 != -1)
+			sb.append("  reactTerminal2 = " + reactTerminal2);	
+		sb.append("  reactChirality = " + reactChirality);
+		sb.append("\n");
+		
+		sb.append("  prodChiralAtom = " + prodChiralAtom);
+		if (prodLigands != null) {
+			sb.append("  prodLigands= ");
+			for (int i = 0; i < prodLigands.length; i++)
+				sb.append(" " + prodLigands[i]);
+		}
+		if (prodLigandsReactMap != null) {
+			sb.append("  prodLigandsReactMap= ");
+			for (int i = 0; i < prodLigandsReactMap.length; i++)
+				sb.append(" " + prodLigandsReactMap[i]);
+		}
+		if (prodTerminal1 != -1)
+			sb.append("  prodTerminal1 = " + prodTerminal1);
+		if (prodTerminal2 != -1)
+			sb.append("  prodTerminal2 = " + prodTerminal2);	
+		sb.append("  prodChirality = " + prodChirality);
+				
+		return sb.toString();
+	}
 }
